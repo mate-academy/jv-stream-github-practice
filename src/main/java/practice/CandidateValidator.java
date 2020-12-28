c890084dffab24ff2490947ec4ab57ec26bc9917
Serhii Semenykhin <semyonich@users.noreply.github.com>
2020-12-28 21:07:31 +0200

    StreamPractice methods and CandidateValidator class implemented

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,18 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
 
+public class CandidateValidator implements Predicate<Candidate> {
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] splitedYears = candidate.getPeriodsInUkr().split("-");
+        int[] yearsFromTo = new int[]
+                {Integer.parseInt(splitedYears[0]), Integer.parseInt(splitedYears[1])};
+        return candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && yearsFromTo[1] - yearsFromTo[0] >= 10;
+    }
 }
