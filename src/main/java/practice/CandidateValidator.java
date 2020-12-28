c08912034409d5dedc805dfade6609f7c9fde721
Vadim <Vedroidua@gmail.com>
2020-12-28 20:00:08 +0200

    Tasks completed

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
 
+public class CandidateValidator implements Predicate<Candidate> {
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && getPeriods(candidate) >= 10;
+    }
+
+    private int getPeriods(Candidate candidate) {
+        String[] periods = candidate.getPeriodsInUkr().split("-");
+        return Integer.parseInt(periods[1]) - Integer.parseInt(periods[0]);
+    }
 }
