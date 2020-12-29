a724b1df2d76afcc5a412c27c52d9964ec06af68
OleksandrPokotylenko <aln.nko.of@gmail.com>
2020-12-29 22:12:08 +0200

    created privat periodInUkr() method for CandidateValidator; created MIN_REQUIRED_AGE constant for CandidateValidator; deleted unnecessary empty lines in StreamPracticeTest.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,14 +4,18 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_REQUIRED_AGE = 35;
     private static final int MIN_REQUIRED_PERIOD = 10;
 
     @Override
     public boolean test(Candidate candidate) {
-        String[] splitResult = candidate.getPeriodsInUkr().split("-");
-        int periodInUkr = Integer.parseInt(splitResult[1]) - Integer.parseInt(splitResult[0]);
-        return candidate.getAge() >= 35 && candidate.isAllowedToVote()
+        return candidate.getAge() >= MIN_REQUIRED_AGE && candidate.isAllowedToVote()
                 && candidate.getNationality().equals("Ukrainian")
-                && periodInUkr >= MIN_REQUIRED_PERIOD;
+                && periodInUkr(candidate) >= MIN_REQUIRED_PERIOD;
+    }
+
+    private int periodInUkr(Candidate candidate) {
+        String[] splitResult = candidate.getPeriodsInUkr().split("-");
+        return Integer.parseInt(splitResult[1]) - Integer.parseInt(splitResult[0]);
     }
 }
