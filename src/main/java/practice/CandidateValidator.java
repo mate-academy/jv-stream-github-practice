20ecbebbe91f6a718d9fe6eec05686d34afa3cfd
Roman Saviak <saviak.roman@gmail.com>
2020-12-29 13:34:54 +0200

    added solutions for stream tasks

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,27 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
 
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MINIMUM_AGE = 35;
+    private static final int START_POSITION = 0;
+    private static final int SEPARATOR_POSITION = 4;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getNationality().equals("Ukrainian")
+                && (candidate.isAllowedToVote()) && candidate.getAge() >= MINIMUM_AGE
+                && ukrainianPeriod(candidate) > 10;
+    }
+
+    private int ukrainianPeriod(Candidate candidate) {
+        if (candidate.getPeriodsInUkr() == null || candidate.getPeriodsInUkr().length() == 0) {
+            throw new RuntimeException("Unappropriate period");
+        }
+        int start = Integer.parseInt(candidate.getPeriodsInUkr()
+                .substring(START_POSITION, SEPARATOR_POSITION));
+        int end = Integer.parseInt(candidate.getPeriodsInUkr().substring(SEPARATOR_POSITION + 1));
+        return end - start;
+    }
 }
