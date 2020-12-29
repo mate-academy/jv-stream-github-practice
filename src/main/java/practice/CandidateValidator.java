c74892cc93f9dfe4dd92454ac9e1682a3f3c4913
AndreiYazadzhi <belator251000@gmail.com>
2020-12-29 11:34:11 +0200

    Removed magic numbers

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,15 +4,17 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-
+private static  final int REQUIRED_AGE = 35;
+private static  final int REQUIRED_PERIOD = 10;
+    private static  final int DATE_LENGTH = 4;
     @Override
     public boolean test(Candidate candidate) {
-        int fromPeriod = Integer.parseInt(candidate.getPeriodsInUkr().substring(0,4));
+        int fromPeriod = Integer.parseInt(candidate.getPeriodsInUkr().substring(0,DATE_LENGTH));
         int toPeriod = Integer.parseInt(candidate.getPeriodsInUkr()
-                .substring(candidate.getPeriodsInUkr().length() - 4));
-        return candidate.getAge() >= 35
+                .substring(candidate.getPeriodsInUkr().length() - DATE_LENGTH));
+        return candidate.getAge() >= REQUIRED_AGE
                 && candidate.getNationality().equals("Ukrainian")
                 && candidate.isAllowedToVote()
-                && toPeriod - fromPeriod >= 10;
+                && toPeriod - fromPeriod >= REQUIRED_PERIOD;
     }
 }
