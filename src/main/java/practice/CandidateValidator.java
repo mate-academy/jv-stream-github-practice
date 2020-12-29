80ed01f60cbc2d1d9a3a409ec0b2c4810c7f7fa7
ilya-brinchuk <ilya.brinchuk@gmail.com>
2020-12-29 20:42:36 +0200

    added logic to StreamPractice class methods implemented interface Predicate in CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
 
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int OLDER_THEN = 35;
+    private static final int MIN_PERIOD_IN_UKRAINE = 10;
+    private static final String NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+
+        return candidate.getAge() >= OLDER_THEN
+                && candidate.getNationality().equals(NATIONALITY)
+                && candidate.isAllowedToVote()
+                && periodInUkraine(candidate.getPeriodsInUkr()) >= MIN_PERIOD_IN_UKRAINE;
+    }
+
+    private int periodInUkraine(String period) {
+        return Integer.parseInt(period.split("-")[1]) - Integer.parseInt(period.split("-")[0]);
+    }
 }
