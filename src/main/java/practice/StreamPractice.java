921fdaecad7c9b51079770a702cedaa205315c9c
antoniukivan <ivan.antonuk1992@gmail.com>
2020-12-29 17:38:43 +0200

    checkstyle fix

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -25,7 +25,7 @@ public class StreamPractice {
                 .filter(x -> x % 2 == 0)
                 .min()
                 .orElseThrow(() ->
-                        new RuntimeException("Can't get min value from list: method_input_list"));
+                        new RuntimeException("Can't get min value from list: " + numbers));
     }
 
     /**
