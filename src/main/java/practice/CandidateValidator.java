9aac9721cd6db36f7070cb5c65f4f968132ec3a1
Volodymyr Lykhvar <vlykhvar@gmail.com>
2020-12-29 12:11:35 +0200

    all done

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,25 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    private static final int AGE = 35;
+    private static final int LIVE_IN_UKRAINE = 10;
+    private static final String NATIONALITY = "ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getNationality().equalsIgnoreCase(NATIONALITY)
+                && candidate.isAllowedToVote()
+                && periodsInUkr(candidate.getPeriodsInUkr()) >= LIVE_IN_UKRAINE
+                && candidate.getAge() >= AGE;
+    }
+
+    private int periodsInUkr(String periodsInUkr) {
+        String[] periodInUkr = periodsInUkr.split("-");
+        return Integer.parseInt(periodInUkr[1]) - Integer.parseInt(periodInUkr[0]);
+    }
 
 }
