99f70470502395635ccfe336e7a4af983325de11
antoniukivan <ivan.antonuk1992@gmail.com>
2020-12-29 17:37:39 +0200

    add solution

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,15 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
 
+public class CandidateValidator implements Predicate<Candidate> {
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] period = candidate.getPeriodsInUkr().split("[^\\d]");
+        boolean isLivedInUkraine = (Integer.parseInt(period[1]) - Integer.parseInt(period[0])) > 10;
+        return candidate.getAge() >= 35 && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian") && isLivedInUkraine;
+    }
 }
