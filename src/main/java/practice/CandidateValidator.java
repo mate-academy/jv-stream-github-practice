36e8f8aa1d3de66625df992755b583731e51f429
Alex Korzun <koleksandr4@gmail.com>
2020-12-29 15:05:30 +0200

    done all tasks for stream api

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
 
+public class CandidateValidator implements Predicate<Candidate> {
+    static final int MAX_YEARS = 35;
+    static final int YEARS_IN_UKRAINE = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] livedInUkraine = candidate.getPeriodsInUkr().split("-");
+        int fromYears = Integer.parseInt(livedInUkraine[0]);
+        int toYears = Integer.parseInt(livedInUkraine[1]);
+        return candidate.getNationality().equals("Ukrainian")
+                && candidate.getAge() >= MAX_YEARS
+                && toYears - fromYears >= YEARS_IN_UKRAINE
+                && candidate.isAllowedToVote();
+    }
 }
