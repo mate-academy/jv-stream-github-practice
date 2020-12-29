16f41d6c1e717d65199ae15d8b90fa160cbd0bf2
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-29 15:25:36 +0200

    refactoring of CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,14 +4,21 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final String REGEX = "-";
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int MIN_CANDIDATE_AGE = 35;
+    private static final int MIN_TIME_RESIDENT = 10;
+    private static final int PARSE_INDEX_START_YEAR = 0;
+    private static final int PARSE_INDEX_END_YEAR = 1;
+    
     @Override
     public boolean test(Candidate candidate) {
-        String[] yearsInUkraine = candidate.getPeriodsInUkr().split("-");
-        int periodInUkraine = Integer.parseInt(yearsInUkraine[1])
-                - Integer.parseInt(yearsInUkraine[0]);
-        return candidate.getAge() >= 35
+        String[] yearsInUkraine = candidate.getPeriodsInUkr().split(REGEX);
+        int periodInUkraine = Integer.parseInt(yearsInUkraine[PARSE_INDEX_END_YEAR])
+                - Integer.parseInt(yearsInUkraine[PARSE_INDEX_START_YEAR]);
+        return candidate.getAge() >= MIN_CANDIDATE_AGE
                 && candidate.isAllowedToVote()
-                && candidate.getNationality().equals("Ukrainian")
-                && periodInUkraine >= 10;
+                && candidate.getNationality().equals(NATIONALITY)
+                && periodInUkraine >= MIN_TIME_RESIDENT;
     }
 }
