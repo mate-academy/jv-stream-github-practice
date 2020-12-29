54e43f9bfc804c4d0827b4344f7bccfa66cd66f2
RuslanIbIsh <ruslan8ishankulov@gmail.com>
2020-12-29 18:03:56 +0200

    first commit - implemented tesks

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
 
+public class CandidateValidator implements Predicate<Candidate> {
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && isLivedInUkr10Yeas(candidate.getPeriodsInUkr());
+    }
+
+    private boolean isLivedInUkr10Yeas(String getPeriodInUkr) {
+        String[] datesInUkr = getPeriodInUkr.split("-");
+        return (Integer.valueOf(datesInUkr[1]) - Integer.valueOf(datesInUkr[0])) >= 10;
+    }
 }
