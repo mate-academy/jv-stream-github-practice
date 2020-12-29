3e8b23f92293cb632d67052bc0d47dcafb415876
Likh-Alex <likhachov991@protonmail.com>
2020-12-29 17:50:44 +0200

    removed `magic numbers` from `CandidateValidator` class

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,6 +4,8 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int YEAR_FROM = 0;
+    private static final int YEAR_TO = 1;
     private static final int MINIMUM_AGE = 35;
     private static final String NATIONALITY = "Ukrainian";
     private static final int MINIMUM_YEARS_IN_UKRAINE = 10;
@@ -11,8 +13,8 @@ public class CandidateValidator implements Predicate<Candidate> {
     @Override
     public boolean test(Candidate candidate) {
         String[] yearsInUkraine = candidate.getPeriodsInUkr().split("-");
-        int yearFrom = Integer.parseInt(yearsInUkraine[0]);
-        int yearTo = Integer.parseInt(yearsInUkraine[1]);
+        int yearFrom = Integer.parseInt(yearsInUkraine[YEAR_FROM]);
+        int yearTo = Integer.parseInt(yearsInUkraine[YEAR_TO]);
         return candidate.isAllowedToVote()
                 && candidate.getAge() >= MINIMUM_AGE
                 && candidate.getNationality().equals(NATIONALITY)
