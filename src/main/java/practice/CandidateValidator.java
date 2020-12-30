65968639721996f55c0471b6e30037dc36c4b37e
gigbat <b.kostevich@gmail.com>
2020-12-30 17:31:04 +0200

    Introduce commit

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,22 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
 
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int AGE = 35;
+    private static final String NATIONAL = "Ukrainian";
+    private static final String SEPARATE = "-";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] yearsFromTill = candidate.getPeriodsInUkr().split(SEPARATE);
+        int period = Integer.parseInt(yearsFromTill[1])
+                - Integer.parseInt(yearsFromTill[0]);
+
+        return candidate.getAge() >= AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONAL)
+                && period >= 10;
+    }
 }
