1c2f86c5b06a5ca415acbe78e8761f0c9b68bf21
nikelok01@gmail.com <163amefeZ>
2020-12-30 17:30:43 +0200

    - Deleted redundant Exception

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -26,7 +26,7 @@ public class StreamPractice {
                 .map(i -> i % 2 != 0 ? numbers.get(i) - 1 : numbers.get(i))
                 .filter(n -> n % 2 != 0)
                 .average()
-                .orElseThrow(NoSuchElementException::new);
+                .orElseThrow();
     }
 
     public List<People> selectMenByAge(List<People> peopleList, int fromAge, int toAge) {
