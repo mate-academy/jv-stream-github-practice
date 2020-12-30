a2dae8b8d2a9086bc4ede712a2f718e0a1f006a3
kerrender <agreed_note@hotmail.com>
2020-12-30 12:37:42 +0200

    CandidateValidator simplified;

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,16 +4,13 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int AGE_LOWER_BOUND = 35;
 
     @Override
     public boolean test(Candidate candidate) {
-        if (candidate.getNationality().equals("Ukrainian") && candidate.isAllowedToVote()) {
-            if (candidate.getAge() >= 35) {
-                String[] yearBounds = candidate.getPeriodsInUkr().split("-");
-                int yearDiff = Integer.parseInt(yearBounds[1]) - Integer.parseInt(yearBounds[0]);
-                return yearDiff >= 10;
-            }
-        }
-        return false;
+        return candidate.getNationality().equals("Ukrainian") && candidate.isAllowedToVote()
+                && candidate.getAge() >= AGE_LOWER_BOUND
+                && Integer.parseInt(candidate.getPeriodsInUkr().split("-")[1])
+                - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[0]) >= 10;
     }
 }
