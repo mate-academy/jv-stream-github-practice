8656e56320ca52304ed6b969b99720e72a79fadd
gigbat <b.kostevich@gmail.com>
2020-12-30 17:53:08 +0200

    Fixed travis

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,13 +1,13 @@
 package practice;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import model.Candidate;
 import model.Cat;
 import model.People;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class StreamPractice {
     /**
