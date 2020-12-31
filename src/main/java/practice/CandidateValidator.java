b2e9848734aa16b98c7d58959d0d36ee53dcd3ea
Tonitrus <grom4er@gmail.com>
2020-12-31 14:29:47 +0200

    Fix: Delete not uses fields. Delete patterns

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,18 +1,12 @@
 package practice;
 
 import java.util.function.Predicate;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_AGE = 35;
     private static final String NATIONALITY = "Ukrainian";
     private static final int YEARS_IN_COUNTRY = 10;
-    private static final int PERIOD = 2;
-    private static final String PATTERN_TIME = "[0-9]{4}";
-    private Matcher matcher;
 
     @Override
     public boolean test(Candidate candidate) {
@@ -21,26 +15,11 @@ public class CandidateValidator implements Predicate<Candidate> {
                 && candidate.isAllowedToVote() && checkPeriod(candidate.getPeriodsInUkr());
     }
 
-    private boolean checkPeriod(String period) {
-        if (period == null || isMatch(period)) {
+    public boolean checkPeriod(String period) {
+        if (period == null) {
             return false;
         }
-        int[] periods = new int[PERIOD];
-        matcher = Pattern.compile(PATTERN_TIME).matcher(period);
-        for (int i = 0; i < PERIOD; i++) {
-            if (matcher.find()) {
-                periods[i] = Integer.parseInt(matcher.group());
-            }
-        }
-        return Math.abs(periods[0] - periods[1]) >= YEARS_IN_COUNTRY;
-    }
-
-    private boolean isMatch(String period) {
-        matcher = Pattern.compile(PATTERN_TIME).matcher(period);
-        int timePeriod = 0;
-        while (matcher.find()) {
-            timePeriod++;
-        }
-        return timePeriod != PERIOD;
+        String[] data = period.split("-");
+        return Math.abs(Integer.parseInt(data[0]) - Integer.parseInt(data[1])) >= YEARS_IN_COUNTRY;
     }
 }
--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,13 +1,10 @@
 package practice;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
 import model.Candidate;
 import model.Cat;
 import model.People;
