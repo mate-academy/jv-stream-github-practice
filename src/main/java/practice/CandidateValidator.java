a414dd85c4130c754e15f35449e020c5f578c93b
Alekearl <alexandrhurko@gmail.com>
2020-12-31 14:16:00 +0200

    created class CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
 
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int SHOULD_BE_OLDER = 35;
+    private static final int LIVE_IN_UKRAINE = 10;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final String SPLITERATOR = "-";
+
+    @Override
+    public boolean test(Candidate anotherOne) {
+        int period = Integer.parseInt(anotherOne.getPeriodsInUkr().split(SPLITERATOR)[1])
+                - Integer.parseInt(anotherOne.getPeriodsInUkr().split(SPLITERATOR)[0]);
+        return anotherOne.isAllowedToVote()
+                && anotherOne.getAge() >= SHOULD_BE_OLDER
+                && anotherOne.getNationality().equals(NATIONALITY)
+                && period >= LIVE_IN_UKRAINE;
+    }
 }
