bd79c360ad7c983e825a598665dc90ec0b35e8cb
ShemsheyMaxim <shanhaymax@gmail.com>
2020-12-31 04:01:46 +0200

    added realised for class CandidateValidator, added methods test() and getPeriodCandidateInUkr() in CandidateValidator class, added constant in CandidateValidator, added realised methods findMinEvenNumber(), getOddNumsAverage(), selectMenByAge(), getWorkablePeople(), getCatsNames(), validateCandidates() in class StreamPractice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,25 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
 
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE_CANDIDATE = 35;
+    private static final int MIN_PERIOD_IN_UKR = 10;
+    private static final String NATIONALITY_UKRAINE = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MIN_AGE_CANDIDATE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY_UKRAINE)
+                && getPeriodCandidateInUkr(candidate) >= MIN_PERIOD_IN_UKR;
+    }
+
+    private int getPeriodCandidateInUkr(Candidate candidate) {
+        return Integer.parseInt(candidate.getPeriodsInUkr()
+                .substring(candidate.getPeriodsInUkr().indexOf("-") + 1))
+                - Integer.parseInt(candidate.getPeriodsInUkr()
+                .substring(0, candidate.getPeriodsInUkr().indexOf("-")));
+    }
 }
