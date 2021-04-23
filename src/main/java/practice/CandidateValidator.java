d4ee49ee3a973cb073564daa2878b62c6695980b
Dalai <semenov.dalay@gmail.com>
2021-04-23 13:11:21 +0300

    added const for separator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,10 +7,11 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int MAX_AGE = 35;
     private static final int PERIOD_IN_UKRAINE = 10;
     private static final String NATIONALITY = "Ukrainian";
+    private static final String SEPARATOR_BETWEEN_DATES = "-";
 
     @Override
     public boolean test(Candidate candidate) {
-        String[] period = candidate.getPeriodsInUkr().split("-");
+        String[] period = candidate.getPeriodsInUkr().split(SEPARATOR_BETWEEN_DATES);
         return candidate.getAge() >= MAX_AGE
                 && candidate.getNationality().equals(NATIONALITY)
                 && (Integer.parseInt(period[1])
