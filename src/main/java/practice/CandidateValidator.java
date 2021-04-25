01dc7ca9bf1af70626e2e9a24e9f69bd08818828
dant0n <dmibuc@gmail.com>
2021-04-25 17:15:36 +0300

    Finish homework requirements.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
 
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int REQUIRED_AGE = 35;
+    private static final int REQUIRED_YEARS_PERIOD = 10;
+    private static final String REQUIRED_NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] yearsLived = candidate.getPeriodsInUkr().split("-");
+        int livedInCountry = Integer.parseInt(yearsLived[1]) - Integer.parseInt(yearsLived[0]);
+        return candidate.getAge() >= REQUIRED_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(REQUIRED_NATIONALITY)
+                && livedInCountry >= REQUIRED_YEARS_PERIOD;
+    }
 }
