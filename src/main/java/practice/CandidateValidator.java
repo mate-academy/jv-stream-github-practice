45d30eca367d5d3340342a3a5e88b8c54c272e69
unknown <vovazalli@gmail.com>
2021-04-25 23:20:32 +0300

    Stream practise

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,13 +4,16 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MINIMUM_AGE = 35;
+    private static final String NATIONALITY_REQUIRED = "Ukrainian";
+    private static final int YEARS_STRAIGHT_IN_UKRAINE = 10;
 
     public static boolean checkCandidate(Candidate candidate) {
         String[] yearsInUkraine = candidate.getPeriodsInUkr().split("-");
-        return candidate.getAge() >= 35
-                && candidate.getNationality().equals("Ukrainian")
+        return candidate.getAge() >= MINIMUM_AGE
+                && candidate.getNationality().equals(NATIONALITY_REQUIRED)
                 && Integer.parseInt(yearsInUkraine[1])
-                - Integer.parseInt(yearsInUkraine[0]) >= 10
+                - Integer.parseInt(yearsInUkraine[0]) >= YEARS_STRAIGHT_IN_UKRAINE
                 && candidate.isAllowedToVote();
     }
 
