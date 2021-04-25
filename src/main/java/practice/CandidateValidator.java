d4b395c76aec58f705d2dd55afed811d3fa53ec1
unknown <vovazalli@gmail.com>
2021-04-25 23:15:23 +0300

    Stream practice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
 
+public class CandidateValidator implements Predicate<Candidate> {
+
+    public static boolean checkCandidate(Candidate candidate) {
+        String[] yearsInUkraine = candidate.getPeriodsInUkr().split("-");
+        return candidate.getAge() >= 35
+                && candidate.getNationality().equals("Ukrainian")
+                && Integer.parseInt(yearsInUkraine[1])
+                - Integer.parseInt(yearsInUkraine[0]) >= 10
+                && candidate.isAllowedToVote();
+    }
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return checkCandidate(candidate);
+    }
 }
