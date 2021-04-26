1e37d6baf47f683f229dd34a3e31b76b2221351a
roman <nashpatsa@gmai.com>
2021-04-27 02:46:18 +0300

    created stream methods

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
 
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int LIVE_IN_UKR = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] yearsInUkr = candidate.getPeriodsInUkr().split("-");
+        int isCandidateLiveEnoughYears = Integer.parseInt(yearsInUkr[1])
+                - Integer.parseInt(yearsInUkr[0]);
+        return isCandidateLiveEnoughYears >= LIVE_IN_UKR
+                && candidate.getAge() >= MIN_AGE
+                && candidate.getNationality().equals(NATIONALITY)
+                && candidate.isAllowedToVote();
+    }
 }
