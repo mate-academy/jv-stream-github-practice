49c978a96ec032898602e5992c149271da4d5244
fotomassimo <maksym.pavliuchenko@gmail.com>
2021-04-26 18:37:07 +0300

    jv-stream-github-practice, first commit

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
 
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int ELIGIBLE_AGE = 35;
+    private static final String ELIGIBLE_NATIONALITY = "Ukrainian";
+    private static final int ELIGIBLE_RESIDENCE_PERIOD = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] yearsInUkr = candidate.getPeriodsInUkr().split("-");
+        return candidate.getAge() >= ELIGIBLE_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(ELIGIBLE_NATIONALITY)
+                && (Integer.parseInt(yearsInUkr[1]) - Integer.parseInt(yearsInUkr[0]))
+                >= ELIGIBLE_RESIDENCE_PERIOD;
+    }
 }
