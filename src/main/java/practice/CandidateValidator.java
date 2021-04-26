2f2e0621f3f0d97eaf1bca26ff1f85f24bf57052
Den4eg007 <68178197+Den4eg007@users.noreply.github.com>
2021-04-26 22:28:38 +0300

    Initial implementation of StreamPractice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,26 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
 
+public class CandidateValidator<T> implements Predicate<Candidate> {
+    private static final int REQUIRED_AGE = 35;
+    private static final int REQUIRED_YEARS_IN_THE_COUNTRY = 10;
+    private static final int FROM_YEAR = 0;
+    private static final int TO_YEAR = 1;
+    private static final String REQUIRED_NATIONALITY = "Ukrainian";
+    private static final String SPLITTER = "-";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= REQUIRED_AGE
+                && candidate.getNationality().equals(REQUIRED_NATIONALITY)
+                && candidate.isAllowedToVote()
+                && yearsInTheCountry(candidate) >= REQUIRED_YEARS_IN_THE_COUNTRY;
+    }
+
+    public int yearsInTheCountry(Candidate candidate) {
+        String[] periodsInUkr = candidate.getPeriodsInUkr().split(SPLITTER);
+        return Integer.parseInt(periodsInUkr[TO_YEAR]) - Integer.parseInt(periodsInUkr[FROM_YEAR]);
+    }
 }
