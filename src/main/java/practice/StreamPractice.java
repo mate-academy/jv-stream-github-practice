b2ec084359346172b001c86673174818520e6455
safordog <safordog@gmail.com>
2021-04-26 09:54:25 +0300

    removed constant

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -10,8 +10,6 @@ import model.Cat;
 import model.People;
 
 public class StreamPractice {
-    private static final String NO_MIN_VALUE_MESSAGE = "Can't get min value from list: ";
-
     /**
      * Given list of strings where each element contains 1+ numbers:
      * input = {"5,30,100", "0,22,7", ...}
@@ -25,7 +23,8 @@ public class StreamPractice {
                 .mapToInt(Integer::parseInt)
                 .filter(num -> num % 2 == 0)
                 .min()
-                .orElseThrow(() -> new RuntimeException(NO_MIN_VALUE_MESSAGE + numbers));
+                .orElseThrow(() -> new RuntimeException("Can't get min value from list: "
+                        + numbers));
     }
 
     /**
