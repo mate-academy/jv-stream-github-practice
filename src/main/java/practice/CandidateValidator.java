61e629cf121b5a7cefc7a977bbecc3ed7d6d16a6
redxp <redxpertus1@gmail.com>
2021-04-27 15:32:55 +0300

    streamApi hw vo1

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
 
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int REQUIRED_MINIMUM_AGE = 35;
+    private static final String REQUIRED_NATIONALITY = "Ukrainian";
+    private static final int MINIMUM_YEARS_STRAIGHT_IN_UKRAINE = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] yearsInUkraine = candidate.getPeriodsInUkr().split("-");
+        int stayPeriod = Integer.parseInt(yearsInUkraine[1]) - Integer.parseInt(yearsInUkraine[0]);
+        return candidate.getAge() >= REQUIRED_MINIMUM_AGE
+                && candidate.getNationality().equals(REQUIRED_NATIONALITY)
+                && stayPeriod >= MINIMUM_YEARS_STRAIGHT_IN_UKRAINE
+                && candidate.isAllowedToVote();
+    }
 }
