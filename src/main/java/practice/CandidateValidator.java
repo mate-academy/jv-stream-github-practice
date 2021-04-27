79be4fb476ad7bd5ad05994b879ea37f8fc5ad08
DeStinyShell <dmitriy4ever97@gmail.com>
2021-04-27 14:03:47 +0300

    Done Homework.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,17 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
 
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int REQUIRED_AGE = 35;
+    private static final String REQUIRED_NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= REQUIRED_AGE && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(REQUIRED_NATIONALITY)
+                && Math.abs(Integer.parseInt(candidate.getPeriodsInUkr().split("-")[0])
+                - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[1])) >= 10;
+    }
 }
