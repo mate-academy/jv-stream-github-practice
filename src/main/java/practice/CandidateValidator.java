a83901ac47ffb5ebd1eebe164e64767a9faa1a81
Cyouffy <cyouffy@gmail.com>
2021-04-27 13:12:50 +0300

    added solution

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
 
+public class CandidateValidator<T extends Candidate> implements Predicate<T> {
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int AGE = 35;
+    private static final int YEARS_LIVE_IN_UKRAINE = 10;
+
+    @Override
+    public boolean test(T candidate) {
+        String[] rangeYearsLivedInUkraine = candidate.getPeriodsInUkr().split("-");
+        int livedInUkraine = Integer.parseInt(rangeYearsLivedInUkraine[1])
+                - Integer.parseInt(rangeYearsLivedInUkraine[0]);
+        return candidate.getAge() >= AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && livedInUkraine >= YEARS_LIVE_IN_UKRAINE;
+    }
 }
