8a7cc0157e22ae8a33b2265a440946fbf80c3f37
oleksandr-shulha <shulgalex@gmail.com>
2021-04-27 11:51:51 +0300

    All tests works

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
+
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
 
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_PERIOD = 10;
+    private static final int LOWER_AGE = 35;
+    private static final String DELIMITER = "-";
+    private static final String NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] yearsInCountry = candidate.getPeriodsInUkr().split(DELIMITER);
+        int years = Integer.parseInt(yearsInCountry[1])
+                - Integer.parseInt(yearsInCountry[0]);
+        return candidate.getAge() >= LOWER_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && years >= MIN_PERIOD;
+    }
 }
