c76f5ce0025c967325257df1f9bbd6acc4674cc3
Koziarevskyi <koziarevskyi@gmail.com>
2021-04-27 04:56:00 +0300

    C001 - All Done! For Review!

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
 
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_ALLOWABLE_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int YEARS_LIVING_IN_UKRAINE = 10;
+    private static final String SPLIT_REGEX = "-";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] period = candidate.getPeriodsInUkr().split(SPLIT_REGEX);
+        int periodInUkraine = Integer.parseInt(period[1])
+                - Integer.parseInt(period[0]);
+
+        return candidate.getAge() >= MIN_ALLOWABLE_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && periodInUkraine >= YEARS_LIVING_IN_UKRAINE;
+    }
 }
