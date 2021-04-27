0d64ab11b2174673b03b9cb55c16d247e5130561
Yaroslav Rybalko <yar.rybka@gmail.com>
2021-04-27 17:20:44 +0300

    2ND ATTEMPT WITH STREAM -added Predicates -deleted Whitespaces

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,7 +7,6 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final String NATIONALITY = "Ukrainian";
     private static final int AGE = 35;
     private static final int PERIOD = 10;
-
     private static final String SPLITERATOR = "-";
     private static final int START_PERIOD = 0;
     private static final int END_PERIOD = 1;
@@ -20,6 +19,5 @@ public class CandidateValidator implements Predicate<Candidate> {
                 && candidate.getNationality().equals(NATIONALITY)
                 && Integer.parseInt(dates[END_PERIOD]) - Integer.parseInt(dates[START_PERIOD])
                     >= PERIOD;
-
     }
 }
