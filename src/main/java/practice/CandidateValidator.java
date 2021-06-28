5a04f76c96a26d6388f4118cb4e0d2dba231e0e1
Anastasiia <81781479+aaananas@users.noreply.github.com>
2021-06-28 13:17:54 +0200

    Update CandidateValidator.java

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,6 +5,7 @@ import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
     private static final String REQUIRED_NATIONALITY = "Ukrainian";
+    private static final String YEARS_SEPARATOR = "-";
     private static final int REQUIRED_MINIMUM_AGE = 35;
     private static final int REQUIRED_PERIOD_IN_COUNTRY = 10;
     private static final int BEGINNING = 0;
@@ -12,13 +13,16 @@ public class CandidateValidator implements Predicate<Candidate> {
 
     @Override
     public boolean test(Candidate candidate) {
-        int beginningOfPeriodInUkr = Integer.parseInt(
-                candidate.getPeriodsInUkr().split("-")[BEGINNING]);
-        int endOfPeriodInUkr = Integer.parseInt(
-                candidate.getPeriodsInUkr().split("-")[END]);
         return candidate.getAge() >= REQUIRED_MINIMUM_AGE
                 && candidate.isAllowedToVote()
                 && candidate.getNationality().equals(REQUIRED_NATIONALITY)
-                && endOfPeriodInUkr - beginningOfPeriodInUkr >= REQUIRED_PERIOD_IN_COUNTRY;
+                && calculatePeriodInUkraine(candidate.getPeriodsInUkr()) 
+                        >= REQUIRED_PERIOD_IN_COUNTRY;
+    }
+    
+    private int calculatePeriodInUkraine(String years) {
+        String[] yearsInUkraine = years.split(YEARS_SEPARATOR);
+        return Integer.parseInt(yearsInUkraine[END]) 
+            - Integer.parseInt(yearsInUkraine[BEGINNING]);
     }
 }
