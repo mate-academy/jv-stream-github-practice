f1ab5a6b115ce42081f175eb95e310fcbab2d841
olesiasmitiukh <d.olleska@gmail.com>
2021-06-29 21:14:19 +0300

    implements Predicate, overrided method test, added helped method isValidateLivePeriodInUkraine

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,24 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final String SEPARATOR = "-";
+    private static final int MIN_CANDIDATES_AGE = 35;
+    private static final int MIN_LIVING_PERIOD = 10;
+    private static final String CANDIDATES_NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MIN_CANDIDATES_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(CANDIDATES_NATIONALITY)
+                && isValidateLivePeriodInUkraine(candidate.getPeriodsInUkr());
+    }
+
+    private boolean isValidateLivePeriodInUkraine(String periodsInUkr) {
+        String[] years = periodsInUkr.split(SEPARATOR);
+        return (Integer.parseInt(years[1]) - Integer.parseInt(years[0])) >= MIN_LIVING_PERIOD;
+    }
 }
