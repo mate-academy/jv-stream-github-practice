e1993fcc24f7ab21f29eeed3c560f2c3076a3c43
Anastasiia <81781479+aaananas@users.noreply.github.com>
2021-06-30 11:27:09 +0200

    Update StreamPractice.java

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 import model.Candidate;
 import model.Cat;
 import model.Person;
