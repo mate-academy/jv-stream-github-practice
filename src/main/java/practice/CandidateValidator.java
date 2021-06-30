fd301c0dd25411f0a801f065837eb7acdc9d1806
Tamara-Paskalova <forptv@gmail.com>
2021-06-30 18:20:39 +0300

    added logic at all methods, implemented Predicate at CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,26 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int ACCEPTED_AGE = 35;
+    private static final int ACCEPTED_NUMBERS_OF_YEARS_IN_UKRAINE = 10;
+    private static final int INDEX_OF_LAST_YEAR_IN_UKRAINE = 1;
+    private static final int INDEX_OF_FIRST_YEAR_IN_UKRAINE = 0;
+    private static final String ACCEPTED_NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] yearsLivedInUkraine = candidate.getPeriodsInUkr().split("-");
+        if (Integer.parseInt(yearsLivedInUkraine[INDEX_OF_LAST_YEAR_IN_UKRAINE])
+                - Integer.parseInt(yearsLivedInUkraine[INDEX_OF_FIRST_YEAR_IN_UKRAINE])
+                < ACCEPTED_NUMBERS_OF_YEARS_IN_UKRAINE) {
+            return false;
+        }
+        if (candidate.getAge() < ACCEPTED_AGE || !candidate.isAllowedToVote()) {
+            return false;
+        }
+        return candidate.getNationality().equals(ACCEPTED_NATIONALITY);
+    }
 }
