b090f4c74059857d89d59aac46117aa67ae32fb9
Daniel Kuruch <kuruch1004@gmail.com>
2021-06-30 12:57:04 +0300

    refactored code

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -8,20 +8,21 @@ public class CandidateValidator implements Predicate<Candidate> {
     public static final int START_PERIOD_INDEX = 0;
     public static final int FINISH_PERIOD_INDEX = 1;
     public static final String UKRAINIAN_NATIONALITY = "Ukrainian";
-    public static final int MIN_AGE_PERIOD = 10;
+    public static final int MIN_YEARS_PERIOD = 10;
 
     @Override
     public boolean test(Candidate candidate) {
-        if (candidate == null
-                || candidate.getAge() < CandidateValidator.MIN_AGE
-                || !candidate.isAllowedToVote()
-                || !candidate.getNationality().equals(UKRAINIAN_NATIONALITY)) {
-            return false;
-        }
+        return candidate != null
+                && isPeriodAppropiate(candidate)
+                && candidate.getAge() >= MIN_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(UKRAINIAN_NATIONALITY);
+    }
+
+    private boolean isPeriodAppropiate(Candidate candidate) {
         String[] period = candidate.getPeriodsInUkr().split("-");
         int start = Integer.parseInt(period[START_PERIOD_INDEX]);
         int finish = Integer.parseInt(period[FINISH_PERIOD_INDEX]);
-        return finish - start >= MIN_AGE_PERIOD;
+        return finish - start >= MIN_YEARS_PERIOD;
     }
-    //write your code here
 }
