030e0b9fe7ec0ec351e553ac23e488695d13a468
Vladyslav Baydak <74090608+Optipist@users.noreply.github.com>
2021-07-01 15:59:19 +0300

    Update src/main/java/practice/CandidateValidator.java
    Co-authored-by: Sofia Diakonova <36365171+Sofasmile@users.noreply.github.com>

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,7 +6,7 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int VALID_AGE = 35;
     private static final String VALID_NATIONALITY = "Ukrainian";
-    private static final int IN_UKRAINE = 10;
+    private static final int MIN_YEARS_IN_UKRAINE = 10;
 
     @Override
     public boolean test(Candidate candidate) {
