e209a44d55dcc559969b1b8e1eb5156513cfdc5e
docer <vlasovengineer@gmail.com>
2021-07-01 13:18:13 +0300

    create methods: findMinEvenNumber getOddNumsAverage selectMenByAge getWorkablePeople getCatsNames validateCandidates and CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    public static final int MIN_AGE_FOR_CANDIDATE = 35;
+    public static final int MIN_YEARS_IN_UKRAINE = 10;
+    public static final int START_LIVE_IN_UKRAINE = 0;
+    public static final int FINISH_LIVE_IN_UKRAINE = 1;
+    public static final String NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate person) {
+        String[] periodsInUkr = person.getPeriodsInUkr().split("-");
+        int yearsInUkr = Integer.parseInt(periodsInUkr[FINISH_LIVE_IN_UKRAINE])
+                - Integer.parseInt(periodsInUkr[START_LIVE_IN_UKRAINE]);
+        return person.getAge() >= MIN_AGE_FOR_CANDIDATE
+                && person.isAllowedToVote()
+                && person.getNationality().equals(NATIONALITY)
+                && yearsInUkr >= MIN_YEARS_IN_UKRAINE;
+    }
 }
