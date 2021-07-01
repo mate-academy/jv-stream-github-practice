17e7dcac614b54a2f2d4f7821217c339b2a68018
Optipist <vlad.baydak28@gmail.com>
2021-07-01 14:45:53 +0300

    Added methods implementation, using Stream API

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,14 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] periodYears = candidate.getPeriodsInUkr().split("-");
+        return candidate.getAge() >= 35 && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && Integer.parseInt(periodYears[1]) - Integer.parseInt(periodYears[0]) >= 10;
+    }
 }
