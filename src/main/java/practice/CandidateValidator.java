8245786273592d6796aa5151355ea5d727287283
Dana Khromenko <danadiadius@gmail.com>
2021-07-28 08:35:11 +0300

    Implemented CandidateValidator and solved tasks

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,27 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import model.Candidate;
+
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate != null
+                && candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && yearsInUkraine(candidate.getPeriodsInUkr()) >= 10;
+    }
+
+    private int yearsInUkraine(String periodsInUkr) {
+        if (periodsInUkr == null || periodsInUkr.length() != 9) {
+            return 0;
+        }
+        try {
+            return Integer.parseInt(periodsInUkr.substring(5, 9)) - Integer.parseInt(periodsInUkr.substring(0, 4));
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+    }
 }
