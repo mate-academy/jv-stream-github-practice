2d5c1c2a1169189a2f11e61be2f54b809f493fa5
Dima Petrushenko <dmytro.petrushenko@gmail.com>
2021-09-06 12:15:52 +0300

    Teat Ok & mvn OK

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,19 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
     //write your code here
+    static final int validAge = 35;
+    static final int validLiveInUkraine = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] periods = candidate.getPeriodsInUkr().split("-");
+        int liveInUkraine = Integer.parseInt(periods[1]) - Integer.parseInt(periods[0]);
+
+        return candidate.getNationality().equals("Ukrainian") && candidate.isAllowedToVote()
+                && candidate.getAge() >= validAge && liveInUkraine >= validLiveInUkraine;
+    }
 }
