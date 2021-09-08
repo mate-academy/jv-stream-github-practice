b7ab0b7a2d9df02dd687ed152926dd6e031938bf
mishamolnar <michaellmuller423@gmail.com>
2021-09-08 14:29:35 +0300

    filter logic changed

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -17,7 +17,6 @@ public class StreamPractice {
      * If there is no needed data throw RuntimeException with message
      * "Can't get min value from list: < Here is our input 'numbers' >"
      */
-
     public int findMinEvenNumber(List<String> numbers) {
         return numbers.stream()
                 .flatMap(line -> Arrays.stream(line.split(",")))
