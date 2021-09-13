3fdef0721fef0aa391614a61c120214f7201d149
mishamolnar <michaellmuller423@gmail.com>
2021-09-13 13:55:56 +0300

    predicates created

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,11 +6,12 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int ACCEPTABLE_AGE = 35;
     private static final int ACCEPTABLE_YEARS = 10;
+    private static final String UKRAINIAN_NATIONALITY = "Ukrainian";
 
     @Override
     public boolean test(Candidate candidate) {
         return candidate.getAge() >= ACCEPTABLE_AGE
-                && "Ukrainian".equals(candidate.getNationality())
+                && UKRAINIAN_NATIONALITY.equals(candidate.getNationality())
                 && candidate.isAllowedToVote()
                 && Integer.parseInt(candidate.getPeriodsInUkr().split("-")[1])
                 - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[0]) >= ACCEPTABLE_YEARS;
