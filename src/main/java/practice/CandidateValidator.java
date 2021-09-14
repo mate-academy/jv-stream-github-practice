f5d0be2fec66d580267cad6a126c56051b173d94
vitalii_yelinskyi <yelinskyizp@gmail.com>
2021-09-14 13:09:51 +0300

    added two constant in CandidateValidator class

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,14 +7,16 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int AGE_CANDIDATE = 35;
     private static final int PERIOD_IN_UKRAINE = 10;
     private static final String NATIONALITY = "Ukrainian";
+    private static final int INDEX_YEAR_FROM = 0;
+    private static final int INDEX_YEAR_TO = 1;
 
     @Override
     public boolean test(Candidate candidate) {
         return candidate.getAge() >= AGE_CANDIDATE
                 && candidate.isAllowedToVote()
                 && candidate.getNationality().equals(NATIONALITY)
-                && (Integer.parseInt(candidate.getPeriodsInUkr().split("-")[1])
-                - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[0])
+                && (Integer.parseInt(candidate.getPeriodsInUkr().split("-")[INDEX_YEAR_TO])
+                - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[INDEX_YEAR_FROM])
                 >= PERIOD_IN_UKRAINE);
     }
 }
