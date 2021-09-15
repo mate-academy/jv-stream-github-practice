acebdcc6f2bcbf541c25501c5d31c45c14b87470
Yehor Zapara <egor.zapara1994@gmail.com>
2021-09-15 12:45:31 +0300

    code was optimized

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,17 +1,28 @@
 package practice;
 
+import java.util.function.Predicate;
 import model.Candidate;
 
-import java.util.function.Predicate;
-
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int MIN_AGE = 35;
+    private static final int MIN_YEARS = 10;
+
     @Override
     public boolean test(Candidate candidate) {
-        return candidate.getAge() >= 35 && candidate.isAllowedToVote()
-                && candidate.getNationality().equals("Ukrainian")
-                && Integer.parseInt(candidate.getPeriodsInUkr().substring(candidate.getPeriodsInUkr().indexOf("-") + 1))
-                - Integer.parseInt(candidate.getPeriodsInUkr().substring(0, candidate.getPeriodsInUkr().indexOf("-")))
-                >= 10;
+        return candidate.getAge() >= MIN_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && livedInUkraineSince(candidate) - leftUkraineIn(candidate) >= MIN_YEARS;
     }
 
+    private int livedInUkraineSince(Candidate candidate) {
+        return Integer.parseInt(candidate.getPeriodsInUkr()
+                .substring(candidate.getPeriodsInUkr().indexOf("-") + 1));
+    }
+
+    private int leftUkraineIn(Candidate candidate) {
+        return Integer.parseInt(candidate.getPeriodsInUkr()
+                .substring(0, candidate.getPeriodsInUkr().indexOf("-")));
+    }
 }
