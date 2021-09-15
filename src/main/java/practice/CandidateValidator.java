c0a94055ec7c8fce16a84411417ddfe67c5893ff
Solomiia Hrynda <solomiyaaaa@gmail.com>
2021-09-16 00:11:04 +0300

    implemented all methods to tasks. Added 2 additional methods

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,22 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE_FOR_APPLYING = 35;
+    private static final String REQUIRED_NATIONALITY = "Ukrainian";
+    private static final int MIN_YEAR_LIVING_IN_UKRAINE = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] periodsInUkraine = candidate.getPeriodsInUkr().split("-");
+        int yearsInUkraine = Integer.parseInt(periodsInUkraine[1])
+                - Integer.parseInt(periodsInUkraine[0]);
+        return candidate.getAge() >= MIN_AGE_FOR_APPLYING
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(REQUIRED_NATIONALITY)
+                && yearsInUkraine >= MIN_YEAR_LIVING_IN_UKRAINE;
+    }
 }
+
