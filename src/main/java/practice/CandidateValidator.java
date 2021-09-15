eda86684bb824b5f4f16f71d6cffdfe5be20803d
Serhii Blakh <s.a.blakh@ukr.net>
2021-09-15 16:42:00 +0300

    added constants and suggested changes

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,15 +4,20 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int VALID_CANDIDATE_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int INDEX_PERIOD_IN_UKRAINE_START = 0;
+    private static final int INDEX_PERIOD_IN_UKRAINE_END = 1;
+    private static final int VALID_PERIOD_IN_UKRAINE = 10;
 
     @Override
     public boolean test(Candidate candidate) {
         String[] periodInUkraine = candidate.getPeriodsInUkr().split("-");
-        int periodInUkraineStart = Integer.parseInt(periodInUkraine[0]);
-        int periodInUkraineFinish = Integer.parseInt(periodInUkraine[1]);
+        int periodInUkraineStart = Integer.parseInt(periodInUkraine[INDEX_PERIOD_IN_UKRAINE_START]);
+        int periodInUkraineFinish = Integer.parseInt(periodInUkraine[INDEX_PERIOD_IN_UKRAINE_END]);
         return candidate.isAllowedToVote()
-                && candidate.getAge() >= 35
-                && candidate.getNationality().equals("Ukrainian")
-                && periodInUkraineFinish - periodInUkraineStart > 10;
+                && candidate.getAge() >= VALID_CANDIDATE_AGE
+                && candidate.getNationality().equals(NATIONALITY)
+                && periodInUkraineFinish - periodInUkraineStart > VALID_PERIOD_IN_UKRAINE;
     }
 }
