20e92170d6b3a8c73daa3dd6aad35b2012ff8fc2
misha8096 <sichmisha@gmail.com>
2021-09-16 00:13:09 +0300

    implemented method test() in CandidateValidator class, solved all tasks in StreamPractice class.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_CANDIDATE_AGE = 35;
+    private static final int MIN_YEARS_IN_UKR = 10;
+    private static final String NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split("-");
+        int yearsInUkraine = Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
+        return candidate.getNationality().equals(NATIONALITY)
+                && candidate.isAllowedToVote()
+                && yearsInUkraine >= MIN_YEARS_IN_UKR
+                && candidate.getAge() >= MIN_CANDIDATE_AGE;
+    }
 }
