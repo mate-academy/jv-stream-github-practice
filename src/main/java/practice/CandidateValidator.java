e83eb8e24eb90fa744d183c3fe0d4582cd3b8bb8
misha8096 <sichmisha@gmail.com>
2021-09-16 12:10:57 +0300

    created constants, combined map and flatMap methods

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,12 +6,16 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_CANDIDATE_AGE = 35;
     private static final int MIN_YEARS_IN_UKR = 10;
+    private static final int FROM_YEARS_INDEX = 0;
+    private static final int TO_YEARS_INDEX = 1;
     private static final String NATIONALITY = "Ukrainian";
+    private static final String SPLIT_SYMBOL = "-";
 
     @Override
     public boolean test(Candidate candidate) {
-        String[] years = candidate.getPeriodsInUkr().split("-");
-        int yearsInUkraine = Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
+        String[] years = candidate.getPeriodsInUkr().split(SPLIT_SYMBOL);
+        int yearsInUkraine = Integer.parseInt(years[TO_YEARS_INDEX])
+                - Integer.parseInt(years[FROM_YEARS_INDEX]);
         return candidate.getNationality().equals(NATIONALITY)
                 && candidate.isAllowedToVote()
                 && yearsInUkraine >= MIN_YEARS_IN_UKR
--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,7 +1,6 @@
 package practice;
 
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
@@ -14,9 +13,8 @@ public class StreamPractice {
 
     public int findMinEvenNumber(List<String> numbers) {
         return numbers.stream()
-                .map(s -> s.split(","))
-                .flatMap(Arrays::stream)
-                .mapToInt(Integer::valueOf)
+                .flatMap(i -> Arrays.stream(i.split(",")))
+                .mapToInt(Integer::parseInt)
                 .filter(number -> number % 2 == 0)
                 .min()
                 .orElseThrow(() -> new RuntimeException("Can't get min value from list: "
@@ -55,8 +53,7 @@ public class StreamPractice {
         return peopleList.stream()
                 .filter(person -> person.getSex() == Person.Sex.WOMAN
                 && person.getAge() >= femaleAge)
-                .map(Person::getCats)
-                .flatMap(Collection::stream)
+                .flatMap(p -> p.getCats().stream())
                 .map(Cat::getName)
                 .collect(Collectors.toList());
     }
