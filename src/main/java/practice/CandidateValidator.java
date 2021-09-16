eb7cf51ea0ac90e383f9dba24b256bb825fe507f
Roman Legendzevych <green3942@gmail.com>
2021-09-16 15:35:57 +0300

    fixed some constant names

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,18 +4,18 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    private static final int AGE = 35;
+    private static final int MIN_AGE = 35;
     private static final String NATIONALITY = "Ukrainian";
-    private static final int YEARS = 10;
+    private static final int REQUIRED_LIVING_PERIOD = 10;
     private static final int START = 1;
     private static final int END = 0;
 
     @Override
     public boolean test(Candidate candidate) {
         String[] years = candidate.getPeriodsInUkr().split("-");
-        return candidate.getAge() >= AGE
+        return candidate.getAge() >= MIN_AGE
                 && candidate.isAllowedToVote()
                 && candidate.getNationality().equals(NATIONALITY)
-                && Integer.parseInt(years[START]) - Integer.parseInt(years[END]) >= YEARS;
+                && Integer.parseInt(years[START]) - Integer.parseInt(years[END]) >= REQUIRED_LIVING_PERIOD;
     }
 }
