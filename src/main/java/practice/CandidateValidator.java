89518f0fc800084d2bd23c00ae62efe2637def63
Roman Dandali <Dandali2018@gmail.com>
2021-09-16 15:30:55 +0300

    Implements all methods

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,25 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int FROM_AGE = 35;
+    private static final int FIRST_YEAR = 0;
+    private static final int SECOND_YEAR = 1;
+    private static final int YEARS_IN_UKRAINE = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= FROM_AGE && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && checkYearsInUkr(candidate);
+    }
+
+    private boolean checkYearsInUkr(Candidate candidate) {
+        String[] periods = candidate.getPeriodsInUkr().split("-");
+        return Integer.parseInt(periods[SECOND_YEAR])
+                - Integer.parseInt(periods[FIRST_YEAR])
+                >= YEARS_IN_UKRAINE;
+    }
 }
