50a3702a1c3cc94c612929e082af4aac0cd1804c
Roman Legendzevych <green3942@gmail.com>
2021-09-16 14:29:22 +0300

    data was filtered

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,22 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int YEARS = 10;
+    private static final int START = 1;
+    private static final int END = 0;
+
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split("-");
+        return candidate.getAge() >= AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && Integer.parseInt(years[START]) - Integer.parseInt(years[END]) >= YEARS;
+    }
 }
