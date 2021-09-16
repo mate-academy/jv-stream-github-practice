ca42b98e503be9ba548a6113697447352fcd8bd0
Denys Stefanko <denys.stefanko@gmail.com>
2021-09-16 16:37:40 +0300

    fixes after review

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,11 +1,10 @@
 package practice;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.Collection;
 import model.Candidate;
 import model.Cat;
 import model.Person;
