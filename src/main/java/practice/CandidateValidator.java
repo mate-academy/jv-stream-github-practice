acfbba2159405a92e98d250b9a8a509475399a09
Alexandr <netiazhevskyi@gmail.com>
2021-09-16 14:12:21 +0300

    stream practice completed

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,10 +4,16 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int TO_YEAR = 1;
+    private static final int FROM_YEAR = 0;
+    private static final int YEARS_REQUIRED = 10;
+
     @Override
     public boolean test(Candidate candidate) {
         String[] years = candidate.getPeriodsInUkr().split("-");
-        boolean isPeriodInUkr = ((Integer.parseInt(years[1])) - (Integer.parseInt(years[0]))) >= 10;
+        boolean isPeriodInUkr = ((Integer.parseInt(years[TO_YEAR]))
+                - (Integer.parseInt(years[FROM_YEAR])))
+                >= YEARS_REQUIRED;
         return candidate.getAge() >= 35 && candidate.isAllowedToVote()
                 && candidate.getNationality().equals("Ukrainian") && isPeriodInUkr;
     }
