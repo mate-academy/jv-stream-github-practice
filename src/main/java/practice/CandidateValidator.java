0bfdb7150af9bade647ff0f5211fde0c6bdf91ed
Nazar Hetun <nazarhetun@gmail.com>
2021-09-16 17:04:08 +0300

    StreamPractice done

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,16 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    @Override
+    public boolean test(Candidate candidate) {
+        int periodInUkraine = Integer.parseInt(candidate.getPeriodsInUkr().split("-")[1])
+                - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[0]);
+        return candidate.isAllowedToVote() && candidate.getAge() >= 35
+                && candidate.getNationality().equals("Ukrainian")
+                && periodInUkraine >= 10;
+    }
 }
