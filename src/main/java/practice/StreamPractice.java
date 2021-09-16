cb4cac3af8e8be3ac4daf4ee7617a5a7377622cc
Dmytro Heleta <dmytroheleta@gmail.com>
2021-09-16 15:39:39 +0300

    simplified verification in method getWorkablePeople

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -40,10 +40,9 @@ public class StreamPractice {
     public List<Person> getWorkablePeople(int fromAge, int femaleToAge,
                                           int maleToAge, List<Person> peopleList) {
         return peopleList.stream()
-                .filter(p -> (p.getSex().equals(Person.Sex.MAN)
-                        && p.getAge() >= fromAge && p.getAge() <= maleToAge)
-                        || (p.getSex().equals(Person.Sex.WOMAN)
-                        && p.getAge() >= fromAge && p.getAge() <= femaleToAge))
+                .filter(p -> (fromAge <= p.getAge()
+                        && ((p.getSex() == Person.Sex.MAN && p.getAge() <= maleToAge)
+                        || (p.getSex() == Person.Sex.WOMAN && p.getAge() <= femaleToAge))))
                 .collect(Collectors.toList());
 
     }
