c33a8c0d6acb969a7dcd9670a1336905ea589888
Rostyslav Fostyk <rostyslavfostykjob@gmail.com>
2021-09-16 14:01:20 +0300

    renamed constants in CandidateValidator class

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,19 +4,19 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    private static final int CANDIDATE_VALID_AGE = 35;
+    private static final int CANDIDATE_VALID_AGE_FROM = 35;
     private static final String CANDIDATE_NATIONALITY = "Ukrainian";
     private static final int PERIOD_FROM_INDEX = 0;
     private static final int PERIOD_TO_INDEX = 1;
-    private static final int PERIOD_IN_UKRAINE = 10;
+    private static final int MIN_PERIOD_IN_UKRAINE = 10;
 
     @Override
     public boolean test(Candidate candidate) {
         String[] period = candidate.getPeriodsInUkr().split("-");
-        return candidate.getAge() >= CANDIDATE_VALID_AGE
+        return candidate.getAge() >= CANDIDATE_VALID_AGE_FROM
                 && candidate.isAllowedToVote()
                 && candidate.getNationality().equals(CANDIDATE_NATIONALITY)
                 && Integer.parseInt(period[PERIOD_TO_INDEX])
-                    - Integer.parseInt(period[PERIOD_FROM_INDEX]) >= PERIOD_IN_UKRAINE;
+                    - Integer.parseInt(period[PERIOD_FROM_INDEX]) >= MIN_PERIOD_IN_UKRAINE;
     }
 }
