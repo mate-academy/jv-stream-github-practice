adf83e2df866fec8148fd50cb44b9c1a39d5df0e
Rostyslav Fostyk <rostyslavfostykjob@gmail.com>
2021-09-16 12:25:05 +0300

    refactored getWorkablePeople method

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -40,9 +40,10 @@ public class StreamPractice {
     public List<Person> getWorkablePeople(int fromAge, int femaleToAge,
                                           int maleToAge, List<Person> peopleList) {
         return peopleList.stream()
-                .filter(person -> person.getSex() == Person.Sex.MAN
-                                ? person.getAge() >= fromAge && person.getAge() <= maleToAge
-                                : person.getAge() >= fromAge && person.getAge() <= femaleToAge)
+                .filter(person -> person.getAge() >= fromAge
+                        && (person.getSex().equals(Person.Sex.MAN)
+                        ? person.getAge() <= maleToAge
+                        : person.getAge() <= femaleToAge))
                 .collect(Collectors.toList());
     }
 
