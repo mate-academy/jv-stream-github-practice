f0271ffae83fd8e1cadd431d36a5080bbf5e3ae8
Dmytro Heleta <dmytroheleta@gmail.com>
2021-09-16 17:38:43 +0300

    removed redundant import

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,7 +1,6 @@
 package practice;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
