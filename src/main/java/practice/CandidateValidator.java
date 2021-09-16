76372018309099a1ef9abbc8e908450ca3e6b93b
P.Zbarazkyy <mtu09@ukr.net>
2021-09-16 16:10:59 +0300

    implemented methods findMinEvenNumber, getOddNumsAverage, selectMenByAge, getWorkablePeople, getCatsNames, validateCandidates

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final int MIN_TIME_IN_UKR = 10;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int INDEX_FROM = 0;
+    private static final int INDEX_TO = 1;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] periodSplit = candidate.getPeriodsInUkr().split("-");
+        int periodInUkr = Integer.parseInt(periodSplit[INDEX_TO])
+                - Integer.parseInt(periodSplit[INDEX_FROM]);
+        return candidate.getAge() >= MIN_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && periodInUkr >= MIN_TIME_IN_UKR;
+    }
 }
