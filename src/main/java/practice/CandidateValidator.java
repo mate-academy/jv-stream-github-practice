e44460d4b72e64fdb7c6b49bf83acecb619ee6d5
Gleb Kreshchuk <gleb.net@gmail.com>
2021-09-18 13:38:50 +0300

    StreamPractice taks completed

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,22 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int SUITABLE_AGE = 35;
+    private static final int YEARS_IN_UKRAINE_OK = 10;
+    private static final int YEARS_IN_UKRAINE_START = 0;
+    private static final int YEARS_IN_UKRAINE_END = 1;
+    private static final String NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= SUITABLE_AGE
+            && NATIONALITY.equals(candidate.getNationality())
+            && candidate.isAllowedToVote()
+            && Integer.parseInt(candidate.getPeriodsInUkr().split("-")[YEARS_IN_UKRAINE_END])
+            - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[YEARS_IN_UKRAINE_START])
+                >= YEARS_IN_UKRAINE_OK;
+    }
 }
