e9540d6a619c1533f7b344d660e7b23ca215421e
Oleg Kvasha <oleg.kvasha.00@gmail.com>
2021-09-25 13:11:49 +0300

    implemented StreamPractice and CandidateValidator classes

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,19 @@
 package practice;
 
-public class CandidateValidator {
+import model.Candidate;
+
+import java.util.Arrays;
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        int[] periodsInUkr = Arrays.stream(candidate.getPeriodsInUkr().split("-"))
+                .mapToInt(Integer::parseInt).toArray();
+        boolean hasTenYearsInUkr = periodsInUkr[1] - periodsInUkr[0] >= 10;
+        boolean isUkrainian = candidate.getNationality().equals("Ukrainian");
+        boolean allowedAge = candidate.getAge() >= 35;
+        return hasTenYearsInUkr && candidate.isAllowedToVote() && isUkrainian && allowedAge;
+    }
     //write your code here
 }
