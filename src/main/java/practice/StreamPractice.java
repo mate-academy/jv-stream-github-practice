b48874216be6145b77c5072d0a52369ac63ad392
Oleg Kvasha <oleg.kvasha.00@gmail.com>
2021-09-29 19:24:10 +0300

    Fixed checkstyle error

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -2,8 +2,6 @@ package practice;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import model.Candidate;
