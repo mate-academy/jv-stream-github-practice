4dddb493ddaef0213e343bfaae0d82f21c05898a
Alex Hazniuk <ohazniuk@gmail.com>
2021-11-04 18:03:52 +0200

    implemented task stream

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,28 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int FIRST_INDEX = 0;
+    private static final int SECOND_INDEX = 1;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && getLastYear(candidate.getPeriodsInUkr())
+                - getFirstYear(candidate.getPeriodsInUkr()) >= 10;
+    }
+
+    private int getFirstYear(String period) {
+        String[] dates = period.split("-");
+        return Integer.parseInt(dates[FIRST_INDEX]);
+    }
+
+    private int getLastYear(String period) {
+        String[] dates = period.split("-");
+        return Integer.parseInt(dates[SECOND_INDEX]);
+    }
 }
