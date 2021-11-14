85b8cd2741a0431007942cac44c9affdcaabcd3f
Tyvoniuk Dmytro <dmytro.tyvoniuk@gmail.com>
2021-11-14 18:48:42 +0200

    All methods are implemented

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,18 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] splittedYearsInUkraine
+                = candidate.getPeriodsInUkr().split("-");
+        int yearsInUkraine = Integer.parseInt(splittedYearsInUkraine[1])
+                - Integer.parseInt(splittedYearsInUkraine[0]);
+        return candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && candidate.getAge() >= 35
+                && yearsInUkraine >= 10;
+    }
 }
