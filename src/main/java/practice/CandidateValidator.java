fef0fd5b5062c709b08becf15dd594da144814ee
Andrey Sidorkin <andrei.sidorkin@gmail.com>
2021-11-15 20:26:53 +0200

    correction added some variables for more readable code

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,16 +4,18 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    public static final int SUITABLE_AGE = 35;
-    public static final int SUITABLE_PERIOD_IN_UKRAINE = 10;
+    private static final int SUITABLE_AGE = 35;
+    private static final int SUITABLE_PERIOD_IN_UKRAINE = 10;
+    private static final String SUITABLE_NATIONALITY = "Ukrainian";
 
     @Override
     public boolean test(Candidate candidate) {
-        return candidate.getNationality().equals("Ukrainian")
-            && Integer.parseInt(candidate.getPeriodsInUkr()
-                .replaceAll("\\d+-", ""))
-            - Integer.parseInt(candidate.getPeriodsInUkr()
-                .replaceAll("-\\d+", "")) >= SUITABLE_PERIOD_IN_UKRAINE
+        int startedToLiveInCountry = Integer.parseInt(candidate.getPeriodsInUkr()
+                .replaceAll("-\\d+", ""));
+        int finishedToLiveInCountry = Integer.parseInt(candidate.getPeriodsInUkr()
+                .replaceAll("\\d+-", ""));
+        return candidate.getNationality().equals(SUITABLE_NATIONALITY)
+            && finishedToLiveInCountry - startedToLiveInCountry >= SUITABLE_PERIOD_IN_UKRAINE
             && candidate.getAge() >= SUITABLE_AGE && candidate.isAllowedToVote();
     }
 }
