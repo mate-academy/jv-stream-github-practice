e29a11009f9c321afe5f374dbfb706a5443565b9
Oleg Shiriaev <mystidreamer@gmail.com>
2021-11-16 09:58:58 +0200

    constants was changed

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,6 +7,8 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_CANDIDATE_AGE = 35;
     private static final int MIN_CANDIDATE_LIVE_PERIOD_IN_COUNTRY = 10;
     private static final String REQUIRED_CANDIDATE_NATIONALITY = "Ukrainian";
+    private static final int ZERO_DATE_VALUE_INDEX = 0;
+    private static final int FIRST_DATE_VALUE_INDEX = 1;
     /**
      * Your help with a election is needed. Given list of candidates, where each element
      * has Candidate.class type.
@@ -23,8 +25,8 @@ public class CandidateValidator implements Predicate<Candidate> {
     @Override
     public boolean test(Candidate candidate) {
         String[] years = candidate.getPeriodsInUkr().split("-");
-        final int periodStart = Integer.parseInt(years[1]);
-        final int periodEnd = Integer.parseInt(years[0]);
+        int periodStart = Integer.parseInt(years[FIRST_DATE_VALUE_INDEX]);
+        int periodEnd = Integer.parseInt(years[ZERO_DATE_VALUE_INDEX]);
         return candidate.getAge() >= MIN_CANDIDATE_AGE
                 && candidate.isAllowedToVote()
                 && candidate.getNationality().equals(REQUIRED_CANDIDATE_NATIONALITY)
