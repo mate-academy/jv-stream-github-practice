121ed59ca60dac0866ed15d4f05ab93d5a442034
Konstantin <www.kosta.kon@gmail.com>
2021-11-17 14:46:24 +0200

    All methods were implemented.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,24 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    public static final int MIN_AGE = 35;
+    public static final int MIN_PERIOD_IN_UK = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return getPeriodFromStringRange(candidate.getPeriodsInUkr()) >= MIN_PERIOD_IN_UK
+                && candidate.getAge() >= MIN_AGE
+                && candidate.getNationality().equals("Ukrainian")
+                && candidate.isAllowedToVote();
+    }
+
+    private int getPeriodFromStringRange(String range) {
+        String[] fromTo = range.split("-");
+        int from = Integer.parseInt(fromTo[0]);
+        int to = Integer.parseInt(fromTo[1]);
+        return to - from;
+    }
 }
