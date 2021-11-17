40f1c22f3f8f54dc6e39d7f4a4f175e5ea35fb57
SonyaPakhomova <88778908+SonyaPakhomova@users.noreply.github.com>
2021-11-17 16:58:44 +0200

    After third student rewier

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -8,12 +8,14 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_YEARS_OF_LIVING_IN_UKRAINE = 10;
     private static final String NATIONALITY = "Ukrainian";
     private static final String DELIMITER = "-";
+    private static final int YEAR_FROM = 0;
+    private static final int YEAR_TO = 1;
 
     @Override
     public boolean test(Candidate candidate) {
         String[] period = candidate.getPeriodsInUkr().split(DELIMITER);
-        int start = Integer.parseInt(period[0]);
-        int end = Integer.parseInt(period[1]);
+        int start = Integer.parseInt(period[YEAR_FROM]);
+        int end = Integer.parseInt(period[YEAR_TO]);
         return candidate.getAge() >= MIN_AGE_FOR_THE_CANDIDATE
                 && candidate.getNationality().equals(NATIONALITY)
                 && end - start >= MIN_YEARS_OF_LIVING_IN_UKRAINE
