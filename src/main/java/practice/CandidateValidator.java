022a95116dab38429140f16cc9fd2fdf52249490
SonyaPakhomova <88778908+SonyaPakhomova@users.noreply.github.com>
2021-11-17 16:49:41 +0200

    After second student rewier

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,14 +7,13 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_AGE_FOR_THE_CANDIDATE = 35;
     private static final int MIN_YEARS_OF_LIVING_IN_UKRAINE = 10;
     private static final String NATIONALITY = "Ukrainian";
+    private static final String DELIMITER = "-";
 
     @Override
     public boolean test(Candidate candidate) {
-        int index = candidate.getPeriodsInUkr().indexOf("-");
-        int start = Integer.parseInt(candidate.getPeriodsInUkr()
-                .substring(0, index));
-        int end = Integer.parseInt(candidate.getPeriodsInUkr()
-                  .substring(index + 1));
+        String[] period = candidate.getPeriodsInUkr().split(DELIMITER);
+        int start = Integer.parseInt(period[0]);
+        int end = Integer.parseInt(period[1]);
         return candidate.getAge() >= MIN_AGE_FOR_THE_CANDIDATE
                 && candidate.getNationality().equals(NATIONALITY)
                 && end - start >= MIN_YEARS_OF_LIVING_IN_UKRAINE
