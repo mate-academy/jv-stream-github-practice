0047dca29b095e9ddab212558db438507cde40d8
Yaroslav Kavun <yka40znp@gmail.com>
2021-11-18 14:14:33 +0200

    implemented class CandidateValidator and added 6 filter statistics methods

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MINIMUM_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int LIVING_IN_UKRAINE = 10;
+    private static final int FIRST_DATE_INDEX = 0;
+    private static final int LAST_DATE_INDEX = 1;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] period = candidate.getPeriodsInUkr().split("-");
+        int fromYear = Integer.parseInt(period[FIRST_DATE_INDEX]);
+        int toYear = Integer.parseInt(period[LAST_DATE_INDEX]);
+        return candidate.getAge() >= MINIMUM_AGE
+                && candidate.getNationality().equals(NATIONALITY)
+                && toYear - fromYear >= LIVING_IN_UKRAINE
+                && candidate.isAllowedToVote();
+    }
 }
