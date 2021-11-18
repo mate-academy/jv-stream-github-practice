74868356da5e8af170ba01e121a50af29a47ac57
SonyaPakhomova <88778908+SonyaPakhomova@users.noreply.github.com>
2021-11-18 11:35:23 +0200

    After second  mentor review

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,13 +7,12 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_AGE_FOR_THE_CANDIDATE = 35;
     private static final int MIN_YEARS_OF_LIVING_IN_UKRAINE = 10;
     private static final String REQUIRED_NATIONALITY = "Ukrainian";
-    private static final String DELIMITER = "-";
     private static final int YEAR_FROM_INDEX = 0;
     private static final int YEAR_TO_INDEX = 1;
 
     @Override
     public boolean test(Candidate candidate) {
-        String[] period = candidate.getPeriodsInUkr().split(DELIMITER);
+        String[] period = candidate.getPeriodsInUkr().split("-");
         int yearFrom = Integer.parseInt(period[YEAR_FROM_INDEX]);
         int yearTo = Integer.parseInt(period[YEAR_TO_INDEX]);
         return candidate.getAge() >= MIN_AGE_FOR_THE_CANDIDATE
