e3672e4d48e966d8f928ce2c37683e0720d42706
Vladyslav Chyver <lione2009lione@gmail.com>
2021-11-18 09:13:25 +0200

    code was optimized

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,33 +1,38 @@
 package practice;
 
-import model.Candidate;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import model.Candidate;
 
-public class CandidateValidator implements Predicate<Candidate>{
-  public List<String> selectEligibleCandidates(List<Candidate> candidates) {
-    if (candidates == null){
-      throw new RuntimeException("Input list is null");
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int MIN_TERM_OF_RESIDENCE = 10;
+
+    public List<String> selectEligibleCandidates(List<Candidate> candidates) {
+        if (candidates == null) {
+            throw new RuntimeException("Input list is null");
+        }
+        return candidates.stream().filter(this::test)
+                .map(Candidate::getName)
+                .sorted()
+                .collect(Collectors.toList());
     }
-    return candidates.stream().filter(this::test)
-            .map(Candidate::getName)
-            .sorted()
-            .collect(Collectors.toList());
-  }
-  private boolean isValidFields(Candidate candidate) {
-    return candidate.getNationality() != null && candidate.getPeriodsInUkr() != null
-            && candidate.getName() != null;
-  }
 
-  private static int countYearsInUkraine(String period) {
-    return Integer.parseInt(period.split("-")[1]) - Integer.parseInt(period.split("-")[0]);
-  }
+    private boolean isValidFields(Candidate candidate) {
+        return candidate.getNationality() != null && candidate.getPeriodsInUkr() != null
+                && candidate.getName() != null;
+    }
 
-  @Override
-  public  boolean test(Candidate candidate) {
-    return isValidFields(candidate) && candidate.getAge() >= 35
-            && candidate.isAllowedToVote() && candidate.getNationality().equals("Ukrainian")
-            && countYearsInUkraine(candidate.getPeriodsInUkr()) > 10;
-  }
+    private static int countYearsInUkraine(String period) {
+        return Integer.parseInt(period.split("-")[1]) - Integer.parseInt(period.split("-")[0]);
+    }
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return isValidFields(candidate) && candidate.getAge() >= MIN_AGE
+                && candidate.isAllowedToVote() && candidate.getNationality().equals(NATIONALITY)
+                && countYearsInUkraine(candidate.getPeriodsInUkr()) > MIN_TERM_OF_RESIDENCE;
+    }
 }
