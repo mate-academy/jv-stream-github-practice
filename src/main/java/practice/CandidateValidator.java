e46633421653c3db7a19b70555c260ce0f24579c
Natalia Vidzivashets <nata.vidzivashets@gmail.com>
2021-11-19 00:44:13 +0200

    implemented task jv-stream-practice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,19 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final int MIN_COUNT_YEARS_IN_UKR = 10;
+    private static final String NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split("-");
+        int timeOfLifeInUkraine = Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
+        return candidate.getAge() >= MIN_AGE && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && timeOfLifeInUkraine >= MIN_COUNT_YEARS_IN_UKR;
+    }
 }
