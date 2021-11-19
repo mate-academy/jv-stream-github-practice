ded063c5f9386aeea4a9b1bf3d297656d5b4fedc
Kate Deshchinska <katerinka9636@gmail.com>
2021-11-19 13:23:29 +0200

    add constants

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,14 +4,17 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private final static int MIN_AGE_TO_BE_PRESIDENT = 35;
+    private final static int MIN_PERIOD_IN_UKRAINE = 10;
+    private final static String UKRAINIAN_NATIONALITY = "Ukrainian";
 
     @Override
     public boolean test(Candidate candidate) {
         int periodInUkraine = Integer.parseInt(candidate.getPeriodsInUkr().split("-")[1])
                 - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[0]);
-        return candidate.getAge() >= 35
+        return candidate.getAge() >= MIN_AGE_TO_BE_PRESIDENT
                 && candidate.isAllowedToVote()
-                && candidate.getNationality().equals("Ukrainian")
-                && periodInUkraine >= 10;
+                && candidate.getNationality().equals(UKRAINIAN_NATIONALITY)
+                && periodInUkraine >= MIN_PERIOD_IN_UKRAINE;
     }
 }
