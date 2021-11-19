f0a64ce77e97dec9ad23baac43b814467b4904c9
farfardesu <savavlad003@gmail.com>
2021-11-19 11:57:24 +0200

    StreamPractice methods were implemented as well as CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,19 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int YEAR_FROM = 0;
+    private static final int YEAR_TO = 1;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] splittedYears = candidate.getPeriodsInUkr().split("-");
+        return candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && Integer.parseInt(splittedYears[YEAR_TO])
+                - Integer.parseInt(splittedYears[YEAR_FROM]) >= 10;
+    }
 }
