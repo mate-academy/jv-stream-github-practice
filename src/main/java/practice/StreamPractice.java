03dcc1614ef50e44ebfdffe15a610ed56815d509
Kate Deshchinska <katerinka9636@gmail.com>
2021-11-19 14:19:36 +0200

    commited new changes

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -58,7 +58,7 @@ public class StreamPractice {
 
     public List<String> validateCandidates(List<Candidate> candidates) {
         return candidates.stream()
-                .filter(c -> new CandidateValidator().test(c))
+                .filter(new CandidateValidator())
                 .map(Candidate::getName)
                 .sorted()
                 .collect(Collectors.toList());
