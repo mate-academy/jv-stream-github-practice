a356d69f6e2f74c3001531e6159307549379c8be
Volodimir <vova.stepanenko2020@gmail.com>
2021-11-19 09:51:47 +0200

    kill magic number? rename method checkPeriod to isValidPeriod, rename constant fields

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,24 +4,24 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    private static final int MIN_LIVE_IN_UA = 10;
+    private static final int MIN_YEARS_TO_LIVE_IN_UA = 10;
     private static final String REQUIRED_NATIONALITY = "Ukrainian";
-    private static final int REQUIRED_AGE = 35;
+    private static final int REQUIRED_MIN_AGE = 35;
 
     @Override
     public boolean test(Candidate candidate) {
         return candidate.isAllowedToVote()
-                && candidate.getAge() >= REQUIRED_AGE
+                && candidate.getAge() >= REQUIRED_MIN_AGE
                 && candidate.getNationality().equals(REQUIRED_NATIONALITY)
-                && checkPeriod(candidate.getPeriodsInUkr());
+                && isValidPeriod(candidate.getPeriodsInUkr());
     }
 
-    private boolean checkPeriod(String period) {
+    private boolean isValidPeriod(String period) {
         String[] split = period.split("-");
-        int yearFrom = Integer.parseInt(split[0]);
-        int yearTo = Integer.parseInt(split[1]);
-        return yearTo
-                - yearFrom
-                >= MIN_LIVE_IN_UA;
+        int indexFrom = 0;
+        int indexTo = 1;
+        int yearFrom = Integer.parseInt(split[indexFrom]);
+        int yearTo = Integer.parseInt(split[indexTo]);
+        return yearTo - yearFrom >= MIN_YEARS_TO_LIVE_IN_UA;
     }
 }
