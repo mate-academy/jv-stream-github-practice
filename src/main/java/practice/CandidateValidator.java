cbe7df101817a3d9fec0c2566d63431bbbab8fc8
Oleksandr Ostapchuk <shurion.ua@gmail.com>
2021-11-19 15:02:47 +0200

    Task implemented

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final String NATIONALITY_TO_VOTE = "Ukrainian";
+    private static final int MIN_AGE_TO_VOTE = 35;
+    private static final int MIN_YEARS_LIVE_TO_VOTE = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        StringBuilder periodInUkrStr = new StringBuilder(candidate.getPeriodsInUkr());
+        return (candidate.getAge() >= MIN_AGE_TO_VOTE
+                   && candidate.getNationality().equals(NATIONALITY_TO_VOTE))
+            && (candidate.isAllowedToVote())
+            && (Integer.parseInt(periodInUkrStr.substring(periodInUkrStr.indexOf("-") + 1))
+                   - Integer.parseInt(periodInUkrStr.substring(0, periodInUkrStr.indexOf("-"))))
+                > MIN_YEARS_LIVE_TO_VOTE;
+    }
 }
