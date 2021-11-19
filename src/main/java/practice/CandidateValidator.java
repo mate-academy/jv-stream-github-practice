4b204559399d91b7a5193d188fdfaee8b08775b8
Illia Lavryniuk <lavryniuk.dev@gmail.com>
2021-11-19 16:38:57 +0200

    better names added

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,8 +7,8 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final String ALLOWED_NATIONALITY = "Ukrainian";
     private static final int MIN_CANDIDATE_AGE = 35;
     private static final int MIN_TERM_LIVE_IN_UKR = 10;
-    private static final int ARRIVAL_YEAR_INDEX = 0;
-    private static final int LAST_YEAR_INDEX = 1;
+    private static final int YEAR_FROM_INDEX = 0;
+    private static final int YEAR_TO_INDEX = 1;
 
     @Override
     public boolean test(Candidate candidate) {
@@ -16,8 +16,8 @@ public class CandidateValidator implements Predicate<Candidate> {
         return candidate.getAge() >= MIN_CANDIDATE_AGE
                 && candidate.isAllowedToVote()
                 && candidate.getNationality().equals(ALLOWED_NATIONALITY)
-                && Integer.parseInt(period[LAST_YEAR_INDEX])
-                - Integer.parseInt(period[ARRIVAL_YEAR_INDEX])
+                && Integer.parseInt(period[YEAR_TO_INDEX])
+                - Integer.parseInt(period[YEAR_FROM_INDEX])
                 >= MIN_TERM_LIVE_IN_UKR;
     }
 }
