e3fd06f4ee36c70e9fcb6edff323eac3161b8cc7
Yurii Khomiak <ykhomiak@st.swps.edu.pl>
2021-11-19 14:20:03 +0100

    Stream API practice solution

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.Objects;
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final int MIN_YEARS_IN_UKRAINE = 10;
+    private static final String CITIZENSHIP = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] period = candidate.getPeriodsInUkr().split("-");
+        int yearsInUkraine = Integer.parseInt(period[1]) - Integer.parseInt(period[0]);
+        return candidate.getAge() >= MIN_AGE
+                && candidate.isAllowedToVote()
+                && yearsInUkraine >= MIN_YEARS_IN_UKRAINE
+                && Objects.equals(candidate.getNationality(), CITIZENSHIP);
+    }
 }
