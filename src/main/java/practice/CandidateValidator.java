df5f73e4b77fcdac4256caa73b7095722b349190
Oleh Mazurkov <o.mazurkov@gmail.com>
2021-11-26 12:43:03 +0200

    solving hw-11 "StreamPractice"

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,22 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final String PRESIDENT_NATIONALITY = "Ukrainian";
+    private static final int PRESIDENT_AGE = 35;
+    private static final int PRESIDENT_IN_UKRAINE = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split("-");
+        int periodInUkraine = Integer.valueOf(years[1]) - Integer.valueOf(years[0]);
+        boolean trueCandidate = candidate.isAllowedToVote()
+                && candidate.getNationality().equals(PRESIDENT_NATIONALITY)
+                && candidate.getAge() >= PRESIDENT_AGE
+                && periodInUkraine >= PRESIDENT_IN_UKRAINE;
+        return trueCandidate;
+    }
 }
+
