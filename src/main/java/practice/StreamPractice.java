a5c823a7529ccf136df0f6fb23869889e5c63131
morozovyrv <72643876+morozovyrv@users.noreply.github.com>
2021-12-20 03:56:36 +0200

    validateCandidates + CandidateValidator implemented

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -114,6 +114,10 @@ public class StreamPractice {
      * parametrized with Candidate in CandidateValidator.
      */
     public List<String> validateCandidates(List<Candidate> candidates) {
-        return Collections.emptyList();
+        return candidates.stream()
+                .filter(candidate -> new CandidateValidator().test(candidate))
+                .map(candidate -> candidate.getName())
+                .sorted()
+                .collect(Collectors.toList());
     }
 }
