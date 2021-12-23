385594d01c8034ee607f3665b2438a96be3b7689
Viktor Stadnik <stadhik.victor@gmail.com>
2021-12-23 23:38:17 +0200

    Finished Stream Practice. Implements own Predicate

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,17 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        int yearTo = Integer.parseInt(candidate.getPeriodsInUkr()
+                .substring(0, candidate.getPeriodsInUkr().indexOf("-")));
+        int yearAfter = Integer.parseInt(candidate.getPeriodsInUkr()
+                .substring(candidate.getPeriodsInUkr().indexOf("-") + 1));
+        return (yearAfter - yearTo) > 10 && candidate.getAge() >= 35
+                && candidate.getNationality().equals("Ukrainian")
+                && candidate.isAllowedToVote();
+    }
 }
