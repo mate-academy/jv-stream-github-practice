ab3c533cda1efed124a11c0b3d3ea4e2194d5c83
Maksym Pokras <mxpokras@gmail.com>
2022-01-13 18:56:47 +0200

    Predicate<Candidate> implemented

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,8 +5,10 @@ import model.Candidate;
 import java.util.function.Predicate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    //write your code here
     private static final int MIN_AGE = 35;
     private static final String VALID_NATIONALITY = "Ukrainian";
+    private static final int PERIOD_IN_UKR_LIMIT = 10;
     @Override
     public boolean test(Candidate candidate) {
         if (candidate == null) {
@@ -21,8 +23,22 @@ public class CandidateValidator implements Predicate<Candidate> {
         if (!VALID_NATIONALITY.equals(candidate.getNationality())) {
             return false;
         }
-
+        if(periodsIsInvalid(candidate.getPeriodsInUkr())) {
+            return false;
+        }
         return true;
     }
-    //write your code here
+
+    private boolean periodsIsInvalid(String periodsInUkr) {
+        try {
+            int startPeriod = Integer.parseInt(periodsInUkr.substring(0,3));
+            int endPeriod = Integer.parseInt(periodsInUkr.substring(5,8));
+            if (endPeriod - startPeriod < PERIOD_IN_UKR_LIMIT) {
+                return true;
+            }
+        } catch (Exception e) {
+            return true;
+        }
+        return false;
+    }
 }
