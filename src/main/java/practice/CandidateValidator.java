37ad1adf7b66416944820894a103c04329e8f6c2
Maksym Pokras <mxpokras@gmail.com>
2022-01-13 15:58:16 +0200

    CandidateValidator test method before periodsInUkr checking

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,28 @@
 package practice;
 
-public class CandidateValidator {
+import model.Candidate;
+
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final String VALID_NATIONALITY = "Ukrainian";
+    @Override
+    public boolean test(Candidate candidate) {
+        if (candidate == null) {
+            return false;
+        }
+        if (!candidate.isAllowedToVote()) {
+            return false;
+        }
+        if (candidate.getAge() < MIN_AGE) {
+            return false;
+        }
+        if (!VALID_NATIONALITY.equals(candidate.getNationality())) {
+            return false;
+        }
+
+        return true;
+    }
     //write your code here
 }
