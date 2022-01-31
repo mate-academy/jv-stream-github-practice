9be35c571f85101b25eb938d10a485fed770798d
Vitaliy Zahrenko <vzagorenko@gmail.com>
2022-02-01 00:18:31 +0200

    mvn pass all done

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,22 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.Arrays;
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    @Override
+    public boolean test(Candidate candidate) {
+
+        int liveInUkraine = Arrays.stream(candidate.getPeriodsInUkr().split("-"))
+                 .map(Integer::parseInt)
+                 .reduce((s1,s2) -> s2 - s1)
+                 .map(Math::abs)
+                 .get();
+        return candidate.getAge() >= 35
+                && candidate.getNationality().equals("Ukrainian")
+                && liveInUkraine >= 10
+                && candidate.isAllowedToVote();
+    }
 }
