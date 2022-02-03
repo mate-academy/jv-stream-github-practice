65a5e7284429cc6e9b1e1ca75dadb536d193df55
LinkClink <shekaqwe@gmail.com>
2022-02-03 22:57:43 +0200

    first ver

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,24 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.Arrays;
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int MIN_AGE = 35;
+    private static final int MIN_PERIOD = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        int[] periods = Arrays.stream(candidate.getPeriodsInUkr().split("-"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        return candidate.isAllowedToVote()
+                && NATIONALITY.equals(candidate.getNationality())
+                && candidate.getAge() >= MIN_AGE
+                && periods[1]
+                - periods[0]
+                >= MIN_PERIOD;
+    }
 }
