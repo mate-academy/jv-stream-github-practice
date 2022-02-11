55bc403aaf18514cfd9651070fc0dbd2af431a11
Liubov Chernohor <liubovchernohor@gmail.com>
2022-02-11 18:06:13 +0200

    Six StreamPractice class' methods and CandidateValidator class' method were implemented

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int PERIOD_INUKR_SEPARATOR = 4;
+    private static final int PERIOD_INUKR_LENGTH = 9;
+    private static final int MIN_PERIOD_INUKR = 10;
+    private static final int MIN_ALLOWED_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.isAllowedToVote()
+                && candidate.getAge() >= MIN_ALLOWED_AGE
+                && candidate.getNationality().equals(NATIONALITY)
+                && Integer.parseInt(candidate.getPeriodsInUkr()
+                .substring(PERIOD_INUKR_SEPARATOR + 1, PERIOD_INUKR_LENGTH))
+                - Integer.parseInt(candidate.getPeriodsInUkr()
+                .substring(0, PERIOD_INUKR_SEPARATOR)) >= MIN_PERIOD_INUKR;
+    }
 }
