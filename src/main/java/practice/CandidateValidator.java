4e53665aed1e906be8064257f5fa56c016844406
Misha Dzhuzha <dzhuzhamichael@gmail.com>
2022-02-13 23:28:27 +0200

    mvn clean package

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,20 +1,19 @@
 package practice;
 
-import model.Candidate;
 import java.util.function.Predicate;
-
+import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_AGE = 35;
     private static final int MIN_PERIOD_OF_LIVING = 10;
     private static final int YEAR_FROM_INDEX = 0;
     private static final int YEAR_TO_INDEX = 1;
-    private static final String REGEX = "-";
+    private static final String SIGN = "-";
     private static final String NATIONALITY = "Ukrainian";
 
     @Override
     public boolean test(Candidate candidate) {
-        String[] yearsInUkraine = candidate.getPeriodsInUkr().split(REGEX);
+        String[] yearsInUkraine = candidate.getPeriodsInUkr().split(SIGN);
         int periodInUkraine = Integer.parseInt(yearsInUkraine[YEAR_TO_INDEX])
                 - Integer.parseInt(yearsInUkraine[YEAR_FROM_INDEX]);
         if (candidate.getAge() >= MIN_AGE
