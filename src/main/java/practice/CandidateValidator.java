d1a2adc4feb6a81d1ab5378553ead193fd974d49
Stelmakh Yevhen <stelmakh.m.d@gmail.com>
2022-02-15 20:14:33 +0200

    First implementation HW-JV Stream practice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,26 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final String SEPARATOR = "-";
+    private static final int AGE_LIMIT = 35;
+    private static final int PERIODS_IN_UKR_LIMIT = 10;
+    private static final boolean BE_ALLOWED_TO_VOTE = true;
+    private static final String NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= AGE_LIMIT
+                && periodsInUkrValidator(candidate.getPeriodsInUkr(),PERIODS_IN_UKR_LIMIT)
+                && candidate.isAllowedToVote() == BE_ALLOWED_TO_VOTE
+                && candidate.getNationality().equals(NATIONALITY);
+    }
+
+    private boolean periodsInUkrValidator(String actualPeriod, int periodsLimit) {
+        String[] years = actualPeriod.split(SEPARATOR);
+        return (Integer.parseInt(years[1]) - Integer.parseInt(years[0]))
+                > periodsLimit;
+    }
 }
