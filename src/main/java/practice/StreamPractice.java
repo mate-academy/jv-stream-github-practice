332e93c02ee19f363f2c2dbb923e34338a4096a6
Vadym <vadya829@gmail.com>
2022-02-15 13:09:32 +0100

    CheckStyle fix

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -2,12 +2,9 @@ package practice;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.IntPredicate;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
 import model.Candidate;
 import model.Cat;
 import model.Person;
