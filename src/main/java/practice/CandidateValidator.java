4e808ab654f09407331824fa63928c39fa683f19
Anastasia <nastyaprokhorenko527@gmail.com>
2022-02-15 12:03:39 +0200

    Stream practice was implemented

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int INDEX_ZERO = 0;
+    private static final int INDEX_ONE = 1;
+    private static final String DASH_REGEX = "-";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] periodInUkr = candidate.getPeriodsInUkr().split(DASH_REGEX);
+        int period = Integer.parseInt(periodInUkr[INDEX_ONE])
+                - Integer.parseInt(periodInUkr[INDEX_ZERO]);
+        return candidate.getAge() >= 35 && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && period >= 10;
+    }
 }
