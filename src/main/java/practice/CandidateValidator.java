af5b040319b804e1b198124571aa8495a35bd063
Nazarii Yatsiuk <nazariiyatsiuk@gmail.com>
2022-02-15 19:26:38 +0200

    ÐÐA rough cut

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
+import model.Candidate;
+
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.isAllowedToVote()
+                && candidate.getAge() >= 35
+                && candidate.getNationality().equals("Ukrainian")
+                && findPeriodInUkraine(candidate) >= 10;
+    }
+    private int findPeriodInUkraine (Candidate candidate){
+        String[] yearsInUkraine = candidate.getPeriodsInUkr().split("-");
+        return Integer.valueOf(yearsInUkraine[1]) - Integer.valueOf(yearsInUkraine[0]);
+    }
     //write your code here
 }
