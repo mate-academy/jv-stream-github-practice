d22ae0d561ea8bd4de8fd1a1c95faffb9b827ba5
Anastasia <nastyaprokhorenko527@gmail.com>
2022-02-15 13:45:39 +0200

    constants were added

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,14 +7,16 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int INDEX_ZERO = 0;
     private static final int INDEX_ONE = 1;
     private static final String DASH_REGEX = "-";
+    private static final int MIN_AGE = 35;
+    private static final int MIN_PERIOD = 10;
 
     @Override
     public boolean test(Candidate candidate) {
         String[] periodInUkr = candidate.getPeriodsInUkr().split(DASH_REGEX);
         int period = Integer.parseInt(periodInUkr[INDEX_ONE])
                 - Integer.parseInt(periodInUkr[INDEX_ZERO]);
-        return candidate.getAge() >= 35 && candidate.isAllowedToVote()
+        return candidate.getAge() >= MIN_AGE && candidate.isAllowedToVote()
                 && candidate.getNationality().equals("Ukrainian")
-                && period >= 10;
+                && period >= MIN_PERIOD;
     }
 }
