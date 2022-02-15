53b4aa51f664c8dd73b84460843fac131c27f99f
AprioriKS <sounddummies@gmail.com>
2022-02-15 15:43:35 +0200

    fixed mistake3

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,23 +4,22 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    public static final int INDEX_OF_DATE_FROM = 0;
-    public static final int INDEX_OF_DATE_TO = 1;
-    public static final int MINIMUM_AGE_OF_CANDIDATE = 35;
-    public static final int MINIMUM_YEARS_LIVED_IN_UKRAINE = 10;
+    private static final int FIRST_YEAR_INDEX = 0;
+    private static final int LAST_YEAR_INDEX = 1;
+    private static final int MIN_AGE_CANDIDATE = 35;
+    private static final int MIN_YEAR_IN_UKRAINE = 10;
 
     @Override
     public boolean test(Candidate candidate) {
-        String[] rangeLiveInUkraine = candidate.getPeriodsInUkr().split("-");
-        int liveInUkraineFrom = Integer.parseInt(rangeLiveInUkraine[INDEX_OF_DATE_FROM]);
-        int liveInUkraineTo = Integer.parseInt(rangeLiveInUkraine[INDEX_OF_DATE_TO]);
-        return candidate.getAge() >= MINIMUM_AGE_OF_CANDIDATE && candidate.isAllowedToVote()
-            && candidate.getNationality().equals("Ukrainian")
-            && yearsInUkr(liveInUkraineFrom, liveInUkraineTo);
+        return candidate.getAge() >= MIN_AGE_CANDIDATE
+            & candidate.getNationality().equals("Ukrainian")
+            & candidate.isAllowedToVote()
+            & getAllYearsInUkr(candidate.getPeriodsInUkr()) >= MIN_YEAR_IN_UKRAINE;
     }
 
-    private boolean yearsInUkr(int liveInUkraineFrom, int liveInUkraineTo) {
-        return liveInUkraineTo - liveInUkraineFrom >= MINIMUM_YEARS_LIVED_IN_UKRAINE;
+    private int getAllYearsInUkr(String periodInUkr) {
+        String[] firstAndLastYear = periodInUkr.split("-");
+        return Integer.parseInt(firstAndLastYear[LAST_YEAR_INDEX])
+            - Integer.parseInt(firstAndLastYear[FIRST_YEAR_INDEX]);
     }
 }
-
