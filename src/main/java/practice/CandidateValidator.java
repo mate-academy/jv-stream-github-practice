4e3b7bd498c9c5d2ab8df54616db2675f0912f45
Cergei Borovski <borovskicerg0001@gmail.com>
2022-02-20 22:53:27 +0300

    All tests done, build success.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int FIRST_DATE_INDEX = 0;
+    private static final int SECOND_DATE_INDEX = 1;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] dates = candidate.getPeriodsInUkr().split("-");
+        if (Integer.parseInt(dates[SECOND_DATE_INDEX])
+                - Integer.parseInt(dates[FIRST_DATE_INDEX]) >= 10) {
+            return 35 <= candidate.getAge() && candidate.isAllowedToVote()
+                    && candidate.getNationality() != null
+                    && candidate.getNationality().equals("Ukrainian");
+        }
+        return false;
+    }
 }
