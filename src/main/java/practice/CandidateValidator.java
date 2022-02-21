c86e59d9a066d3ae178129a455a067be32959fed
maksim <maksim.sokur90@gmail.com>
2022-02-21 21:44:28 +0200

    1. Override test predicate method and have boolean value; 2. Find value in list and return even int number; 3. Find odd numbers and return double average value; 4. Find person's with given parameters, collect and return list of person; 5. Find workable person man and woman with given parameter, collect and return list; 6. Find women with cats and aage parameter from list collect and return list; 7. Using CandidateValidator realisation and find in list of candidates pearson with needed parameter, collect and return list;

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
+import model.Candidate;
+
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
     //write your code here
+    private static final String NATIONAL = "Ukrainian";
+    private static final String DELIMITER = "-";
+    private static final int MIN_YEAR_TO_LIVE = 10;
+    private static final int MIN_AGE = 35;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split(DELIMITER);
+        int parseYears = Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
+        return candidate.getAge() >= MIN_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONAL)
+                && parseYears >= MIN_YEAR_TO_LIVE;
+    }
 }
