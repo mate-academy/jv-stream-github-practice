739707a8bf35a256da178b59ff8a871fef1199b9
Liubomyr <liubomyr.stepanenko@ukma.edu.ua>
2022-03-10 22:00:07 +0200

    All streams and CandidateValidator were implemented and tested

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
     //write your code here
+    @Override
+    public boolean test(Candidate candidate) {
+        boolean mostRequirements = candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian");
+        if (!mostRequirements) {
+            return false;
+        }
+        String[] years = candidate.getPeriodsInUkr().split("-");
+        int fromYear = Integer.parseInt(years[0]);
+        int toYear = Integer.parseInt(years[1]);
+        return toYear - fromYear >= 10;
+    }
 }
