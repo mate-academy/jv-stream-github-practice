b0fc25562e771af56e953ab32df4564610b7e2cc
Liubomyr <liubomyr.stepanenko@ukma.edu.ua>
2022-03-11 16:12:03 +0200

    CandidateValidator and StreamPractice classes had some edits

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,18 +4,21 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    //write your code here
+    private static final int MIN_AGE = 35;
+    private static final int MIN_PERIOD_OF_STAY = 10;
+    private static final String NECESSARY_NATIONALITY = "Ukrainian";
+
     @Override
     public boolean test(Candidate candidate) {
-        boolean mostRequirements = candidate.getAge() >= 35
+        boolean mostRequirements = candidate.getAge() >= MIN_AGE
                 && candidate.isAllowedToVote()
-                && candidate.getNationality().equals("Ukrainian");
+                && candidate.getNationality().equals(NECESSARY_NATIONALITY);
         if (!mostRequirements) {
             return false;
         }
         String[] years = candidate.getPeriodsInUkr().split("-");
         int fromYear = Integer.parseInt(years[0]);
         int toYear = Integer.parseInt(years[1]);
-        return toYear - fromYear >= 10;
+        return toYear - fromYear >= MIN_PERIOD_OF_STAY;
     }
 }
--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import model.Candidate;
 import model.Cat;
 import model.Person;
@@ -20,8 +21,7 @@ public class StreamPractice {
     public int findMinEvenNumber(List<String> numbers) {
         return numbers
                 .stream()
-                .map(x -> x.split(","))
-                .flatMap(Arrays::stream)
+                .flatMap(x -> Arrays.stream(x.split(",")))
                 .mapToInt(Integer::parseInt)
                 .filter(x -> x % 2 == 0)
                 .min()
@@ -36,9 +36,9 @@ public class StreamPractice {
      * But before that subtract 1 from each element on an odd position (having the odd index).
      */
     public Double getOddNumsAverage(List<Integer> numbers) {
-        return numbers
-                .stream()
-                .mapToDouble(x -> (numbers.indexOf(x) % 2 > 0) ? x - 1 : x)
+        return IntStream
+                .range(0, numbers.size())
+                .map(i -> ((i % 2 > 0) ? numbers.get(i) - 1 : numbers.get(i)))
                 .filter(x -> x % 2 > 0)
                 .distinct()
                 .average()
@@ -54,8 +54,7 @@ public class StreamPractice {
      * Example: select men who can be recruited to army (from 18 to 27 years old inclusively).
      */
     public List<Person> selectMenByAge(List<Person> peopleList, int fromAge, int toAge) {
-        return peopleList
-                .stream()
+        return peopleList.stream()
                 .filter(x -> x.getSex().equals(Person.Sex.MAN)
                         && x.getAge() >= fromAge
                         && x.getAge() <= toAge)
