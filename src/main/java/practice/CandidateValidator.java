99910ac3b61e47733a91fda9dcb86d7c69b3f9b6
oleg-new <9213195@gmail.com>
2022-03-12 21:50:45 +0200

    all methods are implemented according to the task

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,24 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_RESIDENCE_TIME = 10;
+    private static final int MIN_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+
+    private boolean rightOfResidence(String years) {
+        String[] yearsInUkraine = years.split("-");
+        return Integer.parseInt(yearsInUkraine[1]) - Integer.parseInt(yearsInUkraine[0])
+                >= MIN_RESIDENCE_TIME;
+    }
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return rightOfResidence(candidate.getPeriodsInUkr())
+                 && candidate.isAllowedToVote()
+                 && candidate.getAge() >= MIN_AGE
+                && candidate.getNationality().equals(NATIONALITY);
+    }
 }
