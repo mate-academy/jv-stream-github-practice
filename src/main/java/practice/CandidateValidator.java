1f8856f4b425f1e86fba8f159e1da5f454ec3bd6
Yurii <wervmer>
2022-03-22 13:28:07 +0200

    fixed: restored three constants - VALID_NATIONALITY, OPTIMAL_AGE, VALID_YEARS_IN_UKR

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,6 +5,9 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int OPTIMAL_AGE = 35;
+    private static final int VALID_YEARS_IN_UKR = 10;
+    private static final String VALID_NATIONALITY = "Ukrainian";
 
     @Override
     public boolean test(Candidate candidate) {
@@ -12,10 +15,10 @@ public class CandidateValidator implements Predicate<Candidate> {
     }
 
     private static boolean isValidCandidate(Candidate candidate) {
-        return candidate.getNationality().equals("Ukrainian")
+        return candidate.getNationality().equals(VALID_NATIONALITY)
                 && candidate.isAllowedToVote()
-                && candidate.getAge() >= 35
-                && getYearsFromPeriod(candidate.getPeriodsInUkr()) >= 10;
+                && candidate.getAge() >= OPTIMAL_AGE
+                && getYearsFromPeriod(candidate.getPeriodsInUkr()) >= VALID_YEARS_IN_UKR;
     }
 
     private static int getYearsFromPeriod(String periodsInUkr) {
