8ea1ff90d562fb284a83d01eaaf8c4eb788f84b3
Vitalii Bozadzhy <VitaliiBozadzhy@icloud.com>
2022-03-25 11:46:04 +0100

    - added method test, which will test the candidates (ukrainians (men), older 35 years old,)

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,28 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.Arrays;
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return isValidCandidate(candidate);
+    }
+
+    private static boolean isValidCandidate(Candidate candidate) {
+        return candidate.getNationality().equals("Ukrainian")
+                && candidate.isAllowedToVote()
+                && candidate.getAge() >= 35
+                && getYearsFromPeriod(candidate.getPeriodsInUkr()) >= 10;
+    }
+
+    private static int getYearsFromPeriod(String periodsInUkr) {
+        return Arrays.stream(periodsInUkr.split("-"))
+                .map(Integer::valueOf)
+                .mapToInt(i -> i)
+                .reduce(0, (left, right) -> right - left);
+
+    }
 }
