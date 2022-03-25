eb2587ce30cc92b13237e27cea2e4ff85aa206db
Vitalii Bozadzhy <VitaliiBozadzhy@icloud.com>
2022-03-25 13:11:39 +0100

    - added constants with requared datas

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,6 +5,9 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int requiredAge = 35;
+    private static final int requaredYears = 10;
+    private static final String requaredNationality = "Ukrainian";
 
     @Override
     public boolean test(Candidate candidate) {
@@ -12,10 +15,10 @@ public class CandidateValidator implements Predicate<Candidate> {
     }
 
     private static boolean isValidCandidate(Candidate candidate) {
-        return candidate.getNationality().equals("Ukrainian")
+        return candidate.getNationality().equals(requaredNationality)
                 && candidate.isAllowedToVote()
-                && candidate.getAge() >= 35
-                && getYearsFromPeriod(candidate.getPeriodsInUkr()) >= 10;
+                && candidate.getAge() >= requiredAge
+                && getYearsFromPeriod(candidate.getPeriodsInUkr()) >= requaredYears;
     }
 
     private static int getYearsFromPeriod(String periodsInUkr) {
