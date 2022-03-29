949aa8af66c25b788c9a37819f0e8f1d886b56c1
bukoz <bukoz@meta.ua>
2022-03-29 13:16:00 +0300

    Implemented six methods via streams

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MINIMUM_YEARS_OLD = 35;
+    private static final int MINIMUM_YEARS_IN_UA = 10;
+    private static final String RIGHT_NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        int startOfStayInUA = Integer.parseInt(candidate.getPeriodsInUkr().substring(0, 4));
+        int endOfStayInUA = Integer.parseInt(candidate.getPeriodsInUkr().substring(5, 9));
+        int yearsInUA = endOfStayInUA - startOfStayInUA;
+        return candidate.isAllowedToVote()
+                && candidate.getAge() >= MINIMUM_YEARS_OLD
+                && candidate.getNationality().equals(RIGHT_NATIONALITY)
+                && yearsInUA >= MINIMUM_YEARS_IN_UA;
+    }
 }
