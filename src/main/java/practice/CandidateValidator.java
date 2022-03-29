9fe0b8f99c6b4f9218fd747fef5fb7b0194459da
bukoz <bukoz@meta.ua>
2022-03-29 17:48:10 +0300

    Added some fixes[2]

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,12 +7,13 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int MINIMUM_YEARS_OLD = 35;
     private static final int MINIMUM_YEARS_IN_UA = 10;
     private static final String RIGHT_NATIONALITY = "Ukrainian";
-    private static final String SLIT_YEARS = "-";
+    private static final int DATE_FROM = 0;
+    private static final int DATE_TO = 1;
 
     @Override
     public boolean test(Candidate candidate) {
-        int startOfStayInUA = Integer.parseInt(candidate.getPeriodsInUkr().split(SLIT_YEARS)[0]);
-        int endOfStayInUA = Integer.parseInt(candidate.getPeriodsInUkr().split(SLIT_YEARS)[1]);
+        int startOfStayInUA = Integer.parseInt(candidate.getPeriodsInUkr().split("-")[DATE_FROM]);
+        int endOfStayInUA = Integer.parseInt(candidate.getPeriodsInUkr().split("-")[DATE_TO]);
         int yearsInUA = endOfStayInUA - startOfStayInUA;
         return candidate.isAllowedToVote()
                 && candidate.getAge() >= MINIMUM_YEARS_OLD
