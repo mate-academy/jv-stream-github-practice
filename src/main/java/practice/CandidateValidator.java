e935e179fdb2238eef3b796ffcf184fc52b8d3c4
JadeRowan <rougesoul.m@gmail.com>
2022-04-07 00:13:46 +0300

    Methods are implemented. Main goal completed

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,18 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] period = candidate.getPeriodsInUkr().split("-");
+        Integer periodStars = Integer.parseInt(period[0]);
+        Integer periodFinish = Integer.parseInt(period[1]);
+        return candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && periodFinish - periodStars >= 10;
+    }
 }
