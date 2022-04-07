b9490c6e1ac3ea1a26a3995d88f3c9140e5a3d26
JadeRowan <rougesoul.m@gmail.com>
2022-04-07 10:13:26 +0300

    Advanced checkstyle fix

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,16 +6,19 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int MINIMUM_AGE = 35;
     private static final int NEEDED_YEARS_IN_UKRAINE = 10;
+    private static final String UKRAINIAN = "Ukrainian";
+    private static final int PERIOD_START_INDEX = 0;
+    private static final int PERIOD_FINISH_INDEX = 1;
 
     @Override
     public boolean test(Candidate candidate) {
 
         String[] period = candidate.getPeriodsInUkr().split("-");
-        Integer periodStar = Integer.parseInt(period[0]);
-        Integer periodFinish = Integer.parseInt(period[1]);
+        Integer periodStar = Integer.parseInt(period[PERIOD_START_INDEX]);
+        Integer periodFinish = Integer.parseInt(period[PERIOD_FINISH_INDEX]);
         return candidate.getAge() >= MINIMUM_AGE
                 && candidate.isAllowedToVote()
-                && candidate.getNationality().equals("Ukrainian")
+                && candidate.getNationality().equals(UKRAINIAN)
                 && periodFinish - periodStar >= NEEDED_YEARS_IN_UKRAINE;
     }
 }
