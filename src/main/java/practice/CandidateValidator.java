e25553fa9b572e6f83e1d1683ba05514f97639bd
yuriikozar <yuriikozar@gmail.com>
2022-04-15 20:56:50 +0300

    Commit 1 Solution + Checkstyle

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.isAllowedToVote() && candidate.getAge() >= 35
+                && candidate.getNationality().matches("Ukrainian")
+                && getPeriodsInUkraineSupplier(candidate);
+    }
+
+    private boolean getPeriodsInUkraineSupplier(Candidate candidate) {
+        String[] array = candidate.getPeriodsInUkr().split("-");
+        return (Integer.parseInt(array[1]) - Integer.parseInt(array[0])) > 10;
+    }
 }
+    //write your code here
+
