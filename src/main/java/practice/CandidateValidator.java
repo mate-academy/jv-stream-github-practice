bc1715df990dceede057fda79c8f1985296809dd
yuriikozar <yuriikozar@gmail.com>
2022-04-18 21:38:15 +0300

    Commit 2 Checkstyle

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,17 +4,21 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int FROM_AGE = 35;
+    private static final int MIN_YEARS_IN_UKRAINE = 10;
+    private static final String VALID_NATIONALITY = "Ukrainian";
+
     @Override
     public boolean test(Candidate candidate) {
-        return candidate.isAllowedToVote() && candidate.getAge() >= 35
-                && candidate.getNationality().matches("Ukrainian")
+        return candidate.isAllowedToVote() && candidate.getAge() >= FROM_AGE
+                && candidate.getNationality().matches(VALID_NATIONALITY)
                 && getPeriodsInUkraineSupplier(candidate);
     }
 
     private boolean getPeriodsInUkraineSupplier(Candidate candidate) {
-        String[] array = candidate.getPeriodsInUkr().split("-");
-        return (Integer.parseInt(array[1]) - Integer.parseInt(array[0])) > 10;
+        String[] dateArray = candidate.getPeriodsInUkr().split("-");
+        return (Integer.parseInt(dateArray[1]) - Integer.parseInt(dateArray[0]))
+                > MIN_YEARS_IN_UKRAINE;
     }
 }
-    //write your code here
 
