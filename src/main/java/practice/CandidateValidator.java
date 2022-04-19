88637c0c8a737760920269f66b524addd028d7e6
Denys.Sh <7860denis@gmail.com>
2022-04-19 23:48:04 +0300

    my solution get ready

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,26 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.Arrays;
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_YEARS_LIVE_IN_UA = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        if (candidate.getAge() >= 35
+                && candidate.getNationality().toLowerCase().equals("ukrainian")
+                && candidate.isAllowedToVote()
+                && getDifferenceBetweenYears(candidate) >= MIN_YEARS_LIVE_IN_UA) {
+            return true;
+        }
+        return false;
+    }
+
+    private int getDifferenceBetweenYears(Candidate candidate) {
+        return Arrays.stream(candidate.getPeriodsInUkr().split("-"))
+                .mapToInt(Integer::parseInt)
+                .reduce((s1, s2) -> s2 - s1).orElse(0);
+    }
 }
