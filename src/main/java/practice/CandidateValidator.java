297fc0249daa82d72d302d386a94836a0978c1d6
Nickolai Vakulich <nickolai.vakulich@gmail.com>
2022-04-21 21:13:51 +0300

    implement CandidateValidator practice with streams

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    public static final int AGE = 35;
+    public static final int YEARS_IN_UKRAINE = 10;
+    public static final String NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && periodInUkr(candidate.getPeriodsInUkr()) >= YEARS_IN_UKRAINE;
+    }
+
+    private int periodInUkr(String period) {
+        String[] periodFromTo = period.split("-");
+        return Integer.parseInt(periodFromTo[1]) - Integer.parseInt(periodFromTo[0]);
+    }
 }
