fe2f434ddc290db64a3022e367f7515b96d02e5b
Volodymyr Klimyuk <klimyuk054@gmail.com>
2022-04-22 15:34:04 +0300

    implemented findMinEvenNumber, getOddNumsAverage, selectMenByAge, getWorkablePeople, getCatsNames, validateCandidates method in practice package and implemented CandidateValidator class in model package

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,17 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<String> {
+    private static final int MINIMUM_NUMBER_YEARS = 10;
+    private static final int START_YEAR = 0;
+    private static final int END_YEAR = 1;
+
+    @Override
+    public boolean test(String periodsInUkr) {
+        String[] years = periodsInUkr.split("-");
+        return (Integer.parseInt(years[END_YEAR])
+                - Integer.parseInt(years[START_YEAR])) >= MINIMUM_NUMBER_YEARS;
+    }
 }
+
