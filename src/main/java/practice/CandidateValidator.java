341ddb7b05fe24382fc2b543484d306b3a112311
Ksenia Bykh <ksenia.bykh@gmail.com>
2022-04-24 13:48:03 +0200

    stream practice is done

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MINIMAL_AGE = 35;
+    private static final int MINIMAL_PERIOD = 10;
+
+    public int getPeriod(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split("-");
+        return Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
+    }
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MINIMAL_AGE && candidate.getNationality().equals("Ukrainian")
+                && getPeriod(candidate) >= MINIMAL_PERIOD && candidate.isAllowedToVote();
+    }
 }
