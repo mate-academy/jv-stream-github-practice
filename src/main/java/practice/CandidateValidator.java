d0862da3654dd68a18dea1624844991a377e1530
Ksenia Bykh <ksenia.bykh@gmail.com>
2022-04-25 13:12:27 +0200

    "Ukrainian" was added as a constant field.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,6 +6,7 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int MINIMAL_AGE = 35;
     private static final int MINIMAL_PERIOD = 10;
+    private static final String NATIONALITY = "Ukrainian";
 
     public int getPeriod(Candidate candidate) {
         String[] years = candidate.getPeriodsInUkr().split("-");
@@ -14,7 +15,7 @@ public class CandidateValidator implements Predicate<Candidate> {
 
     @Override
     public boolean test(Candidate candidate) {
-        return candidate.getAge() >= MINIMAL_AGE && candidate.getNationality().equals("Ukrainian")
+        return candidate.getAge() >= MINIMAL_AGE && candidate.getNationality().equals(NATIONALITY)
                 && getPeriod(candidate) >= MINIMAL_PERIOD && candidate.isAllowedToVote();
     }
 }
