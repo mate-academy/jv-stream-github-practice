ed3acda8156fba07ce933387c4f8adf067cf0937
Ksenia Bykh <ksenia.bykh@gmail.com>
2022-04-25 13:23:11 +0200

    OrElseThrow was changed by getAsDouble

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -2,7 +2,6 @@ package practice;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -39,7 +38,7 @@ public class StreamPractice {
                 .mapToDouble(n -> n)
                 .filter(num -> num % 2 != 0)
                 .average()
-                .orElseThrow(NoSuchElementException::new);
+                .getAsDouble();
     }
 
     /**
