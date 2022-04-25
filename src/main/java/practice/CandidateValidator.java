476e84827f0a18fea89407004f63aff7852a7149
OLEH VIESHAIEV <sred_a@i.ua>
2022-04-25 19:23:45 +0300

    HW-solution was created

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,22 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int VALIDATE_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int YEARS_IN_UKRAINE = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= VALIDATE_AGE && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && periodsInUkraine(candidate.getPeriodsInUkr()) >= YEARS_IN_UKRAINE;
+    }
+
+    private int periodsInUkraine(String period) {
+        String[] yearsFromPeriod = period.split("-");
+        return Integer.parseInt(yearsFromPeriod[1]) - Integer.parseInt(yearsFromPeriod[0]);
+    }
 }
