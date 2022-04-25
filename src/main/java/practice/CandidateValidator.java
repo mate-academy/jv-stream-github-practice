2bda88d9adafb3cf7fa2a1faf4487e4c25362dcf
Dmytro Koliada <dmitrikstar@gmail.com>
2022-04-25 11:48:28 +0300

    added stream api and CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,25 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.Objects;
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int CANDIDATE_MIN_AGE = 35;
+    private static final int MIN_CANDIDATE_BEEN_IN_UKRAINE = 10;
+    private static final String CANDIDATE_NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= CANDIDATE_MIN_AGE
+                && getYearCandidateBeenInUkraine(candidate.getPeriodsInUkr())
+                >= MIN_CANDIDATE_BEEN_IN_UKRAINE
+                && Objects.equals(candidate.getNationality(), CANDIDATE_NATIONALITY)
+                && candidate.isAllowedToVote();
+    }
+
+    private int getYearCandidateBeenInUkraine(String date) {
+        String[] dateInt = date.split("\\-");
+        return Integer.parseInt(dateInt[1]) - Integer.parseInt(dateInt[0]);
+    }
 }
