2f2ae98e0f002036487e552b90042ee1bb4709ae
Oleksander <ghjwtyrj25@gmail.com>
2022-04-26 06:40:28 +0300

    Created methods by using Stream

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,9 +5,11 @@ import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_AGE = 35;
-    private static final String NATIONALITY = "Ukrainian";
     private static final int YEARS_IN_UKRAINE = 10;
+    private static final int FIRST_YEAR = 0;
+    private static final int LAST_YEAR = 1;
     private static final String SEPARATOR = "-";
+    private static final String NATIONALITY = "Ukrainian";
 
     @Override
     public boolean test(Candidate candidate) {
@@ -15,6 +17,6 @@ public class CandidateValidator implements Predicate<Candidate> {
         return candidate.getAge() >= MIN_AGE
                 && candidate.isAllowedToVote()
                 && candidate.getNationality().equals(NATIONALITY)
-                && YEARS_IN_UKRAINE <= (Integer.parseInt(years[1]) - Integer.parseInt(years[0]));
+                && YEARS_IN_UKRAINE <= (Integer.parseInt(years[LAST_YEAR]) - Integer.parseInt(years[FIRST_YEAR]));
     }
 }
