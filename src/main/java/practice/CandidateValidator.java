bdb61a45fa837174269054ea2c8dcccef0e42c3b
Viktoriia Ihnatiuk <victoriia.ihnatiuk@gmail.com>
2022-04-26 12:41:26 +0300

    Stream homework solution added

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,22 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MINIMAL_ALLOWED_AGE = 35;
+    private static final String ALLOWED_NATIONALITY = "Ukrainian";
+    private static final int MINIMAL_ALLOWED_PERIOD_IN_UKRAINE = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] candidatePeriodInUkraine = candidate.getPeriodsInUkr().split("-");
+        return candidate.isAllowedToVote()
+                && candidate.getAge() >= MINIMAL_ALLOWED_AGE
+                && candidate.getNationality().equals(ALLOWED_NATIONALITY)
+                && Integer.parseInt(candidatePeriodInUkraine[1])
+                - Integer.parseInt(candidatePeriodInUkraine[0])
+                >= MINIMAL_ALLOWED_PERIOD_IN_UKRAINE;
+
+    }
 }
