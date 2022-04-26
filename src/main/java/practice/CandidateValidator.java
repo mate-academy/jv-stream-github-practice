f18ed13cf5e16868a5afae873375384c96bcdbfb
Ksenia Bykh <ksenia.bykh@gmail.com>
2022-04-26 13:23:25 +0200

    Method getPeriod became private and was moved under the public

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -8,14 +8,14 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int MINIMAL_PERIOD = 10;
     private static final String NATIONALITY = "Ukrainian";
 
-    public int getPeriod(Candidate candidate) {
-        String[] years = candidate.getPeriodsInUkr().split("-");
-        return Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
-    }
-
     @Override
     public boolean test(Candidate candidate) {
         return candidate.getAge() >= MINIMAL_AGE && candidate.getNationality().equals(NATIONALITY)
                 && getPeriod(candidate) >= MINIMAL_PERIOD && candidate.isAllowedToVote();
+    }
+
+    private int getPeriod(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split("-");
+        return Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
     }
 }
