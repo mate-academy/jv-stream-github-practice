dc96b0a9d5b08f0d3f7e4019ef0e6f36d3d0fa3c
Vladyslav Fomenko <with.god.victorius@gmail.com>
2022-04-26 16:54:45 +0300

    removed unnecessary constants

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,19 +4,16 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    private static final int AGE_BOUND = 35;
+    private static final int AGE_LOWER_BOUND = 35;
     private static final String NATION = "Ukrainian";
     private static final int LIVED_IN_UKRAINE = 10;
-    private static final boolean VOTE_RIGHT = true;
-    private static final int LAST_YEAR = 1;
-    private static final int FIRST_YEAR = 0;
 
     @Override
     public boolean test(Candidate candidate) {
         String[] inUkraine = candidate.getPeriodsInUkr().split("-");
-        int yearInUkraine = Integer.parseInt(inUkraine[LAST_YEAR])
-                - Integer.parseInt(inUkraine[FIRST_YEAR]);
-        return candidate.getAge() >= AGE_BOUND
+        int yearInUkraine = Integer.parseInt(inUkraine[1])
+                - Integer.parseInt(inUkraine[0]);
+        return candidate.getAge() >= AGE_LOWER_BOUND
                 && candidate.getNationality().equals(NATION)
                 && candidate.isAllowedToVote()
                 && yearInUkraine >= LIVED_IN_UKRAINE;
--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -55,7 +55,7 @@ public class StreamPractice {
      */
     public List<Person> selectMenByAge(List<Person> peopleList, int fromAge, int toAge) {
         return peopleList.stream()
-                .filter(person -> person.getAge() > fromAge
+                .filter(person -> person.getAge() >= fromAge
                         && person.getAge() <= toAge
                         && person.getSex().equals(Person.Sex.MAN))
                 .collect(Collectors.toList());
