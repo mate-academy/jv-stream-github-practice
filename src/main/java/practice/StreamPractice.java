ccac958e25255586ff5f7e1fa3d32c200ba19f3b
Andrii Fabersky <Andriifabersky@gmail.com>
2022-04-26 17:43:18 +0300

    Sorry my bad

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -6,7 +6,6 @@ import java.util.NoSuchElementException;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 import model.Candidate;
 import model.Cat;
 import model.Person;
