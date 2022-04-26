967f6dfc465f25c66ffebb734c99e6c968fa019f
Nesstor Fedak <nesstortor@gmail.com>
2022-04-26 16:38:31 +0300

    mistakes are fixed

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -8,15 +8,15 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int MINIMAL_PERIOD_IN_UKRAINE = 10;
     private static final String CANDIDATE_NATIONALITY = "Ukrainian";
 
-    private int getPeriod(Candidate candidate) {
-        String[] years = candidate.getPeriodsInUkr().split("-");
-        return Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
-    }
-
     @Override
     public boolean test(Candidate candidate) {
         return candidate.getAge() >= MINIMAL_CANDIDATE_AGE
                 && candidate.getNationality().equals(CANDIDATE_NATIONALITY)
                 && getPeriod(candidate) >= MINIMAL_PERIOD_IN_UKRAINE && candidate.isAllowedToVote();
+    }
+
+    private int getPeriod(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split("-");
+        return Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
     }
 }
