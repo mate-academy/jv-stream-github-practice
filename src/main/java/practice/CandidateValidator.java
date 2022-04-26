aa4878e2cea7349f1dfb7df1979bae097cc362b7
Viktoriia Ihnatiuk <victoriia.ihnatiuk@gmail.com>
2022-04-26 14:52:59 +0300

    solution fixed

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -8,12 +8,6 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final String ALLOWED_NATIONALITY = "Ukrainian";
     private static final int MINIMAL_ALLOWED_PERIOD_IN_UKRAINE = 10;
 
-    private boolean verifyPeriodsInUkraine(Candidate candidate) {
-        return Integer.parseInt(candidate.getPeriodsInUkr().split("-")[1])
-                - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[0])
-                >= MINIMAL_ALLOWED_PERIOD_IN_UKRAINE;
-    }
-
     @Override
     public boolean test(Candidate candidate) {
         return candidate.isAllowedToVote()
@@ -21,4 +15,12 @@ public class CandidateValidator implements Predicate<Candidate> {
                 && candidate.getNationality().equals(ALLOWED_NATIONALITY)
                 && verifyPeriodsInUkraine(candidate);
     }
+
+    private boolean verifyPeriodsInUkraine(Candidate candidate) {
+        String[] yearsOfPeriod = candidate.getPeriodsInUkr().split("-");
+        return Integer.parseInt(yearsOfPeriod[1])
+                - Integer.parseInt(yearsOfPeriod[0])
+                >= MINIMAL_ALLOWED_PERIOD_IN_UKRAINE;
+    }
+
 }
--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -68,11 +68,11 @@ public class StreamPractice {
     public List<Person> getWorkablePeople(int fromAge, int femaleToAge,
                                           int maleToAge, List<Person> peopleList) {
         return peopleList.stream()
-                        .filter(person -> person.getSex() == Person.Sex.MAN
-                                ? person.getAge() >= fromAge
-                                && person.getAge() <= maleToAge
-                                : person.getAge() >= fromAge
-                                && person.getAge() <= femaleToAge)
+                        .filter(person -> (person.getAge() >= fromAge)
+                                && ((person.getSex() == (Person.Sex.MAN)
+                                && person.getAge() <= maleToAge)
+                                || (person.getSex() == (Person.Sex.WOMAN)
+                                && person.getAge() <= femaleToAge)))
                                 .collect(Collectors.toList());
     }
 
