14f82d4dc333ece4aa3b9b8c126242bb2bb46dc8
GerasimovTA-92 <olexandr.gerasimov.02@gmail.com>
2022-04-26 14:39:54 +0300

    deleted all comments

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,7 +4,6 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    //write your code here
     private static final int MIN_AGE = 35;
     private static final String NATIONALITY = "Ukrainian";
     private static final int PERIOD_IN_UKRAINE = 10;
