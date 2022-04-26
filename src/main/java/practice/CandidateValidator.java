bc35e8da85477e3807fc3ffabd26e13e87fbfb0a
Nesstor Fedak <nesstortor@gmail.com>
2022-04-26 09:33:51 +0300

    All tasks are done by streams

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MINIMAL_CANDIDATE_AGE = 35;
+    private static final int MINIMAL_PERIOD_IN_UKRAINE = 10;
+
+    public int getPeriod(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split("-");
+        return Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
+    }
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MINIMAL_CANDIDATE_AGE
+                && candidate.getNationality().equals("Ukrainian")
+                && getPeriod(candidate) >= MINIMAL_PERIOD_IN_UKRAINE && candidate.isAllowedToVote();
+    }
 }
