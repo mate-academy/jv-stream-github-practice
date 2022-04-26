85525a1a6715f6c193fa50e551d7128fa0c3104d
Gigawatt94 <101740223+Gigawatt94@users.noreply.github.com>
2022-04-26 14:55:02 +0300

    work was done. mvn build success

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,24 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int VALID_AGE_OF_CANDIDATE = 35;
+    private static final String CANDIDATE_NATIONALITY = "Ukrainian";
+    private static final String SEPARATOR = "-";
+    private static final int MIN_YEARS_LIVING = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split(SEPARATOR);
+        if (candidate.getAge() >= VALID_AGE_OF_CANDIDATE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(CANDIDATE_NATIONALITY)
+                && Integer.parseInt(years[1])
+                - Integer.parseInt(years[0]) >= MIN_YEARS_LIVING) {
+            return true;
+        }
+        return false;
+    }
 }
