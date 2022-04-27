30608c5b0567476ca6801f07f7c59e09503964a2
SerhiiPankov <pankoff.serg@gmail.com>
2022-04-27 11:02:49 +0300

    fix bags for jv-stream-github-practice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,13 +7,14 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_AGE = 35;
     private static final int MIN_PERIOD = 10;
     private static final String EXPECTED_NATIONALITY = "Ukrainian";
-    private static final int INDEX_START = 0;
-    private static final int INDEX_FINISH = 1;
+    private static final int DATE_FROM_INDEX = 0;
+    private static final int DATE_TO_INDEX = 1;
 
     @Override
     public boolean test(Candidate candidate) {
-        String[] split = candidate.getPeriodsInUkr().split("-");
-        int period = Integer.parseInt(split[INDEX_FINISH]) - Integer.parseInt(split[INDEX_START]);
+        String[] dates = candidate.getPeriodsInUkr().split("-");
+        int period = Integer.parseInt(dates[DATE_TO_INDEX])
+                - Integer.parseInt(dates[DATE_FROM_INDEX]);
         return candidate.getAge() >= MIN_AGE
             && candidate.getNationality().equals(EXPECTED_NATIONALITY)
             && period >= MIN_PERIOD
