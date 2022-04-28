d38caf5abbbcb16e0288656350bb0963b1d8fbe4
Iryna Khrustalova <blackkitty2204@gmail.com>
2022-04-28 18:48:02 +0200

    API-practice-was-done

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MAX_CANDIDATES_AGE = 35;
+    private static final String CANDIDATES_NATIONALITY = "Ukrainian";
+    private static final int PERIOD_IN_UKRAINE = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+
+        return candidate.getAge() >= MAX_CANDIDATES_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(CANDIDATES_NATIONALITY)
+                && Integer.parseInt(candidate.getPeriodsInUkr().split("-")[1])
+                - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[0]) >= PERIOD_IN_UKRAINE;
+    }
 }
