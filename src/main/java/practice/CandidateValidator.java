bc516476fa7d6bd86d94568e6d8239dd7273a14e
Lyudmila D <dunda94lyudmila@gmail.com>
2022-04-29 14:35:03 +0300

    created methods using stream

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,17 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] split = candidate.getPeriodsInUkr().split("-");
+        int yearInUkr = Integer.parseInt(split[1]) - Integer.parseInt(split[0]);
+        if (candidate.isAllowedToVote() && candidate.getAge() >= 35
+                && candidate.getNationality().equals("Ukrainian") && yearInUkr >= 10) {
+            return true;
+        }
+        return false;
+    }
 }
