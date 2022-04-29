36bd56f18fc4c2ad814616563980f1053090d3ab
Iryna Khrustalova <blackkitty2204@gmail.com>
2022-04-29 12:39:42 +0200

    CandidateValidator_was_redacted

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,18 +7,16 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int MAX_CANDIDATES_AGE = 35;
     private static final String CANDIDATES_NATIONALITY = "Ukrainian";
     private static final int MIN_PERIOD_IN_UKRAINE = 10;
-    private static final String SPLIT = "-";
-    private static final int START_YEAR = 0;
-    private static final int END_YEAR = 1;
-    private String[] periodInUkr;
+    private static final int START_YEAR_INDEX = 0;
+    private static final int END_YEAR_INDEX = 1;
 
     @Override
     public boolean test(Candidate candidate) {
-        periodInUkr = candidate.getPeriodsInUkr().split(SPLIT);
+        String[] periodInUkr = candidate.getPeriodsInUkr().split("-");
         return candidate.getAge() >= MAX_CANDIDATES_AGE
                 && candidate.isAllowedToVote()
                 && candidate.getNationality().equals(CANDIDATES_NATIONALITY)
-                && Integer.parseInt(periodInUkr[END_YEAR])
-                - Integer.parseInt(periodInUkr[START_YEAR]) >= MIN_PERIOD_IN_UKRAINE;
+                && Integer.parseInt(periodInUkr[END_YEAR_INDEX])
+                - Integer.parseInt(periodInUkr[START_YEAR_INDEX]) >= MIN_PERIOD_IN_UKRAINE;
     }
 }
