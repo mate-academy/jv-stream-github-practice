827035c8bae227fe7f7e89656ad19e92353771ae
Kate Chernyavska <ccarts.kpi@gmail.com>
2022-05-10 17:00:17 +0300

    implemented Predicate's method test

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,13 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.isAllowedToVote();
+    }
+
     //write your code here
 }
