eab866a04581d576407a07fe3805e52d984fb5c0
Kate Chernyavska <ccarts.kpi@gmail.com>
2022-05-11 14:24:22 +0300

    changed names of variables

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,16 +5,19 @@ import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_AGE = 35;
-    private static final String NATIONALITY = "Ukrainian";
-    private static final int LIVE_IN_COUNTRY = 10;
+    private static final String REQUIRED_NATIONALITY = "Ukrainian";
+    private static final int REQUIRED_PERIOD_IN_COUNTRY = 10;
+    private static final int INDEX_OF_FIRST_DATE_OF_PERIOD = 0;
+    private static final int INDEX_OF_SECOND_DATE_OF_PERIOD = 1;
 
     @Override
     public boolean test(Candidate candidate) {
         String period = candidate.getPeriodsInUkr();
-        String[] arrYears = period.split("-");
+        String[] years = period.split("-");
         return (candidate.getAge() >= MIN_AGE && candidate.isAllowedToVote()
-               && candidate.getNationality().equals(NATIONALITY)
-               && (Integer.parseInt(arrYears[1]) - Integer.parseInt(arrYears[0]))
-               >= LIVE_IN_COUNTRY);
+               && candidate.getNationality().equals(REQUIRED_NATIONALITY)
+               && (Integer.parseInt(years[INDEX_OF_SECOND_DATE_OF_PERIOD])
+                - Integer.parseInt(years[INDEX_OF_FIRST_DATE_OF_PERIOD]))
+               >= REQUIRED_PERIOD_IN_COUNTRY);
     }
 }
