9e6adb2c6d7377561bc3a1603e6ea560fbe0df79
Kate Chernyavska <ccarts.kpi@gmail.com>
2022-05-11 14:07:14 +0300

    moved logic about ideal candidate in this class

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,10 +4,17 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int LIVE_IN_COUNTRY = 10;
+
     @Override
     public boolean test(Candidate candidate) {
-        return candidate.isAllowedToVote();
+        String period = candidate.getPeriodsInUkr();
+        String[] arrYears = period.split("-");
+        return (candidate.getAge() >= MIN_AGE && candidate.isAllowedToVote()
+               && candidate.getNationality().equals(NATIONALITY)
+               && (Integer.parseInt(arrYears[1]) - Integer.parseInt(arrYears[0]))
+               >= LIVE_IN_COUNTRY);
     }
-
-    //write your code here
 }
