70ce7809221debba98d19721fdffe2e13467749b
Vadym Datsiuk <lutskvadym@gmail.com>
2022-05-11 12:02:41 +0300

    for approving, checked

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MINIMUM_AGE = 35;
+    private static final int MINIMUM_PERIOD = 10;
+    private static final String NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MINIMUM_AGE
+                && candidate.getNationality().equals(NATIONALITY)
+                && getPeriod(candidate) >= MINIMUM_PERIOD
+                && candidate.isAllowedToVote();
+    }
+
+    private int getPeriod(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split("-");
+        return Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
+    }
 }
