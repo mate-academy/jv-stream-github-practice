de489e4a642b0dfcab0aee561ad3a1f73e6f6678
Vitalii Smahlenko <smaglenko@gmail.com>
2022-05-23 21:43:28 +0300

    corrected

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,22 +6,23 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_AGE = 35;
     private static final int MIN_PERIOD_OF_RESIDENCE_UKRAINE = 10;
-    private static final String NATIONALITY_UKRAINIAN = "Ukrainian";
+    private static final String REQUIRED_NATIONALITY = "Ukrainian";
+    private static final int LIVED_IN_COUNTRY_FOR_YEAR = 0;
+    private static final int LIVED_IN_COUNTRY_TO_YEAR = 1;
 
     @Override
     public boolean test(Candidate candidate) {
-
         return candidate.getAge() >= MIN_AGE
                 && candidate.isAllowedToVote()
-                && candidate.getNationality().equals(NATIONALITY_UKRAINIAN)
+                && candidate.getNationality().equals(REQUIRED_NATIONALITY)
                 && liveInUkraineForYears(candidate) >= MIN_PERIOD_OF_RESIDENCE_UKRAINE;
     }
 
     private int liveInUkraineForYears(Candidate candidate) {
         String periodsInUkr = candidate.getPeriodsInUkr();
         String[] years = periodsInUkr.split("-");
-        int yearFrom = Integer.parseInt(years[0]);
-        int yearTo = Integer.parseInt(years[1]);
+        int yearFrom = Integer.parseInt(years[LIVED_IN_COUNTRY_FOR_YEAR]);
+        int yearTo = Integer.parseInt(years[LIVED_IN_COUNTRY_TO_YEAR]);
         return yearTo - yearFrom;
     }
 }
