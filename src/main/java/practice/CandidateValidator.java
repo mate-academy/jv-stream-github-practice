600e97edae988d8d51ab6768510a54a8abc76f1b
Vitalii Smahlenko <smaglenko@gmail.com>
2022-05-24 11:38:06 +0300

    Done

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,8 +7,8 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_AGE = 35;
     private static final int MIN_PERIOD_OF_RESIDENCE_UKRAINE = 10;
     private static final String REQUIRED_NATIONALITY = "Ukrainian";
-    private static final int LIVED_IN_COUNTRY_FOR_YEAR = 0;
-    private static final int LIVED_IN_COUNTRY_TO_YEAR = 1;
+    private static final int YEAR_FROM_INDEX = 0;
+    private static final int YEAR_TO_INDEX = 1;
 
     @Override
     public boolean test(Candidate candidate) {
@@ -21,8 +21,8 @@ public class CandidateValidator implements Predicate<Candidate> {
     private int liveInUkraineForYears(Candidate candidate) {
         String periodsInUkr = candidate.getPeriodsInUkr();
         String[] years = periodsInUkr.split("-");
-        int yearFrom = Integer.parseInt(years[LIVED_IN_COUNTRY_FOR_YEAR]);
-        int yearTo = Integer.parseInt(years[LIVED_IN_COUNTRY_TO_YEAR]);
+        int yearFrom = Integer.parseInt(years[YEAR_FROM_INDEX]);
+        int yearTo = Integer.parseInt(years[YEAR_TO_INDEX]);
         return yearTo - yearFrom;
     }
 }
