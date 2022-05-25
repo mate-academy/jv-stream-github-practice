5e058ba2461ad09a33b499dd8d3b153e557ed2fd
Denys Skrinnik <skrinnikden2000@gmail.com>
2022-05-25 18:16:54 +0300

    CONSTANT_INDEX

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,14 +7,15 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_CANDIDATE_AGE = 35;
     private static final int MIN_AMOUNT_OF_YEARS_LIVING_IN_UKRAINE = 10;
     private static final String REQUIRED_NATIONALITY = "Ukrainian";
-    public static final int INDEX_OF_YEAR_ENTERING_UKRAINE = 0;
-    public static final int INDEX_OF_LAST_YEAR_STAYING_IN_UKRAINE = 1;
+    private static final int INDEX_OF_YEAR_ENTERING_UKRAINE = 0;
+    private static final int INDEX_OF_LAST_YEAR_STAYING_IN_UKRAINE = 1;
 
     @Override
     public boolean test(Candidate candidate) {
         String[] inUkraineFromAndTill = candidate.getPeriodsInUkr().split("-");
         int inUkraineFrom = Integer.parseInt(inUkraineFromAndTill[INDEX_OF_YEAR_ENTERING_UKRAINE]);
-        int inUkraineTill = Integer.parseInt(inUkraineFromAndTill[INDEX_OF_LAST_YEAR_STAYING_IN_UKRAINE]);
+        int inUkraineTill =
+                Integer.parseInt(inUkraineFromAndTill[INDEX_OF_LAST_YEAR_STAYING_IN_UKRAINE]);
         int yearsInUkraine = inUkraineTill - inUkraineFrom;
         return candidate.getAge() >= MIN_CANDIDATE_AGE
                 && candidate.getNationality().equals(REQUIRED_NATIONALITY)
