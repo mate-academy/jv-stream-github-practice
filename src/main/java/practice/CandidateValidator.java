d927a8d24327b3f2f0beec825d21874468131f34
Maksym Lutovinov <90464472+maxlutovinov@users.noreply.github.com>
2022-05-25 18:35:42 +0300

    Implement methods in StreamPractice class

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.Arrays;
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    public static final int AGE_THRESHOLD = 35;
+    public static final int PERIOD_IN_UKRAINE = 10;
+    public static final String UKRAINIAN = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate c) {
+        return c.getAge() >= AGE_THRESHOLD
+                && c.getNationality().equals(UKRAINIAN)
+                && c.isAllowedToVote()
+                && Arrays.stream(c.getPeriodsInUkr().split("-"))
+                .map(Integer::valueOf)
+                .reduce(0, (i, i2) -> i2 - i) >= PERIOD_IN_UKRAINE;
+    }
 }
