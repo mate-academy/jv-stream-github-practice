38a9a9bfd5ee341ddf1ab78c0f014f12e995b5c8
Anton Karpov <antonkarpov547@gmail.com>
2022-06-10 14:46:07 +0200

    Get next bigger number

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MINIMUM_AGE = 35;
+    private static final int MINIMUM_PERIOD = 10;
+    private static final int START_OF_PERIOD_INDEX = 0;
+    private static final int END_OF_PERIOD_INDEX = 1;
+    private static final String PERIOD_SEPARATOR = "-";
+    private static final String NATIONALITY = "Ukrainian";
+
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MINIMUM_AGE
+                && candidate.getNationality().equals(NATIONALITY)
+                && candidate.isAllowedToVote()
+                && Integer.parseInt(candidate.getPeriodsInUkr()
+                .split(PERIOD_SEPARATOR)[END_OF_PERIOD_INDEX])
+                - Integer.parseInt(candidate.getPeriodsInUkr()
+                        .split(PERIOD_SEPARATOR)[START_OF_PERIOD_INDEX]) >= MINIMUM_PERIOD;
+    }
 }
