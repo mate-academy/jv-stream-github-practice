322e29317f8b2f514920b458c305a6ab62628486
Zinchuk-Yevhenii-Mate <zenya87@gmail.com>
2022-06-12 11:57:33 +0300

    Added constant to predicate CandidateValidation

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,8 +5,9 @@ import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
     public static final int MIN_AGE_FOR_RUNNING = 35;
-    public static final int PERIOD_LIVING_IN_UKRAINE= 10;
-    public static final  String NATIONALITY_FOR_RUNNING = "Ukrainian";
+    public static final int PERIOD_LIVING_IN_UKRAINE = 10;
+    public static final String NATIONALITY_FOR_RUNNING = "Ukrainian";
+
     @Override
     public boolean test(Candidate candidate) {
         return candidate.getClass().equals(Candidate.class)
@@ -14,6 +15,7 @@ public class CandidateValidator implements Predicate<Candidate> {
                 && candidate.getNationality().equals(NATIONALITY_FOR_RUNNING)
                 && candidate.isAllowedToVote()
                 && (Integer.parseInt(candidate.getPeriodsInUkr().substring(5))
-                - Integer.parseInt(candidate.getPeriodsInUkr().substring(0, 4))) >= PERIOD_LIVING_IN_UKRAINE;
+                - Integer.parseInt(candidate.getPeriodsInUkr().substring(0, 4)))
+                >= PERIOD_LIVING_IN_UKRAINE;
     }
 }
