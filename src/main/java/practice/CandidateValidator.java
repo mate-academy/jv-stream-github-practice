19c50afee1a4e3df5c9d06b2579b4e14e396c369
Andrey Chumak <chumakandrey7@gmail.com>
2022-06-13 08:32:20 +0300

    Adjustments made after student review

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,16 +6,18 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_VALID_AGE = 35;
     private static final int MIN_VALID_PERIOD_IN_UKR = 10;
+    private static final String VALID_NATIONALITY = "ukrainian";
+    private static final String DATE_SEPARATOR = "-";
 
     @Override
     public boolean test(Candidate candidate) {
-        String[] periodsInUkr = candidate.getPeriodsInUkr().split("-");
+        String[] periodsInUkr = candidate.getPeriodsInUkr().split(DATE_SEPARATOR);
         int periodFrom = Integer.parseInt(periodsInUkr[0]);
         int periodTo = Integer.parseInt(periodsInUkr[1]);
-        int tenure = periodTo - periodFrom;
+        int tenureInUkraine = periodTo - periodFrom;
 
         return candidate.getAge() >= MIN_VALID_AGE && candidate.isAllowedToVote()
-                && candidate.getNationality().equalsIgnoreCase("ukrainian")
-                && tenure > MIN_VALID_PERIOD_IN_UKR;
+                && candidate.getNationality().equalsIgnoreCase(VALID_NATIONALITY)
+                && tenureInUkraine > MIN_VALID_PERIOD_IN_UKR;
     }
 }
