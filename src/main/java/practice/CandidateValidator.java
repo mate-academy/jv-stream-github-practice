2b96060c3dea6a7c28b2796978c74e726577874a
Nikita Semisoshenko <jaderussell197@gmail.com>
2022-06-13 18:13:38 +0300

    naming of constants was сÑcorrected

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,14 +7,14 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int TEN_YEARS_OF_LIVING = 10;
     private static final int MINIMUM_AGE = 35;
     private static final String UKRAINIAN_NATIONALITY = "Ukrainian";
-    private static final int UPPER_BOUND_OF_LIVING = 1;
-    private static final int BOTTOM_BOUND_OF_LIVING = 0;
+    private static final int UPPER_BOUND_OF_LIVING_INDEX = 1;
+    private static final int BOTTOM_BOUND_OF_LIVING_INDEX = 0;
 
     @Override
     public boolean test(Candidate candidate) {
         String[] splitPeriods = candidate.getPeriodsInUkr().split("-");
-        int livingUpperBound = Integer.parseInt(splitPeriods[UPPER_BOUND_OF_LIVING]);
-        int livingBottomBound = Integer.parseInt(splitPeriods[BOTTOM_BOUND_OF_LIVING]);
+        int livingUpperBound = Integer.parseInt(splitPeriods[UPPER_BOUND_OF_LIVING_INDEX]);
+        int livingBottomBound = Integer.parseInt(splitPeriods[BOTTOM_BOUND_OF_LIVING_INDEX]);
         int howLongLive = livingUpperBound - livingBottomBound;
         boolean isLivingMoreThanTenYears = howLongLive >= TEN_YEARS_OF_LIVING;
 
