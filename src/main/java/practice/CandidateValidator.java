a963c1d4839e39851bd32315dc54cbea65cac51e
Zinchuk-Yevhenii-Mate <zenya87@gmail.com>
2022-06-13 14:36:17 +0300

    added constant index_from_age, index_to_age

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,14 +6,16 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_AGE_FOR_RUNNING = 35;
     private static final int MIN_PERIOD_LIVING_IN_UKRAINE = 10;
+    private static final int INDEX_FROM_AGE = 0;
+    private static final int INDEX_TO_AGE = 1;
     private static final String NATIONALITY_FOR_RUNNING = "Ukrainian";
     private static final String SEPARATOR = "-";
 
     @Override
     public boolean test(Candidate candidate) {
         String[] periodInUkraineArray = candidate.getPeriodsInUkr().split(SEPARATOR);
-        int fromAge = Integer.parseInt(periodInUkraineArray[0]);
-        int toAge = Integer.parseInt(periodInUkraineArray[1]);
+        int fromAge = Integer.parseInt(periodInUkraineArray[INDEX_FROM_AGE]);
+        int toAge = Integer.parseInt(periodInUkraineArray[INDEX_TO_AGE]);
         int periodLivingInUkraine = toAge - fromAge;
         return candidate.getAge() >= MIN_AGE_FOR_RUNNING
                 && candidate.getNationality().equals(NATIONALITY_FOR_RUNNING)
