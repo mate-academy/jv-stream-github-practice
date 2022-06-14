eceb1350a6a51a1ff82469d6754adaa61c1b67c9
Maksym Karamavrov <makcumkaka@gmail.com>
2022-06-14 19:19:24 +0300

    Indexes for yearsInUkraine were added

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,6 +5,8 @@ import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int ELIGIBLE_AGE = 35;
+    private static final int INDEX_OF_AGE_FROM = 0;
+    private static final int INDEX_OF_AGE_TO = 1;
     private static final int ELIGIBLE_PERIOD = 10;
     private static final String ELIGIBLE_NATIONALITY = "Ukrainian";
 
@@ -13,7 +15,8 @@ public class CandidateValidator implements Predicate<Candidate> {
         String[] temp = candidate.getPeriodsInUkr().split("-");
         return candidate.isAllowedToVote()
                 && candidate.getAge() >= ELIGIBLE_AGE
-                && Integer.parseInt(temp[1]) - Integer.parseInt(temp[0]) >= ELIGIBLE_PERIOD
+                && Integer.parseInt(temp[INDEX_OF_AGE_TO])
+                    - Integer.parseInt(temp[INDEX_OF_AGE_FROM]) >= ELIGIBLE_PERIOD
                 && candidate.getNationality().equals(ELIGIBLE_NATIONALITY);
     }
 }
