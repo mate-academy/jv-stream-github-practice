65e167dff73f505b26616ff2d38cb3022a25f7a4
Maksym Karamavrov <makcumkaka@gmail.com>
2022-06-14 14:59:18 +0300

    All methods were implements, all tests run clear.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,27 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int ELIGIBLE_AGE = 35;
+    private static final int ELIGIBLE_PERIOD = 10;
+    private static final int GET_DATE_FROM = 0;
+    private static final int GET_DATE_TO = 1;
+    private static final String ELIGIBLE_NATIONALITY = "Ukrainian";
+
+    private boolean getYearsInUkraine(Candidate candidate) {
+        String[] temp = candidate.getPeriodsInUkr().split("-");
+        int yearFrom = Integer.parseInt(temp[GET_DATE_FROM]);
+        int yearTo = Integer.parseInt(temp[GET_DATE_TO]);
+        return (yearTo - yearFrom) >= ELIGIBLE_PERIOD;
+    }
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.isAllowedToVote()
+                && candidate.getAge() >= ELIGIBLE_AGE
+                && getYearsInUkraine(candidate)
+                && candidate.getNationality().equals(ELIGIBLE_NATIONALITY);
+    }
 }
