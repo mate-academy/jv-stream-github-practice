84eb8ac40a249d7f65b7a341f99f15df2b1025b5
Oleksii Mospan <alexey.mospan@gmail.com>
2022-06-15 21:38:22 +0300

    Added constants

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,12 +4,15 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int AGE_THRESHOLD = 35;
+    private static final int MIN_PERIOD_IN_UKRAINE = 10;
+    private static final String DEMONYM = "Ukrainian";
     @Override
     public boolean test(Candidate c) {
-        return c.getAge() >= 35
+        return c.getAge() >= AGE_THRESHOLD
                 && c.isAllowedToVote()
-                && c.getNationality().equals("Ukrainian")
+                && c.getNationality().equals(DEMONYM)
                 && (Integer.parseInt(c.getPeriodsInUkr().split("-")[1])
-                - Integer.parseInt(c.getPeriodsInUkr().split("-")[0])) >= 10;
+                - Integer.parseInt(c.getPeriodsInUkr().split("-")[0])) >= MIN_PERIOD_IN_UKRAINE;
     }
 }
