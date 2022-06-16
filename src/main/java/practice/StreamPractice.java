7611118d52fd33ac2f5b222bb121d6a732df6985
Oleksii Mospan <alexey.mospan@gmail.com>
2022-06-16 14:33:27 +0300

    Combined filters in getWorkablePeople

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -68,9 +68,9 @@ public class StreamPractice {
     public List<Person> getWorkablePeople(int fromAge, int femaleToAge,
                                           int maleToAge, List<Person> peopleList) {
         return peopleList.stream()
-                .filter(p -> p.getAge() >= fromAge)
-                .filter(p -> p.getAge() <= femaleToAge && p.getSex().equals(Person.Sex.WOMAN)
-                        || p.getAge() <= maleToAge && p.getSex().equals(Person.Sex.MAN))
+                .filter(p -> p.getAge() >= fromAge
+                        && (p.getAge() <= femaleToAge && p.getSex().equals(Person.Sex.WOMAN)
+                        || p.getAge() <= maleToAge && p.getSex().equals(Person.Sex.MAN)))
                 .collect(Collectors.toList());
     }
 
