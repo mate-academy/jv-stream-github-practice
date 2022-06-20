87b90e60d302490661fad3c97887a664e73b2e39
kqlynero <kqlynero@gmail.com>
2022-06-20 15:39:02 +0300

    -fixed mistakes(replace orElseThrow() to getAsDouble())

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -2,7 +2,6 @@ package practice;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -40,7 +39,7 @@ public class StreamPractice {
                 .filter(n -> n % 2 == 1)
                 .mapToDouble(Double::valueOf)
                 .average()
-                .orElseThrow(NoSuchElementException::new);
+                .getAsDouble();
     }
 
     /**
