4d6180195a724dd57a0549f351efb39fb976c5ae
Mykola Yakymiv <mykola8426@gmail.com>
2022-06-20 13:51:56 +0300

    methods and class were implemented

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,26 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE_OF_CANDIDATE = 35;
+    private static final int START_OF_FIRST_YEAR_IN_PERIOD = 0;
+    private static final int START_OF_SECOND_YEAR_IN_PERIOD = 5;
+    private static final int MIN_STAY_IN_UKRAINE = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MIN_AGE_OF_CANDIDATE
+                && candidate.getNationality().equals("Ukrainian")
+                && isValidPeriodInUkr(candidate.getPeriodsInUkr()) && candidate.isAllowedToVote();
+    }
+
+    private boolean isValidPeriodInUkr(String period) {
+        int fromYear = Integer.parseInt(
+                period.substring(START_OF_FIRST_YEAR_IN_PERIOD, period.indexOf("-")));
+        int toYear = Integer.parseInt(period.substring(START_OF_SECOND_YEAR_IN_PERIOD));
+        int yearsInUkraine = toYear - fromYear;
+        return yearsInUkraine >= MIN_STAY_IN_UKRAINE;
+    }
 }
