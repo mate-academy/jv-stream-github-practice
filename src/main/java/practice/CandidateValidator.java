640c7f3918f06995fee58e88ba63db312b01eaa3
Illia Khomych <illia.homych@knu.ua>
2022-06-20 14:44:04 +0300

    StreamPractice methods implemented, CandidateValidator implements Predicate implemented

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,25 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    public static final int PERIOD_START_ID = 0;
+    public static final int PERIOD_END_ID = 1;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        if (candidate.getAge() < 35) {
+            return false;
+        }
+        if (!candidate.getNationality().equals("Ukrainian")) {
+            return false;
+        }
+        if (!candidate.isAllowedToVote()) {
+            return false;
+        }
+        String[] period = candidate.getPeriodsInUkr().split("-");
+        return Integer.parseInt(period[PERIOD_END_ID])
+                - Integer.parseInt(period[PERIOD_START_ID]) >= 10;
+    }
 }
