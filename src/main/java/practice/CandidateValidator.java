40d6f4b0f81b63bad6b2c32f424bf3d1cf8b285d
Olesya Khalak <khalak.olesya.official@gmail.com>
2022-07-01 12:11:25 +0300

    done

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    public static final int MIN_YEAR_OLD = 35;
+    public static final int MIN_YEAR_LIVE = 10;
+    public static final int POS_YEAR_FROM = 0;
+    public static final int POS_YEAR_TO = 1;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return "Ukrainian".equals(candidate.getNationality())
+                && candidate.getAge() >= MIN_YEAR_OLD
+                && candidate.isAllowedToVote()
+                && candidate.getPeriodsInUkr() != null
+                && Integer.parseInt(candidate.getPeriodsInUkr().split("-")[POS_YEAR_TO])
+                - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[POS_YEAR_FROM])
+                >= MIN_YEAR_LIVE;
+    }
 }
+
