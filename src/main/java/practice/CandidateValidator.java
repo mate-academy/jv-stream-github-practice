e204918b1fd5e6d331e141e6629aabb1fb87e86c
Oleksandr Zaporozhets <zpc23041988@gmail.com>
2022-07-05 13:22:40 +0300

    StreamPractice complete fixed

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,19 +4,19 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    private static final int FROM_AGE = 35;
-    private static final int PERIODS_IN_UKR = 10;
-    private static final String NATIONALITY = "Ukrainian";
-    private static final int START_OF_RESIDENCE = 0;
-    private static final int END_OF_RESIDENCE = 1;
+    private static final int MIN_AGE = 35;
+    private static final int MIN_PERIOD_IN_UKR = 10;
+    private static final String REQUIRED_NATIONALITY = "Ukrainian";
+    private static final int FIRST_YEAR_INDEX = 0;
+    private static final int LAST_YEAR_INDEX = 1;
 
     @Override
     public boolean test(Candidate candidate) {
-        return candidate.getAge() >= FROM_AGE
+        return candidate.getAge() >= MIN_AGE
                 && candidate.isAllowedToVote()
-                && candidate.getNationality().equals(NATIONALITY)
-                && Integer.parseInt(candidate.getPeriodsInUkr().split("-")[END_OF_RESIDENCE])
-                - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[START_OF_RESIDENCE])
-                > PERIODS_IN_UKR;
+                && candidate.getNationality().equals(REQUIRED_NATIONALITY)
+                && Integer.parseInt(candidate.getPeriodsInUkr().split("-")[LAST_YEAR_INDEX])
+                - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[FIRST_YEAR_INDEX])
+                > MIN_PERIOD_IN_UKR;
     }
 }
