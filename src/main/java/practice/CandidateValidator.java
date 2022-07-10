7ca7f06b389f843f28c6b08dbbe555335f7aceed
Vasiliy Sheretko <vasil.sheretko@yahoo.com>
2022-07-10 23:49:20 +0300

    Implemented necessary methods using stream.API according to task description

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,19 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int AGE_OF_ELIGIBILITY_FOR_ELECTION = 35;
+    private static final int YEARS_OF_RESIDENTIAL = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= AGE_OF_ELIGIBILITY_FOR_ELECTION
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && Integer.parseInt(candidate.getPeriodsInUkr().substring(5))
+                - Integer.parseInt(candidate.getPeriodsInUkr().substring(0, 4))
+                >= YEARS_OF_RESIDENTIAL;
+    }
 }
