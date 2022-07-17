1760197840563a1b60a2590d7a23f9d331fa641c
Fokin <Fokin1@ukr.net>
2022-07-17 06:21:58 +0200

    implemented method test in class Candidate Validator and methods  in class StreamPractice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,17 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.Arrays;
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String [] periodsInUkr = candidate.getPeriodsInUkr().split("-");
+        int[] ints = Arrays.stream(periodsInUkr)
+                .flatMapToInt(i -> IntStream.of(Integer.parseInt(i))).toArray();
+        return ints[1] - ints[0] > 10 ? true : false;
+    }
 }
