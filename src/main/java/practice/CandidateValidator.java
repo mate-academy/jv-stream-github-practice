176181d8fcf1255b1e0e0620401448ace5bc5f9a
Vitaliy Ozhelivskiy <ozelivskijvitalij@gmail.com>
2022-07-20 16:11:00 +0300

    Replaced constant name CITIZENSHIP for REQUIRED_CITIZENSHIP

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,7 +7,7 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_PERIOD_IN_UKRAINE = 10;
     private static final int START_PERIOD_INDEX = 0;
     private static final int END_PERIOD_INDEX = 1;
-    private static final String CITIZENSHIP = "Ukrainian";
+    private static final String REQUIRED_CITIZENSHIP = "Ukrainian";
     private static final int MIN_AGE = 35;
 
     public boolean test(Candidate candidate) {
@@ -15,7 +15,7 @@ public class CandidateValidator implements Predicate<Candidate> {
         int periodsInUkraine = Integer.parseInt(periods[END_PERIOD_INDEX])
                 - Integer.parseInt(periods[START_PERIOD_INDEX]);
         return candidate.getAge() >= MIN_AGE
-                && candidate.getNationality().equals(CITIZENSHIP)
+                && candidate.getNationality().equals(REQUIRED_CITIZENSHIP)
                 && periodsInUkraine >= MIN_PERIOD_IN_UKRAINE
                 && candidate.isAllowedToVote();
     }
