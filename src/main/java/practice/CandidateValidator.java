d9f831ec84cf3334cca745116fedd907af1cb01e
Fokin <Fokin1@ukr.net>
2022-07-20 18:35:55 +0200

    corrected method test in class CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,19 +6,18 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_YEAR_LIVE_COUNTRY = 10;
     private static final int MIN_CANDIDATE_AGE = 35;
+    private static final int FROM_YEAR = 0;
+    private static final int BEFORE_YEAR = 1;
+    private static final String CANDIDATE_FROM_COUNTRY = "Ukrainian";
 
     @Override
     public boolean test(Candidate candidate) {
         String[] periodsInUkr = candidate.getPeriodsInUkr().split("-");
-        int periodsInUkrFrom = Integer.parseInt(periodsInUkr[0]);
-        int periodsInUkrBefore = Integer.parseInt(periodsInUkr[1]);
-
-        if (periodsInUkrBefore - periodsInUkrFrom > MIN_YEAR_LIVE_COUNTRY
+        int periodsInUkrFrom = Integer.parseInt(periodsInUkr[FROM_YEAR]);
+        int periodsInUkrBefore = Integer.parseInt(periodsInUkr[BEFORE_YEAR]);
+        return periodsInUkrBefore - periodsInUkrFrom > MIN_YEAR_LIVE_COUNTRY
                 && candidate.getAge() >= MIN_CANDIDATE_AGE
-                && candidate.isAllowedToVote() == true
-                && candidate.getNationality().equals("Ukrainian")) {
-            return true;
-        }
-        return false;
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(CANDIDATE_FROM_COUNTRY);
     }
 }
