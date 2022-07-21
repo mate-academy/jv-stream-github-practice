f6f7e5c384bb97083d212376cd4e9ef9e2176338
Fokin <Fokin1@ukr.net>
2022-07-21 07:17:27 +0200

    Rename constants

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,20 +4,20 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    private static final int MIN_YEAR_LIVE_COUNTRY = 10;
+    private static final int MIN_YEARS_LIVE_IN_COUNTRY = 10;
     private static final int MIN_CANDIDATE_AGE = 35;
-    private static final int FROM_YEAR = 0;
-    private static final int BEFORE_YEAR = 1;
-    private static final String CANDIDATE_FROM_COUNTRY = "Ukrainian";
+    private static final int YEAR_FROM = 0;
+    private static final int YEAR_TO = 1;
+    private static final String REQUIRED_NATIONALITY = "Ukrainian";
 
     @Override
     public boolean test(Candidate candidate) {
         String[] periodsInUkr = candidate.getPeriodsInUkr().split("-");
-        int periodsInUkrFrom = Integer.parseInt(periodsInUkr[FROM_YEAR]);
-        int periodsInUkrBefore = Integer.parseInt(periodsInUkr[BEFORE_YEAR]);
-        return periodsInUkrBefore - periodsInUkrFrom > MIN_YEAR_LIVE_COUNTRY
+        int periodsInUkrFrom = Integer.parseInt(periodsInUkr[YEAR_FROM]);
+        int periodsInUkrBefore = Integer.parseInt(periodsInUkr[YEAR_TO]);
+        return periodsInUkrBefore - periodsInUkrFrom > MIN_YEARS_LIVE_IN_COUNTRY
                 && candidate.getAge() >= MIN_CANDIDATE_AGE
                 && candidate.isAllowedToVote()
-                && candidate.getNationality().equals(CANDIDATE_FROM_COUNTRY);
+                && candidate.getNationality().equals(REQUIRED_NATIONALITY);
     }
 }
