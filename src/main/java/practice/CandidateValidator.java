d4889a42194e731b8d16c33ad20adcfd31fd241e
elenapokaliuk <100062370+elenapokaliuk@users.noreply.github.com>
2022-07-21 10:11:40 +0300

    added implementation for all methods from StreamPractice class added implementation for CandidateValidator, override method test and create method calculatePeriodInUkr to filter candidates who lived in Ukraine more than 10 years

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,24 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MINIMUM_AGE = 35;
+    private static final String CANDIDATE_NATIONALITY = "Ukrainian";
+    private static final int MINIMUM_PERIOD_IN_UKRAINE = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MINIMUM_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(CANDIDATE_NATIONALITY)
+                && calculatePeriodInUkr(candidate) >= MINIMUM_PERIOD_IN_UKRAINE;
+    }
+
+    private int calculatePeriodInUkr(Candidate candidate) {
+        String[] getYears = candidate.getPeriodsInUkr().split("-");
+        return Integer.parseInt(getYears[1]) - Integer.parseInt(getYears[0]);
+    }
+
 }
