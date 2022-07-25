263d5e99753d002ac24eb4387ab8e5bc1e42d738
Nikita Melnikov <bam19921303@gmail.com>
2022-07-25 23:35:48 +0300

    There were implementation issues in the getOddNumsAverage method. The rest of the tasks are completed.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,17 @@
 package practice;
+import model.Candidate;
+import java.util.function.Predicate;
 
-public class CandidateValidator {
-    //write your code here
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return (candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && Integer.parseInt(candidate.getPeriodsInUkr().split("-")[1])
+                - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[0]) >= 10);
+
+    }
 }
