35cb50a47ba58331a2702cf21b89a676d4e16c2a
ssplot56 <ssplot56@gmail.com>
2022-07-31 00:33:57 +0300

    implemented methods using stream api

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,16 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] timeInUkr = candidate.getPeriodsInUkr().split("-");
+        int periodInUkr = Integer.parseInt(timeInUkr[1]) - Integer.parseInt(timeInUkr[0]);
+        return candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && periodInUkr >= 10;
+    }
 }
