76ac64065df9f88de7cff00647a7859c866d47b7
Maksym Bakunov <mpbakunov.it@gmail.com>
2022-08-01 15:59:51 +0200

    Implementation of the task conditions

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    public static final String NATIONALITY = "Ukrainian";
+    public static final int MIN_AGE = 35;
+    public static final int MIN_LIVING_PERIOND = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] period = candidate.getPeriodsInUkr().split("-");
+        return candidate.getNationality().equals(NATIONALITY)
+                && candidate.getAge() >= MIN_AGE && candidate.isAllowedToVote()
+                && (Integer.parseInt(period[1])
+                - Integer.parseInt(period[0]) >= MIN_LIVING_PERIOND);
+    }
 }
+
