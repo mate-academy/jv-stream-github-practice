1428729fdfa0d31e6df89ab759716758cc405ab9
ssplot56 <ssplot56@gmail.com>
2022-08-01 12:40:05 +0300

    my fault, I'll pay more attention

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,13 +4,17 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final int MIN_PERIOD = 10;
+    private static final String REQUIRED_NATIONALITY = "Ukrainian";
+
     @Override
     public boolean test(Candidate candidate) {
         String[] timeInUkr = candidate.getPeriodsInUkr().split("-");
         int periodInUkr = Integer.parseInt(timeInUkr[1]) - Integer.parseInt(timeInUkr[0]);
-        return candidate.getAge() >= 35
+        return candidate.getAge() >= MIN_AGE
                 && candidate.isAllowedToVote()
-                && candidate.getNationality().equals("Ukrainian")
-                && periodInUkr >= 10;
+                && candidate.getNationality().equals(REQUIRED_NATIONALITY)
+                && periodInUkr >= MIN_PERIOD;
     }
 }
