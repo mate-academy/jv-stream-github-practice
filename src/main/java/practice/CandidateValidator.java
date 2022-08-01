574342ca359412dcc86ca67e7445d3e9bfa53a41
Viktor A <viktor.antkevych@gmail.com>
2022-08-01 15:42:15 +0200

    Code corrected according to mentor comments. CandidateValidator class corrected.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,19 +4,21 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final String REQUIRED_NATIONALITY = "Ukrainian";
+    private static final int MIN_YEARS_TO_LIVE = 10;
+    private static final int MIN_AGE = 35;
+    private static final int END_OF_PERIOD = 1;
+    private static final int START_OF_PERIOD = 0;
 
     @Override
     public boolean test(Candidate candidate) {
-        String[] periodSplit = candidate.getPeriodsInUkr().split("-");
-        int years = Integer.parseInt(periodSplit[1])
-                - Integer.parseInt(periodSplit[0]);
-        String requiredNationality = "Ukrainian";
-        int minYearsToLive = 10;
-        int minAge = 35;
+        String[] years = candidate.getPeriodsInUkr().split("-");
+        int period = Integer.parseInt(years[END_OF_PERIOD])
+                - Integer.parseInt(years[START_OF_PERIOD]);
 
-        return candidate.getAge() >= minAge
+        return candidate.getAge() >= MIN_AGE
                 && candidate.isAllowedToVote()
-                && candidate.getNationality().equals(requiredNationality)
-                && years > minYearsToLive;
+                && candidate.getNationality().equals(REQUIRED_NATIONALITY)
+                && period > MIN_YEARS_TO_LIVE;
     }
 }
