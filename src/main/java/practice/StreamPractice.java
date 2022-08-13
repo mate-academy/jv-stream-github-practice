da4d02bedd1151a0a58895151a35e94637b8f3de
Valerii-Sh <val-sh@ukr.net>
2022-08-13 14:28:50 +0300

    code has changed after review

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
 import model.Candidate;
 import model.Cat;
 import model.Person;
