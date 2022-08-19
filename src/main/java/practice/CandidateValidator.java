ef3abe713983ce5bf643363c38471861e28a9ae9
Nata <natserbo@gmail.com>
2022-08-19 21:39:14 +0300

    mvn corrections

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,10 +4,11 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    private static final int MIN_VALID_AGE= 35;
+    private static final int MIN_VALID_AGE = 35;
     private static final int MIN_VALID_YEARS_LIVED_IN_UKRAINE = 10;
     private static final int FIRST_YEAR_IN_LIVE_PERIOD_INDEX = 0;
     private static final int SECOND_YEAR_IN_LIVE_PERIOD_INDEX = 1;
+
     @Override
     public boolean test(Candidate candidate) {
         String[] liveYears = candidate.getPeriodsInUkr().split("-");
