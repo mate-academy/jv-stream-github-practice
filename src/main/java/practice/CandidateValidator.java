a1ffeccecdebfb5c123af1182905d5c162298da1
Serhii Buria <sergeiburya@gmail.com>
2022-08-19 10:11:42 +0300

    fixed bugs at the direction of the mentor

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,19 +5,20 @@ import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_AGE = 35;
-    private static final int LIVES_IN_UKRAINE = 10;
-    private static final String NATIONALITY = "Ukrainian";
+    private static final int REQUIRED_AMOUNT_OF_YEARS_IN_COUNTRY = 10;
+    private static final String REQUIRED_NATIONALITY = "Ukrainian";
+    private static final String DATE_SEPARATOR = "-";
 
     @Override
     public boolean test(Candidate candidate) {
         return candidate.isAllowedToVote()
                 && candidate.getAge() >= MIN_AGE
-                && candidate.getNationality().equals(NATIONALITY)
-                && getYearsLivingInUkraine(candidate) >= LIVES_IN_UKRAINE;
+                && candidate.getNationality().equals(REQUIRED_NATIONALITY)
+                && getAmountOfYearsInCountry(candidate) >= REQUIRED_AMOUNT_OF_YEARS_IN_COUNTRY;
     }
 
-    private int getYearsLivingInUkraine(Candidate candidate) {
-        String[] years = candidate.getPeriodsInUkr().split("-");
+    private int getAmountOfYearsInCountry(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split(DATE_SEPARATOR);
         return Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
     }
 }
