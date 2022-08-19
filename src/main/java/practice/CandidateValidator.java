23b2fddc60d33e9a0c45c4d70db7c619dc5c4e34
Serhii Buria <sergeiburya@gmail.com>
2022-08-19 21:22:59 +0300

    changed method of checking amound of year in country

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,18 +7,23 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_AGE = 35;
     private static final int REQUIRED_AMOUNT_OF_YEARS_IN_COUNTRY = 10;
     private static final String REQUIRED_NATIONALITY = "Ukrainian";
-    private static final String DATE_SEPARATOR = "-";
+    private static final String PERIODS_SEPARATOR = "-";
+    private static final int START_PERIOD_INDEX = 0;
+    private static final int END_PERIOD_INDEX = 1;
 
     @Override
     public boolean test(Candidate candidate) {
         return candidate.isAllowedToVote()
                 && candidate.getAge() >= MIN_AGE
                 && candidate.getNationality().equals(REQUIRED_NATIONALITY)
-                && getAmountOfYearsInCountry(candidate) >= REQUIRED_AMOUNT_OF_YEARS_IN_COUNTRY;
+                && checkAmountOfYearsInCountry(candidate);
     }
 
-    private int getAmountOfYearsInCountry(Candidate candidate) {
-        String[] years = candidate.getPeriodsInUkr().split(DATE_SEPARATOR);
-        return Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
+    private boolean checkAmountOfYearsInCountry(Candidate candidate) {
+        return Integer.parseInt(candidate.getPeriodsInUkr()
+                .split(PERIODS_SEPARATOR)[END_PERIOD_INDEX])
+                        - Integer.parseInt(candidate.getPeriodsInUkr()
+                .split(PERIODS_SEPARATOR)[START_PERIOD_INDEX])
+                        >= REQUIRED_AMOUNT_OF_YEARS_IN_COUNTRY;
     }
 }
