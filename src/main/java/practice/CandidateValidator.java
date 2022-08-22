5e882dabeafe7f850d4048d55adaa786e62c2271
Vasyl Shevtsiv <vasyl.shevtsiv@gmail.com>
2022-08-22 14:36:16 +0300

    adding constants to candidate-validator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,16 +4,20 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private final String NATIONALITY = "Ukrainian";
+    private final int MIN_AGE_REQUIRED = 35;
+    private final int YEARS_OF_RESIDENCY_REQUIRED = 10;
+
     @Override
     public boolean test(Candidate candidate) {
         return candidate.isAllowedToVote()
-                && candidate.getNationality().equals("Ukrainian")
-                && candidate.getAge() >= 35
+                && candidate.getNationality().equals(NATIONALITY)
+                && candidate.getAge() >= MIN_AGE_REQUIRED
                 && Integer.parseInt(candidate.getPeriodsInUkr()
                 .substring(candidate.getPeriodsInUkr().indexOf("-") + 1))
                 - Integer.parseInt(candidate.getPeriodsInUkr()
                 .substring(0,candidate.getPeriodsInUkr().indexOf("-")))
-                >= 10;
+                >= YEARS_OF_RESIDENCY_REQUIRED;
     }
     //write your code here
 }
