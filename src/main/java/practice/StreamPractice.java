4ebbe3d852a845bb99dd271ebea8e2908873e0f2
Dmytro Dmytrenko <dmitrynetfx@gmail.com>
2022-08-22 09:21:26 +0300

    debuged code

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,17 +1,13 @@
 package practice;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
 import model.Candidate;
 import model.Cat;
 import model.Person;
-
 
 public class StreamPractice {
     /**
@@ -78,9 +74,9 @@ public class StreamPractice {
     public List<Person> getWorkablePeople(int fromAge, int femaleToAge,
                                           int maleToAge, List<Person> peopleList) {
         return peopleList.stream()
-                .filter(p -> p.getAge() > fromAge
-                        && ((p.getAge() < femaleToAge && p.getSex().equals(Person.Sex.WOMAN))
-                        || (p.getAge() < maleToAge && p.getSex().equals(Person.Sex.MAN))))
+                .filter(p -> p.getAge() >= fromAge
+                        && ((p.getAge() <= femaleToAge && p.getSex().equals(Person.Sex.WOMAN))
+                        || (p.getAge() <= maleToAge && p.getSex().equals(Person.Sex.MAN))))
                 .collect(Collectors.toList());
     }
 
@@ -92,7 +88,7 @@ public class StreamPractice {
     public List<String> getCatsNames(List<Person> peopleList, int femaleAge) {
 
         return peopleList.stream()
-                .filter(p -> p.getSex().equals(Person.Sex.WOMAN) && p.getAge() == femaleAge)
+                .filter(p -> p.getSex().equals(Person.Sex.WOMAN) && p.getAge() >= femaleAge)
                 .flatMap(p -> p.getCats().stream())
                 .map(Cat::getName)
                 .collect(Collectors.toList());
