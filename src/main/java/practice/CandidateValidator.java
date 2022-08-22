b967f9841732f68662a534fbe4703894a4ee9952
asha <ashajanebrown@gmail.com>
2022-08-22 18:17:28 +0300

    implemented Stream API methods, implemented CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator<T extends Candidate> implements Predicate<T> {
+    private static final int MIN_AGE = 35;
+    private static final String REQUIRED_NATIONALITY = "Ukrainian";
+    private static final int MIN_PERIOD = 10;
+
+    @Override
+    public boolean test(T candidate) {
+        int startPointOfPeriod = Integer.parseInt(candidate.getPeriodsInUkr().split("-")[0]);
+        int endPointOfPeriod = Integer.parseInt(candidate.getPeriodsInUkr().split("-")[1]);
+
+        return candidate.getAge() >= MIN_AGE && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(REQUIRED_NATIONALITY)
+                && endPointOfPeriod - startPointOfPeriod >= MIN_PERIOD;
+    }
 }
