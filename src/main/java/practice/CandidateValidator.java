640fdafe0fb968dd1cde3ccfa0a51c07e104dd42
max-bolotin <realprof.mx@gmail.com>
2022-08-23 23:31:06 +0300

    all tests passed, checkstyle ok

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,15 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator<T> implements Predicate<T> {
+    @Override
+    public boolean test(T t) {
+        Candidate candidate = (Candidate) t;
+        int yearsInUkraine = Integer.parseInt(candidate.getPeriodsInUkr().substring(5, 9))
+                - Integer.parseInt(candidate.getPeriodsInUkr().substring(0, 4));
+        return candidate.getAge() >= 35 && candidate.isAllowedToVote()
+                && "Ukrainian".equals(candidate.getNationality()) && yearsInUkraine >= 10;
+    }
 }
