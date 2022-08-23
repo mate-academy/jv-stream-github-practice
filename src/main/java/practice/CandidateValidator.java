3c1ad54328693fde70a7dd8c8fa86a836aa15b55
Anna Popova <anitapopova1996@gmail.com>
2022-08-23 17:04:57 +0300

    Implemented methods in StreamPractice. Implemented by CandidateValidator Predicate and implemented the logic into test method.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,25 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int YEARS_SPENT_IN_UA = 10;
+    private static final int YEAR_FROM = 0;
+    private static final int YEAR_TO = 1;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MIN_AGE && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && totalYearsSpentInUA(candidate.getPeriodsInUkr()) >= YEARS_SPENT_IN_UA;
+    }
+
+    private int totalYearsSpentInUA(String periodsInUA) {
+        String[] splittedYears = periodsInUA.split("-");
+        return Integer.parseInt(splittedYears[YEAR_TO])
+                - Integer.parseInt(splittedYears[YEAR_FROM]);
+    }
 }
