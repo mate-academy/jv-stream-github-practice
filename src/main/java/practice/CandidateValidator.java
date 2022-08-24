249303f681b65e960df588af6937f1bcf9d18de5
Anatoliy Olkhovskiy <Anatoltbcm@gmail.com>
2022-08-24 22:39:12 +0300

    add DATE_SEPARATOR constant

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -9,11 +9,12 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final String NATIONALITY = "Ukrainian";
     private static final int INDEX_START_LIVE_IN_UKRAIN = 0;
     private static final int INDEX_END_LIVE_IN_UKRAIN = 1;
+    private static final String DATE_SEPARATOR = "-";
 
     //write your code here
     @Override
     public boolean test(Candidate candidate) {
-        String[] liveFromTo = candidate.getPeriodsInUkr().split("-");
+        String[] liveFromTo = candidate.getPeriodsInUkr().split(DATE_SEPARATOR);
         int peroid = Integer.parseInt(liveFromTo[INDEX_END_LIVE_IN_UKRAIN])
                 - Integer.parseInt(liveFromTo[INDEX_START_LIVE_IN_UKRAIN]);
         return candidate.getAge() >= MIN_AGE
