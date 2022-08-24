f45af65ccb153e83d93a36d4492932b50e871715
IraHurko <iragurko19@gmail.com>
2022-08-25 01:49:05 +0300

    Added some constants

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,12 +7,15 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int PERMISSIBLE_AGE_FOR_CANDIDATES = 35;
     private static final int MIN_PERIOD_IN_UKRAINE = 10;
     private static final String EXPECTED_NATIONALITY = "Ukrainian";
+    private static final String SEPARATOR_BETWEEN_YEARS = "-";
+    private static final int INDEX_OF_FIRST_ELEMENT = 0;
+    private static final int INDEX_OF_SECOND_ELEMENT = 1;
 
     @Override
     public boolean test(Candidate candidate) {
-        String[] periods = candidate.getPeriodsInUkr().split("-");
-        int yearOfArrival = Integer.parseInt(periods[0]);
-        int currentYear = Integer.parseInt(periods[1]);
+        String[] periods = candidate.getPeriodsInUkr().split(SEPARATOR_BETWEEN_YEARS);
+        int yearOfArrival = Integer.parseInt(periods[INDEX_OF_FIRST_ELEMENT]);
+        int currentYear = Integer.parseInt(periods[INDEX_OF_SECOND_ELEMENT]);
         return candidate.getAge() >= PERMISSIBLE_AGE_FOR_CANDIDATES
                 && candidate.getNationality().equals(EXPECTED_NATIONALITY)
                 && currentYear - yearOfArrival >= MIN_PERIOD_IN_UKRAINE
