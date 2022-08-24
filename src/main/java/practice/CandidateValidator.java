a55e2b9d339fb1746478b9b460f929567fc53829
Alex Alexandrov <alexandrov.alex.al@gmail.com>
2022-08-24 18:30:42 +0300

    The "body-solutions" were added to every method.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,25 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int REQUIRED_CANDIDATE_AGE = 35;
+    private static final int PERIOD_FROM_INDEX = 0;
+    private static final int PERIOD_TO_INDEX = 1;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] period = candidate.getPeriodsInUkr().split("-");
+        int periodInUkraine = Integer.parseInt(period[PERIOD_TO_INDEX])
+                - Integer.parseInt(period[PERIOD_FROM_INDEX]);
+        if (candidate.getAge() >= REQUIRED_CANDIDATE_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && periodInUkraine >= 10) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
