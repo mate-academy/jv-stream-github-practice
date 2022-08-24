703a9a2c19a3144190d83edaa7c1c52662caa4ee
Andrii Nesterenko <nesterenkoandrii23@gmail.com>
2022-08-24 21:02:18 +0300

    added and changed constant names in CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,18 +5,19 @@ import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int ELIGIBLE_AGE = 35;
-    private static final int INDEX_OF_AGE_FROM = 0;
-    private static final int INDEX_OF_AGE_TO = 1;
+    private static final int INDEX_OF_YEAR_FROM = 0;
+    private static final int INDEX_OF_YEAR_TO = 1;
     private static final int ELIGIBLE_PERIOD = 10;
     private static final String ELIGIBLE_NATIONALITY = "Ukrainian";
+    private static final String DATA_SPLITTER = "-";
 
     @Override
     public boolean test(Candidate candidate) {
-        String[] temp = candidate.getPeriodsInUkr().split("-");
+        String[] temp = candidate.getPeriodsInUkr().split(DATA_SPLITTER);
         return candidate.isAllowedToVote()
                 && candidate.getAge() >= ELIGIBLE_AGE
-                && Integer.parseInt(temp[INDEX_OF_AGE_TO])
-                - Integer.parseInt(temp[INDEX_OF_AGE_FROM]) >= ELIGIBLE_PERIOD
+                && Integer.parseInt(temp[INDEX_OF_YEAR_TO])
+                - Integer.parseInt(temp[INDEX_OF_YEAR_FROM]) >= ELIGIBLE_PERIOD
                 && candidate.getNationality().equals(ELIGIBLE_NATIONALITY);
     }
 }
