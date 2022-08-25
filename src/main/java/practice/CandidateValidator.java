76f2a02222d082181ad965bf4920e86291465035
max-bolotin <realprof.mx@gmail.com>
2022-08-25 15:51:10 +0300

    replaced indexes and hyphen with constants

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,13 +5,17 @@ import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
     private static final String NATIONALITY = "Ukrainian";
+    private static final String HYPHEN = "-";
+    private static final int YEAR_FROM_INDEX = 0;
+    private static final int YEAR_UNTIL_INDEX = 1;
     private static final int MIN_AGE = 35;
     private static final int MIN_YEARS_IN_UKRAINE = 10;
 
     @Override
     public boolean test(Candidate candidate) {
-        String[] years = candidate.getPeriodsInUkr().split("-");
-        int yearsInUkraine = Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
+        String[] years = candidate.getPeriodsInUkr().split(HYPHEN);
+        int yearsInUkraine = Integer.parseInt(years[YEAR_UNTIL_INDEX])
+                - Integer.parseInt(years[YEAR_FROM_INDEX]);
         return candidate.getAge() >= MIN_AGE && candidate.isAllowedToVote()
                 && NATIONALITY.equals(candidate.getNationality())
                 && yearsInUkraine >= MIN_YEARS_IN_UKRAINE;
