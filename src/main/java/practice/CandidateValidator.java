908f851835a88530a28702ebf6ca29f86e8ca76d
Myroslav_Voskoboinyk <maestro.of.pain@gmail.com>
2022-08-25 20:30:04 +0300

    test passed

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,24 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int AGE = 35;
+    private static final int PERIOD_IN_UKRAINE = 10;
+    private static final int YEAR_FROM_INDEX = 0;
+    private static final int YEAR_TO_INDEX = 1;
+    private static final String PERIOD_REGEX = "-";
+    private static final String NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split(PERIOD_REGEX);
+        return candidate.getAge() >= AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && Integer.parseInt(years[YEAR_TO_INDEX])
+                - Integer.parseInt(years[YEAR_FROM_INDEX])
+                >= PERIOD_IN_UKRAINE;
+    }
 }
