dcbb2a3ce3bf2e513ac5680081e77deb2094f351
Yaroslav Shkvarla <slav.shkvarlo@gmail.com>
2022-08-25 16:03:04 +0300

    complete task StreamPractice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,19 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int FROM_YEAR_INDEX = 0;
+    private static final int TO_YEAR_INDEX = 1;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] splitedYears = candidate.getPeriodsInUkr().split("-");
+        return candidate.isAllowedToVote()
+                && candidate.getAge() >= 35
+                && candidate.getNationality().equals("Ukrainian")
+                && (Integer.parseInt(splitedYears[TO_YEAR_INDEX])
+                - Integer.parseInt(splitedYears[FROM_YEAR_INDEX])) >= 10;
+    }
 }
