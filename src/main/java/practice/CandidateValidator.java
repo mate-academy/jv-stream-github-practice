c1afaa70fe5242d17b72e68a2df23376d65898b6
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-08-25 16:40:15 +0300

    add constants in CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,12 +4,16 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MINIMUM_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int YEARS_IN_UKR = 10;
+
     @Override
     public boolean test(Candidate candidate) {
-        return candidate.getAge() >= 35
-                && candidate.getNationality().equals("Ukrainian")
+        return candidate.getAge() >= MINIMUM_AGE
+                && candidate.getNationality().equals(NATIONALITY)
                 && Integer.parseInt(candidate.getPeriodsInUkr().split("-")[1])
-                - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[0]) >= 10
+                - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[0]) >= YEARS_IN_UKR
                 && candidate.isAllowedToVote();
     }
 }
