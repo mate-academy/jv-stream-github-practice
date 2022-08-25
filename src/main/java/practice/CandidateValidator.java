f1b8e08e74d6f4155695d9f350b6f2b19f33c6f2
ArturZhadan <artur.zhadan.17@gmail.com>
2022-08-26 00:01:32 +0300

    added constants to CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,14 +4,17 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int CANDIDATE_AGE = 35;
+    private static final String CANDIDATE_NATIONALITY = "Ukrainian";
+    private static final int PERIODS_IN_UKRAINE = 10;
 
     @Override
     public boolean test(Candidate candidate) {
         String[] periods = candidate.getPeriodsInUkr().split("-");
         int period = Integer.parseInt(periods[1]) - Integer.parseInt(periods[0]);
-        return candidate.getAge() >= 35
-                && candidate.getNationality().equals("Ukrainian")
+        return candidate.getAge() >= CANDIDATE_AGE
+                && candidate.getNationality().equals(CANDIDATE_NATIONALITY)
                 && candidate.isAllowedToVote()
-                && period >= 10;
+                && period >= PERIODS_IN_UKRAINE;
     }
 }
