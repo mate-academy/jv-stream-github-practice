0a8eab951d7a2aee6773fab0af9c154c63ae3697
Alex Alexandrov <alexandrov.alex.al@gmail.com>
2022-08-25 15:01:26 +0300

    Codestyle fix.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -9,10 +9,11 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final String REQUIRED_CANDIDATE_NATIONALITY = "Ukrainian";
     private static final int PERIOD_FROM_INDEX = 0;
     private static final int PERIOD_TO_INDEX = 1;
+    private static final String STRING_SEPARATOR = "-";
 
     @Override
     public boolean test(Candidate candidate) {
-        String[] period = candidate.getPeriodsInUkr().split("-");
+        String[] period = candidate.getPeriodsInUkr().split(STRING_SEPARATOR);
         int periodInUkraine = Integer.parseInt(period[PERIOD_TO_INDEX])
                 - Integer.parseInt(period[PERIOD_FROM_INDEX]);
         return candidate.getAge() >= REQUIRED_CANDIDATE_AGE
