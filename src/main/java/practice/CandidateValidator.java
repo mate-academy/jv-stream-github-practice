3b512c1815502f8edb26cf07fd2a2a512f08e95f
Aptypio13 <aptypio668@gmail.com>
2022-08-25 14:35:37 +0300

    create constant regex

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,12 +7,13 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_AGE = 35;
     private static final int MIN_PERIOD_IN_UKR = 10;
     private static final String NATIONALITY = "Ukrainian";
+    private static final String REGEX = "-";
 
     @Override
     public boolean test(Candidate candidate) {
         int periodInUkr =
-                Integer.parseInt(candidate.getPeriodsInUkr().split("-")[1])
-                - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[0]);
+                Integer.parseInt(candidate.getPeriodsInUkr().split(REGEX)[1])
+                - Integer.parseInt(candidate.getPeriodsInUkr().split(REGEX)[0]);
         return candidate.getAge() >= MIN_AGE
                 && candidate.getNationality().equals(NATIONALITY)
                 && candidate.isAllowedToVote()
