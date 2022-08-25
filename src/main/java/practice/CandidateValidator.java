dda7e622e17f9495eeeef6b66e93c2b3fd7bf74a
Alex Alexandrov <alexandrov.alex.al@gmail.com>
2022-08-25 13:15:48 +0300

    Made a constant. Refactored Predicate method in CandidateValidator.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,6 +5,7 @@ import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int REQUIRED_CANDIDATE_AGE = 35;
+    private static final String REQUIRED_CANDIDATE_NATIONALITY = "Ukrainian";
     private static final int PERIOD_FROM_INDEX = 0;
     private static final int PERIOD_TO_INDEX = 1;
 
@@ -13,13 +14,9 @@ public class CandidateValidator implements Predicate<Candidate> {
         String[] period = candidate.getPeriodsInUkr().split("-");
         int periodInUkraine = Integer.parseInt(period[PERIOD_TO_INDEX])
                 - Integer.parseInt(period[PERIOD_FROM_INDEX]);
-        if (candidate.getAge() >= REQUIRED_CANDIDATE_AGE
+        return (candidate.getAge() >= REQUIRED_CANDIDATE_AGE
                 && candidate.isAllowedToVote()
-                && candidate.getNationality().equals("Ukrainian")
-                && periodInUkraine >= 10) {
-            return true;
-        } else {
-            return false;
-        }
+                && candidate.getNationality().equals(REQUIRED_CANDIDATE_NATIONALITY)
+                && periodInUkraine >= 10);
     }
 }
