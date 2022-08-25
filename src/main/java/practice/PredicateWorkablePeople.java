b213f2bedeccb319a19d42a92d76bbe2ed131f0f
vadimkik2 <vadimkik@ukr.net>
2022-08-25 20:14:51 +0300

    Added solution.

---
+++ src/main/java/practice/PredicateWorkablePeople.java
@@ -0,0 +1,26 @@
+package practice;
+
+import java.util.function.Predicate;
+import model.Person;
+
+public class PredicateWorkablePeople implements Predicate<Person> {
+    private int fromAge;
+    private int maleToAge;
+    private int femaleToAge;
+
+    public PredicateWorkablePeople(int fromAge, int maleToAge, int femaleToAge) {
+        this.fromAge = fromAge;
+        this.maleToAge = maleToAge;
+        this.femaleToAge = femaleToAge;
+    }
+
+    @Override
+    public boolean test(Person person) {
+        return person.getSex() == Person.Sex.MAN
+                && person.getAge() >= fromAge
+                && person.getAge() <= maleToAge
+                || person.getSex() == Person.Sex.WOMAN
+                && person.getAge() >= fromAge
+                && person.getAge() <= femaleToAge;
+    }
+}
