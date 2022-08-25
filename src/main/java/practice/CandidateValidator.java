8d64042e12fae1560c9d79e9782d89491a527e25
Volodymyr Kachan <vladimirkachan1996@gmail.com>
2022-08-25 22:27:20 +0300

    my hw jv-stream-github-practice v01

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,28 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.Comparator;
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] yearLiveInUkraine = candidate.getPeriodsInUkr().split("-");
+        int periodsInUkr = Integer.parseInt(yearLiveInUkraine[1])
+                - Integer.parseInt(yearLiveInUkraine[0]);
+        if (candidate.getAge() >= 35 && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian") && periodsInUkr > 10) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    static class CandidateComparator implements Comparator<Candidate> {
+        @Override
+        public int compare(Candidate candidate1, Candidate candidate2) {
+            return candidate1.getName().toUpperCase().compareTo(candidate2.getName().toUpperCase());
+        }
+    }
 }
