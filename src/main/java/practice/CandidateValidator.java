c8cece7e151a7dea3c2c07ea0d506ba6dfbcbe96
Alexhqolma <66127994+Alexhqolma@users.noreply.github.com>
2022-08-25 12:16:25 +0300

    Changed, I just understand indexes as a markers )

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,13 +7,13 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_AGE = 35;
     private static final int MIN_PERIOD = 10;
     private static final String NATIONALITY = "Ukrainian";
-    private static final int INDEX_0 = 0;
-    private static final int INDEX_1 = 1;
+    private static final int FIRST_YEAR_POSITION = 0;
+    private static final int LAST_YEAR_POSITION = 1;
 
     @Override
     public boolean test(Candidate candidate) {
         String[] period = candidate.getPeriodsInUkr().split("-");
-        int periodResult = Integer.parseInt(period[INDEX_1]) - Integer.parseInt(period[INDEX_0]);
+        int periodResult = Integer.parseInt(period[LAST_YEAR_POSITION]) - Integer.parseInt(period[FIRST_YEAR_POSITION]);
         return candidate.getAge() >= MIN_AGE
                 && candidate.isAllowedToVote()
                 && candidate.getNationality().equals(NATIONALITY)
