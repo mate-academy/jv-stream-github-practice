4a13d64be32e78692c2b4ddee68a184d58a0905a
Heorhiy Nezbrytskiy <georgiy.nezbritskiy@gmail.com>
2022-08-25 16:18:22 +0300

    fix mistakes using Oleg's tips

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -8,12 +8,18 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_CANDIDATE_AGE = 35;
     private static final int MIN_AMOUNT_YEAR_IN_UKRAINE = 10;
     private static final String NATIONALITY = "Ukrainian";
+    private static final String SEPARATOR = "-";
+    private static final int INDEX_START_LIVING_IN_UKRAIN = 0;
+    private static final int INDEX_FINISH_LIVING_IN_UKRAIN = 1;
 
     @Override
     public boolean test(Candidate candidate) {
-        String[] periodString = candidate.getPeriodsInUkr().split("-");
-        int[] periodNumber = {Integer.parseInt(periodString[0]), Integer.parseInt(periodString[1])};
-        int period = periodNumber[1] - periodNumber[0];
+        String[] periodString = candidate.getPeriodsInUkr().split(SEPARATOR);
+        int startLivinInUkraine = Integer
+                .parseInt(periodString[INDEX_START_LIVING_IN_UKRAIN]);
+        int finishLiviningInUkrkaine = Integer
+                .parseInt(periodString[INDEX_FINISH_LIVING_IN_UKRAIN]);
+        int period = finishLiviningInUkrkaine - startLivinInUkraine;
         return candidate.getAge() >= MIN_CANDIDATE_AGE
                 && candidate.isAllowedToVote()
                 && candidate.getNationality().equals(NATIONALITY)
--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -7,6 +7,7 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import model.Candidate;
+import model.Cat;
 import model.Person;
 
 public class StreamPractice {
@@ -19,8 +20,7 @@ public class StreamPractice {
      */
     public int findMinEvenNumber(List<String> numbers) {
         return numbers.stream()
-                .map(n -> n.split(","))
-                .flatMap(Arrays::stream)
+                .flatMap(n -> Arrays.stream(n.split(",")))
                 .mapToInt(Integer::parseInt)
                 .filter(n -> n % 2 == 0)
                 .min()
@@ -75,16 +75,12 @@ public class StreamPractice {
      */
     public List<Person> getWorkablePeople(int fromAge, int femaleToAge,
                                           int maleToAge, List<Person> peopleList) {
-        Predicate<Person> predicate = new Predicate<Person>() {
-            @Override
-            public boolean test(Person person) {
-                return person.getSex() == Person.Sex.MAN
-                        ? person.getAge() >= fromAge && person.getAge() <= maleToAge
-                        : person.getAge() >= fromAge && person.getAge() <= femaleToAge;
-            }
-        };
         return peopleList.stream()
-                .filter(predicate)
+                .filter((person -> person.getAge() >= fromAge
+                        && ((person.getSex() == Person.Sex.MAN
+                        && person.getAge() <= maleToAge)
+                        || (person.getSex() == Person.Sex.WOMAN
+                        && person.getAge() <= femaleToAge))))
                 .collect(Collectors.toList());
     }
 
@@ -96,10 +92,8 @@ public class StreamPractice {
     public List<String> getCatsNames(List<Person> peopleList, int femaleAge) {
         return peopleList.stream()
                 .filter(p -> p.getSex() == Person.Sex.WOMAN && p.getAge() >= femaleAge)
-                .map(w -> w.getCats())
-                .flatMap(l -> l.stream())
-                .map(c -> c.getName())
-                .distinct()
+                .flatMap(l -> l.getCats().stream())
+                .map(Cat::getName)
                 .collect(Collectors.toList());
     }
 
@@ -118,8 +112,8 @@ public class StreamPractice {
     public List<String> validateCandidates(List<Candidate> candidates) {
         CandidateValidator candidateValidator = new CandidateValidator();
         return candidates.stream()
-                .filter(c -> candidateValidator.test(c))
-                .map(c -> c.getName())
+                .filter(candidateValidator)
+                .map(Candidate::getName)
                 .sorted()
                 .collect(Collectors.toList());
     }
