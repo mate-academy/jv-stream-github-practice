a32b983f26a6eceb8d53fd793d5821ea318c56c8
EagleAndrew <tortloveduck4@gmail.com>
2022-08-26 13:59:01 +0200

    getWorkablePeople() left only one fromAge

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -39,10 +39,11 @@ public class StreamPractice {
     public List<Person> getWorkablePeople(int fromAge, int femaleToAge,
                                           int maleToAge, List<Person> peopleList) {
         return peopleList.stream()
-                .filter(p -> p.getSex() == Person.Sex.MAN && p.getAge() >= fromAge
+                .filter(p -> p.getAge() >= fromAge
+                        && (p.getSex() == Person.Sex.MAN
                         && p.getAge() <= maleToAge
-                        || p.getSex() == Person.Sex.WOMAN && p.getAge() >= fromAge
-                        && p.getAge() <= femaleToAge)
+                        || p.getSex() == Person.Sex.WOMAN
+                        && p.getAge() <= femaleToAge))
                 .collect(Collectors.toList());
     }
 
