30e57ca7b60b5b8b304f28e31d3bca44c46b868f
“Bohdan Stasiv“ <badyaxa@gmail.com>
2022-08-26 14:08:35 +0300

    added constant YEAR_SEPARATOR

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,13 +6,14 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int YEAR_FROM_POSITION = 0;
     private static final int YEAR_TOO_POSITION = 1;
+    private static final String YEAR_SEPARATOR = "-";
     private static final int YEARS_IN_UKRAINE = 10;
     private static final int MINIMUM_AGE = 35;
     private static final String NATIONALITY = "Ukrainian";
 
     @Override
     public boolean test(Candidate candidate) {
-        String[] years = candidate.getPeriodsInUkr().split("-");
+        String[] years = candidate.getPeriodsInUkr().split(YEAR_SEPARATOR);
         int livedInCountry = Integer.parseInt(years[YEAR_TOO_POSITION])
                 - Integer.parseInt(years[YEAR_FROM_POSITION]);
         return candidate.getAge() >= MINIMUM_AGE
