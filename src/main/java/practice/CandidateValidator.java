d10d5e73bbea9c3c33f93463fde3d684ec4e2736
Ilja Nekrituk <Nekrit99@gmail.com>
2022-08-26 13:03:31 +0300

    Stream blow my mind, but I do it!

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_ALLOWED_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int MIN_YEARS_IN_UA = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] yearsInUa = candidate.getPeriodsInUkr().split("-");
+        int firstYear = Integer.parseInt(yearsInUa[0]);
+        int lastYear = Integer.parseInt(yearsInUa[yearsInUa.length - 1]);
+
+        return candidate.getAge() >= MIN_ALLOWED_AGE && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && lastYear - firstYear >= MIN_YEARS_IN_UA;
+    }
 }
