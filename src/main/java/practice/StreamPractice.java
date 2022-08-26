38884e43ae6e1fc00430fd2831d93eac57b3fefc
LubusUzh <lubus.uzh@gmail.com>
2022-08-26 10:09:09 +0300

    Add: implementation of findMinEvenNumber

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,7 +1,10 @@
 package practice;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.NoSuchElementException;
+
 import model.Candidate;
 import model.Person;
 
@@ -14,7 +17,14 @@ public class StreamPractice {
      * "Can't get min value from list: < Here is our input 'numbers' >"
      */
     public int findMinEvenNumber(List<String> numbers) {
-        return 0;
+        return numbers.stream()
+                .map(s -> s.split(","))
+                .flatMap(s -> Arrays.stream(s))
+                .mapToInt(s -> Integer.valueOf(s))
+                .filter(n -> n % 2 == 0)
+                .min()
+                .orElseThrow(() -> new NoSuchElementException(
+                        "Can't get min value from list: " + numbers));
     }
 
     /**
