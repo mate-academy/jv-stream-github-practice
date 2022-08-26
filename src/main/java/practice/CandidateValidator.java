de89bfcb2bcffbfbd1e5c6a10f6530e3c5cc858d
Vasyl Pankiv <wasilius2@gmail.com>
2022-08-26 08:57:12 +0300

    implement Predicate interface in CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,25 @@
 package practice;
 
-public class CandidateValidator {
+import model.Candidate;
+
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final String YEARS_SEPARATOR = "-";
+    private static final int START_YEAR_INDEX = 0;
+    private static final int FINISH_YEAR_INDEX = 1;
+    private static final int MIN_AMOUNT_YEARS_IN_UKRAINE = 10;
+    private static final int MIN_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] periodInUkraineFromTo = candidate.getPeriodsInUkr().split(YEARS_SEPARATOR);
+        int period = Integer.parseInt(periodInUkraineFromTo[START_YEAR_INDEX])
+                - Integer.parseInt(periodInUkraineFromTo[FINISH_YEAR_INDEX]);
+
+        return candidate.getAge() >= MIN_AGE
+                && period >= MIN_AMOUNT_YEARS_IN_UKRAINE
+                && candidate.getNationality().equals(NATIONALITY);
+    }
     //write your code here
 }
