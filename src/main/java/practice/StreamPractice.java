c0aef9edeb4300f61617d7e4d9f82db21e11ce65
Myroslav_Voskoboinyk <maestro.of.pain@gmail.com>
2022-08-26 13:12:35 +0300

    ternary expression in getWorkablePeople method  was replaced to boolean expression

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -71,9 +71,8 @@ public class StreamPractice {
                                           int maleToAge, List<Person> peopleList) {
         return peopleList.stream()
                 .filter(p -> p.getAge() >= fromAge
-                        && (p.getSex() == Person.Sex.MAN
-                        ? p.getAge() <= maleToAge
-                        : p.getAge() <= femaleToAge))
+                        && (p.getSex() == Person.Sex.MAN && p.getAge() <= maleToAge
+                        || p.getSex() == Person.Sex.WOMAN && p.getAge() <= femaleToAge))
                 .collect(Collectors.toList());
     }
 
