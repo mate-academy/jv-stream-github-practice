93c05effcaeb92bb547464faaf3efb54e903eede
EagleAndrew <tortloveduck4@gmail.com>
2022-08-26 11:47:04 +0200

    StreamPractice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,18 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.Arrays;
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_YEARS = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        int[] period = Arrays.stream(candidate.getPeriodsInUkr().split("-"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int isMoreThenTen = period[1] - period[0];
+        return isMoreThenTen >= MIN_YEARS;
+    }
 }
