2832ca2adbaf87c8cda526865eb5cab5a0e7d61b
azarovartur <99978155+azarovartur@users.noreply.github.com>
2022-08-28 00:13:38 +0300

    Update src/main/java/practice/CandidateValidator.java
    Co-authored-by: Anastasia Len <81781479+aaananas@users.noreply.github.com>

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,10 +6,11 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     @Override
     public boolean test(Candidate candidate) {
-        return candidate.getAge() >= 35
-                && candidate.getNationality().equals("Ukrainian")
+        String[] splitted = candidate.getPeriodsInUkr().split("-");
+        return candidate.getAge() >= REQUIRED_MIN_AGE
+                && candidate.getNationality().equals(REQUIRED_NATIONALITY)
                 && candidate.isAllowedToVote()
-                && (Integer.parseInt(candidate.getPeriodsInUkr().split("-")[1])
-                - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[0]) >= 10);
+                && (Integer.parseInt(splitted[PERIOD_TO_INDEX])
+                - Integer.parseInt(splitted[PERIOD_FROM_INDEX]) >= REUIRED_MIN_PERIOD_IN_COUNTRY);
     }
 }
