05f83e13497dbb3634d2a49ffa9d4f8a295c12ea
Azarov Artur <azarovartyr@gmail.com>
2022-08-28 00:21:50 +0300

    fixed all errors mentioned

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,6 +4,12 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int REQUIRED_MIN_AGE = 35;
+    private static final int PERIOD_TO_INDEX = 1;
+    private static final int PERIOD_FROM_INDEX = 0;
+    private static final int REQUIRED_MIN_PERIOD_IN_COUNTRY = 10;
+    private static final String REQUIRED_NATIONALITY = "Ukrainian";
+
     @Override
     public boolean test(Candidate candidate) {
         String[] splitted = candidate.getPeriodsInUkr().split("-");
@@ -11,6 +17,6 @@ public class CandidateValidator implements Predicate<Candidate> {
                 && candidate.getNationality().equals(REQUIRED_NATIONALITY)
                 && candidate.isAllowedToVote()
                 && (Integer.parseInt(splitted[PERIOD_TO_INDEX])
-                - Integer.parseInt(splitted[PERIOD_FROM_INDEX]) >= REUIRED_MIN_PERIOD_IN_COUNTRY);
+                - Integer.parseInt(splitted[PERIOD_FROM_INDEX]) >= REQUIRED_MIN_PERIOD_IN_COUNTRY);
     }
 }
