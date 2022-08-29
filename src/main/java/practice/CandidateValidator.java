e18dcd44843b19135dde94ff067b52c0c08302c2
Alina Nikiforova <0500302717a@gmail.com>
2022-08-29 20:32:40 +0200

    change StreamPractice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,20 +4,20 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    private static final int MIN_AGE = 35;
-    private static final String NATIONALITY = "Ukrainian";
-    private static final int MIN_PERIOD_IN_COUNTRY = 10;
-    private static final int FROM_INDEX = 0;
-    private static final int TO_INDEX = 1;
+    private static final int REQUIRED_MIN_AGE = 35;
+    private static final String REQUIRED_NATIONALITY = "Ukrainian";
+    private static final int REQUIRED_MIN_PERIOD_IN_COUNTRY = 10;
+    private static final int PERIOD_FROM_INDEX = 0;
+    private static final int PERIOD_TO_INDEX = 1;
 
     @Override
     public boolean test(Candidate candidate) {
         String[] splitPeriod = candidate.getPeriodsInUkr().split("-");
-        int period = Integer.parseInt(splitPeriod[TO_INDEX])
-                - Integer.parseInt(splitPeriod[FROM_INDEX]);
-        return candidate.getAge() >= MIN_AGE
+        int period = Integer.parseInt(splitPeriod[PERIOD_TO_INDEX])
+                - Integer.parseInt(splitPeriod[PERIOD_FROM_INDEX]);
+        return candidate.getAge() >= REQUIRED_MIN_AGE
                 && candidate.isAllowedToVote()
-                && candidate.getNationality().equals(NATIONALITY)
-                && period >= MIN_PERIOD_IN_COUNTRY;
+                && candidate.getNationality().equals(REQUIRED_NATIONALITY)
+                && period >= REQUIRED_MIN_PERIOD_IN_COUNTRY;
     }
 }
