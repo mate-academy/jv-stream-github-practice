ccc16d7b5156b7892bcfb611da40f8f4214f2084
Andriy Linkov <linkoff.as@gmail.com>
2022-09-07 16:16:28 +0300

    Refactored code

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -8,18 +8,20 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int REQUIRED_PERIOD_IN_COUNTRY = 10;
     private static final String REQUIRED_NATIONALITY = "Ukrainian";
     private static final String SPLIT_SIGN = "-";
+    private static final int IN_COUNTRY_TO_PERIOD = 1;
+    private static final int IN_COUNTRY_FROM_PERIOD = 0;
 
     @Override
     public boolean test(Candidate candidate) {
         return candidate.getAge() >= MIN_AGE
                 && candidate.isAllowedToVote()
-                && candidate.getNationality() == REQUIRED_NATIONALITY
+                && REQUIRED_NATIONALITY.equals(candidate.getNationality())
                 && getPeriodsInUkr(candidate) >= REQUIRED_PERIOD_IN_COUNTRY;
     }
 
     private int getPeriodsInUkr(Candidate candidate) {
         String[] splitedData = candidate.getPeriodsInUkr().split(SPLIT_SIGN);
-        return Integer.parseInt(splitedData[splitedData.length - 1])
-                - Integer.parseInt(splitedData[0]);
+        return Integer.parseInt(splitedData[IN_COUNTRY_TO_PERIOD])
+                - Integer.parseInt(splitedData[IN_COUNTRY_FROM_PERIOD]);
     }
 }
