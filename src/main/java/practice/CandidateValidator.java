d126cfbbb1b0d354d57414d150d1214e12db901f
Oleksandr Gotin <alekander.gotin@gmail.com>
2022-09-17 19:19:32 +0300

    task done

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,24 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int AGE_MORE_THAN = 35;
+    private static final int RESIDENCE_MORE_THAN = 10;
+    private static final String BELONGING_TO_NATIONALITY = "UKRAINIAN";
+    private static final String SEPARATOR_SIGN = "-";
+    private static final byte PERIOD_TO_INDEX = 1;
+    private static final byte PERIOD_FROM_INDEX = 0;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] splitted = candidate.getPeriodsInUkr().split(SEPARATOR_SIGN);
+        int period = Integer.parseInt(splitted[PERIOD_TO_INDEX])
+                - Integer.parseInt(splitted[PERIOD_FROM_INDEX]);
+        return candidate.isAllowedToVote()
+                && candidate.getAge() >= AGE_MORE_THAN
+                && candidate.getNationality().equals(BELONGING_TO_NATIONALITY)
+                && period >= RESIDENCE_MORE_THAN;
+    }
 }
