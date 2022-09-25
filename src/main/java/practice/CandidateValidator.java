2adf20159934d0ea53dc2515ddd3e829638fb422
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-09-25 14:06:47 +0300

    Create custom predicate

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int AGE_MIN = 35;
+    private static final int PERIOD = 10;
+
+    @Override
+
+    public boolean test(final Candidate candidate) {
+        String[] fromToYearsInUkraine = candidate.getPeriodsInUkr().split("-");
+        int periodInUkraine = Integer.parseInt(fromToYearsInUkraine[1])
+                - Integer.parseInt(fromToYearsInUkraine[0]);
+        return candidate.getAge() >= AGE_MIN
+                && candidate.getNationality().equals("Ukrainian")
+                && candidate.isAllowedToVote()
+                && periodInUkraine >= PERIOD;
+    }
 }
