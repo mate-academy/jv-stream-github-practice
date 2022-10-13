6d404509d1c235f9f2ea4e40c6f10a18b5cbabe0
Ivan Litvynets <Litvyvanya@gmail.com>
2022-10-13 19:06:40 +0200

    Stream practice (corrected)

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,8 +1,6 @@
 package practice;
 
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
@@ -21,11 +19,10 @@ public class StreamPractice {
      */
     public int findMinEvenNumber(List<String> numbers) {
         return numbers.stream()
-                .map(n -> n.split(","))
-                .flatMap(Arrays::stream)
+                .flatMap(n -> Arrays.stream(n.split(",")))
                 .map(Integer::parseInt)
                 .filter(n -> n % 2 == 0)
-                .min(Comparator.comparingInt(o -> o))
+                .min(Integer::compareTo)
                 .orElseThrow(() ->
                         new RuntimeException("Can't get min value from list: method_input_list"));
     }
@@ -37,17 +34,13 @@ public class StreamPractice {
      */
     public Double getOddNumsAverage(List<Integer> numbers) {
         return IntStream.range(0, numbers.size())
-                .mapToObj(i -> {
-                    if (i % 2 != 0) {
-                        return numbers.get(i) - 1;
-                    } else {
-                        return numbers.get(i);
-                    }
-                })
+                .mapToDouble(i -> i % 2 != 0
+                        ? numbers.get(i) - 1
+                        : numbers.get(i))
                 .filter(n -> n % 2 != 0)
-                .mapToInt(i -> i)
                 .average()
-                .orElseThrow(NoSuchElementException::new);
+                .orElseThrow(() ->
+                        new NoSuchElementException("List is empty"));
     }
 
     /**
@@ -95,8 +88,7 @@ public class StreamPractice {
                 .filter(p -> p.getSex() == Person.Sex.WOMAN
                         && p.getAge() >= femaleAge
                         && !p.getCats().isEmpty())
-                .map(Person::getCats)
-                .flatMap(Collection::stream)
+                .flatMap(p -> p.getCats().stream())
                 .map(Cat::getName)
                 .collect(Collectors.toList());
     }
