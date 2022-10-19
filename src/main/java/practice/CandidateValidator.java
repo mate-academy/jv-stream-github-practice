92744c27d14e0761a6c7a6b63351fa4721e9f4c3
Anton Pavliuk <anton.pavliuk20@gmail.com>
2022-10-19 20:33:09 +0300

    Add cnstants to CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,21 +1,27 @@
 package practice;
 
 import model.Candidate;
-
 import java.util.function.Predicate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MINIMAL_CANDIDATE_AGE = 35;
+    private static final String CANDIDATE_NATIONALITY = "Ukrainian";
+    private static final int MINIMAL_CANDIDATE_PERIOD_IN_COUNTRY = 10;
+    private static final String CANDIDATE_PERIOD_FORMAT_SPLITTER = "-";
+    private static final int CANDIDATE_PERIOD_FORMAT_LAST_YEAR = 1;
+    private static final int CANDIDATE_PERIOD_FORMAT_FIRST_YEAR = 0;
     @Override
     public boolean test(Candidate candidate) {
         return candidate.isAllowedToVote()
-                && candidate.getAge() >= 35
-                && candidate.getNationality().equals("Ukrainian")
+                && candidate.getAge() >= MINIMAL_CANDIDATE_AGE
+                && candidate.getNationality().equals(CANDIDATE_NATIONALITY)
                 && checkPeriodInUkraine(candidate.getPeriodsInUkr());
     }
 
     private boolean checkPeriodInUkraine(String period) {
-        String[] years = period.split("-");
-        int yearsInUkraine = Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
-        return yearsInUkraine >= 10;
+        String[] years = period.split(CANDIDATE_PERIOD_FORMAT_SPLITTER);
+        int yearsInUkraine = Integer.parseInt(years[CANDIDATE_PERIOD_FORMAT_LAST_YEAR])
+                - Integer.parseInt(years[CANDIDATE_PERIOD_FORMAT_FIRST_YEAR]);
+        return yearsInUkraine >= MINIMAL_CANDIDATE_PERIOD_IN_COUNTRY;
     }
 }
