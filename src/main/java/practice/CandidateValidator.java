2e606d8faa4e8f2f3fc88e51f5a9d7695a0fec8a
Yura Hryniv <superyura444@gmail.com>
2022-10-22 15:09:51 +0300

    Streams and CandidateValidator are implemented

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,22 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_PERIOD_UKR = 10;
+    private static final int MIN_CANDIDATE_AGE = 35;
+    private static final String CANDIDATE_NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MIN_CANDIDATE_AGE && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(CANDIDATE_NATIONALITY)
+                && getIntUkrPeriod(candidate.getPeriodsInUkr()) >= MIN_PERIOD_UKR;
+    }
+
+    private Integer getIntUkrPeriod(String period) {
+        return Integer.parseInt(period.substring(period.indexOf("-") + 1))
+                - Integer.parseInt(period.substring(0, period.indexOf("-")));
+    }
 }
