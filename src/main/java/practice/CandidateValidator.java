4f40a637597b359f0af12edfdc31c6071643f5f9
Misha Getsko <getsko.gil@gmail.com>
2022-10-24 15:26:04 +0300

    all tasks are done

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,18 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] ints = candidate.getPeriodsInUkr().split("-");
+        int firstYear = Integer.parseInt(ints[0]);
+        int secondYear = Integer.parseInt(ints[1]);
+        int result = secondYear - firstYear;
+        return candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && result >= 10;
+    }
 }
