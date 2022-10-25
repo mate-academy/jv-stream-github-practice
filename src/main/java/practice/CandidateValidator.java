5849aea2862b3ab623315349a7ea97907432d8a2
Alex Anofriev <alex.linkoln7391@gmail.com>
2022-10-25 17:56:27 +0300

    stream api practise

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.Arrays;
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= 35 && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && getLifePeriod(candidate.getPeriodsInUkr()) >= 10;
+    }
+
+    private int getLifePeriod(String years) {
+        return Arrays.stream(years.split("-"))
+                .mapToInt(Integer::parseInt)
+                .reduce((x,y) -> y - x).getAsInt();
+    }
 }
