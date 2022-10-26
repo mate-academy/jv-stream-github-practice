0482b655620dc55a79e6afc9b9ce0e0b26a922b8
Anastasia Vasylyk <anastasiavasylykv@gmail.com>
2022-10-26 14:03:53 +0300

    Stream Prectice solution implement

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final String SPLIT_FOR_DATE = "-";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && isValidPeriods(candidate);
+    }
+
+    private boolean isValidPeriods(Candidate candidate) {
+        String[] date = candidate.getPeriodsInUkr().split(SPLIT_FOR_DATE);
+        int yearFrom = Integer.parseInt(date[0]);
+        int yearTo = Integer.parseInt(date[1]);
+        return yearTo - yearFrom >= 10;
+    }
 }
