bf251e1920acb32c247397be1c93432d94f209ac
Anton Pavliuk <anton.pavliuk20@gmail.com>
2022-10-26 13:27:29 +0300

    CleanUp code

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -2,7 +2,6 @@ package practice;
 
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
