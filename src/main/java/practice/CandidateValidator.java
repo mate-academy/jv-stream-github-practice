0ee05e95e06c818778e128ea49facf591fba7bba
Anton Pushkar <tosha.pushkar@gmail.com>
2022-10-28 16:12:04 +0300

    Steam Practice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import model.Candidate;
+
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MINIMAL_AGE = 35;
+    private static final int LIVING_IN_UKRAiNE = 10;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final String SPLIT = "-";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split(SPLIT);
+        int livingYear = Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
+        return candidate.getAge() >= MINIMAL_AGE & candidate.isAllowedToVote()
+                & candidate.getNationality().equals(NATIONALITY) & livingYear >= LIVING_IN_UKRAiNE;
+    }
 }
