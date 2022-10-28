7478319716a3beab2b5bc56fcd3e773e0965577b
NSay0 <krvipa@gmail.com>
2022-10-28 14:06:11 +0300

    Implemented StreamPractice class

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,28 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import model.Candidate;
+
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int ALLOWED_CANDIDATE_AGE = 35;
+    private static final String ALLOWED_CANDIDATE_NATIONALITY = "Ukrainian";
+    private static final int PERIOD_IN_COUNTRY = 10;
+    private static final String YEARS_DELIMITER = "-";
+    private static final int YEAR_FROM_INDEX = 0;
+    private static final int YEAR_TO_INDEX = 1;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        if (candidate == null) {
+            return false;
+        }
+        final String[] yearsOfLivingInCountry = candidate.getPeriodsInUkr().split(YEARS_DELIMITER);
+        final int yearFrom = Integer.parseInt(yearsOfLivingInCountry[YEAR_FROM_INDEX]);
+        final int yearTo = Integer.parseInt(yearsOfLivingInCountry[YEAR_TO_INDEX]);
+        return candidate.getAge() >= ALLOWED_CANDIDATE_AGE
+                && ALLOWED_CANDIDATE_NATIONALITY.equals(candidate.getNationality())
+                && candidate.isAllowedToVote()
+                && yearTo - yearFrom >= PERIOD_IN_COUNTRY;
+    }
 }
---
+++ src/main/java/practice/WorkablePeopleValidator.java
@@ -0,0 +1,24 @@
+package practice;
+
+import model.Person;
+
+import java.util.function.Predicate;
+
+public class WorkablePeopleValidator implements Predicate<Person> {
+    private final int fromAge;
+    private final int femaleToAge;
+    private final int maleToAge;
+
+    public WorkablePeopleValidator(int fromAge, int femaleToAge, int maleToAge) {
+        this.fromAge = fromAge;
+        this.femaleToAge = femaleToAge;
+        this.maleToAge = maleToAge;
+    }
+
+    @Override
+    public boolean test(Person person) {
+        return person.getAge() >= fromAge
+                && ((Person.Sex.MAN == person.getSex() && person.getAge() <= maleToAge)
+                || (Person.Sex.WOMAN == person.getSex() && person.getAge() <= femaleToAge));
+    }
+}
