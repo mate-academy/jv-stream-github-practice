526651cb3003029ff6a719141ebe4b7b3d9b9d9f
Anastasiia Romanova <ana.roma.vi@gmail.com>
2022-10-28 23:24:28 +0300

    completed all tasks in the Stream practice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final String ALLOWED_NATIONALITY = "Ukrainian";
+    private static final int MIN_AGE = 35;
+    private static final int MIN_LEAVING_PERIOD_IN_UKRAINE = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] leavingPeriod = candidate.getPeriodsInUkr().split("-");
+        return candidate.getAge() >= MIN_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(ALLOWED_NATIONALITY)
+                && Integer.parseInt(leavingPeriod[1]) - Integer.parseInt(leavingPeriod[0])
+                >= MIN_LEAVING_PERIOD_IN_UKRAINE;
+    }
 }
