72009503cf6bc543ff1a43b059a0af102409c6e8
Zoreslav Todorenko <wzxzzx@gmail.com>
2022-10-29 17:07:34 +0300

    Task complited

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,19 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    @Override
+    public boolean test(Candidate candidate) {
+        if (Integer.parseInt(candidate.getPeriodsInUkr().split("-")[1])
+                - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[0]) > 10
+                && candidate.getAge() >= 35
+                && candidate.getNationality().equals("Ukrainian")
+                && candidate.isAllowedToVote()) {
+            return true;
+        }
+        return false;
+    }
 }
