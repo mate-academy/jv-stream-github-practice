b1ae19b455110c061faa4caa86b89cca489f0a2e
AlinaOryshak <alinaoryshak@gmail.com>
2022-10-29 22:10:08 +0300

    Stream practice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,6 +4,8 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int VOTING_AGE = 35;
+    private static final int NEEDED_PERIOD_IN_UKR = 10;
     private static final String UKR_NATIONALITY = "Ukrainian";
     private static final String DELIMITER = "-";
     private static final int FROM_YEAR_INDEX = 0;
@@ -11,10 +13,10 @@ public class CandidateValidator implements Predicate<Candidate> {
 
     @Override
     public boolean test(Candidate candidate) {
-        return candidate.getAge() >= 35
+        return candidate.getAge() >= VOTING_AGE
                 && candidate.isAllowedToVote()
                 && candidate.getNationality().equals(UKR_NATIONALITY)
-                && periodInUkr(candidate) >= 10;
+                && periodInUkr(candidate) >= NEEDED_PERIOD_IN_UKR;
     }
 
     private int periodInUkr(Candidate candidate) {
