f63bad6ff5ce9df55a8dd614966d8ab8aefcd66c
Oleg Marchuk <mar4uk14@gmail.com>
2022-10-30 12:00:15 +0200

    Create test method for candidates and implement other methods

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final int MIN_PERIOD_IN_UKR = 10;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final String NEEDED_REGEX = "-";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] neededPeriod = candidate.getPeriodsInUkr().split(NEEDED_REGEX);
+        int result = Integer.parseInt(neededPeriod[1]) - Integer.parseInt(neededPeriod[0]);
+        return candidate.getAge() >= MIN_AGE
+                && result >= MIN_PERIOD_IN_UKR
+                && candidate.getNationality().equals(NATIONALITY)
+                && candidate.isAllowedToVote();
+    }
 }
