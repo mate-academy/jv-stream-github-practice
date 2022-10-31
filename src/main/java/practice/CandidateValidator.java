25ba6d2d5f4bf5f485a57342a63c3e5f23a32c87
Sidir0m <romangolovatyi@yahoo.com>
2022-10-31 16:14:52 +0200

    Added test solution. 5 test failed

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,22 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final String PROPER_NATIONALITY = "Ukrainian";
+    private static final String DATA_SEPARATOR = "-";
+    private static final int MIN_AGE_VALUE = 35;
+    private static final int PERIOD_IN_UKR = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] splitData = candidate.getPeriodsInUkr().split(DATA_SEPARATOR);
+        int periodLivingInUkraine = Integer.parseInt(splitData[1])
+                - Integer.parseInt(splitData[0]);
+        return candidate.isAllowedToVote()
+                && candidate.getNationality().equals(PROPER_NATIONALITY)
+                && candidate.getAge() >= MIN_AGE_VALUE
+                && periodLivingInUkraine >= PERIOD_IN_UKR;
+    }
 }
