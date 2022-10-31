1d79125618f503f25743110a0f14553a02c485a0
Pavlo Hryhorash <pavlo.hryhorash@gmail.com>
2022-10-31 02:25:17 +0200

    solved jv stream github practice task

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,22 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MINIMUM_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int MINIMUM_LIVE_IN_UKRAINE = 10;
+    private static final String REGEX = "-";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] liveInUkraine = candidate.getPeriodsInUkr().split(REGEX);
+        int years = Integer.parseInt(liveInUkraine[1])
+                - Integer.parseInt(liveInUkraine[0]);
+        return candidate.getAge() >= MINIMUM_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && years >= MINIMUM_LIVE_IN_UKRAINE;
+    }
 }
