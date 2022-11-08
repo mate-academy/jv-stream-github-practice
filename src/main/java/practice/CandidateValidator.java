2751b814133954c2b89abde59515e7f2d0878479
Serhii Rovenskyi <katapulta13@gmail.com>
2022-11-08 17:22:59 +0200

    Version 2.Mistakes were fixed

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,40 +4,19 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    private int neededPeriodInUkr = 10;
-    private int minCandidatesAge = 35;
-    private String neededNationality = "Ukrainian";
-
-    public int getNeededPeriodInUkr() {
-        return neededPeriodInUkr;
-    }
-
-    public void setNeededPeriodInUkr(int neededPeriodInUkr) {
-        this.neededPeriodInUkr = neededPeriodInUkr;
-    }
-
-    public int getMinCandidateAge() {
-        return minCandidatesAge;
-    }
-
-    public void setMinCandidateAge(int minCandidateAge) {
-        this.minCandidatesAge = minCandidateAge;
-    }
-
-    public String getNeededNationality() {
-        return neededNationality;
-    }
-
-    public void setNeededNationality(String neededNationality) {
-        this.neededNationality = neededNationality;
-    }
+    private static final int NEEDED_PERIOD_IN_UKR = 10;
+    private static final int MIN_CANDIDATES_AGE = 35;
+    private static final String NEEDED_NATIONALITY = "Ukrainian";
+    private static final int INDEX_BEGIND = 0;
+    private static final int INDEX_END = 1;
 
     @Override
     public boolean test(Candidate candidate) {
         String[] period = candidate.getPeriodsInUkr().split("-");
-        return (Integer.parseInt(period[1]) - Integer.parseInt(period[0]) >= neededPeriodInUkr)
-                && candidate.getNationality().equals(neededNationality)
-                && candidate.getAge() >= minCandidatesAge
+        return (Integer.parseInt(period[INDEX_END]) - Integer
+                .parseInt(period[INDEX_BEGIND]) >= NEEDED_PERIOD_IN_UKR)
+                && candidate.getNationality().equals(NEEDED_NATIONALITY)
+                && candidate.getAge() >= MIN_CANDIDATES_AGE
                 && candidate.isAllowedToVote();
     }
 }
--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import model.Candidate;
 import model.Cat;
 import model.Person;
@@ -17,15 +18,13 @@ public class StreamPractice {
      * "Can't get min value from list: < Here is our input 'numbers' >"
      */
     public int findMinEvenNumber(List<String> numbers) {
-        if (numbers.isEmpty()) {
-            throw new RuntimeException("Can't get min value from list: "
-                    + "< Here is our input 'numbers' >");
-        }
         return numbers.stream()
                 .flatMap(s -> Arrays.stream(s.split(",")))
                 .map(Integer::parseInt)
                 .filter(integer -> integer % 2 == 0)
-                .reduce(Integer::min).get();
+                .reduce(Integer::min)
+                .orElseThrow(() -> new RuntimeException("Can't get min value from list: "
+                        + "< Here is our input 'numbers' >"));
     }
 
     /**
@@ -34,11 +33,9 @@ public class StreamPractice {
      * But before that subtract 1 from each element on an odd position (having the odd index).
      */
     public Double getOddNumsAverage(List<Integer> numbers) {
-        for (int i = 1; i < numbers.size(); i = i + 2) {
-            numbers.set(i, numbers.get(i) - 1);
-        }
-        return numbers.stream()
-                .filter(i -> i % 2 == 1)
+        return IntStream.range(0,numbers.size())
+                .mapToObj(i -> (i % 2 != 1) ? numbers.get(i) : numbers.get(i) - 1)
+                .filter(integer -> integer % 2 == 1)
                 .mapToDouble(i -> i)
                 .average()
                 .getAsDouble();
