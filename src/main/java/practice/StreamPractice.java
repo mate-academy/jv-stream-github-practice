3dc3f9b02aa9ae43dd692630840e3c1b4e242dca
AndriySichniy <sichniy.andriy@gmail.com>
2022-11-14 13:04:29 +0200

    added implements getCatsNames

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,10 +1,12 @@
 package practice;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import model.Candidate;
+import model.Cat;
 import model.Person;
 
 public class StreamPractice {
@@ -106,7 +108,15 @@ public class StreamPractice {
      * return the names of all cats whose owners are women from `femaleAge` years old inclusively.
      */
     public List<String> getCatsNames(List<Person> peopleList, int femaleAge) {
-        return Collections.emptyList();
+        return peopleList
+                .stream()
+                .filter(person -> person.getSex() == Person.Sex.WOMAN
+                               && person.getAge() >= femaleAge)
+                .map(Person::getCats)
+                .flatMap(Collection::stream)
+                .map(Cat::getName)
+                .distinct()
+                .collect(Collectors.toList());
     }
 
     /**
