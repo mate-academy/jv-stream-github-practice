f6e8e9f9d9d16e6996f09c9e57b6954a16c2d4d2
AndriySichniy <sichniy.andriy@gmail.com>
2022-11-14 17:05:37 +0200

    fixes CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,21 +7,20 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_AGE = 35;
     private static final int MIN_YEAR_IN_UA = 10;
     private static final String BEST_NATIONALITY = "Ukrainian";
+    private static final String DATE_SPLIT_REGEX = "-";
+    private static final int YEAR_TO_INDEX = 0;
+    private static final int YEAR_FROM_INDEX = 1;
 
     //write your code here
     @Override
     public boolean test(Candidate candidate) {
-    String[] periodInUkr = person.getPeriodsInUkr().split(DATE_SPLIT_REGEX);
-    int periodDuration = Integer.parseInt(periodInUkr [YEAR_TO_INDEX])
-      - Integer.parseInt(periodInUkr [YEAR_FROM_INDEX]);
-    boolean livedInUrkEnough = periodDuration >= MIN_YEAR_IN_UA;
-        if (candidate == null
-                || !candidate.isAllowedToVote()
-                || candidate.getAge() < MIN_AGE
-                || !candidate.getNationality().equals(BEST_NATIONALITY)) {
-            return false;
-        }
-        String[] tmp = candidate.getPeriodsInUkr().split("-");
-        return Integer.parseInt(tmp[1]) - Integer.parseInt(tmp[0]) >= MIN_YEAR_IN_UA;
+        String[] periodInUkr = candidate.getPeriodsInUkr().split(DATE_SPLIT_REGEX);
+        int periodDuration = Integer.parseInt(periodInUkr[YEAR_TO_INDEX])
+                - Integer.parseInt(periodInUkr[YEAR_FROM_INDEX]);
+        boolean livedInUrkEnough = periodDuration >= MIN_YEAR_IN_UA;
+        return livedInUrkEnough
+                && candidate.isAllowedToVote()
+                && candidate.getAge() >= MIN_AGE
+                && candidate.getNationality().equals(BEST_NATIONALITY);
     }
 }
