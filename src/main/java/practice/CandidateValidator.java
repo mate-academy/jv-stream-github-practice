136275448af7607e73b83f95606652e4df37b64a
Kateryna Mykhailovska <112807544+kateryna-mykh@users.noreply.github.com>
2022-11-15 13:02:01 +0200

    Update CandidateValidator.java
    fixes were made

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,12 +7,14 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_AGE = 35;
     private static final String NATIONALITY = "Ukrainian";
     private static final int LIVING_PERIOD = 10;
+    private static final int YEAR_STATRT_ID = 0;
+    private static final int YEAR_END_ID = 1;
 
     @Override
     public boolean test(Candidate candidate) {
         String[] periodInUk = candidate.getPeriodsInUkr().split("-");
-        int currentLivingPeriod = Integer.parseInt(periodInUk[1])
-                - Integer.parseInt(periodInUk[0]);
+        int currentLivingPeriod = Integer.parseInt(periodInUk[YEAR_END_ID])
+                - Integer.parseInt(periodInUk[YEAR_STATRT_ID]);
         return candidate.getAge() >= MIN_AGE 
                 && candidate.isAllowedToVote()
                 && candidate.getNationality() == NATIONALITY
