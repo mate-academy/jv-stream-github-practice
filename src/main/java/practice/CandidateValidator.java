c287b2efe8fbe958e8660345cdeb953953926578
Sofiia <sofiakazzz@gmail.com>
2022-11-18 01:21:56 +0200

    All necessary methods were implemented using Stream. We sorted people according to the necessary criteria, so our country is in good hands. ( + empty line )

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -18,21 +18,22 @@ import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
 
-    private static int FROM_AGE = 35;
-    private static String NATIONALITY = "Ukrainian";
-    private static String PERIODS_IN_UK = "2002-2015";
+    private static int MIN_AGE = 35;
+    private static String REQUIRED_NATIONALITY = "Ukrainian";
     private static String REGEX = "-";
     private static int MIN_DURATION_IN_UK = 10;
+    private static int FROM_YEAR_OF_LIVE_IN_UK = 0;
+    private static int TO_YEAR_OF_LIVE_IN_UK = 1;
 
     @Override
     public boolean test(Candidate candidate) {
         String[] periodsInUk = candidate.getPeriodsInUkr().split(REGEX);
-        int durationLivingInUK = Integer.parseInt(periodsInUk[1])
-                - Integer.parseInt(periodsInUk[0]);
+        int durationLivingInUK = Integer.parseInt(periodsInUk[TO_YEAR_OF_LIVE_IN_UK])
+                - Integer.parseInt(periodsInUk[FROM_YEAR_OF_LIVE_IN_UK]);
         boolean durationTrue = durationLivingInUK >= MIN_DURATION_IN_UK;
         return durationTrue
                 && candidate.isAllowedToVote()
-                && candidate.getAge() >= FROM_AGE
-                && candidate.getNationality().equals(NATIONALITY);
+                && candidate.getAge() >= MIN_AGE
+                && candidate.getNationality().equals(REQUIRED_NATIONALITY);
     }
 }
--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -92,9 +92,8 @@ public class StreamPractice {
     public List<String> getCatsNames(List<Person> peopleList, int femaleAge) {
         return peopleList.stream()
                 .filter(person -> person.getSex()
-                        .equals(Person.Sex.WOMAN) && person.getAge() >= femaleAge)
-                .map(Person::getCats)
-                .flatMap(Collection::stream)
+                 .equals(Person.Sex.WOMAN) && person.getAge() >= femaleAge)
+                .flatMap(person -> person.getCats().stream())
                 .map(Cat::getName)
                 .collect(Collectors.toList());
     }
