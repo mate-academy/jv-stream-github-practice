1cf1559e20c7aaff865a7b2d323df1af047ce793
Vitalina Volkivska <113260060+Vitalina-V@users.noreply.github.com>
2022-11-20 00:48:02 +0200

    all methods are implemented

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.Arrays;
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MINIMUM_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int MINIMUM_PERIOD_IN_COUNTRY = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MINIMUM_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && Arrays.stream(candidate.getPeriodsInUkr().split("-"))
+                .map(Integer::valueOf)
+                .reduce(0, (a, b) -> b - a) >= MINIMUM_PERIOD_IN_COUNTRY;
+    }
 }
