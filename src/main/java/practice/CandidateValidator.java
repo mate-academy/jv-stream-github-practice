33e761f4e2fd2c55c8295570ef1c0952528830ef
Andrii Malenko <andmalenko@gmail.com>
2022-11-22 14:39:43 +0200

    Here is my solution for hw-jv-stream-github-practice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,15 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] yearsInUkraine = candidate.getPeriodsInUkr().split("-");
+        return candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && Integer.parseInt(yearsInUkraine[1]) - Integer.parseInt(yearsInUkraine[0]) >= 10;
+    }
 }
