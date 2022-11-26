5ff54e8c8df4686d60636dfb0b6bce7427eca8b7
Roman Novosad <roman.novosad@yahoo.com>
2022-11-27 00:40:09 +0100

    solution to task StreamPractice / Java 8 / Java Core

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MINIMUM_AGE = 35;
+    private static final int MINIMUM_AGES_IN_UKRAINE = 10;
+    private static final String UKRAINIAN = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        int toYearLivingInUkraine = Integer.parseInt(candidate.getPeriodsInUkr().substring(5));
+        int fromYearLivingInUkraine = Integer.parseInt(candidate.getPeriodsInUkr().substring(0,4));
+        int yearsLivingInUkraine = toYearLivingInUkraine - fromYearLivingInUkraine;
+
+        return candidate.getAge() >= MINIMUM_AGE && candidate.getNationality().equals(UKRAINIAN)
+                && candidate.isAllowedToVote() && yearsLivingInUkraine >= MINIMUM_AGES_IN_UKRAINE;
+    }
 }
