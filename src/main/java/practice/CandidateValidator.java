9ff7188a0dced1d9e433e55a285fa63a17862ff3
Olexii Melnik <alexmelnikk@gmail.com>
2022-12-08 15:07:27 +0200

    practice is done

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int START_YEAR = 2002;
+    private static final int FINISH_YEAR = 2015;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] split = candidate.getPeriodsInUkr().split("-");
+        int first = Integer.valueOf(split[0]);
+        int second = Integer.valueOf(split[1]);
+        if (candidate.getAge() >= 35 && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && second - first >= 10
+                && (first >= START_YEAR || second <= FINISH_YEAR)) {
+            return true;
+        }
+        return false;
+    }
 }
