d295867e2bd497c00f9c4af7d33a8b663c5828d9
Anton Kalenskyi <anton_kalenskiy@ukr.net>
2022-12-08 13:16:56 +0200

    Changed method test in CandidateValidator class. Removed method validPeriodsInUkraine.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,23 +6,17 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_CANDIDATE_AGE = 35;
     private static final int MIN_VALID_PERIOD = 10;
-    private static final int INDEX_YEAR_TO = 1;
-    private static final int INDEX_YEAR_FROM = 0;
+    private static final int YEAR_TO_INDEX = 1;
+    private static final int YEAR_FROM_INDEX = 0;
 
     @Override
     public boolean test(Candidate candidate) {
+        String[] splitedArray = candidate.getPeriodsInUkr().split("-");
+        int periodDuration = Integer.parseInt(splitedArray[YEAR_TO_INDEX])
+                - Integer.parseInt(splitedArray[YEAR_FROM_INDEX]);
         return candidate.getAge() >= MIN_CANDIDATE_AGE
                 && candidate.getNationality().equals("Ukrainian")
                 && candidate.isAllowedToVote()
-                && validPeriodsInUkraine(candidate) >= MIN_VALID_PERIOD;
-    }
-
-    private int validPeriodsInUkraine(Candidate candidate) {
-        String[] splitedArray = candidate.getPeriodsInUkr().split("-");
-        Integer[] integersArray = new Integer[splitedArray.length];
-        for (int i = 0; i < splitedArray.length; i++) {
-            integersArray[i] = Integer.parseInt(splitedArray[i]);
-        }
-        return integersArray[INDEX_YEAR_TO] - integersArray[INDEX_YEAR_FROM];
+                && periodDuration >= MIN_VALID_PERIOD;
     }
 }
