8f643b44f4a862d7fe40c5fbf7a2ed1bcd9bb0c5
maksim-untilov <maksimuntilov7@gmail.com>
2022-12-13 14:06:10 +0200

    Stream practice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split("-");
+        Integer yearsTo = Integer.parseInt(years[1]);
+        Integer yearsFrom = Integer.parseInt(years[0]);
+        if (candidate.isAllowedToVote() == true
+                && candidate.getAge() >= 35
+                && candidate.getNationality().equals("Ukrainian")
+                && (yearsTo - yearsFrom) >= 10) {
+            return true;
+        }
+        return false;
+    }
 }
