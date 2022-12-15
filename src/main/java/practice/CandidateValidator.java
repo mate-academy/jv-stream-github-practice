1f0aa3c23d26a95c2af7a8ca52296b1a9863b028
Andrii Shevchuk <zovten156@gmail.com>
2022-12-15 21:27:43 +0200

    Followed the instructions for completing the tasks, but made some mistakes with Git when try send it

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,22 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final int MIN_PERIOD_IN_UKRAINE = 10;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int FROM_YEAR = 0;
+    private static final int TO_YEAR = 1;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] liveInUkraine = candidate.getPeriodsInUkr().split("-");
+        return candidate.getAge() >= MIN_AGE
+                && candidate.getNationality().equals(NATIONALITY)
+                && candidate.isAllowedToVote()
+                && Integer.parseInt(liveInUkraine[TO_YEAR])
+                - Integer.parseInt(liveInUkraine[FROM_YEAR]) > MIN_PERIOD_IN_UKRAINE;
+    }
 }
