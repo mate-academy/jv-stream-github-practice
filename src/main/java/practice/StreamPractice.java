c8fa170e58e874088f94c3c4c3aa56fdfaee812d
Ivan Vovnyanko <mooooonkit@gmail.com>
2022-12-25 17:59:59 +0200

    maven

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -2,8 +2,6 @@ package practice;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import model.Candidate;
