550585fa94741fb1b3a668c9f9e0cce892c0ac69
Uka0001 <ukraine0001@gmail.com>
2022-12-26 15:31:16 +0200

    stream solutions fixed

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,23 +4,26 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-
-    public static final String VALID_NATIONALITY = "Ukrainian";
-    public static final int PERIOD_TO = 1;
-    public static final int PERIOD_FROM = 0;
-    public static final int VALID_PERIOD = 10;
-    public static final int VALID_AGE = 35;
+    public static final String REQUIRED_NATIONALITY = "Ukrainian";
+    public static final int YEAR_TO_INDEX = 1;
+    public static final int YEAR_FROM_INDEX = 0;
+    public static final int REQUIRED_MIN_PERIOD = 10;
+    public static final int REQUIRED_MIN_AGE = 35;
 
     @Override
     public boolean test(Candidate candidate) {
         return candidate.isAllowedToVote()
-                && candidate.getNationality().equals(VALID_NATIONALITY)
-                && candidate.getAge() >= VALID_AGE
-                && Integer.parseInt(candidate
+                && candidate.getNationality().equals(REQUIRED_NATIONALITY)
+                && candidate.getAge() >= REQUIRED_MIN_AGE
+                && countPeriodInUkraine(candidate) >= REQUIRED_MIN_PERIOD;
+    }
+
+    private static int countPeriodInUkraine(Candidate candidate) {
+        return Integer.parseInt(candidate
                 .getPeriodsInUkr()
-                .split("-")[PERIOD_TO])
+                .split("-")[YEAR_TO_INDEX])
                 - Integer.parseInt(candidate
                 .getPeriodsInUkr()
-                .split("-")[PERIOD_FROM]) >= VALID_PERIOD;
+                .split("-")[YEAR_FROM_INDEX]);
     }
 }
--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -72,8 +72,8 @@ public class StreamPractice {
     public List<Person> getWorkablePeople(int fromAge, int femaleToAge,
                                           int maleToAge, List<Person> peopleList) {
         return peopleList.stream()
-                .filter(p -> p.getAge() >= 18)
-                .filter(p -> p.getSex()
+                .filter(p -> p.getAge() >= fromAge
+                        && p.getSex()
                         .equals(Person.Sex.MAN)
                         ? p.getAge() <= maleToAge
                         : p.getAge() <= femaleToAge)
