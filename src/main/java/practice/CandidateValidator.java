493518bd466fb904f112049a354ffe3f28f889d5
Vyacheslav Sukhov <makasin_12@hotmail.com>
2022-12-26 22:02:20 +0100

    1st try commit 26/12

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -3,7 +3,6 @@ package practice;
 import java.util.function.Predicate;
 import model.Candidate;
 
-
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int MINIMAL_AGE = 35;
     private static final String ALLOWED_NATIONALITY = "Ukrainian";
