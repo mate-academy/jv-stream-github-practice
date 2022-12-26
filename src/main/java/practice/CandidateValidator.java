72aa03046c0d96f29875fbc3d09ab1ed3b041ad3
Nikita Akosnov <nikitaua4226@gmail.com>
2022-12-26 14:59:57 +0200

    moved magic numbers to const

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,15 +5,17 @@ import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
     private static final String REQ_NATIONALITY = "Ukrainian";
+    private static final int REQ_AGE = 35;  
+    private static final int MIN_YEARS_IN_COUNTRY = 10;
     private static final int FROM_YEAR_INDEX = 0;    
-    private static final int TO_YEAR_INDEX = 1;    
+    private static final int TO_YEAR_INDEX = 1;  
     
     @Override
     public boolean test(Candidate candidate) {
-        return candidate.getAge() >= 35 
+        return candidate.getAge() >= REQ_AGE 
                 && candidate.isAllowedToVote() == true 
                 && candidate.getNationality().equals(REQ_NATIONALITY) 
-                && getYearsInCounty(candidate.getPeriodsInUkr()) >= 10;
+                && getYearsInCounty(candidate.getPeriodsInUkr()) >= MIN_YEARS_IN_COUNTRY;
     }
 
     private int getYearsInCounty(String period) {
