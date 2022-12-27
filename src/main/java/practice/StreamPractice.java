ea8d9f070b7e14254955866a25463de120f21dc7
Vyacheslav Sukhov <makasin_12@hotmail.com>
2022-12-27 19:50:44 +0100

    2nd try commit 27/12

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,13 +1,10 @@
 package practice;
 
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
-
 import model.Candidate;
 import model.Cat;
 import model.Person;
