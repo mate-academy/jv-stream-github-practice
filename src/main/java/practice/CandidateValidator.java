97c34b65e52b3c952e28791add0da8e25bf46962
Oksana Rudenko <0ksaha_rudenko@ukr.net>
2023-01-01 20:27:59 +0200

    jv-stream-github-practice solution

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int AGE_FOR_ELECTION = 35;
+    private static final int POSITION_ZERO = 0;
+    private static final int POSITION_FIRST = 1;
+    private static final int PERIOD = 10;
+    private static final String NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.isAllowedToVote()
+                && candidate.getAge() >= AGE_FOR_ELECTION
+                && candidate.getNationality().equals(NATIONALITY)
+                && Integer.parseInt(candidate.getPeriodsInUkr().split("-")[POSITION_FIRST])
+                - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[POSITION_ZERO]) >= PERIOD;
+    }
 }
