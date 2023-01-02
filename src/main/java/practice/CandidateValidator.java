5766fbbec5afbed1c427cda99d0c3f28119b15a9
Dmytro Koval <dimonkoval@gmail.com>
2023-01-02 23:31:26 +0200

    6

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -8,17 +8,13 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int BEGIN_PERIOD_INDEX = 0;
     private static final int END_PERIOD_INDEX = 1;
 
-    public boolean validate(Candidate candidate) {
+    @Override
+    public boolean test(Candidate candidate) {
         return candidate.getAge() >= 35
                 && candidate.isAllowedToVote()
                 && candidate.getNationality().equals("Ukrainian")
                 && Integer.parseInt(candidate.getPeriodsInUkr().split("-")[END_PERIOD_INDEX])
                 - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[BEGIN_PERIOD_INDEX])
                 > YEARS_IN_COUNTRY;
-    }
-
-    @Override
-    public boolean test(Candidate candidate) {
-        return validate(candidate);
     }
 }
