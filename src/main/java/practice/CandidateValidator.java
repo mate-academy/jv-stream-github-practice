4864bf22c808a52ea6d2325453004606fdff3a28
Dmytro Koval <dimonkoval@gmail.com>
2023-01-02 20:42:08 +0200

    4

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,16 +5,16 @@ import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int YEARS_IN_COUNTRY = 10;
-    private static final int INDEX_FIRST = 0;
-    private static final int INDEX_SECOND = 1;
+    private static final int BEGIN_PERIOD = 0;
+    private static final int END_PERIOD = 1;
 
     @Override
     public boolean test(Candidate candidate) {
         return candidate.getAge() >= 35
                 && candidate.isAllowedToVote()
                 && candidate.getNationality().equals("Ukrainian")
-                && Integer.parseInt(candidate.getPeriodsInUkr().split("-")[INDEX_SECOND])
-                - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[INDEX_FIRST])
+                && Integer.parseInt(candidate.getPeriodsInUkr().split("-")[END_PERIOD])
+                - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[BEGIN_PERIOD])
                 > YEARS_IN_COUNTRY;
     }
 }
