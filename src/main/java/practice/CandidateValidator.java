42c836375c3ee733b979bee40b950b8c6e534fa1
Diana <dlomei283@gmail.com>
2023-01-02 19:24:00 +0200

    done

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,6 +4,8 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static int ZERO_INDEX = 0;
+    private static int FIRST_INDEX = 1;
     private static final int MIN_AGE = 35;
     private static final String NATIONALITY = "Ukrainian";
     private static final int LIVE_IN_UKRAINE = 10;
@@ -17,7 +19,8 @@ public class CandidateValidator implements Predicate<Candidate> {
     }
 
     private boolean livesInUkraine(Candidate candidate) {
-        String[] p = candidate.getPeriodsInUkr().split("-");
-        return Integer.parseInt(p[1]) - Integer.parseInt(p[0]) >= LIVE_IN_UKRAINE;
+        String[] value = candidate.getPeriodsInUkr().split("-");
+        return Integer.parseInt(value[FIRST_INDEX])
+                - Integer.parseInt(value[ZERO_INDEX]) >= LIVE_IN_UKRAINE;
     }
 }
--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,11 +1,10 @@
 package practice;
 
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 import model.Candidate;
 import model.Cat;
 import model.Person;
@@ -22,8 +21,7 @@ public class StreamPractice {
 
     public int findMinEvenNumber(List<String> numbers) {
         return numbers.stream()
-                .map(s -> s.split(","))
-                .flatMap(Arrays::stream)
+                .flatMap(s -> Stream.of(s.split(",")))
                 .mapToInt(Integer::parseInt)
                 .filter(i -> i % 2 == 0)
                 .min()
@@ -86,8 +84,7 @@ public class StreamPractice {
     public List<String> getCatsNames(List<Person> peopleList, int femaleAge) {
         return peopleList.stream()
                 .filter(p -> p.getSex().equals(Person.Sex.WOMAN) && p.getAge() >= femaleAge)
-                .map(Person::getCats)
-                .flatMap(Collection::stream)
+                .flatMap(person -> person.getCats().stream())
                 .map(Cat::getName)
                 .collect(Collectors.toList());
     }
