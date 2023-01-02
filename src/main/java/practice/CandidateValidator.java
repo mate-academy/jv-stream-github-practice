3e33167591d30fe213595bf0c8a7339068a4d7a8
Diana <dlomei283@gmail.com>
2023-01-02 20:03:53 +0200

    I changed the names of the constants

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,8 +4,8 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    private static int ZERO_INDEX = 0;
-    private static int FIRST_INDEX = 1;
+    private static int LIVES_IN_UKRAINE_FROM_INDEX = 0;
+    private static int LIVES_IN_UKRAINE_TO_INDEX = 1;
     private static final int MIN_AGE = 35;
     private static final String NATIONALITY = "Ukrainian";
     private static final int LIVE_IN_UKRAINE = 10;
@@ -20,7 +20,7 @@ public class CandidateValidator implements Predicate<Candidate> {
 
     private boolean livesInUkraine(Candidate candidate) {
         String[] value = candidate.getPeriodsInUkr().split("-");
-        return Integer.parseInt(value[FIRST_INDEX])
-                - Integer.parseInt(value[ZERO_INDEX]) >= LIVE_IN_UKRAINE;
+        return Integer.parseInt(value[LIVES_IN_UKRAINE_TO_INDEX])
+                - Integer.parseInt(value[LIVES_IN_UKRAINE_FROM_INDEX]) >= LIVE_IN_UKRAINE;
     }
 }
