87c78dc8f8813d69115a0ce4ca1ef729ad00c2b4
codered23 <oleg2003andriy@gmail.com>
2023-01-05 21:33:15 +0200

    final commit

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,14 +4,16 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int PERIOD_START_INDEX = 0;
+    private static final int PERIOD_END_INDEX = 1;
     private static final int MIN_AGE = 35;
     private static final String VALID_NATIONALITY = "Ukrainian";
     private static final int VALID_PERIOD_IN_UKR = 10;
 
     @Override
     public boolean test(Candidate candidate) {
-        int periodInUkr = Integer.parseInt(candidate.getPeriodsInUkr().split("-")[1])
-                - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[0]);
+        int periodInUkr = Integer.parseInt(candidate.getPeriodsInUkr().split("-")[PERIOD_END_INDEX])
+                - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[PERIOD_START_INDEX]);
         return candidate.getAge() >= MIN_AGE && candidate.isAllowedToVote()
                 && candidate.getNationality().equals(VALID_NATIONALITY)
                 && periodInUkr >= VALID_PERIOD_IN_UKR;
