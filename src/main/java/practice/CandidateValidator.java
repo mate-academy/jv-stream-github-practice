3378f6cc205644d8f5457c32ea477636d379560c
Yurii Dikalo <dikalotel@gmail.com>
2023-01-08 19:30:26 +0200

    implemented task: all tests have passed; mvn clean package - build success.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,27 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.Arrays;
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final int FROM_YEAR_INDEX = 0;
+    private static final int TO_YEAR_INDEX = 1;
+    private static final int MIN_YEARS_IN_UKRAINE = 10;
+    private static final String DELIMITER = "-";
+    private static final String NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        int [] periods = Arrays.stream(candidate.getPeriodsInUkr().split(DELIMITER))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int yearsInUkr = periods[TO_YEAR_INDEX] - periods[FROM_YEAR_INDEX];
+        return candidate.isAllowedToVote()
+                && candidate.getAge() >= MIN_AGE
+                && NATIONALITY.equals(candidate.getNationality())
+                && yearsInUkr >= MIN_YEARS_IN_UKRAINE;
+    }
     //write your code here
 }
