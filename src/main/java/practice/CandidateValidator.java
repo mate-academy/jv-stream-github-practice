8dc0bb795bdda71b7b0d7b8377fe9549a1c8f81f
Oleksandr Kostiukevych <kaasnake2@gmail.com>
2023-01-09 22:10:49 +0200

    StreamPractice 2d commit

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,19 +5,19 @@ import model.Candidate;
 
 public class CandidateValidator<T extends Candidate> implements Predicate<T> {
     private static final int MIN_AGE = 35;
-    private static final String NATIONALITY = "Ukrainian";
-    private static final int MIN_YEARS_LIVES_IN_COUNTRY = 10;
-    private static final int PERIODS_IN_UKR_FROM_INDEX = 0;
-    private static final int PERIODS_IN_UKR_TO_INDEX = 1;
+    private static final String REQUIRED_NATIONALITY = "Ukrainian";
+    private static final int MIN_PERIOD_IN_COUNTRY = 10;
+    private static final int FROM_YEAR_INDEX = 0;
+    private static final int TO_YEAR_INDEX = 1;
 
     @Override
     public boolean test(T candidate) {
         String[] years = candidate.getPeriodsInUkr().split("-");
-        int yearsLiveInCountry = Integer.parseInt(years[PERIODS_IN_UKR_TO_INDEX])
-                - Integer.parseInt(years[PERIODS_IN_UKR_FROM_INDEX]);
+        int yearsLiveInCountry = Integer.parseInt(years[TO_YEAR_INDEX])
+                - Integer.parseInt(years[FROM_YEAR_INDEX]);
         return candidate.isAllowedToVote()
                 && (candidate.getAge() >= MIN_AGE)
-                && (candidate.getNationality().equals(NATIONALITY))
-                && (yearsLiveInCountry >= MIN_YEARS_LIVES_IN_COUNTRY);
+                && (candidate.getNationality().equals(REQUIRED_NATIONALITY))
+                && (yearsLiveInCountry >= MIN_PERIOD_IN_COUNTRY);
     }
 }
