2e72f637748ba8270646bb9eba96025ce62f2287
Oleksandr Kostiukevych <kaasnake2@gmail.com>
2023-01-09 17:15:19 +0200

    StreamPractice 1st commit

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator<T extends Candidate> implements Predicate<T> {
+    private static final int MIN_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int MIN_YEARS_LIVES_IN_COUNTRY = 10;
+    private static final int PERIODS_IN_UKR_FROM_INDEX = 0;
+    private static final int PERIODS_IN_UKR_TO_INDEX = 1;
+
+    @Override
+    public boolean test(T candidate) {
+        String[] years = candidate.getPeriodsInUkr().split("-");
+        int yearsLiveInCountry = Integer.parseInt(years[PERIODS_IN_UKR_TO_INDEX])
+                - Integer.parseInt(years[PERIODS_IN_UKR_FROM_INDEX]);
+        return candidate.isAllowedToVote()
+                && (candidate.getAge() >= MIN_AGE)
+                && (candidate.getNationality().equals(NATIONALITY))
+                && (yearsLiveInCountry >= MIN_YEARS_LIVES_IN_COUNTRY);
+    }
 }
