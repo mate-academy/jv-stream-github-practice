8fe414d1b920cbb461688433c1358cfeee13bce3
Oleksii Zaloznyi <o.zaloznyi@gmail.com>
2023-01-10 13:20:04 +0200

    names of constants changed

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,21 +6,21 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_VALID_AGE = 35;
     private static final int MIN_VALID_PERIOD = 10;
-    private static final int INDEX_YEARS_FROM = 0;
-    private static final int INDEX_YEARS_TO = 1;
-    private static final String NATIONALITY = "Ukrainian";
+    private static final int YEAR_FROM_INDEX = 0;
+    private static final int YEAR_TO_INDEX = 1;
+    private static final String VALID_NATIONALITY = "Ukrainian";
 
     @Override
     public boolean test(Candidate candidate) {
         return candidate.getAge() >= MIN_VALID_AGE && candidate.isAllowedToVote()
-                && candidate.getNationality().equals(NATIONALITY)
+                && candidate.getNationality().equals(VALID_NATIONALITY)
                 && checkPeriod(candidate);
     }
 
     private boolean checkPeriod(Candidate candidate) {
         String[] yearsInUkraine = candidate.getPeriodsInUkr().split("-");
-        int intYearsFrom = Integer.parseInt(yearsInUkraine[INDEX_YEARS_FROM]);
-        int intYearsTo = Integer.parseInt(yearsInUkraine[INDEX_YEARS_TO]);
+        int intYearsFrom = Integer.parseInt(yearsInUkraine[YEAR_FROM_INDEX]);
+        int intYearsTo = Integer.parseInt(yearsInUkraine[YEAR_TO_INDEX]);
         return (intYearsTo - intYearsFrom) >= MIN_VALID_PERIOD;
     }
 }
