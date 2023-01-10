4d75db40d1598945e0974d90867399d387331800
Polina Lysytska <nikolavna100500@gmail.com>
2023-01-10 05:07:15 +0200

    all methods were implemented and all tests were passed except one

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,26 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import model.Candidate;
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private final static int MIN_VALID_AGE = 35;
+    private final static String VALID_NATIONALITY = "Ukrainian";
+    private final static int MIN_VALID_PERIODS = 10;
+    private final static int FROM_YEAR_INDEX = 0;
+    private final static int TO_YEAR_INDEX = 1;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MIN_VALID_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(VALID_NATIONALITY)
+                && checkPeriodsInUkr(candidate);
+    }
+
+    private boolean checkPeriodsInUkr(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split("-");
+        return Integer.parseInt(years[TO_YEAR_INDEX])
+                - Integer.parseInt(years[FROM_YEAR_INDEX]) >= MIN_VALID_PERIODS;
+    }
 }
