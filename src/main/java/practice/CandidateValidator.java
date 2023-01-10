c9ba9e74f1fae163ff408c5bd66d267a2be25e41
Oleksii Zaloznyi <o.zaloznyi@gmail.com>
2023-01-10 12:48:54 +0200

    method checkPeriod added, unnecessary lines removed

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,19 +4,23 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    private static final int MIN_AGE = 35;
-    private static final int YEARS_IN_UKRAINE = 10;
+    private static final int MIN_VALID_AGE = 35;
+    private static final int MIN_VALID_PERIOD = 10;
     private static final int INDEX_YEARS_FROM = 0;
     private static final int INDEX_YEARS_TO = 1;
+    private static final String NATIONALITY = "Ukrainian";
 
     @Override
     public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MIN_VALID_AGE && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && checkPeriod(candidate);
+    }
+
+    private boolean checkPeriod(Candidate candidate) {
         String[] yearsInUkraine = candidate.getPeriodsInUkr().split("-");
         int intYearsFrom = Integer.parseInt(yearsInUkraine[INDEX_YEARS_FROM]);
         int intYearsTo = Integer.parseInt(yearsInUkraine[INDEX_YEARS_TO]);
-        int intYearsInUkraine = intYearsTo - intYearsFrom;
-        return candidate.getAge() >= MIN_AGE && candidate.isAllowedToVote()
-                && candidate.getNationality().equals("Ukrainian")
-                && intYearsInUkraine >= YEARS_IN_UKRAINE;
+        return (intYearsTo - intYearsFrom) >= MIN_VALID_PERIOD;
     }
 }
