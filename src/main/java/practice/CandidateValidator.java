9f1e663ebe4933bff044cfd09adbb409c0561801
Dmitriy Krasnopir <dmitriy.krasnopir@gmail.com>
2023-01-10 03:19:33 +0200

    stream practice was complited

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,30 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MAX_VALID_AGE = 35;
+    private static final int MIN_VALID_PERIOD_OF_LIVING_IN_UKRAiNE = 10;
+    private static final String VALID_NATIONALITY = "Ukrainian";
+    private static final int FIRST_DATE_INDEX = 0;
+    private static final int SECOND_DATE_INDEX = 1;
+    private static final String SPLIT_SYMBOL = "-";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        int periodsInUkr = getPeriodOfLeaving(candidate);
+        return candidate.getAge() >= MAX_VALID_AGE
+                & candidate.isAllowedToVote()
+                & candidate.getNationality().equals(VALID_NATIONALITY)
+                & periodsInUkr >= MIN_VALID_PERIOD_OF_LIVING_IN_UKRAiNE;
+    }
+
+    private int getPeriodOfLeaving(Candidate candidate) {
+        int firstDate = Integer.parseInt(candidate.getPeriodsInUkr()
+                .split(SPLIT_SYMBOL)[FIRST_DATE_INDEX]);
+        int secondDate = Integer.parseInt(candidate.getPeriodsInUkr()
+                .split(SPLIT_SYMBOL)[SECOND_DATE_INDEX]);
+        return secondDate - firstDate;
+    }
 }
