a79d0e2b1d536ce663fe2184ad758f59cef1e8f0
Bohdan Halitskyi <b.a.galitskiy@gmail.com>
2023-01-10 05:24:56 +0200

    Build success with total time of 11.102 s

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,11 +1,17 @@
 package practice;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import model.Candidate;
+import model.Cat;
 import model.Person;
 
 public class StreamPractice {
+    private static final int EVEN_NUMBERS_INDEX = 2;
+    private static final String SPLIT_SYMBOL = ",";
     /**
      * Given list of strings where each element contains 1+ numbers:
      * input = {"5,30,100", "0,22,7", ...}
@@ -13,8 +19,17 @@ public class StreamPractice {
      * If there is no needed data throw RuntimeException with message
      * "Can't get min value from list: < Here is our input 'numbers' >"
      */
+
     public int findMinEvenNumber(List<String> numbers) {
-        return 0;
+
+        return numbers.stream()
+              .map(n -> n.split(SPLIT_SYMBOL))
+              .flatMap(Arrays::stream)
+              .mapToInt(Integer::valueOf)
+              .filter(n -> n % EVEN_NUMBERS_INDEX == 0)
+              .min()
+              .orElseThrow(() -> new RuntimeException(
+                      "Can't get min value from list: " + numbers));
     }
 
     /**
@@ -23,7 +38,12 @@ public class StreamPractice {
      * But before that subtract 1 from each element on an odd position (having the odd index).
      */
     public Double getOddNumsAverage(List<Integer> numbers) {
-        return 0D;
+        return IntStream.range(0, numbers.size())
+                .map(i -> i % EVEN_NUMBERS_INDEX != 0 ? numbers.get(i) - 1 : numbers.get(i))
+                .filter(e -> e % EVEN_NUMBERS_INDEX != 0)
+                .average()
+                .orElseThrow(() -> new NoSuchElementException(
+                        "Can't get average value from list: " + numbers));
     }
 
     /**
@@ -35,7 +55,10 @@ public class StreamPractice {
      * Example: select men who can be recruited to army (from 18 to 27 years old inclusively).
      */
     public List<Person> selectMenByAge(List<Person> peopleList, int fromAge, int toAge) {
-        return Collections.emptyList();
+        return peopleList.stream()
+                .filter(p -> p.getSex().equals(Person.Sex.MAN)
+                        & p.getAge() >= fromAge & p.getAge() <= toAge)
+                .collect(Collectors.toList());
     }
 
     /**
@@ -50,7 +73,13 @@ public class StreamPractice {
      */
     public List<Person> getWorkablePeople(int fromAge, int femaleToAge,
                                           int maleToAge, List<Person> peopleList) {
-        return Collections.emptyList();
+        return peopleList.stream()
+                .filter(p -> p.getAge() >= fromAge
+                        & ((p.getSex().equals(Person.Sex.MAN)
+                                & p.getAge() <= maleToAge)
+                        || (p.getSex().equals(Person.Sex.WOMAN))
+                                & p.getAge() <= femaleToAge))
+                .collect(Collectors.toList());
     }
 
     /**
@@ -59,22 +88,32 @@ public class StreamPractice {
      * return the names of all cats whose owners are women from `femaleAge` years old inclusively.
      */
     public List<String> getCatsNames(List<Person> peopleList, int femaleAge) {
-        return Collections.emptyList();
+        return peopleList.stream()
+               .filter(p -> p.getSex().equals(Person.Sex.WOMAN)
+               & p.getAge() >= femaleAge)
+               .flatMap(p -> p.getCats().stream())
+               .map(Cat::getName)
+               .collect(Collectors.toList());
     }
 
     /**
-     * Your help with a election is needed. Given list of candidates, where each element
-     * has Candidate.class type.
+     * Your help with an election is needed. Given list of candidates, where each element
+     * has Candidate. class type.
      * Check which candidates are eligible to apply for president position and return their
      * names sorted alphabetically.
      * The requirements are: person should be older than 35 years, should be allowed to vote,
      * have nationality - 'Ukrainian'
      * and live in Ukraine for 10 years. For the last requirement use field periodsInUkr,
-     * which has following view: "2002-2015"
-     * We want to reuse our validation in future, so let's write our own impl of Predicate
+     * which has the following view: "2002-2015"
+     * We want to reuse our validation in the future, so let's write our own impl of Predicate
      * parametrized with Candidate in CandidateValidator.
      */
     public List<String> validateCandidates(List<Candidate> candidates) {
-        return Collections.emptyList();
+        CandidateValidator candidateValidator = new CandidateValidator();
+        return candidates.stream()
+                .filter(candidateValidator)
+                .map(Candidate::getName)
+                .sorted()
+                .collect(Collectors.toList());
     }
 }
