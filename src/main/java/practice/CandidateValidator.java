89deba4434915de1aaed7a4d35f5276181f42bed
Yevhen Fedorchuk <evgenfedorchuk89@gmail.com>
2023-01-12 15:02:43 +0100

    Initial commit

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int MIN_YEARS_IN_UA = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.isAllowedToVote()
+                && candidate.getAge() >= MIN_AGE
+                && candidate.getNationality().equals(NATIONALITY)
+                && isPeriodEnough(candidate.getPeriodsInUkr());
+    }
+
+    private boolean isPeriodEnough(String period) {
+        return (Integer.parseInt(period.split("-")[1]) - Integer.parseInt(period.split("-")[0]))
+                >= MIN_YEARS_IN_UA;
+    }
 }
