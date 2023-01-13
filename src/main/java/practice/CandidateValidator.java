139dead003c379e9748daaa2ebd6503b24ac1a65
Denis Pugovkin <den.pugovkin@gmail.com>
2023-01-13 21:40:48 +0200

    1. Renamed all wrong named constants. 2. Created all additional constants.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,19 +4,22 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    private static final int PERIOD_InUKR = 10;
-    private static final int CANDIDATE_ValidAGE = 35;
+    private static final int MIN_YEARS_IN_UKR = 10;
+    private static final int MIN_AGE = 35;
     private static final String VALID_NATIONALITY = "Ukrainian";
+    private static final int START_LIVING = 0;
+    private static final int FINISH_LIVING = 1;
+    private static final String SEPARATOR = "-";
 
     @Override
     public boolean test(Candidate candidate) {
-        return getPeriodsInUkr(candidate.getPeriodsInUkr()) > PERIOD_InUKR
-                && candidate.isAllowedToVote() && candidate.getAge() >= CANDIDATE_ValidAGE
+        return getPeriodsInUkr(candidate.getPeriodsInUkr()) > MIN_YEARS_IN_UKR
+                && candidate.isAllowedToVote() && candidate.getAge() >= MIN_AGE
                 && candidate.getNationality().equals(VALID_NATIONALITY);
     }
 
     private int getPeriodsInUkr(String getPeriodsInUkr) {
-        String[] period = getPeriodsInUkr.split("-");
-        return Integer.parseInt(period[1]) - Integer.parseInt(period[0]);
+        String[] period = getPeriodsInUkr.split(SEPARATOR);
+        return Integer.parseInt(period[FINISH_LIVING]) - Integer.parseInt(period[START_LIVING]);
     }
 }
