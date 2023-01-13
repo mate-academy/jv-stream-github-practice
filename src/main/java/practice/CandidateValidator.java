a3634e0afed278ac8c794b277056104aa0d765b5
Denis Pugovkin <den.pugovkin@gmail.com>
2023-01-13 18:39:08 +0200

    1. All tasks have been done. 2. In last task method CandidateValidator.test has been created, which determine candidate accordingly to conditions for president post.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,22 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int PERIOD_InUKR = 10;
+    private static final int CANDIDATE_ValidAGE = 35;
+    private static final String VALID_NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return getPeriodsInUkr(candidate.getPeriodsInUkr()) > PERIOD_InUKR
+                && candidate.isAllowedToVote() && candidate.getAge() >= CANDIDATE_ValidAGE
+                && candidate.getNationality().equals(VALID_NATIONALITY);
+    }
+
+    private int getPeriodsInUkr(String getPeriodsInUkr) {
+        String[] period = getPeriodsInUkr.split("-");
+        return Integer.parseInt(period[1]) - Integer.parseInt(period[0]);
+    }
 }
