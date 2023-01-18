244d9687ebf8ae2eca731770cf02dedb82286a3c
sasha <radovietsoleksandr@gmail.com>
2023-01-18 14:49:23 +0200

    streamApi#

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,22 +4,19 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    private Predicate<Candidate> checkAge35 = c -> c.getAge() >= 30;
-    private Predicate<Candidate> allowedToVote = Candidate::isAllowedToVote;
-    private Predicate<Candidate> nationality = c ->
-            "Ukrainian".equalsIgnoreCase(c.getNationality());
-    private Predicate<Candidate> liveInUA = c -> {
-        String[] years = c.getPeriodsInUkr().split("-");
-        int diff = Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
-        return diff >= 10;
-    };
+    public static final int MIN_PERIOD = 10;
+    public static final int MIN_OLD_YEARS = 35;
+    public static final String NATIONALS = "Ukrainian";
 
     @Override
     public boolean test(Candidate candidate) {
-        return checkAge35
-                .and(allowedToVote)
-                .and(nationality)
-                .and(liveInUA)
-                .test(candidate);
+        String[] split = candidate.getPeriodsInUkr().split("-");
+        final int different = Integer.parseInt(split[1]) - Integer.parseInt(split[0]);
+        if (candidate.getAge() >= MIN_OLD_YEARS
+                && candidate.getNationality().equalsIgnoreCase(NATIONALS)
+                && different >= MIN_PERIOD) {
+            return candidate.isAllowedToVote();
+        }
+        return false;
     }
 }
