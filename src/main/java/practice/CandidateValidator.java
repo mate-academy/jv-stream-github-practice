169c075a0b3272dcb75866a3c0b5dac2f597f435
Oleksandr Kravchenko <oleksandr.kravchenko@spirent.com>
2023-01-27 00:25:26 +0200

    feat: jv-stream-practice #comment stream practice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final int MIN_PERIOD_IN_UKRAINE = 10;
+    private static final String MANDATORY_NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split("-");
+        int period = Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
+        return candidate.getAge() >= MIN_AGE
+              && candidate.isAllowedToVote()
+              && MANDATORY_NATIONALITY.equals(candidate.getNationality())
+              && period >= MIN_PERIOD_IN_UKRAINE;
+    }
 }
