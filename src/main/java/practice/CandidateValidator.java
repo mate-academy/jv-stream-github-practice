7dcd7460e79c55a95a8dc5c6cc19c5c18ec5a12e
Oleksandr Kravchenko <oleksandr.kravchenko@spirent.com>
2023-01-27 14:14:17 +0200

    fixes after first review

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,12 +6,17 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_AGE = 35;
     private static final int MIN_PERIOD_IN_UKRAINE = 10;
+    private static final int START_PERIOD_INDEX = 0;
+    private static final int END_PERIOD_INDEX = 1;
     private static final String MANDATORY_NATIONALITY = "Ukrainian";
+    private static final String PERIOD_SPLITTER = "-";
 
     @Override
     public boolean test(Candidate candidate) {
-        String[] years = candidate.getPeriodsInUkr().split("-");
-        int period = Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
+        String[] years = candidate.getPeriodsInUkr().split(PERIOD_SPLITTER);
+        int period = Integer.parseInt(years[END_PERIOD_INDEX])
+                - Integer.parseInt(years[START_PERIOD_INDEX]);
+
         return candidate.getAge() >= MIN_AGE
               && candidate.isAllowedToVote()
               && MANDATORY_NATIONALITY.equals(candidate.getNationality())
