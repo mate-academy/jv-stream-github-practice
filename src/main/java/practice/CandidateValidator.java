20c0df5fe24355be62e699ab81a35e3ead9a8b16
Serhii Nazarenko <nasarenko82@gmail.com>
2023-01-31 15:42:50 +0200

    test

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,7 +6,10 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_AGE_PRESIDENT = 35;
     private static final String NATIONALITY = "Ukrainian";
-    private static final int MIN_PERIOD_LIVE_IN_UKRAINIAN = 10;
+    private static final int MIN_PERIOD_LIVE_IN_UKRAINE = 10;
+    private static final int DEADLINE_INDEX = 5;
+    private static final int INDEX_ONE_OF_INITIAL_TERM = 0;
+    private static final int INDEX_TWO_OF_INITIAL_TERM = 4;
 
     @Override
     public boolean test(Candidate candidate) {
@@ -14,8 +17,9 @@ public class CandidateValidator implements Predicate<Candidate> {
                 && candidate.getNationality().equals(NATIONALITY)
                 && candidate.isAllowedToVote()
                 && Integer.parseInt(candidate.getPeriodsInUkr()
-                .substring(5)) - Integer
+                .substring(DEADLINE_INDEX)) - Integer
                 .parseInt(candidate.getPeriodsInUkr()
-                .substring(0, 4)) >= MIN_PERIOD_LIVE_IN_UKRAINIAN;
+                .substring(INDEX_ONE_OF_INITIAL_TERM, INDEX_TWO_OF_INITIAL_TERM))
+                >= MIN_PERIOD_LIVE_IN_UKRAINE;
     }
 }
