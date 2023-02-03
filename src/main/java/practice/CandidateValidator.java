b76db5a2690a0eda94d7d65faaf23226e9a56533
Oleksii Klymenko <alexeyklimenkowork@gmail.com>
2023-02-03 21:36:29 +0200

    Rename constants

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -8,8 +8,8 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final String UA_NATIONALITY = "Ukrainian";
     private static final int MIN_AGE = 35;
     private static final int MIN_LIVING_IN_UKRAINE = 10;
-    private static final int FIRST_INDEX = 0;
-    private static final int SECOND_INDEX = 1;
+    private static final int START_YEAR_LIVING_IN_UA_INDEX = 0;
+    private static final int LAST_YEAR_LIVING_IN_UA_INDEX = 1;
 
     @Override
     public boolean test(Candidate candidate) {
@@ -20,6 +20,7 @@ public class CandidateValidator implements Predicate<Candidate> {
 
     private int getPeriodsInUkr(String periodsInUkr) {
         String[] periods = periodsInUkr.split(SEPARATOR);
-        return Integer.parseInt(periods[SECOND_INDEX]) - Integer.parseInt(periods[FIRST_INDEX]);
+        return Integer.parseInt(periods[LAST_YEAR_LIVING_IN_UA_INDEX])
+                - Integer.parseInt(periods[START_YEAR_LIVING_IN_UA_INDEX]);
     }
 }
