6c7879360fcadbf8ce12c5b8c78d6e907a0dc5d6
Stepan Peknyi <stepan_peknyy@ukr.net>
2023-02-05 17:46:29 +0200

    Solution of task Stream practice. All tests are done

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,8 +1,13 @@
 package practice;
 
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 import model.Candidate;
+import model.Cat;
 import model.Person;
 
 public class StreamPractice {
@@ -14,7 +19,16 @@ public class StreamPractice {
      * "Can't get min value from list: < Here is our input 'numbers' >"
      */
     public int findMinEvenNumber(List<String> numbers) {
-        return 0;
+        try {
+            return numbers.stream()
+                    .flatMap(e -> Arrays.stream(e.split(",")))
+                    .map(e -> Integer.valueOf(e))
+                    .filter(i -> i % 2 == 0)
+                    .min(Integer:: compare)
+                    .get();
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Can't get min value from list");
+        }
     }
 
     /**
@@ -23,7 +37,25 @@ public class StreamPractice {
      * But before that subtract 1 from each element on an odd position (having the odd index).
      */
     public Double getOddNumsAverage(List<Integer> numbers) {
-        return 0D;
+        List<Integer> listOdd = IntStream.range(0, numbers.size())
+                .filter(i -> i % 2 == 0)
+                .mapToObj(i -> numbers.get(i))
+                .mapToInt(Integer::intValue)
+                .filter(e -> e % 2 != 0)
+                .boxed()
+                .collect(Collectors.toList());
+        List<Integer> listOddAfterSubtraction = IntStream.range(0, numbers.size())
+                .filter(i -> i % 2 != 0)
+                .mapToObj(i -> numbers.get(i) - 1)
+                .mapToInt(Integer::intValue)
+                .filter(e -> e % 2 != 0)
+                .boxed()
+                .collect(Collectors.toList());
+        return Stream.of(listOdd, listOddAfterSubtraction)
+                .flatMap(Collection::stream)
+                .mapToInt(Integer::intValue)
+                .average()
+                .getAsDouble();
     }
 
     /**
@@ -35,7 +67,10 @@ public class StreamPractice {
      * Example: select men who can be recruited to army (from 18 to 27 years old inclusively).
      */
     public List<Person> selectMenByAge(List<Person> peopleList, int fromAge, int toAge) {
-        return Collections.emptyList();
+        return peopleList.stream()
+                .filter(p -> p.getAge() >= fromAge && p.getAge() <= toAge
+                        && p.getSex() == Person.Sex.MAN)
+                .collect(Collectors.toList());
     }
 
     /**
@@ -50,7 +85,10 @@ public class StreamPractice {
      */
     public List<Person> getWorkablePeople(int fromAge, int femaleToAge,
                                           int maleToAge, List<Person> peopleList) {
-        return Collections.emptyList();
+        WorkablePeopleValidator workablePeopleValidator = new WorkablePeopleValidator();
+        return peopleList.stream()
+                .filter(p -> workablePeopleValidator.check(p, fromAge, femaleToAge, maleToAge))
+                .collect(Collectors.toList());
     }
 
     /**
@@ -59,11 +97,17 @@ public class StreamPractice {
      * return the names of all cats whose owners are women from `femaleAge` years old inclusively.
      */
     public List<String> getCatsNames(List<Person> peopleList, int femaleAge) {
-        return Collections.emptyList();
+
+        return peopleList.stream()
+                .filter(p -> p.getSex() == Person.Sex.WOMAN && p.getAge() >= femaleAge)
+                .flatMap(p -> p.getCats().stream())
+                .map(Cat::getName)
+                .collect(Collectors.toList());
+
     }
 
     /**
-     * Your help with a election is needed. Given list of candidates, where each element
+     * Your help with an election is needed. Given list of candidates, where each element
      * has Candidate.class type.
      * Check which candidates are eligible to apply for president position and return their
      * names sorted alphabetically.
@@ -75,6 +119,11 @@ public class StreamPractice {
      * parametrized with Candidate in CandidateValidator.
      */
     public List<String> validateCandidates(List<Candidate> candidates) {
-        return Collections.emptyList();
+        CandidateValidator candidateValidator = new CandidateValidator();
+        return candidates.stream()
+                .filter(c -> candidateValidator.test(c))
+                .map(Candidate::getName)
+                .sorted()
+                .collect(Collectors.toList());
     }
 }
