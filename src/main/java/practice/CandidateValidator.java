37a235a19768c4f7ac20fd318ad5b3ab3e59db88
alexeyukrainets <qaaq.com.ua@gmail.com>
2023-02-16 17:48:39 +0100

    Version 1

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE_FOR_CANDIDATE = 35;
+    private static final String NATIONALITY_FOR_CANDIDATE = "Ukrainian";
+    private static final int MIN_PERIODS_IN_UKRAINE = 10;
+    private static final int INDEX_FOR_DATE_1 = 1;
+    private static final int INDEX_FOR_DATE_2 = 0;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] splittedPeriod = candidate.getPeriodsInUkr().split("-");
+        int periodInUkraine = Integer.parseInt(String.valueOf(splittedPeriod[INDEX_FOR_DATE_1]))
+                - Integer.parseInt(String.valueOf(splittedPeriod[INDEX_FOR_DATE_2]));
+        return candidate.getAge() >= MIN_AGE_FOR_CANDIDATE
+                && candidate.getNationality().equals(NATIONALITY_FOR_CANDIDATE)
+                && candidate.isAllowedToVote()
+                && periodInUkraine >= MIN_PERIODS_IN_UKRAINE;
+    }
 }
