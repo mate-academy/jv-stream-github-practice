98421b99ea50720e683bee247f3907125fc9bab9
“Ymishok321 <kkoiku3@gmail.com>
2023-02-20 18:08:35 +0200

    Solved all tasks

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,18 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator<T> implements Predicate<T> {
+    @Override
+    public boolean test(T t) {
+        Candidate c = (Candidate) t;
+        int firstDate = Integer.valueOf(c.getPeriodsInUkr().split("-")[0]);
+        int secondDate = Integer.valueOf(c.getPeriodsInUkr().split("-")[1]);
+        return c.getAge() >= 35
+                && c.isAllowedToVote()
+                && c.getNationality().equals("Ukrainian")
+                && (secondDate - firstDate) >= 10;
+    }
     //write your code here
 }
