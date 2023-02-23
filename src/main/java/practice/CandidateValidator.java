0840cf1c9b3a82464f9929d6f6a496a3db9a3cf7
Andrii First <andre_first@ukr.net>
2023-02-23 21:52:44 +0000

    Apply requested changes. Attemp 2.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,12 +7,14 @@ public class CandidateValidator implements Predicate<Candidate> {
     //write your code here
     private static final int MIN_AGE = 35;
     private static final int MIN_LIVING_PERIOD = 10;
+    private static final int FROM = 0;
+    private static final int TO = 1;
 
     @Override
     public boolean test(Candidate candidate) {
         String[] livingPeriod = candidate.getPeriodsInUkr().split("-");
-        int livingPeriodResult = Integer.parseInt(livingPeriod[1])
-                - Integer.parseInt(livingPeriod[0]);
+        int livingPeriodResult = Integer.parseInt(livingPeriod[TO])
+                - Integer.parseInt(livingPeriod[FROM]);
         return candidate.isAllowedToVote() && candidate.getAge() >= MIN_AGE
                 && candidate.getNationality().equals("Ukrainian")
                 && livingPeriodResult >= MIN_LIVING_PERIOD;
