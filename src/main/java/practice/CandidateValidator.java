c434b6610c712f24763be81d4b2bd70c70bc12be
Andrii First <andre_first@ukr.net>
2023-02-23 02:51:26 +0000

    Implemented requested methods. Tests - ok, Build - ok

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,24 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
     //write your code here
+    private static final int MIN_AGE = 35;
+    private static final int MIN_LIVING_PERIOD = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        Integer from = Integer.valueOf(candidate.getPeriodsInUkr().substring(0,4));
+        Integer to = Integer.valueOf(candidate.getPeriodsInUkr().substring(5));
+        if (candidate.isAllowedToVote()) {
+            if (candidate.getAge() >= MIN_AGE) {
+                if (candidate.getNationality().equals("Ukrainian")) {
+                    return (to - from) >= MIN_LIVING_PERIOD;
+                }
+            }
+        }
+        return false;
+    }
 }
