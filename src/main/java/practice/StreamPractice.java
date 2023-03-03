dec7d70e85f8223fee1535a2849ef7740339c189
Yehor-Cherkashyn <gooooodvin1995@gmail.com>
2023-03-04 01:06:43 +0200

    Removed duplicate

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -71,10 +71,11 @@ public class StreamPractice {
     public List<Person> getWorkablePeople(int fromAge, int femaleToAge,
                                           int maleToAge, List<Person> peopleList) {
         return peopleList.stream()
-                .filter(p -> (p.getSex().equals(Person.Sex.MAN)
-                        && p.getAge() >= fromAge && p.getAge() <= maleToAge)
+                .filter(p -> p.getAge() >= fromAge
+                        && ((p.getSex().equals(Person.Sex.MAN)
+                        && p.getAge() <= maleToAge)
                         || (p.getSex().equals(Person.Sex.WOMAN)
-                        && p.getAge() >= fromAge && p.getAge() <= femaleToAge))
+                        && p.getAge() <= femaleToAge)))
                 .collect(Collectors.toList());
     }
 
