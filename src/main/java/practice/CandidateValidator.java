9a2913db6fa0b7cddcb05cb5c4b010c0024af4d6
DOM <kasinets.ulia@gmail.com>
2023-03-05 14:16:15 +0200

    made the implementation of all methods

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AEG_CANDIDATE = 35;
+    private static final String NATIONALITY_CANDIDATE = "Ukrainian";
+    private static final int MIN_PERIOD_IN_UKR = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        int periodsInUkr = Integer.parseInt(candidate.getPeriodsInUkr().split("-")[1])
+                - Integer.parseInt(candidate.getPeriodsInUkr()
+                        .split("-")[0]);
+        return candidate.getAge() >= MIN_AEG_CANDIDATE
+                && candidate.getNationality().equals(NATIONALITY_CANDIDATE)
+                && periodsInUkr >= MIN_PERIOD_IN_UKR
+                && candidate.isAllowedToVote();
+    }
 }
