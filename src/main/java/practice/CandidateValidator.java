3d45924c24d8bb6d11c7fed9479b96fee0ad9b7d
Mykyta <nikita.nikonets@gmail.com>
2023-03-06 16:58:20 +0200

    implemented all methods of StreamPractice class

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,25 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        if (!candidate.isAllowedToVote()) {
+            return false;
+        }
+        if (candidate.getAge() < 35) {
+            return false;
+        }
+        if (!candidate.getNationality().equals("Ukrainian")) {
+            return false;
+        }
+        int startLivingInUkr = Integer.parseInt(candidate.getPeriodsInUkr().split("-")[0]);
+        int endLivingInUkr = Integer.parseInt(candidate.getPeriodsInUkr().split("-")[1]);
+        if (endLivingInUkr - startLivingInUkr < 10) {
+            return false;
+        }
+        return true;
+    }
 }
