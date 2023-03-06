4f5c895cb0637266e76517def0980eeb6d0b0858
Igor Puhach <igorpuhach21@gmail.com>
2023-03-06 16:10:32 +0200

    implemented methods CandidateValidator and StreamPractice with Stream API

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final int FROM_YEAR = 1;
+    private static final int TO_YEAR = 0;
+    private static final int MIN_YEAR = 10;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final String REGEX_SPLIT = "-";
+    @Override
+    public boolean test(Candidate candidate) {
+        String [] years = candidate.getPeriodsInUkr().split(REGEX_SPLIT);
+        int livedInUkraine = Integer.parseInt(years[FROM_YEAR])
+                - Integer.parseInt(years[TO_YEAR]);
+        return candidate.getAge() >= MIN_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && livedInUkraine >= MIN_YEAR;
+    }
 }
