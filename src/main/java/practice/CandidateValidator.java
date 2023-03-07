196551ae1c7aebc84881b90dba0cd36961738a22
Olexandr Leshtaiev <leshtaev13.a@gmail.com>
2023-03-07 18:25:15 +0200

    implemented test method from Predicate, implemented methods using stream API

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && periodCalculator(candidate) >= 10;
+    }
+
+    private int periodCalculator(Candidate candidate) {
+        String[] splited = candidate.getPeriodsInUkr().split("-");
+        return Integer.valueOf(splited[1]) - Integer.valueOf(splited[0]);
+    }
     //write your code here
 }
