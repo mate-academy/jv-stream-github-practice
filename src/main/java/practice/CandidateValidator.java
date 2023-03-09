98a98819a79a2e70e01e0ce72400f2e797d3d916
Olexandr Leshtaiev <leshtaev13.a@gmail.com>
2023-03-09 13:30:08 +0200

    added constants

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,16 +4,21 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int SUITABLE_CANDIDATE_AGE = 35;
+    private static final int YEARS_IN_COUNTRY = 10;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final String REGEX_FOR_PERIODS = "-";
+
     @Override
     public boolean test(Candidate candidate) {
-        return candidate.getAge() >= 35
+        return candidate.getAge() >= SUITABLE_CANDIDATE_AGE
                 && candidate.isAllowedToVote()
-                && candidate.getNationality().equals("Ukrainian")
-                && periodCalculator(candidate) >= 10;
+                && candidate.getNationality().equals(NATIONALITY)
+                && periodCalculator(candidate) >= YEARS_IN_COUNTRY;
     }
 
     private int periodCalculator(Candidate candidate) {
-        String[] splited = candidate.getPeriodsInUkr().split("-");
+        String[] splited = candidate.getPeriodsInUkr().split(REGEX_FOR_PERIODS);
         return Integer.valueOf(splited[1]) - Integer.valueOf(splited[0]);
     }
     //write your code here
