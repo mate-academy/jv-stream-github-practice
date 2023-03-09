558203b2e31a9b4c01806194028c8d23fd1d3553
Artem Ponomarenko <artemponomar98@gmail.com>
2023-03-09 16:31:37 +0200

    Implemented methods using streams

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,17 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.Arrays;
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        final int[] parsedPeriod = Arrays.stream(candidate.getPeriodsInUkr().split("-"))
+                .mapToInt(Integer::valueOf).toArray();
+        return candidate.isAllowedToVote()
+                && candidate.getAge() >= 35
+                && candidate.getNationality().equals("Ukrainian")
+                && parsedPeriod[1] - parsedPeriod[0] > 10;
+    }
 }
