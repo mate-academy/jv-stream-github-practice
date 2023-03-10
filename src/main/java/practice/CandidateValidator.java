a6571ff9007de744ade38a095a10bcf040b574b6
Volodymyr Prysiazhniuk <verticalsyu@gmail.com>
2023-03-10 10:23:06 +0200

    fix

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,26 +6,23 @@ import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
     private static final String DATE_SEPARATOR = "-";
-    private static final String EXPECTED_NATIONALITY = "Ukrainian";
-    private static final int MINIMUM_PERIOD = 10;
-    private static final int MINIMUM_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int MIN_PERIOD = 10;
+    private static final int MIN_AGE = 35;
 
     @Override
     public boolean test(Candidate candidate) {
-        return isEligible(candidate) && hasMinimumPeriod(candidate);
+        return isEligible(candidate);
     }
 
     private boolean isEligible(Candidate candidate) {
-        return candidate.isAllowedToVote()
-                && candidate.getNationality().equals(EXPECTED_NATIONALITY)
-                && candidate.getAge() >= MINIMUM_AGE;
-    }
-
-    private boolean hasMinimumPeriod(Candidate candidate) {
         int period = Arrays.stream(candidate.getPeriodsInUkr().split(DATE_SEPARATOR))
                 .mapToInt(Integer::parseInt)
                 .reduce((start, end) -> end - start)
                 .orElse(0);
-        return period >= MINIMUM_PERIOD;
+        return candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && candidate.getAge() >= MIN_AGE
+                && period >= MIN_PERIOD;
     }
 }
