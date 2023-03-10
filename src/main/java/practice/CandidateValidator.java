dd4ebcddcea7829f402deab0a6a80668dc66ec9c
Andrew kluchnik <andrey.kluchnik.10@gmail.com>
2023-03-10 13:43:05 +0200

    implemented methods

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final String SEPARATOR = "-";
+    private static final int AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int PERIODS_IN_UKRAINE = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split(SEPARATOR);
+        int yearsInUkraine = Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
+        return yearsInUkraine >= PERIODS_IN_UKRAINE
+                && candidate.getAge() >= AGE
+                && candidate.getNationality().equals(NATIONALITY)
+                && candidate.isAllowedToVote();
+    }
 }
