629589632cdc6a40a0c4bbf79fd952921b09e684
Vasyl Vasylchuk <v.vasylchuk18@gmail.com>
2023-03-10 08:58:25 +0200

    change name for constant

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,16 +6,16 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     private static final String YEAR_SPLITTER = "-";
     private static final String VALID_NATIONALITY = "Ukrainian";
-    private static final int FIRST_ARRAY_ELEMENT = 0;
-    private static final int SECOND_ARRAY_ELEMENT = 1;
+    private static final int FIRST_YEAR_IN_UKR = 0;
+    private static final int LAST_YEAR_IN_UKR = 1;
     private static final int MINIMUM_AGE = 35;
     private static final int MINIMUM_YEAR_IN_UKR = 10;
 
     @Override
     public boolean test(Candidate candidate) {
         String[] year = candidate.getPeriodsInUkr().split(YEAR_SPLITTER);
-        int yearInUkr = Integer.parseInt(year[SECOND_ARRAY_ELEMENT])
-                                            - Integer.parseInt(year[FIRST_ARRAY_ELEMENT]);
+        int yearInUkr = Integer.parseInt(year[LAST_YEAR_IN_UKR])
+                                            - Integer.parseInt(year[FIRST_YEAR_IN_UKR]);
         return candidate.isAllowedToVote()
                 && candidate.getAge() >= MINIMUM_AGE
                 && candidate.getNationality().equals(VALID_NATIONALITY)
