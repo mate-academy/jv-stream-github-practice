9430c4e7aef6f311e327aeab4a916014c570bcc9
Nickolay Voloshchuk <124300295+NickolayVoloshchuk@users.noreply.github.com>
2023-03-10 15:09:08 +0200

    refactoring

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -42,8 +42,7 @@ public class StreamPractice {
                                           int maleToAge, List<Person> peopleList) {
         return peopleList.stream()
                 .filter(p -> p.getAge() >= fromAge
-                        && (p.getSex() == Person.Sex.MAN && p.getAge() <= maleToAge
-                            || p.getSex() == Person.Sex.WOMAN && p.getAge() <= femaleToAge))
+                        && p.getAge() <= (p.getSex() == Person.Sex.MAN ? maleToAge : femaleToAge))
                 .collect(Collectors.toList());
     }
 
