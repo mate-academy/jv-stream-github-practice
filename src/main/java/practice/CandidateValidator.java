1fe1d801dc5519585c863d1e72296480fbbf9c0f
Artem Ponomarenko <artemponomar98@gmail.com>
2023-03-10 12:26:37 +0200

    Removed magic numbers from candidate validator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,13 +5,18 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+
+    public static final int REQUIRED_MIN_AGE = 35;
+    public static final String REQUIRED_NATIONALITY = "Ukrainian";
+    public static final int REQUIRED_MIN_PERIOD = 10;
+
     @Override
     public boolean test(Candidate candidate) {
         final int[] parsedPeriod = Arrays.stream(candidate.getPeriodsInUkr().split("-"))
                 .mapToInt(Integer::valueOf).toArray();
         return candidate.isAllowedToVote()
-                && candidate.getAge() >= 35
-                && candidate.getNationality().equals("Ukrainian")
-                && parsedPeriod[1] - parsedPeriod[0] > 10;
+                && candidate.getAge() >= REQUIRED_MIN_AGE
+                && candidate.getNationality().equals(REQUIRED_NATIONALITY)
+                && parsedPeriod[1] - parsedPeriod[0] > REQUIRED_MIN_PERIOD;
     }
 }
