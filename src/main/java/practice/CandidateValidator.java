76474b6612b322102f04c916ac34849171d166ba
Alona Klishchenko <alyonaklischenko@gmail.com>
2023-03-10 14:40:31 +0200

    made implementations for methods by using streams

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final String DELIMITER = "-";
+    private static final int MIN_PERIODS_IN_UKRAINE = 10;
+    private static final int MINIMAL_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] splitted = candidate.getPeriodsInUkr().split(DELIMITER);
+        return candidate.getAge() >= MINIMAL_AGE
+                && candidate.getNationality().equals(NATIONALITY)
+                && (Integer.parseInt(splitted[1]) - Integer.parseInt(splitted[0]))
+                >= MIN_PERIODS_IN_UKRAINE && candidate.isAllowedToVote();
+    }
 }
