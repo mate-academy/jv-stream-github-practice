b225630320107dca544fedb5020d4603feb9a959
Melnychuk <melnychukmyhailo@gmail.com>
2023-03-16 16:01:43 +0200

    Stream practice solution.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,24 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int CANDIDATE_FROM_AGE = 35;
+    private static final int CANDIDATE_PERIOD_IN_UKR = 10;
+    private static final String CANDIDATE_NATIONALITY = "Ukrainian";
+    private static final int START_PERIOD_INDEX = 1;
+    private static final int END_PERIOD_INDEX = 0;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] parse = candidate.getPeriodsInUkr().split("-");
+        int period = Integer.parseInt(parse[START_PERIOD_INDEX])
+                - Integer.parseInt(parse[END_PERIOD_INDEX]);
+        return candidate.getAge() >= CANDIDATE_FROM_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality() != null
+                && candidate.getNationality().equals(CANDIDATE_NATIONALITY)
+                && period >= CANDIDATE_PERIOD_IN_UKR;
+    }
 }
