d8a7badc7e118a8c37aa62971c6172da18617e06
Hryhorii Chernolutskyi <iamchgi1976@gmail.com>
2023-03-19 22:11:46 +0200

    initial commit, internal tests passed.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,18 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    public static final String UKRAINIAN = "Ukrainian";
+    public static final String REGEX_PERIODS = "-";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] periodsInUkr = candidate.getPeriodsInUkr().split(REGEX_PERIODS);
+        return candidate.isAllowedToVote()
+                && candidate.getAge() >= 35
+                && candidate.getNationality().equals(UKRAINIAN)
+                && Integer.parseInt(periodsInUkr[1]) - Integer.parseInt(periodsInUkr[0]) >= 10;
+    }
 }
