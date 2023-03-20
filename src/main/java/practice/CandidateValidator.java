0bfa7966ab3a5330d9c8f61f1a1c1070a54abe03
AndriiBohun <poikka69@gmail.com>
2023-03-20 22:01:44 +0200

    StreamPractice_C003

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,17 +6,17 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int REQUIRED_AGE = 35;
     private static final int REQUIRED_YEARS = 10;
-    private static final int FIRST_DATE = 0;
-    private static final int SECOND_DATE = 1;
-    private static final String NATIONALITY = "Ukrainian";
+    private static final int FIRST_DATE_INDEX = 0;
+    private static final int SECOND_DATE_INDEX = 1;
+    private static final String REQUIRED_NATIONALITY = "Ukrainian";
 
     @Override
     public boolean test(Candidate candidate) {
         String[] yearsSplit = candidate.getPeriodsInUkr().split("-");
         return candidate.getAge() >= REQUIRED_AGE
                 && candidate.isAllowedToVote()
-                && candidate.getNationality().equals(NATIONALITY)
-                && Integer.parseInt(yearsSplit[SECOND_DATE])
-                - Integer.parseInt(yearsSplit[FIRST_DATE]) >= REQUIRED_YEARS;
+                && candidate.getNationality().equals(REQUIRED_NATIONALITY)
+                && Integer.parseInt(yearsSplit[SECOND_DATE_INDEX])
+                - Integer.parseInt(yearsSplit[FIRST_DATE_INDEX]) >= REQUIRED_YEARS;
     }
 }
--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,6 +1,5 @@
 package practice;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
@@ -14,21 +13,20 @@ public class StreamPractice {
     public int findMinEvenNumber(List<String> numbers) {
         return numbers
                 .stream()
-                .flatMapToInt(s -> Stream.of(s.split(","))
-                        .mapToInt(Integer::parseInt)
-                        .filter(n -> n % 2 == 0))
-                .min()
+                .flatMap(s -> Stream.of(s.split(",")))
+                .mapToInt(Integer::parseInt)
+                .filter(n -> n % 2 == 0).min()
                 .orElseThrow(() ->
                         new RuntimeException("Can't get min value from list: " + numbers));
     }
 
     public Double getOddNumsAverage(List<Integer> numbers) {
         return IntStream.range(0, numbers.size())
-                .mapToObj(i -> i % 2 == 1 ? numbers.get(i) - 1 : numbers.get(i))
+                .map(i -> i % 2 == 1 ? numbers.get(i) - 1 : numbers.get(i))
                 .filter(n -> n % 2 == 1)
-                .mapToInt(Integer::intValue)
                 .average()
-                .orElseThrow(NoSuchElementException::new);
+                .orElseThrow(() ->
+                        new NoSuchElementException("Can't get min value from list: " + numbers));
     }
 
     public List<Person> selectMenByAge(List<Person> peopleList, int fromAge, int toAge) {
@@ -58,8 +56,7 @@ public class StreamPractice {
                 .stream()
                 .filter(person -> person.getSex() == Person.Sex.WOMAN
                         && person.getAge() >= femaleAge)
-                .map(Person::getCats)
-                .flatMap(Collection::stream)
+                .flatMap(person -> person.getCats().stream())
                 .map(Cat::getName)
                 .collect(Collectors.toList());
     }
