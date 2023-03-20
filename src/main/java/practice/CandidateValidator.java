d50341032348156e570318283c4f6187cb59ce33
AndriiBohun <poikka69@gmail.com>
2023-03-20 20:44:36 +0200

    StreamPractice_C002

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,6 +1,22 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
 
-    //write your code here
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int REQUIRED_AGE = 35;
+    private static final int REQUIRED_YEARS = 10;
+    private static final int FIRST_DATE = 0;
+    private static final int SECOND_DATE = 1;
+    private static final String NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] yearsSplit = candidate.getPeriodsInUkr().split("-");
+        return candidate.getAge() >= REQUIRED_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && Integer.parseInt(yearsSplit[SECOND_DATE])
+                - Integer.parseInt(yearsSplit[FIRST_DATE]) >= REQUIRED_YEARS;
+    }
 }
