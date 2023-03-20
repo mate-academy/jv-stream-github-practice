e3eaa7d63c34ab21f5ca5dd2764ca36a49c92079
Yaroslav Kostenko <xsander3@gmail.com>
2023-03-20 14:29:56 +0200

    All methods are realized

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,17 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] periods = candidate.getPeriodsInUkr().split("-");
+        int period = Integer.parseInt(periods[1]) - Integer.parseInt(periods[0]);
+        return candidate.isAllowedToVote()
+                && candidate.getAge() >= 35
+                && candidate.getNationality().equals("Ukrainian")
+                && period > 10;
+    }
     //write your code here
 }
