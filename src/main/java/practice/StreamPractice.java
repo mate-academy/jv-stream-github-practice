ca548c216e607b8e2d4c91326a47ba1c02c128ec
IhorPetrytskyi333 <mygit333@gmail.com>
2023-03-20 20:02:08 -0700

    hw-16-solution_1

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,16 +1,16 @@
 package practice;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
 import model.Candidate;
 import model.Cat;
 import model.Person;
 
 public class StreamPractice {
-    CandidateValidator candidateValidator = new CandidateValidator();
+    private CandidateValidator candidateValidator = new CandidateValidator();
 
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
