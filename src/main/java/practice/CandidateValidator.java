03f46b75984ed769e6218dc8804ae3d16bdec939
mykoladymchenko <mykola.dymchenko@insightsoftware.com>
2023-03-23 09:54:48 +0100

    implemented interface Predicate in the class CandidateValidator and added solution for the methods: - findMinEvenNumber - getOddNumsAverage - selectMenByAge - getWorkablePeople - getCatsNames - validateCandidates

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,22 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final int MIN_PERIOD_IN_UKR = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        int yearFrom = Integer.parseInt(candidate
+                .getPeriodsInUkr()
+                .substring(0, candidate.getPeriodsInUkr().indexOf("-")));
+        int yearTo = Integer.parseInt(candidate.getPeriodsInUkr()
+                .substring(candidate.getPeriodsInUkr().indexOf("-") + 1));
+        return candidate.getAge() >= MIN_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && yearTo - yearFrom >= MIN_PERIOD_IN_UKR;
+    }
 }
