5826eed2ed57c0f537c3bc19063f5c01de55b09c
Igor Parovenko <igorparovenko@yahoo.com>
2023-03-29 17:32:20 +0200

    trying to fix Github error

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -43,7 +43,7 @@ public class StreamPractice {
     /**
      * Given a List of `Person` instances (having `name`, `age` and `sex` fields),
      * for example, `Arrays.asList( new Person(«Victor», 16, Sex.MAN),
-     * new Person(«Helen», 42, Sex.WOMAN)),
+     * new Person(«Helen», 42, Sex.WOMAN))`,
      * select from the List only men whose age is from `fromAge` to `toAge` inclusively.
      * <p>
      * Example: select men who can be recruited to army (from 18 to 27 years old inclusively).
