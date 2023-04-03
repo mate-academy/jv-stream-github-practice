bbbecbc0629ac26d697e1e53b7378aa3feca266d
Алексей <m.leshka955@gmail.com>
2023-04-03 10:08:18 +0300

    implementation of the predicate in CandidateValidator implementation Stream

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,24 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import model.Candidate;
+
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MINIMUM_AGE = 35;
+    private static final String CITIZENSHIP = "Ukrainian";
+    private static final int MINIMUM_TIME_IN_THE_COUNTRY = 10;
+    private static final int PERIOD_INT_UKRAINE_START = 0;
+    private static final int PERIOD_INT_UKRAINE_END = 1;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        final String[] periodCandidate = candidate.getPeriodsInUkr().split("-");
+        return candidate.getAge() >= MINIMUM_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(CITIZENSHIP)
+                && (Integer.parseInt(periodCandidate[PERIOD_INT_UKRAINE_END])
+                - (Integer.parseInt(periodCandidate[PERIOD_INT_UKRAINE_START])))
+                >= MINIMUM_TIME_IN_THE_COUNTRY;
+    }
 }
