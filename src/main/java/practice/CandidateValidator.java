d1689b282c74aebb09bb68578f188a62f11d86c4
antonsablev <live.incntr@gmail.com>
2023-04-09 14:55:57 +0200

    My implemenatation of CondidateValidator class and StreamPractice methods

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final int MIN_YEARS_LIVE_IN_UKRAINE = 10;
+    private static final String UA_NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.isAllowedToVote()
+                && candidate.getAge() >= MIN_AGE
+                && candidate.getNationality().equals(UA_NATIONALITY)
+                && countPeriod(candidate) > MIN_YEARS_LIVE_IN_UKRAINE;
+    }
+
+    private int countPeriod(Candidate candidate) {
+        String[] period = candidate.getPeriodsInUkr().split("-");
+        return Integer.parseInt(period[1]) - Integer.parseInt(period[0]);
+    }
 }
