555f803a767018122b094f20da8bdfacdec40147
Volodymyr Sakalosh <sva2005@ukr.net>
2023-04-15 20:26:07 +0300

    Implemented method getWorkablePeople()

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -95,7 +95,12 @@ public class StreamPractice {
      */
     public List<Person> getWorkablePeople(int fromAge, int femaleToAge,
                                           int maleToAge, List<Person> peopleList) {
-        return Collections.emptyList();
+        return peopleList.stream()
+                .filter(p -> (p.getSex() == Person.Sex.MAN
+                        && (p.getAge() >= fromAge && p.getAge() <= maleToAge))
+                        || (p.getSex() == Person.Sex.WOMAN
+                        && (p.getAge() >= fromAge && p.getAge() <= femaleToAge)))
+                .collect(Collectors.toList());
     }
 
     /**
