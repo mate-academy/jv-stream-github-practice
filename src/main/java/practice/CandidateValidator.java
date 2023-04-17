4cfe884b385cf5b76dc63e84dc84410626bd01d5
Bohdan Shevtsov <shevtsov.b@gmail.com>
2023-04-17 14:25:05 +0300

    Before first mentor's check

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,17 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    public boolean test(Candidate candidate) {
+        String[] periodInUAstring = candidate.getPeriodsInUkr().split("-");
+        int periodInUA = Integer.parseInt(periodInUAstring[1])
+                - Integer.parseInt(periodInUAstring[0]);
+        return (candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && periodInUA >= 10);
+    }
     //write your code here
 }
