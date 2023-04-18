af81ef8fd691172e3aa04055c63cd2b0051c0fc4
Volodymyr Sakalosh <sva2005@ukr.net>
2023-04-18 21:42:01 +0300

    Created Predicate fields

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -3,24 +3,12 @@ package practice;
 import java.util.function.Predicate;
 import model.Candidate;
 
-public class CandidateValidator implements Predicate<Candidate> {
-    @Override
-    public Predicate<Candidate> negate() {
-        return null;
-    }
-
-    @Override
-    public Predicate<Candidate> or(Predicate<? super Candidate> other) {
-        return null;
-    }
-
-    @Override
-    public boolean test(Candidate candidate) {
-        return false;
-    }
-
-    @Override
-    public Predicate<Candidate> and(Predicate<? super Candidate> other) {
-        return null;
-    }
+public class CandidateValidator {
+    Predicate<Candidate> toVote = Candidate::isAllowedToVote;
+    Predicate<Candidate> validAge = a -> a.getAge() >= 35;
+    Predicate<Candidate> isNationality = c -> c.getNationality().equals("Ukrainian");
+    Predicate<Candidate> period = p -> {
+        String[] periodOfYear = p.getPeriodsInUkr().split("-");
+        return Integer.parseInt(periodOfYear[1]) - Integer.parseInt(periodOfYear[0]) >= 10;
+    };
 }
