5a64a5737fef752d669054d708a70b3ab7ac0bc3
Volodymyr Sakalosh <sva2005@ukr.net>
2023-04-18 20:51:20 +0300

    Created in CandidateValidator class implementation interface Predicate

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,27 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import model.Candidate;
+
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public Predicate<Candidate> negate() {
+        return null;
+    }
+
+    @Override
+    public Predicate<Candidate> or(Predicate<? super Candidate> other) {
+        return null;
+    }
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return false;
+    }
+
+    @Override
+    public Predicate<Candidate> and(Predicate<? super Candidate> other) {
+        return null;
+    }
 }
