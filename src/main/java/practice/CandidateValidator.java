da5fc3afabf47221aa6fb95ea6c13f14014c5c12
Volodymyr Sakalosh <sva2005@ukr.net>
2023-04-18 21:51:31 +0300

    Fixed fields

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,10 +4,10 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator {
-    Predicate<Candidate> toVote = Candidate::isAllowedToVote;
-    Predicate<Candidate> validAge = a -> a.getAge() >= 35;
-    Predicate<Candidate> isNationality = c -> c.getNationality().equals("Ukrainian");
-    Predicate<Candidate> period = p -> {
+    private Predicate<Candidate> toVote = Candidate::isAllowedToVote;
+    private Predicate<Candidate> validAge = a -> a.getAge() >= 35;
+    private Predicate<Candidate> isNationality = c -> c.getNationality().equals("Ukrainian");
+    private Predicate<Candidate> period = p -> {
         String[] periodOfYear = p.getPeriodsInUkr().split("-");
         return Integer.parseInt(periodOfYear[1]) - Integer.parseInt(periodOfYear[0]) >= 10;
     };
