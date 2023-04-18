73faee8e70901d4322d4b0c40d942f2a6c076f04
Volodymyr Sakalosh <sva2005@ukr.net>
2023-04-18 21:58:26 +0300

    Created methods validVote(), validAge(), validNationality(), validPeriod()

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,10 +5,26 @@ import model.Candidate;
 
 public class CandidateValidator {
     private Predicate<Candidate> toVote = Candidate::isAllowedToVote;
-    private Predicate<Candidate> validAge = a -> a.getAge() >= 35;
+    private Predicate<Candidate> age = a -> a.getAge() >= 35;
     private Predicate<Candidate> isNationality = c -> c.getNationality().equals("Ukrainian");
     private Predicate<Candidate> period = p -> {
         String[] periodOfYear = p.getPeriodsInUkr().split("-");
         return Integer.parseInt(periodOfYear[1]) - Integer.parseInt(periodOfYear[0]) >= 10;
     };
+
+    public boolean validVote(Candidate candidate) {
+        return toVote.test(candidate);
+    }
+
+    public boolean validAge(Candidate candidate) {
+        return age.test(candidate);
+    }
+
+    public boolean validNationality(Candidate candidate) {
+        return isNationality.test(candidate);
+    }
+
+    public  boolean validPeriod(Candidate candidate) {
+        return period.test(candidate);
+    }
 }
