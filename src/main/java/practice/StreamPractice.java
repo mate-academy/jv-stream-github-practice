9360105f42d35598bc77e5cb9297699ef5062456
Volodymyr Sakalosh <sva2005@ukr.net>
2023-04-18 18:35:18 +0300

    Fixed import classes

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -2,9 +2,9 @@ package practice;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
