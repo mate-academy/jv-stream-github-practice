a5be1211e420b4d9bd68e91e1b91e1523781c108
Volodymyr Sakalosh <sva2005@ukr.net>
2023-04-20 20:43:33 +0300

    Deleted three methods

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -13,21 +13,6 @@ public class CandidateValidator implements Predicate<Candidate> {
     };
 
     @Override
-    public Predicate<Candidate> and(Predicate<? super Candidate> other) {
-        return null;
-    }
-
-    @Override
-    public Predicate<Candidate> negate() {
-        return null;
-    }
-
-    @Override
-    public Predicate<Candidate> or(Predicate<? super Candidate> other) {
-        return null;
-    }
-
-    @Override
     public boolean test(Candidate candidate) {
         return toVote.test(candidate) && age.test(candidate)
                 && isNationality.test(candidate) && period.test(candidate);
