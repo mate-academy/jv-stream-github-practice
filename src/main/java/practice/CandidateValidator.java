c043b25ab8244fc6ff2d883a10ee327ee1ad263a
Volodymyr Sakalosh <sva2005@ukr.net>
2023-04-20 20:46:15 +0300

    Added two constants

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,8 +5,8 @@ import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
     private Predicate<Candidate> toVote = Candidate::isAllowedToVote;
-    private Predicate<Candidate> age = a -> a.getAge() >= 35;
-    private Predicate<Candidate> isNationality = c -> c.getNationality().equals("Ukrainian");
+    final static int AGE = 35;
+    final static String NATIONALITY = "Ukrainian";
     private Predicate<Candidate> period = p -> {
         String[] periodOfYear = p.getPeriodsInUkr().split("-");
         return Integer.parseInt(periodOfYear[1]) - Integer.parseInt(periodOfYear[0]) >= 10;
