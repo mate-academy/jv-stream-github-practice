cd82769187a1826dbf2fb152a57de43c163777f2
Volodymyr Sakalosh <sva2005@ukr.net>
2023-04-20 20:54:59 +0300

    Deleted fields, fixed method test() and created method getPeriod()

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,17 +4,17 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    private Predicate<Candidate> toVote = Candidate::isAllowedToVote;
     final static int AGE = 35;
     final static String NATIONALITY = "Ukrainian";
-    private Predicate<Candidate> period = p -> {
-        String[] periodOfYear = p.getPeriodsInUkr().split("-");
-        return Integer.parseInt(periodOfYear[1]) - Integer.parseInt(periodOfYear[0]) >= 10;
-    };
 
     @Override
     public boolean test(Candidate candidate) {
-        return toVote.test(candidate) && age.test(candidate)
-                && isNationality.test(candidate) && period.test(candidate);
+        return candidate.isAllowedToVote() && candidate.getAge() >= AGE
+                && candidate.getNationality().equals(NATIONALITY) && getPeriod(candidate) >= 10;
+    }
+
+    private int getPeriod(Candidate candidate) {
+        String[] periodOfYear = candidate.getPeriodsInUkr().split("-");
+        return Integer.parseInt(periodOfYear[1]) - Integer.parseInt(periodOfYear[0]);
     }
 }
