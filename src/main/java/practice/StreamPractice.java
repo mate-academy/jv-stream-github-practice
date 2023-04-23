798ab74c7f6299e84a61a03d0f8cf7bd6711d1f5
Александр Загородний <git.hub.zagar@gmail.com>
2023-04-23 12:24:12 +0200

    fixed after review

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -14,7 +14,7 @@ public class StreamPractice {
         return numbers.stream()
                 .flatMap(line -> Arrays.stream(line.split(DELIMETR)))
                 .mapToInt(Integer::parseInt)
-                .filter(n -> n % 2 == 0)
+                .filter(number -> number % 2 == 0)
                 .min()
                 .orElseThrow(() -> 
                     new RuntimeException("Can't get min value from list: " + numbers));
