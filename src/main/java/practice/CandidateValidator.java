b5660f0b2954e28df2fa497c8615d24f01d239b7
irynamatveieva <irina.m2004@icloud.com>
2023-04-27 17:22:36 +0300

    implemented methods for stream practice task

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,29 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final String VALID_NATIONALITY = "Ukrainian";
+    private static final String SPLITTING_SYMBOL = "-";
+    private static final int FROM_YEAR = 0;
+    private static final int TO_YEAR = 1;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        if (candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(VALID_NATIONALITY)
+                && getPeriodInUkraine(candidate) >= 10) {
+            return true;
+        }
+        return false;
+    }
+
+    private int getPeriodInUkraine(Candidate candidate) {
+        String[] fromAndToYears = candidate.getPeriodsInUkr().split(SPLITTING_SYMBOL);
+        int fromYear = Integer.parseInt(fromAndToYears[FROM_YEAR]);
+        int toYear = Integer.parseInt(fromAndToYears[TO_YEAR]);
+        return toYear - fromYear;
+    }
 }
