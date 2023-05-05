acbc8b1e0c91c4a29a141f08eaee4e7ed2f49e9b
Viktoriia <reraitviktoria@gmail.com>
2023-05-05 12:19:25 +0200

    HW-jv-stream-github-practice-5-Of-May

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,19 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE_FOR_VOTING = 35;
+    private static final int MIN_TIME_LIVING_IN_UKRAINE = 10;
+    private static final String NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return Integer.parseInt(candidate.getPeriodsInUkr().split("-")[1])
+                - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[0])
+                >= MIN_TIME_LIVING_IN_UKRAINE
+                && candidate.isAllowedToVote() && candidate.getAge() >= MIN_AGE_FOR_VOTING
+                && candidate.getNationality().equals(NATIONALITY);
+    }
 }
