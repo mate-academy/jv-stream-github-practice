9af56141961016d95c21c9b919ab34bd4bd7a4e1
Nikita <katkovnikitka@gmail.com>
2023-05-10 19:03:50 +0200

    Update solution

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,9 +4,11 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    private static final int MIN_AGE = 35;
     private static final String NATIONALITY = "Ukrainian";
+    private static final int MIN_AGE = 35;
     private static final int YEARS_OF_LIVING = 10;
+    private static final int FIRST_INDEX_OF_ARRAY = 0;
+    private static final int SECOND_INDEX_OF_ARRAY = 1;
 
     @Override
     public boolean test(Candidate candidate) {
@@ -18,6 +20,7 @@ public class CandidateValidator implements Predicate<Candidate> {
 
     private int getYearsOfLiving(Candidate candidate) {
         String[] years = candidate.getPeriodsInUkr().split("-");
-        return Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
+        return Integer.parseInt(years[SECOND_INDEX_OF_ARRAY])
+                - Integer.parseInt(years[FIRST_INDEX_OF_ARRAY]);
     }
 }
