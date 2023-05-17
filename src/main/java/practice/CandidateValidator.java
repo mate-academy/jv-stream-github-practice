61964f6883073d7f15682349edd653bb22583263
V-Solianyk <112614843+V-Solianyk@users.noreply.github.com>
2023-05-17 19:42:42 +0300

    implemented all necessary methods using only stream API, and also implemented my CandidateValidator class and used its object in "validateCandidates" method.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,28 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final String SEPARATOR = "-";
+    private static final int MIN_AMOUNT_OF_YEARS = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MIN_AGE && candidate.isAllowedToVote()
+                && candidate.getNationality().equalsIgnoreCase(NATIONALITY)
+                && checkSufficientAmountOfYears(candidate);
+    }
+
+    private boolean checkSufficientAmountOfYears(Candidate candidate) {
+        if (candidate.getPeriodsInUkr() != null || !candidate.getPeriodsInUkr().isEmpty()) {
+            String[] yearsFromTo = candidate.getPeriodsInUkr().split(SEPARATOR);
+            return (Integer.parseInt(yearsFromTo[1]) - Integer.parseInt(yearsFromTo[0]))
+                    >= MIN_AMOUNT_OF_YEARS;
+        }
+        return false;
+    }
+
 }
