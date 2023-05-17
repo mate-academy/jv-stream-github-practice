19b8d289e7bd9acc829dff8b8a3b57d1658456b2
V-Solianyk <112614843+V-Solianyk@users.noreply.github.com>
2023-05-17 20:53:07 +0300

    added 2 constant variables and removed the empty line.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,6 +5,8 @@ import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_AGE = 35;
+    private static final int START_PERIOD = 0;
+    private static final int FINISH_PERIOD = 1;
     private static final String NATIONALITY = "Ukrainian";
     private static final String SEPARATOR = "-";
     private static final int MIN_AMOUNT_OF_YEARS = 10;
@@ -19,10 +21,9 @@ public class CandidateValidator implements Predicate<Candidate> {
     private boolean checkSufficientAmountOfYears(Candidate candidate) {
         if (candidate.getPeriodsInUkr() != null || !candidate.getPeriodsInUkr().isEmpty()) {
             String[] yearsFromTo = candidate.getPeriodsInUkr().split(SEPARATOR);
-            return (Integer.parseInt(yearsFromTo[1]) - Integer.parseInt(yearsFromTo[0]))
-                    >= MIN_AMOUNT_OF_YEARS;
+            return (Integer.parseInt(yearsFromTo[FINISH_PERIOD])
+                    - Integer.parseInt(yearsFromTo[START_PERIOD])) >= MIN_AMOUNT_OF_YEARS;
         }
         return false;
     }
-
 }
