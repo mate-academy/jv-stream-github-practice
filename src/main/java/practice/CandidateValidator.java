4dcfa366c2477f5e575f52a8cc1a5ec4230a4aaa
Yaroslav Khmaruk <yarik9656422@gmail.com>
2023-05-23 17:11:36 +0300

    added all required methods body and implemented CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int ALLOWED_AGE = 35;
+    private static final int NEEDED_PERIOD = 10;
+    private static final int PERIOD_START = 0;
+    private static final int PERIOD_END = 1;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final String SEPARATOR = "-";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] periodsInUkrArray = candidate.getPeriodsInUkr().split(SEPARATOR);
+        return candidate.getAge() >= ALLOWED_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && Integer.parseInt(periodsInUkrArray[PERIOD_END])
+                - Integer.parseInt(periodsInUkrArray[PERIOD_START]) >= NEEDED_PERIOD;
+    }
 }
