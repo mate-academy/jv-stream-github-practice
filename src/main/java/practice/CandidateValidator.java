a865e417060e30639160c1888da0de0ce8280b02
Pasha <pavlokrasota@gmail.com>
2023-05-23 14:48:55 +0300

    Solved task with using StreamAPI

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,26 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int PERSON_FROM_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int PERIOD_OF_LIVING = 10;
+    private static final int START_INDEX = 0;
+    private static final int END_INDEX = 1;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        int years = periodInUkr(candidate.getPeriodsInUkr());
+        return candidate.getAge() >= PERSON_FROM_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && years >= PERIOD_OF_LIVING;
+    }
+
+    private int periodInUkr(String period) {
+        String[] years = period.split("-");
+        return Integer.parseInt(years[END_INDEX]) - Integer.parseInt(years[START_INDEX]);
+    }
 }
