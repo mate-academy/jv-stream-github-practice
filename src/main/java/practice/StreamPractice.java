ab140298fe1edd50c4c08d274db1b9b519c1197b
Pavlo Polezhay <pasha200417@gmail.com>
2023-05-24 23:07:33 +0300

    Fixed

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -2,11 +2,9 @@ package practice;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
 import model.Candidate;
 import model.Cat;
 import model.Person;
