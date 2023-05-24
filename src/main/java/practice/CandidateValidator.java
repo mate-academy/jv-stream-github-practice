93b86d33cdd2ea4495386c4a5003d7e5a1d97c08
Mykyta Udovycehnko <nikitaudav123@gmail.com>
2023-05-24 22:37:42 +0300

    solution jv-stream-practice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int VALID_AGE = 35;
+    private static final String VALID_NATIONALITY = "Ukrainian";
+    private static final int VALID_LIFETIME = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return VALID_NATIONALITY.equals(candidate.getNationality())
+                && candidate.isAllowedToVote()
+                && candidate.getAge() >= VALID_AGE
+                && lifetimeInUkraine(candidate.getPeriodsInUkr()) >= VALID_LIFETIME;
+    }
+
+    private int lifetimeInUkraine(String periodsInUkr) {
+        String[] splitYear = periodsInUkr.split("-");
+        return Integer.parseInt(splitYear[1]) - Integer.parseInt(splitYear[0]);
+    }
 }
