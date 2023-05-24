473c21d35abcdd32e8590b2dc41a0708a3e0423c
Yaroslav Khmaruk <yarik9656422@gmail.com>
2023-05-24 11:31:07 +0300

    add fixes after mentor review

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,19 +5,23 @@ import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int ALLOWED_AGE = 35;
-    private static final int NEEDED_PERIOD = 10;
-    private static final int PERIOD_START = 0;
-    private static final int PERIOD_END = 1;
+    private static final int PERIOD_MIN = 10;
+    private static final int PERIOD_START_INDEX = 0;
+    private static final int PERIOD_END_INDEX = 1;
     private static final String NATIONALITY = "Ukrainian";
     private static final String SEPARATOR = "-";
 
     @Override
     public boolean test(Candidate candidate) {
-        String[] periodsInUkrArray = candidate.getPeriodsInUkr().split(SEPARATOR);
         return candidate.getAge() >= ALLOWED_AGE
                 && candidate.isAllowedToVote()
                 && candidate.getNationality().equals(NATIONALITY)
-                && Integer.parseInt(periodsInUkrArray[PERIOD_END])
-                - Integer.parseInt(periodsInUkrArray[PERIOD_START]) >= NEEDED_PERIOD;
+                && getPeriodInYears(candidate.getPeriodsInUkr()) >= PERIOD_MIN;
+    }
+
+    private int getPeriodInYears(String period) {
+        String[] periodsInUkrArray = period.split(SEPARATOR);
+        return Integer.parseInt(periodsInUkrArray[PERIOD_END_INDEX])
+                - Integer.parseInt(periodsInUkrArray[PERIOD_START_INDEX]);
     }
 }
