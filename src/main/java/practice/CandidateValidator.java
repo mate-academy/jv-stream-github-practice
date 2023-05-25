c06dc2059317cfb25a06030de2b883e4af4d7089
DVArt <dvart@hotmail.com>
2023-05-26 00:42:34 +0300

    Create StreamPractice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,27 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.Arrays;
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    private static final int MIN_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int VALID_PERIOD = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MIN_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && isValidPeriodInUkr(candidate) >= VALID_PERIOD;
+    }
+
+    public int isValidPeriodInUkr(Candidate candidate) {
+        int[] years = Arrays.stream(candidate.getPeriodsInUkr().split("-"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        return Math.abs(years[1] - years[0]);
+    }
 }
