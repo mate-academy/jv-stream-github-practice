04663b281fd21669af39884e3f4aba7a2e441775
Serhii Dudko <nechupara@gmail.com>
2023-05-26 12:30:12 +0300

    implemented mentor's requested changes

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,8 +5,7 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    //write your code here
-    private static final int MINIMUM_AGE = 35;
+    private static final int MIN_AGE = 35;
     private static final int MIN_RESIDENCE_PERIOD = 10;
     private static final String REQUIRED_NATIONALITY = "Ukrainian";
     private static final String DATE_SEPARATOR = "-";
@@ -16,7 +15,7 @@ public class CandidateValidator implements Predicate<Candidate> {
         int residencePeriod = Arrays.stream(candidate.getPeriodsInUkr().split(DATE_SEPARATOR))
                 .mapToInt(Integer::parseInt)
                 .reduce(0, (a, b) -> a == 0 ? a - b : a + b);
-        return candidate.getAge() >= MINIMUM_AGE
+        return candidate.getAge() >= MIN_AGE
                 && candidate.isAllowedToVote()
                 && candidate.getNationality().equals(REQUIRED_NATIONALITY)
                 && residencePeriod >= MIN_RESIDENCE_PERIOD;
