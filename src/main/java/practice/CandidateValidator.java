ec9f0b94753ce8148301d3857f877e7585654db0
Anastasiia Variichuk <stacyvariuchuk@gmail.com>
2023-05-26 12:12:37 +0300

    fixed mentor review

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -8,6 +8,8 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_TIME_LIVING_IN_UKR = 10;
     private static final String NATIONALITY = "Ukrainian";
     private static final String DASH = "-";
+    private static final int FROM_YEAR = 0;
+    private static final int TO_YEAR = 1;
 
     @Override
     public boolean test(Candidate candidate) {
@@ -19,6 +21,6 @@ public class CandidateValidator implements Predicate<Candidate> {
 
     private int getLivingInUkrTime(Candidate candidate) {
         String[] years = candidate.getPeriodsInUkr().split(DASH);
-        return Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
+        return Integer.parseInt(years[TO_YEAR]) - Integer.parseInt(years[FROM_YEAR]);
     }
 }
