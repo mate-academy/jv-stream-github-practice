1ef3b7f036263bab2c38212fbe86b27d15a783de
Володимир <vova.malyarchuk2@gmail.com>
2023-05-26 12:10:50 +0300

    Implemented task VALIDATION-2

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,22 +4,22 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    private static final int AGE = 35;
+    private static final int MIN_AGE = 35;
     private static final String NATIONALITY = "Ukrainian";
-    private static final int PERIODSINUKR = 10;
+    private static final int MIN_PERIODS_IN_UKRAINE = 10;
     private static final String SEPARATOR = "-";
     private static final int END_PERIOD = 1;
     private static final int START_PERIOD = 0;
 
     @Override
     public boolean test(Candidate candidate) {
-        return candidate.getAge() >= AGE
+        return candidate.getAge() >= MIN_AGE
                 && candidate.getNationality().equals(NATIONALITY)
-                && getYearsOfLiving(candidate) >= PERIODSINUKR
+                && requirementYearsOfLiving(candidate) >= MIN_PERIODS_IN_UKRAINE
                 && candidate.isAllowedToVote();
     }
 
-    private int getYearsOfLiving(Candidate candidate) {
+    private int requirementYearsOfLiving(Candidate candidate) {
         String [] years = candidate.getPeriodsInUkr().split(SEPARATOR);
         return Integer.parseInt(years[END_PERIOD])
                 - Integer.parseInt(years[START_PERIOD]);
