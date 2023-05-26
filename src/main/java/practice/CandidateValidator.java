1679eedac58afad022104ac5bfa707ef3b075722
Oleksandr Balan <litecloud.nohax@gmail.com>
2023-05-26 15:22:55 +0300

    added solution for Stream practice task

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_YEARS_IN_UKRAINE = 10;
+    private static final int MIN_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MIN_AGE
+                && candidate.getNationality().equals(NATIONALITY)
+                && getYearsOfLiving(candidate) > MIN_YEARS_IN_UKRAINE
+                && candidate.isAllowedToVote();
+    }
+
+    private int getYearsOfLiving(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split("-");
+        return Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
+    }
 }
