7dd0ff81e2bf157674457f85097188ed21f704c8
Mykola Panasiuk <mikola.panasyuk.01@gmail.com>
2023-05-26 14:05:50 +0300

    fixed checkstyle

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -2,7 +2,6 @@ package practice;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import model.Candidate;
