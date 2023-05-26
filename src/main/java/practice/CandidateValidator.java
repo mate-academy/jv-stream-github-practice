4f3f3dc2d35526b8e387d64fa91016e0cc583265
Victor Ruban <edmont373@gmail.com>
2023-05-26 16:04:05 +0300

    Completed the task StreamPractice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,27 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MINIMUM_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int MINIMUM_LIVE_IN_UA = 10;
+    private static final int INDEX_FROM_YEAR = 0;
+    private static final int INDEX_TO_YEAR = 1;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        int age = candidate.getAge();
+        String nationality = candidate.getNationality();
+        int liveInUa = liveInUkraine(candidate);
+        return age >= MINIMUM_AGE && nationality.equals(NATIONALITY)
+                && liveInUa >= MINIMUM_LIVE_IN_UA
+                && candidate.isAllowedToVote();
+    }
+
+    private int liveInUkraine(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split("-");
+        return Integer.parseInt(years[INDEX_TO_YEAR]) - Integer.parseInt(years[INDEX_FROM_YEAR]);
+    }
 }
