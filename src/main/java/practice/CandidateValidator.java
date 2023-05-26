82c1a035f9cde497131c369ce198010102a4a2d7
eugenetolkunov <yevhentolkunov@gmail.com>
2023-05-26 12:03:34 +0300

    fix mistakes

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -8,17 +8,20 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final String NATIONALITY = "Ukrainian";
     private static final int MIN_PERIOD_IN_UKR = 10;
     private static final String SEPARATOR = "-";
+    private static final int FIRST_INDEX = 0;
+    private static final int LAST_INDEX = 1;
 
     @Override
     public boolean test(Candidate candidate) {
         return (candidate.getAge() >= MIN_AGE
                 && candidate.isAllowedToVote()
                 && candidate.getNationality().equals(NATIONALITY)
-                && ageInUkr(candidate.getPeriodsInUkr()) >= MIN_PERIOD_IN_UKR);
+                && timeSpentInUkr(candidate.getPeriodsInUkr()) >= MIN_PERIOD_IN_UKR);
     }
 
-    private int ageInUkr(String period) {
+    private int timeSpentInUkr(String period) {
         String[] periodInUkr = period.split(SEPARATOR);
-        return Integer.parseInt(periodInUkr[1]) - Integer.parseInt(periodInUkr[0]);
+        return Integer.parseInt(periodInUkr[LAST_INDEX])
+                - Integer.parseInt(periodInUkr[FIRST_INDEX]);
     }
 }
