879c0b553cae9dc7e2c95cb49de7771fd76748bc
Petro Kobevko <kobevkopi@gmail.com>
2023-05-27 14:42:00 +0200

    Refactored code according to the student's comments

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,7 +7,7 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     private static final String UKRAINIAN_NATIONALITY = "Ukrainian";
     private static final int MIN_AGE = 35;
-    private static final String HYPHEN_REGEX = "-";
+    private static final String YEARS_SEPARATOR = "-";
     private static final int MIN_DURATION_OF_RESIDENCE_IN_UKRAINE = 10;
     private static final int INDEX_OF_FIRST_RESIDENCE_YEAR = 0;
     private static final int INDEX_OF_LAST_RESIDENCE_YEAR = 1;
@@ -20,12 +20,11 @@ public class CandidateValidator implements Predicate<Candidate> {
     }
 
     private boolean checkPeriodsInUkr(Candidate candidate) {
-        int[] periodsInUkr = Arrays.stream(candidate.getPeriodsInUkr().split(HYPHEN_REGEX))
+        int[] periodsInUkr = Arrays.stream(candidate.getPeriodsInUkr().split(YEARS_SEPARATOR))
                 .mapToInt(Integer::parseInt)
                 .toArray();
         return periodsInUkr[INDEX_OF_LAST_RESIDENCE_YEAR]
                 - periodsInUkr[INDEX_OF_FIRST_RESIDENCE_YEAR]
                 >= MIN_DURATION_OF_RESIDENCE_IN_UKRAINE;
     }
-    //write your code here
 }
