1e98427d94d02e4a5cbcc4c8b6d8e78a24dc7fb0
Ros <rospsix@gmail.com>
2023-06-02 18:02:23 +0100

    CandidateValidator
    1.Checks if the candidate's age is greater than or equal to the minimum age requirement.
    
    2.Checks if the candidate is allowed to vote.
    
    3. validate the age.
    
    4.validate the nationality.
    
    5. Calls the hasMinPeriodInUkraine method to validate if the candidate has spent the minimum required period in Ukraine.
    - hasMinPeriodInUkraine: Calculates the period of time the candidate has spent in Ukraine.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,29 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final int MIN_PERIOD = 10;
+    private static final String NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MIN_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && hasMinPeriodInUkraine(candidate);
+    }
+
+    private boolean hasMinPeriodInUkraine(Candidate candidate) {
+        int periodInUkr = calculatePeriodInUkraine(candidate.getPeriodsInUkr());
+        return periodInUkr >= MIN_PERIOD;
+    }
+
+    private int calculatePeriodInUkraine(String period) {
+        String[] years = period.split("-");
+        return Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
+    }
+
 }
