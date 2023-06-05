8df59d2f9753ba77ccedd64aefc07e524b10c0a7
Gleb Chebotarov <taronassvolt@gmail.com>
2023-06-05 20:48:47 +0300

    work with stream done.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,38 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.time.Year;
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        String name = candidate.getName();
+        int age = candidate.getAge();
+        boolean allowedToVote = candidate.isAllowedToVote();
+        String nationality = candidate.getNationality();
+        String periodsInUkr = candidate.getPeriodsInUkr();
+
+        return age >= 35 && allowedToVote
+                && nationality.equals("Ukrainian")
+                && hasLivedInUkraineFor10Years(periodsInUkr)
+                && candidate.getName().equals(name);
+    }
+
+    private boolean hasLivedInUkraineFor10Years(String periodsInUkr) {
+        int totalYearsInUkraine = 0;
+
+        String[] years = periodsInUkr.split("-");
+        if (years.length > 1) {
+            int startYear = Integer.parseInt(years[0]);
+            int endYear = Integer.parseInt(years[1]);
+
+            Year startDate = Year.of(startYear);
+            Year endDate = Year.of(endYear);
+
+            totalYearsInUkraine = endDate.compareTo(startDate);
+        }
+
+        return totalYearsInUkraine >= 10;
+    }
 }
