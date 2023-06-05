b59091c611a7e8604c704a609108e3945dcd8246
Vlad Andriichuk <vlad150096@gmail.com>
2023-06-05 15:08:03 +0200

    Realized methods in StreamPractice class and implemented interface Predicate in the class CandidateValidator.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    public static final String NATIONALITY = "Ukrainian";
+    public static final int REQUIRED_AGE = 35;
+    public static final int MIN_TIME_LIVE_IN_UA = 10;
+    public static final boolean ALLOWED_TO_VOTE = true;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] periodInUA = candidate.getPeriodsInUkr().split("-");
+        return candidate.getNationality().equals(NATIONALITY)
+                && candidate.getAge() >= REQUIRED_AGE
+                && Integer.parseInt(periodInUA[1]) - Integer.parseInt(periodInUA[0])
+                >= MIN_TIME_LIVE_IN_UA
+                && candidate.isAllowedToVote() == ALLOWED_TO_VOTE;
+    }
 }
