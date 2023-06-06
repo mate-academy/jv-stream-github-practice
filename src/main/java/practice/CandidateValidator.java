ff2733de6bc54d8521086be0b32938dfb8cef92a
pochtalon <pochtalon1010@gmail.com>
2023-06-06 18:42:07 +0300

    Created 3 constants in CandidateValidator class; Implemented Predicate and overrode its method; Created additional method in CandidateValidator class; Implemented methods in StreamPractice class;

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import model.Candidate;
+
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int validAge = 35;
+    private static final int requirementYears = 10;
+    private static final String validNationality = "Ukrainian";
+
+    private boolean wasLivingTenYearsInCountry (String period) {
+        String[] years = period.split("-");
+        return Integer.parseInt(years[1]) - Integer.parseInt(years[0]) >= requirementYears;
+    }
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= validAge && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(validNationality)
+                && wasLivingTenYearsInCountry(candidate.getPeriodsInUkr());
+    }
 }
