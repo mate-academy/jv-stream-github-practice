2206bcccd4b9d651271cc99cadbd073bc853e42e
misha-levko <mykhailo.levko@gmail.com>
2023-06-12 23:37:47 -0400

    StreamPracticeJava, CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final int TIME_LIVE_IN_UA = 10;
+    private static final String NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MIN_AGE
+                && candidate.getNationality().equals(NATIONALITY)
+                && candidate.isAllowedToVote()
+                && periodsInUkraine(candidate);
+    }
+
+    public static boolean periodsInUkraine(Candidate candidate) {
+        String[] timeInUkr = candidate.getPeriodsInUkr().split("-");
+        return Integer.parseInt(timeInUkr[1]) - Integer.parseInt(timeInUkr[0]) > TIME_LIVE_IN_UA;
+    }
 }
