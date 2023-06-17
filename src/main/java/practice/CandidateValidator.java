8f70a7371a50c47b164c5c6a39c7357c0bcc1f76
marinakusniruk <Kushnirukmv@gmail.com>
2023-06-17 14:43:16 +0200

    Hometask completed jv-stream

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,19 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE_FOR_CANDIDATE = 35;
+    private static final int MIN_YEAR_LIVE_IN_UKRAINE = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MIN_AGE_FOR_CANDIDATE && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && (Integer.parseInt(candidate.getPeriodsInUkr().split("-")[1])
+                - (Integer.parseInt(candidate.getPeriodsInUkr()
+                .split("-")[0])) > MIN_YEAR_LIVE_IN_UKRAINE);
+    }
     //write your code here
 }
