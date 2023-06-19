bf08ae33a226092795e1e6658d320379da622b98
marinakusniruk <Kushnirukmv@gmail.com>
2023-06-19 23:00:43 +0200

    Hometask completed jv-stream required changes done2

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,14 +6,18 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_AGE_FOR_CANDIDATE = 35;
     private static final int MIN_YEAR_LIVE_IN_UKRAINE = 10;
+    private static final String SEPARATOR = "-";
+    private static final int CANDIDATE_INDEX_ZERO = 0;
+    private static final int CANDIDATE_INDEX_ONE = 1;
 
     @Override
     public boolean test(Candidate candidate) {
         return candidate.getAge() >= MIN_AGE_FOR_CANDIDATE && candidate.isAllowedToVote()
                 && candidate.getNationality().equals("Ukrainian")
-                && (Integer.parseInt(candidate.getPeriodsInUkr().split("-")[1])
+                && (Integer.parseInt(candidate.getPeriodsInUkr()
+                .split(SEPARATOR)[CANDIDATE_INDEX_ONE])
                 - (Integer.parseInt(candidate.getPeriodsInUkr()
-                .split("-")[0])) > MIN_YEAR_LIVE_IN_UKRAINE);
+                .split(SEPARATOR)[CANDIDATE_INDEX_ZERO])) > MIN_YEAR_LIVE_IN_UKRAINE);
     }
     //write your code here
 }
