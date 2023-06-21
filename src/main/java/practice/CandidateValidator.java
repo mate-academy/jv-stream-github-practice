ace78f6662ac61de4f929fe74093d40a70a688cb
Dmytro Voronin <d.voronin.owen@gmail.com>
2023-06-21 11:34:33 +0300

    Checkstyle gen3

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,8 +1,9 @@
 package practice;
 
-import model.Candidate;
 import java.time.LocalDate;
 import java.util.function.Predicate;
+
+import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
     //write your code here
