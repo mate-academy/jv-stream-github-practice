f01cc9509d0d9643a0a9c2d3e80d7d5fca2dd1a7
Liubomyr Kniazkyi <knyazkyylubomir@gmail.com>
2023-07-08 18:43:51 +0300

    implemented several methods, created own class CandidateValidator and implements Predicate

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,28 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int FROM_AGE = 35;
+    private static final int INDEX_PERIOD_FROM = 0;
+    private static final int INDEX_PERIOD_TO = 1;
+    private static final int MIN_ALLOWED_YEARS = 10;
+    private static final String ALLOWED_NATIONALITY = "Ukrainian";
+    private static final String DELIMITER = "-";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= FROM_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(ALLOWED_NATIONALITY)
+                && getAgesLivedInUkr(candidate) >= MIN_ALLOWED_YEARS;
+    }
+
+    private int getAgesLivedInUkr(Candidate candidate) {
+        String[] periodInUkr = candidate.getPeriodsInUkr().split(DELIMITER);
+        int livesFrom = Integer.parseInt(periodInUkr[INDEX_PERIOD_FROM]);
+        int livedTo = Integer.parseInt(periodInUkr[INDEX_PERIOD_TO]);
+        return livedTo - livesFrom;
+    }
 }
