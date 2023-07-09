8ec6a391ca7eb11dc7a675a83a8dbb5542c8c358
mIst <bogdankharchenko77@gmail.com>
2023-07-09 12:34:09 +0300

    added jv-stream-github-practice solution

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,29 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE_FOR_CANDIDATE = 35;
+    private static final int MIN_PERIOD_IN_UKR = 10;
+    private static final int INDEX_PERIOD_FROM = 0;
+    private static final int INDEX_PERIOD_TO = 1;
+    private static final String ALLOWED_NATIONALITY = "Ukrainian";
+    private static final String LINE_SPLITERATOR = "-";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MIN_AGE_FOR_CANDIDATE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(ALLOWED_NATIONALITY)
+                && periodInUkr(candidate) >= MIN_PERIOD_IN_UKR;
+    }
+
+    private int periodInUkr(Candidate candidate) {
+        String periodInUkr = candidate.getPeriodsInUkr();
+        String[] periodFromTo = periodInUkr.split(LINE_SPLITERATOR);
+        Integer yearFromInUkr = Integer.parseInt(periodFromTo[INDEX_PERIOD_FROM]);
+        Integer yearToInUkr = Integer.parseInt(periodFromTo[INDEX_PERIOD_TO]);
+        return yearToInUkr - yearFromInUkr;
+    }
 }
