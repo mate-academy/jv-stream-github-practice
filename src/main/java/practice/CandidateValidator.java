a8aaf7aef28f4754deea4f1c2f85ef210e0eb7da
Sergey Bogatkin <daniltisenko22@gmail.com>
2023-07-17 15:20:31 +0300

    Refactoring

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -8,26 +8,18 @@ public class CandidateValidator implements Predicate<Candidate> {
     public static final int MIN_ALLOWED_AGE = 35;
     public static final String ALLOWED_NATIONALITY = "Ukrainian";
     public static final String DELIMITER = "-";
-    public static final int FROM = 0;
-    public static final int TO = 1;
+    public static final int PERIOD_START_INDEX = 0;
+    public static final int PERIOD_END_INDEX = 1;
     public static final int MIN_ALLOWED_PERIOD = 10;
 
     @Override
     public boolean test(Candidate candidate) {
-        if (candidate.getAge() < MIN_ALLOWED_AGE) {
-            return false;
-        }
-        if (!candidate.isAllowedToVote()) {
-            return false;
-        }
-        if (!Objects.equals(candidate.getNationality(), ALLOWED_NATIONALITY)) {
-            return false;
-        }
         String[] inUkraine = candidate.getPeriodsInUkr().split(DELIMITER);
-        int period = Integer.parseInt(inUkraine[TO]) - Integer.parseInt(inUkraine[FROM]);
-        if (period < MIN_ALLOWED_PERIOD) {
-            return false;
-        }
-        return true;
+        int period = Integer.parseInt(inUkraine[PERIOD_END_INDEX])
+                - Integer.parseInt(inUkraine[PERIOD_START_INDEX]);
+        return candidate.getAge() >= MIN_ALLOWED_AGE
+                && candidate.isAllowedToVote()
+                && Objects.equals(candidate.getNationality(), ALLOWED_NATIONALITY)
+                && period >= MIN_ALLOWED_PERIOD;
     }
 }
