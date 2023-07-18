53aa136d3ed6132d223d977b2e6aca9a72ad8af7
Yurii Trokhymenko <uta1965_@ukr.net>
2023-07-18 13:13:21 +0200

    added fixes after review 3

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,14 +5,15 @@ import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
     //write your code here
-    private static final String NATIONALITY_UKR = "Ukrainian";
     private static final int MIN_AGE = 35;
+    private static final String NATIONALITY_UKR = "Ukrainian";
+    private static final int PERIOD_DURATION = 10;
 
     @Override
     public boolean test(Candidate candidate) {
         return candidate.getAge() >= MIN_AGE
                 && candidate.getNationality().equals(NATIONALITY_UKR)
                 && candidate.isAllowedToVote()
-                && candidate.getPeriodInUkrDuration() >= 10;
+                && candidate.getPeriodInUkrDuration() >= PERIOD_DURATION;
     }
 }
