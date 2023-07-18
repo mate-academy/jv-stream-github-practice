410d01eea0b3ba7b47774adb11cd672d26702fae
heilxwotan1 <noobdima228@gmail.com>
2023-07-18 15:37:12 +0300

    jv-stream-practice solution 1. implemented CandidateValidator class 2. implemented all methods in StreamPractice class

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int START_INDEX = 0;
+    private static final int END_INDEX = 1;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] splitPeriod = candidate.getPeriodsInUkr().split("-");
+        int yearsInCountry = Integer.parseInt(splitPeriod[END_INDEX])
+                - Integer.parseInt(splitPeriod[START_INDEX]);
+        return candidate.isAllowedToVote()
+                && candidate.getAge() >= 35
+                && candidate.getNationality().equals(NATIONALITY)
+                && yearsInCountry >= 10;
+    }
 }
