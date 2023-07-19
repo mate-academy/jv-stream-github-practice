5906ffd2a47c5ee4ac550cf04d0a35320c03f93a
Bohdan Karvatskyi <bogdankarvatskiy14@gmail.com>
2023-07-19 22:34:29 +0300

    complate task

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,24 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_YEAR_TO_RUN_FOR_PRESIDENT = 35;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getNationality().equalsIgnoreCase("Ukrainian")
+                && candidate.getAge() >= MIN_YEAR_TO_RUN_FOR_PRESIDENT
+                && calculateYearsInUkraine(candidate) > 10
+                && candidate.isAllowedToVote();
+    }
+
+    private int calculateYearsInUkraine(Candidate candidate) {
+        String periodsInUkr = candidate.getPeriodsInUkr();
+        String[] years = periodsInUkr.split("-");
+        int startYear = Integer.parseInt(years[0]);
+        int endYear = Integer.parseInt(years[1]);
+        return endYear - startYear;
+    }
 }
