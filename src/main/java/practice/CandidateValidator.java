19dbd791ed1aaa1019d20b4f6619ed76d11f507e
Evhenii Polishuchenko <sds191sds@gmail.com>
2023-07-19 22:37:26 +0300

    initial commit

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final String DASH = "-";
+    private static final int beganToLive = 0;
+    private static final int finishedLiving = 1;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && yearsInUkraine(candidate) >= 10;
+    }
+
+    private int yearsInUkraine(Candidate candidate) {
+        String[] data = candidate.getPeriodsInUkr().split(DASH);
+        return Integer.parseInt(data[finishedLiving]) - Integer.parseInt(data[beganToLive]);
+    }
 }
