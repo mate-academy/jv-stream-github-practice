eb52b12b28a3c9d3f1f2710a30a24d7300e4b552
Denis Moroz <moroz06denis@gmail.com>
2023-07-21 17:44:47 +0200

    implemented new methods in StreamPractice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,30 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+
+    private static final String ADMISSIBLE_NATIONALITY = "Ukrainian";
+    private static final int MIN_YEAR_IN_UKRAINE = 10;
+    private static final String DASH = "-";
+    private static final int INDEX_START_OF_RESIDENCE = 0;
+    private static final int INDEX_END_OF_RESIDENCE = 1;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MIN_AGE
+                && candidate.isAllowedToVote()
+                && ADMISSIBLE_NATIONALITY.equals(candidate.getNationality())
+                && hasRequiredYearsInUkraine(candidate);
+    }
+
+    private boolean hasRequiredYearsInUkraine(Candidate candidate) {
+        String[] periodInUkraine = candidate.getPeriodsInUkr().split(DASH);
+        int startOfResidence = Integer.parseInt(periodInUkraine[INDEX_START_OF_RESIDENCE]);
+        int endOfStay = Integer.parseInt(periodInUkraine[INDEX_END_OF_RESIDENCE]);
+
+        return MIN_YEAR_IN_UKRAINE <= endOfStay - startOfResidence;
+    }
 }
