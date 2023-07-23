f14dee66db3470dce95f72d9478aed8c1a9e69fc
Denys-Maksym Kachalup <denys.k82@gmail.com>
2023-07-23 13:25:22 +0300

    deleted magic numbers

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,21 +5,24 @@ import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
     private static final String SEPARATOR = "-";
-    private static final int START_YEAR = 0;
-    private static final int END_YEAR = 1;
+    private static final int START_YEAR_INDEX = 0;
+    private static final int END_YEAR_INDEX = 1;
+    private static final int MIN_AGE_VOTE_PRESIDENT = 35;
+    private static final String NATIONALITY_PRESIDENT = "Ukrainian";
+    private static final int MIN_PERIOD_LIVE_IN_UKRAINE = 10;
 
     @Override
     public boolean test(Candidate candidate) {
-        return candidate.getAge() >= 35
-                && candidate.getNationality().equals("Ukrainian")
+        return candidate.getAge() >= MIN_AGE_VOTE_PRESIDENT
+                && candidate.getNationality().equals(NATIONALITY_PRESIDENT)
                 && candidate.isAllowedToVote()
                 && wasPeriodInUkraineMoreTenYear(candidate.getPeriodsInUkr());
     }
 
     private boolean wasPeriodInUkraineMoreTenYear(String periodInUkraine) {
         String[] periodSplited = periodInUkraine.split(SEPARATOR);
-        return Integer.parseInt(periodSplited[END_YEAR])
-                - Integer.parseInt(periodSplited[START_YEAR]) >= 10;
+        return Integer.parseInt(periodSplited[END_YEAR_INDEX])
+                - Integer.parseInt(periodSplited[START_YEAR_INDEX]) >= MIN_PERIOD_LIVE_IN_UKRAINE;
 
     }
     //write your code here
