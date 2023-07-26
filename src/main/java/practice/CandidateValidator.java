54c416ca84dda3c4264a6ededfc090dbb069b9f6
Влад <dakadofordisluv@gmail.com>
2023-07-26 17:09:27 +0300

    changed CandidateValidator.class : - implemented 'Predicate<Candidate>' - added 'validatePeriodsInUkr' method

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,26 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.Arrays;
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int CANDIDATE_MIN_AGE = 35;
+    private static final int MIN_YEARS_LIVING_IN_UKR = 10;
+    private static final String VALID_NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.isAllowedToVote()
+                && candidate.getAge() >= CANDIDATE_MIN_AGE
+                && candidate.getNationality().equals(VALID_NATIONALITY)
+                && validatePeriodsInUkr(candidate.getPeriodsInUkr());
+    }
+
+    private boolean validatePeriodsInUkr(String periodsInUkr) {
+        return Arrays.stream(periodsInUkr.split("-"))
+                .mapToInt(Integer::parseInt)
+                .reduce((x, y) -> y - x)
+                .getAsInt() >= MIN_YEARS_LIVING_IN_UKR;
+    }
 }
