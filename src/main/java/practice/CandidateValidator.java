a878b80558c98fc2b2709f325cc484c661b25b13
Evhenii Polishuchenko <sds191sds@gmail.com>
2023-07-26 10:00:20 +0300

    fixed constants names

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,8 +5,8 @@ import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
     private static final String DASH = "-";
-    private static final int beganToLive = 0;
-    private static final int finishedLiving = 1;
+    private static final int BEGAN_TO_LIVE_INDEX = 0;
+    private static final int FINISHED_LIVING_INDEX = 1;
     private static final int MIN_AGE = 35;
     private static final int MIN_YEARS_IN_UKRAINE = 10;
 
@@ -20,6 +20,7 @@ public class CandidateValidator implements Predicate<Candidate> {
 
     private int yearsInUkraine(Candidate candidate) {
         String[] data = candidate.getPeriodsInUkr().split(DASH);
-        return Integer.parseInt(data[finishedLiving]) - Integer.parseInt(data[beganToLive]);
+        return Integer.parseInt(data[FINISHED_LIVING_INDEX])
+                - Integer.parseInt(data[BEGAN_TO_LIVE_INDEX]);
     }
 }
