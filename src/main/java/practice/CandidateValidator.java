7b7cd5eb3d55d098ff9feb7a82a12131be309f98
Zhdanov Anton <anton1992xxxx@gmail.com>
2023-07-27 21:01:26 +0300

    stream-api-practice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_PERIOD = 10;
+    private static final int MIN_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final String LINE_SEPARATOR = "-";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] periods = candidate.getPeriodsInUkr().split(LINE_SEPARATOR);
+        int period = Integer.parseInt(periods[1]) - Integer.parseInt(periods[0]);
+        return candidate.isAllowedToVote()
+                && candidate.getAge() >= MIN_AGE
+                && candidate.getNationality().equals(NATIONALITY)
+                && period > MIN_PERIOD;
+    }
 }
