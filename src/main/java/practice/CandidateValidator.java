6df37ebd967df99030b24f726f21f243cecf58fc
Vladyslav Ihnatiuk <123470878+Someboty@users.noreply.github.com>
2023-07-27 21:11:57 +0300

    Wrote methods in StreamPractice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.isAllowedToVote()
+                && candidate.getAge() >= 35
+                && candidate.getNationality().equals("Ukrainian")
+                && getYearsInUkraine(candidate.getPeriodsInUkr()) >= 10;
+    }
+
+    private int getYearsInUkraine(String periodsInUkr) {
+        String[] periodSeparated = periodsInUkr.split("-");
+        return Integer.parseInt(
+                periodSeparated[1]) - Integer.parseInt(periodSeparated[0]);
+    }
 }
