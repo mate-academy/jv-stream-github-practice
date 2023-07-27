6b7fa6975e3c008e51da4bc808327678bdf1c0d2
Arsen Mehdalskyi <arsen25public@gmail.com>
2023-07-27 11:30:45 +0300

    Tasks completed using stream

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,17 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] datesOfLiving = candidate.getPeriodsInUkr().split("-");
+        int periodOfLiving =
+                Integer.parseInt(datesOfLiving[1]) - Integer.parseInt(datesOfLiving[0]);
+        return candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && periodOfLiving >= 10;
+    }
 }
