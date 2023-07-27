fb978270a3d3b9baf9e50c65fb7a8b528b99c0c5
holsoni <soniakostashchuk@gmail,com>
2023-07-27 17:54:48 +0300

    added NATIONALITY constant

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,6 +7,7 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int MINIMAL_AGE = 35;
     private static final int MINIMAL_YEARS_IN_UKRAINE = 10;
     private static final String DIVIDER = "-";
+    private static final String NATIONALITY = "Ukrainian";
 
     @Override
     public boolean test(Candidate candidate) {
@@ -14,7 +15,7 @@ public class CandidateValidator implements Predicate<Candidate> {
         int yearsSpentInUkraine = Integer.parseInt(periodsInUkraine[1])
                 - Integer.parseInt(periodsInUkraine[0]);
         return candidate.getAge() >= MINIMAL_AGE && candidate.isAllowedToVote()
-                && candidate.getNationality().equals("Ukrainian")
+                && candidate.getNationality().equals(NATIONALITY)
                 && yearsSpentInUkraine >= MINIMAL_YEARS_IN_UKRAINE;
 
     }
