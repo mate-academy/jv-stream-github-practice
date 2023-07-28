6a353d79e7f8d66fdba093f6f2b31c7e280f565e
Arsenii Mehdalskyi <114875417+Zel1oy@users.noreply.github.com>
2023-07-28 10:45:47 +0300

    fixed magic indexes

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -8,12 +8,14 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_PERIOD_OF_LIVING_UKRAINE = 10;
     private static final String REQUIRED_NATIONALITY = "Ukrainian";
     private static final String SEPARATOR_FOR_PERIOD = "-";
+    private static final int indexDateFrom = 0;
+    private static final int indexDateTo = 1;
 
     @Override
     public boolean test(Candidate candidate) {
         String[] datesOfLiving = candidate.getPeriodsInUkr().split(SEPARATOR_FOR_PERIOD);
         int periodOfLiving =
-                Integer.parseInt(datesOfLiving[1]) - Integer.parseInt(datesOfLiving[0]);
+                Integer.parseInt(datesOfLiving[indexDateTo]) - Integer.parseInt(datesOfLiving[indexDateFrom]);
         return candidate.getAge() >= MIN_CANDIDATE_AGE
                 && candidate.isAllowedToVote()
                 && candidate.getNationality().equals(REQUIRED_NATIONALITY)
