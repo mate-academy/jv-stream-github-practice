fc5808f368571f4e7405bdf529efc16a5083359e
Mykyta Zuber <nikitazuber@gmail.com>
2023-07-28 14:38:24 +0300

    stream functions

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && "Ukrainian".equals(candidate.getNationality())
+                && isLongTimePeriod(candidate.getPeriodsInUkr());
+    }
+
+    private boolean isLongTimePeriod(String prePeriod) {
+        String[] periodInUkr = prePeriod.split("-");
+        int countYearInUkr = Integer.parseInt(periodInUkr[1]) - Integer.parseInt(periodInUkr[0]);
+        if (countYearInUkr >= 10) {
+            return true;
+        }
+        return false;
+    }
 }
