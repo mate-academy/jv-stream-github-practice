b8fb7557ec846a72502322f481a8b0340acdac61
Arsenii Mehdalskyi <114875417+Zel1oy@users.noreply.github.com>
2023-07-28 10:31:47 +0300

    Fixed magic

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,14 +4,19 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_CANDIDATE_AGE = 35;
+    private static final int MIN_PERIOD_OF_LIVING_UKRAINE = 10;
+    private static final String REQUIRED_NATIONALITY = "Ukrainian";
+    private static final String SEPARATOR_FOR_PERIOD = "-";
+
     @Override
     public boolean test(Candidate candidate) {
-        String[] datesOfLiving = candidate.getPeriodsInUkr().split("-");
+        String[] datesOfLiving = candidate.getPeriodsInUkr().split(SEPARATOR_FOR_PERIOD);
         int periodOfLiving =
                 Integer.parseInt(datesOfLiving[1]) - Integer.parseInt(datesOfLiving[0]);
-        return candidate.getAge() >= 35
+        return candidate.getAge() >= MIN_CANDIDATE_AGE
                 && candidate.isAllowedToVote()
-                && candidate.getNationality().equals("Ukrainian")
-                && periodOfLiving >= 10;
+                && candidate.getNationality().equals(REQUIRED_NATIONALITY)
+                && periodOfLiving >= MIN_PERIOD_OF_LIVING_UKRAINE;
     }
 }
