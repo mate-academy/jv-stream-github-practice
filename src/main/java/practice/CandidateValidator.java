013263ec86f37aff991e650e6ecf993aad04d842
Ivan Fadieiev <ivanfadieiev@gmail.com>
2023-07-28 12:40:29 +0300

    changed constants names

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,8 +4,8 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    private static final int ZERO = 0;
-    private static final int FOUR = 4;
+    private static final int INDEX_FROM = 0;
+    private static final int INDEX_TO = 4;
     private static final int CANDIDATE_VALID_AGE = 35;
     private static final int SUFFICIENT_YEARS_LIVED_IN_UKRAINE = 10;
     private static final String VALID_NATIONALITY = "Ukrainian";
@@ -14,10 +14,10 @@ public class CandidateValidator implements Predicate<Candidate> {
     public boolean test(Candidate candidate) {
         String yearFrom = candidate
                 .getPeriodsInUkr()
-                .substring(ZERO, FOUR);
+                .substring(INDEX_FROM, INDEX_TO);
         String yearUntil = candidate
                 .getPeriodsInUkr()
-                .substring(candidate.getPeriodsInUkr().length() - FOUR);
+                .substring(candidate.getPeriodsInUkr().length() - INDEX_TO);
         return candidate.getAge() >= CANDIDATE_VALID_AGE
                 && candidate.isAllowedToVote()
                 && candidate.getNationality().equals(VALID_NATIONALITY)
