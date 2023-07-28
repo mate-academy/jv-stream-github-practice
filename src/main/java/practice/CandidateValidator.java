8b7d98b43f4145756c3c21a9cb59cf6033c71759
Oleg Stashkiv <stashkivolegg@gmail.com>
2023-07-28 11:37:07 +0300

    adding constants for magic signs

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,16 +6,24 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int YEAR_START_TO_LIVE_POSITION = 0;
     private static final int YEAR_FINISH_TO_LIVE_POSITION = 1;
+    private static final int MIN_YEARS = 35;
+    private static final int MIN_YEARS_LIVE_IN_UKRAINE = 10;
+    private static final String REGEX_SPLIT = "-";
 
     @Override
     public boolean test(Candidate candidate) {
-        int yearStart = Integer.parseInt(candidate.getPeriodsInUkr()
-                .split("-")[YEAR_START_TO_LIVE_POSITION]);
-        int yearFinish = Integer.parseInt(candidate.getPeriodsInUkr()
-                .split("-")[YEAR_FINISH_TO_LIVE_POSITION]);
-        return candidate.getAge() >= 35
+
+        return candidate.getAge() >= MIN_YEARS
                 && candidate.isAllowedToVote()
                 && candidate.getNationality().equals("Ukrainian")
-                && (yearFinish - yearStart) >= 10;
+                && getNumberYearsLivedInUkraine(candidate) >= MIN_YEARS_LIVE_IN_UKRAINE;
+    }
+
+    private int getNumberYearsLivedInUkraine(Candidate candidate) {
+        int yearStart = Integer.parseInt(candidate.getPeriodsInUkr()
+                .split(REGEX_SPLIT)[YEAR_START_TO_LIVE_POSITION]);
+        int yearFinish = Integer.parseInt(candidate.getPeriodsInUkr()
+                .split(REGEX_SPLIT)[YEAR_FINISH_TO_LIVE_POSITION]);
+        return yearFinish - yearStart;
     }
 }
