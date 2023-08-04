46bfaef698e1151f8ca6f901dd84134251b8f4d4
Artem Hlukhoman <giantking1231@gmail.com>
2023-08-04 15:53:58 +0300

    hw streams is done

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int YEARS_IN_UKRAINE_NEEDED = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] split = candidate.getPeriodsInUkr().split("-");
+        Integer startYear = Integer.parseInt(split[0]);
+        Integer endYear = Integer.parseInt(split[1]);
+
+        return (candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && endYear - startYear >= YEARS_IN_UKRAINE_NEEDED);
+    }
 }
