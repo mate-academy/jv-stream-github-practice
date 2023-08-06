21c0671bd77ed4c45eadd1304b13f33226709620
Nikita <kaptetos@gmail.com>
2023-08-06 18:33:15 +0300

    The CandidateValidator class contains the logic for validating a candidate against the requirements.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,25 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && validatePeriodInUkraine(candidate.getPeriodsInUkr());
+    }
+
+    private boolean validatePeriodInUkraine(String periodsInUkr) {
+        String[] years = periodsInUkr.split("-");
+        if (years.length != 2) {
+            return false;
+        }
+
+        int startYear = Integer.parseInt(years[0]);
+        int endYear = Integer.parseInt(years[1]);
+        return endYear - startYear >= 10;
+    }
 }
