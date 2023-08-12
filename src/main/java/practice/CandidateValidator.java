6346892790dba0806510c6643cb729edff48740f
Kovaev <eugenkoval1010@gmail.com>
2023-08-12 22:12:01 +0300

    all streams is done

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,22 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int PERIOD_IN_UKRAINE = 10;
     //write your code here
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= 35 && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && periodsInUkrBool(candidate);
+    }
+
+    private boolean periodsInUkrBool(Candidate candidate) {
+        String[] split = candidate.getPeriodsInUkr().split("-");
+        int i = Integer.parseInt(split[1]) - Integer.parseInt(split[0]);
+        return i >= PERIOD_IN_UKRAINE;
+    }
 }
