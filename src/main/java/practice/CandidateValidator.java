86e007ddfdc5e6c5230506ad827bacec4da879e4
Oleh <popovich158@gmail.com>
2023-08-16 22:33:00 +0300

    Created constants in CandidateValidator's class.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,10 +4,14 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int maxAge = 35;
+    private static final String nationality = "Ukrainian";
+    private static final int periodOfResidenceInUkraine = 10;
+
     @Override
     public boolean test(Candidate candidate) {
-        if (candidate.getAge() < 35 || !candidate.isAllowedToVote()
-                || !candidate.getNationality().equals("Ukrainian")) {
+        if (candidate.getAge() < maxAge || !candidate.isAllowedToVote()
+                || !candidate.getNationality().equals(nationality)) {
             return false;
         }
         String periodsInUkr = candidate.getPeriodsInUkr();
@@ -17,10 +21,9 @@ public class CandidateValidator implements Predicate<Candidate> {
                 int startYear = Integer.parseInt(years[0]);
                 int endYear = Integer.parseInt(years[1]);
                 int yearsInUkraine = endYear - startYear;
-                return yearsInUkraine >= 10;
+                return yearsInUkraine >= periodOfResidenceInUkraine;
             }
         }
-
         return true;
     }
 }
