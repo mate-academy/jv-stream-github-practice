6c8056db444d3994a29cdbfff8f48c00c4d31409
Oleksandr Tymoshenko <aleksandrtimosenko089@gmail.com>
2023-09-04 18:40:15 +0300

    fixed style

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -3,11 +3,9 @@ package practice;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
-import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
 import model.Candidate;
 import model.Cat;
 import model.Person;
