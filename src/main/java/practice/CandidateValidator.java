79b7971db00e366358273506b28c8edad7ff43a5
User <Monsterhunter@ukr.net>
2023-09-06 15:03:01 +0300

    performed a CandidateValidator class using Predicate implementation and improved StreamPractice class where I realized findMinEvenNumber, getOddNumsAverage, selectMenByAge, getWorkablePeople, getCatsNames & validateCandidates.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,19 +5,24 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    int MAX_AGE = 35;
-    int YEARS_IN_UKRAINE = 10;
 
-    @Override
     public boolean test(Candidate candidate) {
         int[] yearsInUkraine = Arrays.stream(candidate.getPeriodsInUkr()
                         .split("-"))
                 .mapToInt(Integer::parseInt)
                 .toArray();
         int years = yearsInUkraine[1] - yearsInUkraine[0];
-        return candidate.getAge() >= MAX_AGE
+        return candidate.getAge() >= getMaxAge()
                 && candidate.getNationality().equals("Ukrainian")
-                && years >= YEARS_IN_UKRAINE
+                && years >= getYearsInUkraine()
                 && candidate.isAllowedToVote();
+    }
+
+    public int getMaxAge() {
+        return 35;
+    }
+
+    public int getYearsInUkraine() {
+        return 10;
     }
 }
