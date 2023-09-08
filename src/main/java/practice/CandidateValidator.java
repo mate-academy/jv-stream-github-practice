a8285da085d2e12d4cef65372414f2db0ad7dbbd
User <Monsterhunter@ukr.net>
2023-09-08 14:46:47 +0300

    performed a CandidateValidator class using Predicate implementation and improved StreamPractice class where I realized findMinEvenNumber, getOddNumsAverage, selectMenByAge, getWorkablePeople, getCatsNames & validateCandidates.(fixed)

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,22 +6,22 @@ import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
 
-    final int MAX_AGE = 35;
-    final int MIN_YEARS_IN_UKRAINE = 10;
-    final String UKRAINIAN_NATIONALITY = "Ukrainian";
-    final int START_YEAR_INDEX = 0;
-    final int END_YEAR_INDEX = 1;
+    final int maxAge = 35;
+    final int minYearsInUkraine = 10;
+    final String ukrainianNationality = "Ukrainian";
+    final int startYearIndex = 0;
+    final int endYearIndex = 1;
 
     public boolean test(Candidate candidate) {
         int[] yearsInUkraine = Arrays.stream(candidate.getPeriodsInUkr().split("-"))
                 .mapToInt(Integer::parseInt)
                 .toArray();
-        int startYear = yearsInUkraine[START_YEAR_INDEX];
-        int endYear = yearsInUkraine[END_YEAR_INDEX];
+        int startYear = yearsInUkraine[startYearIndex];
+        int endYear = yearsInUkraine[endYearIndex];
         int years = endYear - startYear;
-        return candidate.getAge() >= MAX_AGE
-                && candidate.getNationality().equals(UKRAINIAN_NATIONALITY)
-                && years >= MIN_YEARS_IN_UKRAINE
+        return candidate.getAge() >= maxAge
+                && candidate.getNationality().equals(ukrainianNationality)
+                && years >= minYearsInUkraine
                 && candidate.isAllowedToVote();
     }
 }
