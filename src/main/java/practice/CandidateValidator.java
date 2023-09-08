4ef14dec3f69b1b41f6110ce0a8a98921b7dc48d
Vyacheslav <torbin.official@gmail.com>
2023-09-08 22:17:01 +0300

    Impemented CandidateValidator.test, all StreamPractice methods were also implemented

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,19 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= 35
+                && candidate.getNationality().equals("Ukrainian")
+                && validatePeriod(candidate)
+                && candidate.isAllowedToVote();
+    }
+
+    private boolean validatePeriod(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split("-");
+        return Integer.parseInt(years[1]) - Integer.parseInt(years[0]) > 10;
+    }
 }
