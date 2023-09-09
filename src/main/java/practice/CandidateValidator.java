fda22488384863c7f088b92c679880ba43546dce
Oleh Oliiarnyk <oliver.aka.rid@gmail.com>
2023-09-09 19:51:23 +0300

    fixed some mistakes

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,16 +4,16 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    private int validAge = 35;
-    private final int validPeriod = 10;
-    private String nationality = "Ukrainian";
+    private static final int VALID_AGE = 35;
+    private static final int VALID_PERIOD = 10;
+    private static final String NATIONALITY = "Ukrainian";
 
     @Override
     public boolean test(Candidate candidate) {
-        return candidate.getNationality().equals(nationality)
+        return candidate.getNationality().equals(NATIONALITY)
                 && candidate.isAllowedToVote()
-                && candidate.getAge() == validAge
-                && getYearsInUkr(candidate) >= validPeriod;
+                && candidate.getAge() >= VALID_AGE
+                && getYearsInUkr(candidate) >= VALID_PERIOD;
     }
 
     private int getYearsInUkr(Candidate candidate) {
