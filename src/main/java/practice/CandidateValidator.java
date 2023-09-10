009ad97fec8a8967142252ea5efb387c6cde5668
Alex-zzzz <141009519+Alex-zzzz@users.noreply.github.com>
2023-09-10 18:16:36 +0300

    First version, check please

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final int MIN_PERIOD_LIVE_IN_UKRAINE = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+
+        boolean isPeriodOk = Integer.valueOf(candidate.getPeriodsInUkr().substring(5))
+                - Integer.valueOf(candidate.getPeriodsInUkr().substring(0,4))
+                >= MIN_PERIOD_LIVE_IN_UKRAINE;
+
+        return candidate.getAge() >= MIN_AGE && candidate.isAllowedToVote()
+                && candidate.getNationality() == "Ukrainian" && isPeriodOk;
+    }
 }
