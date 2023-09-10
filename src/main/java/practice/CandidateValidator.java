9ec879354ca55bfcae3cb4e581a119d8b25ef47a
Oleksandr Chernyonkov <cayman_raw@ukr.net>
2023-09-10 22:04:01 +0300

    I have implemented all the methods

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,18 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int FIRST_YEAR = 0;
+    private static final int LAST_YEAR = 1;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= 35
+                && candidate.getNationality().equals("Ukrainian")
+                && candidate.isAllowedToVote()
+                && Integer.parseInt(candidate.getPeriodsInUkr().split("-")[LAST_YEAR])
+                - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[FIRST_YEAR]) >= 10;
+    }
 }
