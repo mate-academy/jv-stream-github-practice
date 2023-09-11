8eb020de0ed0548202991d54a8c114f1854fb7f9
Oleksandr Chernyonkov <cayman_raw@ukr.net>
2023-09-11 13:00:09 +0300

    I fixed some mistakes after stedents review and removed redundant import

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import model.Candidate;
 import model.Cat;
