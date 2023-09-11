3508a7eb722565f3f382fa9ea362dcfee8949286
Parashchuk <Andry.parashchuk@gmail.com>
2023-09-11 16:04:46 +0200

    solution 1

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -8,15 +8,17 @@ public class CandidateValidator<C extends Candidate> implements Predicate<C> {
     private static final String UKRAINE_NATIONALITY = "Ukrainian";
     private static final int MIN_AGE = 35;
     private static final int MIN_TIME_LIVED_IN_COUNTRY = 10;
+    private static boolean allowedToVote = true;
 
     @Override
-    public boolean test(C e) {
-        String[] datePeriod = e.getPeriodsInUkr().split(DATE_SEPARATOR);
+    public boolean test(C candidate) {
+        String[] datePeriod = candidate.getPeriodsInUkr().split(DATE_SEPARATOR);
         int livedInCountryFor = Integer.parseInt(datePeriod[1]) - Integer.parseInt(datePeriod[0]);
+        allowedToVote = candidate.isAllowedToVote();
 
-        return e.getAge() >= MIN_AGE
-                && e.isAllowedToVote()
-                && e.getNationality().equals(UKRAINE_NATIONALITY)
+        return candidate.getAge() >= MIN_AGE
+                && allowedToVote
+                && candidate.getNationality().equals(UKRAINE_NATIONALITY)
                 && livedInCountryFor > MIN_TIME_LIVED_IN_COUNTRY;
     }
 }
