702baaffd33c79d9c2382ddb85e7f4efd47299fb
Svyatoslav Tsytsyura <svatik.bos@gmail.com>
2023-09-11 19:23:43 +0300

    made requested changes

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,17 +7,18 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int CANDIDATE_REQUIRED_AGE = 35;
     private static final String CANDIDATE_REQUIRED_NATIONALITY = "Ukrainian";
     private static final int CANDIDATE_REQUIRED_YEARS_IN_UKRAINE = 10;
-    private static final boolean ALLOWED_TO_VOTE = true;
     private static final String SPLITERATOR = "-";
+
+    private int getCandidateYearsInUkraine(Candidate candidate) {
+        String[] periodInUkraine = candidate.getPeriodsInUkr().split(SPLITERATOR);
+        return Integer.parseInt(periodInUkraine[1]) - Integer.parseInt(periodInUkraine[0]);
+    }
 
     @Override
     public boolean test(Candidate candidate) {
-        String[] periodInUkraine = candidate.getPeriodsInUkr().split(SPLITERATOR);
-        int candidateYearsInUkraine = Integer.parseInt(periodInUkraine[1])
-                - Integer.parseInt(periodInUkraine[0]);
         return candidate.getAge() >= CANDIDATE_REQUIRED_AGE
                 && candidate.getNationality().equals(CANDIDATE_REQUIRED_NATIONALITY)
-                && candidate.isAllowedToVote() == ALLOWED_TO_VOTE
-                && candidateYearsInUkraine >= CANDIDATE_REQUIRED_YEARS_IN_UKRAINE;
+                && candidate.isAllowedToVote()
+                && getCandidateYearsInUkraine(candidate) >= CANDIDATE_REQUIRED_YEARS_IN_UKRAINE;
     }
 }
