bc3c3d770f5e02bd0ae004a9411b5df8d70ab3ac
Denis Bohusevich <bogusevich03@gmail.com>
2023-09-14 02:07:15 +0300

    Stream

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int IN_UKR = 0;
+    private static final int OUT_UKR = 1;
+    private static final int MIN_TERM = 10;
+    private static final int MIN_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] periodsInUkr = candidate.getPeriodsInUkr().split("-");
+        int term = Integer.parseInt(periodsInUkr[OUT_UKR])
+                - Integer.parseInt(periodsInUkr[IN_UKR]);
+        return candidate.getAge() >= MIN_AGE
+                && candidate.getNationality().equals(NATIONALITY)
+                && candidate.isAllowedToVote()
+                && term >= MIN_TERM;
+    }
 }
