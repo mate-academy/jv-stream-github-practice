c7008118234b68be472462c8e16451e19f9aaaaf
Solomia Bobyak <mia.bobyak@gmail.com>
2023-09-14 19:49:52 +0300

    create CandidateValidator and implements Predicate<Candidate>

---
+++ src/main/java/core/basesyntax/model/practice/CandidateValidator.java
@@ -0,0 +1,16 @@
+package core.basesyntax.model.practice;
+
+import java.util.function.Predicate;
+import core.basesyntax.model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate>{
+    private static final int MIN_YEAR_IN_UKR = 10;
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] yearStr = candidate.getPeriodsInUkr().split("-");
+        int startYear = Integer.parseInt(yearStr[0]);
+        int endYear = Integer.parseInt(yearStr[1]);
+        int yearsInUkraine = endYear - startYear + 1;
+        return yearsInUkraine >= MIN_YEAR_IN_UKR;
+    }
+}
