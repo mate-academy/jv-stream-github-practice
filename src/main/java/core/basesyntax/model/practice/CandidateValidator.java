0732e36be8a58ea4f5c252df8664a55c1cc89f69
Solomia Bobyak <mia.bobyak@gmail.com>
2023-09-15 15:45:39 +0300

    add constant value

--- src/main/java/core/basesyntax/model/practice/CandidateValidator.java
+++ src/main/java/core/basesyntax/model/practice/CandidateValidator.java
@@ -5,12 +5,15 @@ import java.util.function.Predicate;
 
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_YEAR_IN_UKR = 10;
+    private static final int INDEX_START_YEAR = 0;
+    private static final int INDEX_END_YEAR = 1;
+    private static final String DATE_SEPARATOR = "-";
 
     @Override
     public boolean test(Candidate candidate) {
-        String[] yearStr = candidate.getPeriodsInUkr().split("-");
-        int startYear = Integer.parseInt(yearStr[0]);
-        int endYear = Integer.parseInt(yearStr[1]);
+        String[] yearStr = candidate.getPeriodsInUkr().split(DATE_SEPARATOR);
+        int startYear = Integer.parseInt(yearStr[INDEX_START_YEAR]);
+        int endYear = Integer.parseInt(yearStr[INDEX_END_YEAR]);
         int yearsInUkraine = endYear - startYear + 1;
         return yearsInUkraine >= MIN_YEAR_IN_UKR;
     }
