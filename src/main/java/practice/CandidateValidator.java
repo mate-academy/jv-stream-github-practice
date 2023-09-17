b12262a21145e21f7f894a8204d5d4befd70b407
Denis Gorda <dnsgorda@gmail.com>
2023-09-17 14:44:22 +0300

    Create constants for array positions

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,14 +7,16 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final String YEARS_SEPARATOR = "-";
     private static final int MIN_REQUIRED_AGE = 35;
     private static final int REQUIRED_TIME_IN_COUNTRY = 10;
+    private static final int FIRST_YEAR_POSITION = 0;
+    private static final int LAST_YEAR_POSITION = 1;
     private static final String REQUIRED_NATIONALITY = "Ukrainian";
     private static final boolean IS_ALLOWED_TO_VOTE = true;
 
     @Override
     public boolean test(Candidate candidate) {
         String[] yearsInUkr = candidate.getPeriodsInUkr().split(YEARS_SEPARATOR);
-        int firstYearInCountry = Integer.parseInt(yearsInUkr[0]);
-        int lastYearInCountry = Integer.parseInt(yearsInUkr[1]);
+        int firstYearInCountry = Integer.parseInt(yearsInUkr[FIRST_YEAR_POSITION]);
+        int lastYearInCountry = Integer.parseInt(yearsInUkr[LAST_YEAR_POSITION]);
         int currentPeriodInCountry = lastYearInCountry - firstYearInCountry;
         return candidate.getAge() >= MIN_REQUIRED_AGE
                 && candidate.isAllowedToVote() == IS_ALLOWED_TO_VOTE
