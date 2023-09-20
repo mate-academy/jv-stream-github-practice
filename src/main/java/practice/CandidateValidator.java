35321c170cc857912ee942d453309c88a340b513
Oleksandr Mykhailov <ososkdkc@gmai.com>
2023-09-20 12:28:24 +0300

    Added constants

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,16 +4,21 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MINIMUM_AGE = 35;
+    private static final String UKRAINIAN_NATIONALITY = "Ukrainian";
+    private static final String YEAR_DELIMITER = "-";
+    private static final int MINIMUM_RESIDENCE = 10;
+
     @Override
     public boolean test(Candidate candidate) {
         int age = candidate.getAge();
-        boolean isUkrainian = "Ukrainian".equals(candidate.getNationality());
+        boolean isUkrainian = UKRAINIAN_NATIONALITY.equals(candidate.getNationality());
         boolean hasVotingRights = candidate.isAllowedToVote();
-        String[] periods = candidate.getPeriodsInUkr().split("-");
+        String[] periods = candidate.getPeriodsInUkr().split(YEAR_DELIMITER);
         int startYear = Integer.parseInt(periods[0]);
         int endYear = Integer.parseInt(periods[1]);
-        boolean hasLivedInUkraineFor10Years = (endYear - startYear + 1) >= 10;
+        boolean hasLivedInUkraineFor10Years = (endYear - startYear) >= MINIMUM_RESIDENCE;
 
-        return age >= 35 && isUkrainian && hasVotingRights && hasLivedInUkraineFor10Years;
+        return age >= MINIMUM_AGE && isUkrainian && hasVotingRights && hasLivedInUkraineFor10Years;
     }
 }
