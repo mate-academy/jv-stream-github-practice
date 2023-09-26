4f5f9c43b08d2fa241d047bbbdc05e9c86388a19
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-09-27 02:05:53 +0300

    added changes to the class CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,27 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import model.Candidate;
+
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        // Check if the candidate meets the eligibility criteria
+        return candidate.getAge() >= 35 &&
+                candidate.isAllowedToVote() &&
+                "Ukrainian".equals(candidate.getNationality()) &&
+                hasLivedInUkraineForTenYears(candidate.getPeriodsInUkr());
+    }
+
+    private boolean hasLivedInUkraineForTenYears(String periodsInUkr) {
+        String[] years = periodsInUkr.split("-");
+        if (years.length != 2) {
+            return false;
+        }
+        int startYear = Integer.parseInt(years[0]);
+        int endYear = Integer.parseInt(years[1]);
+        int totalYears = endYear - startYear + 1;
+        return totalYears >= 10;
+    }
 }
