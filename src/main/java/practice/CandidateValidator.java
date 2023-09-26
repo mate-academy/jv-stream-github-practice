bf44678e9068528a5fabe7987a59b9d06b9678cb
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-09-27 02:11:09 +0300

    added changes to the class CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,17 +1,16 @@
 package practice;
 
-import model.Candidate;
-
 import java.util.function.Predicate;
+import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
     @Override
     public boolean test(Candidate candidate) {
         // Check if the candidate meets the eligibility criteria
-        return candidate.getAge() >= 35 &&
-                candidate.isAllowedToVote() &&
-                "Ukrainian".equals(candidate.getNationality()) &&
-                hasLivedInUkraineForTenYears(candidate.getPeriodsInUkr());
+        return candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && "Ukrainian".equals(candidate.getNationality())
+                && hasLivedInUkraineForTenYears(candidate.getPeriodsInUkr());
     }
 
     private boolean hasLivedInUkraineForTenYears(String periodsInUkr) {
