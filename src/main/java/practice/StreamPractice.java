8796b3bdec9c1d48bb294f25db8355fdcdd4f914
Andrey Polezhaiev <polezhaievandrey28@gmail.com>
2023-09-28 14:34:24 +0300

    change equals on ==

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -53,7 +53,7 @@ public class StreamPractice {
      */
     public List<Person> selectMenByAge(List<Person> peopleList, int fromAge, int toAge) {
         return peopleList.stream()
-                .filter(p -> p.getSex().equals(Person.Sex.MAN)
+                .filter(p -> p.getSex() == Person.Sex.MAN
                         && p.getAge() >= fromAge
                         && p.getAge() <= toAge)
                 .collect(Collectors.toList());
