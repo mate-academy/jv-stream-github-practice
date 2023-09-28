cad4bae5e76f9fdf5a5ef1e1350224785bfcaac7
Vitalii Voitseshuk <voitseshuk.v@gmail.com>
2023-09-28 13:52:37 +0300

    add StreamPractice & CandidateValidator implementations

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    public static final int MIN_AGE = 35;
+    public static final String UKRAINIAN_NATIONALITY = "Ukrainian";
+    public static final int MIN_PERIOD = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] period = candidate.getPeriodsInUkr().split("-");
+        return candidate.getAge() >= MIN_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(UKRAINIAN_NATIONALITY)
+                && Integer.parseInt(period[1]) - Integer.parseInt(period[0]) >= MIN_PERIOD;
+    }
 }
