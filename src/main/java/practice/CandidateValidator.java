3dfbcfcce8b80318a1048ca3b84cb5f2088b44b9
Andrey Polezhaiev <polezhaievandrey28@gmail.com>
2023-09-28 14:18:13 +0300

    add constants to CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,12 +4,19 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int START_YEAR_INDEX = 0;
+    private static final int END_YEAR_INDEX = 1;
+    private static final int MIN_PERIOD = 10;
+    private static final int MIN_AGE = 35;
+
     @Override
     public boolean test(Candidate candidate) {
         return candidate.isAllowedToVote()
-                && candidate.getAge() >= 35
+                && candidate.getAge() >= MIN_AGE
                 && candidate.getNationality().equals("Ukrainian")
-                && Integer.parseInt(candidate.getPeriodsInUkr().split("-")[1])
-                - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[0]) >= 10;
+                && Integer.parseInt(candidate.getPeriodsInUkr()
+                .split("-")[END_YEAR_INDEX])
+                - Integer.parseInt(candidate.getPeriodsInUkr()
+                .split("-")[START_YEAR_INDEX]) >= MIN_PERIOD;
     }
 }
