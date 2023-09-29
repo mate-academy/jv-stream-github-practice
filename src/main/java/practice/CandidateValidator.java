de9d364cc45a9feb255f060a4a4cc82e7146757a
Krasnov-Maksim <maksim.v.krasnov@gmail.com>
2023-09-29 19:34:51 +0300

    solved task "StreamPractice" in module "javaCore/Java8-Part2"

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,18 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int AGE_THRESHOLD = 35;
+    private static final int PERIOD_THRESHOLD = 10;
+
+    @Override
+    public boolean test(final Candidate candidate) {
+        int startYear = Integer.parseInt(candidate.getPeriodsInUkr().split("-")[0]);
+        int endYear = Integer.parseInt(candidate.getPeriodsInUkr().split("-")[1]);
+        return candidate.getAge() >= AGE_THRESHOLD && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && endYear - startYear >= PERIOD_THRESHOLD;
+    }
 }
