7af25592e7a50d151b3da90b42ff99f517a19028
Oleksii Borysenko <343502597s@gmail.com>
2023-09-30 14:13:29 +0200

    mvn ok задание с поиском индекса в потоке решил с помощью range как по другому не знаю

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,19 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final int PERIODS = 10;
+    private static final String NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] splitDate = candidate.getPeriodsInUkr().split("-");
+        return candidate.getAge() >= MIN_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && (Integer.parseInt(splitDate[1]) - Integer.parseInt(splitDate[0]) >= PERIODS);
+    }
 }
