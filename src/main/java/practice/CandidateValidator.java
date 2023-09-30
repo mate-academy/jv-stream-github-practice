9a774f718596eeaed8dc4c1f22f1df6322a60866
Krasnov-Maksim <maksim.v.krasnov@gmail.com>
2023-09-30 18:18:41 +0300

    renamed constants

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,16 +6,16 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int AGE_THRESHOLD = 35;
     private static final int PERIOD_THRESHOLD = 10;
-    private static final int INDEX_0 = 0;
-    private static final int INDEX_1 = 1;
+    private static final int FIRST_YEAR_INDEX = 0;
+    private static final int LAST_YEAR_INDEX = 1;
 
     @Override
     public boolean test(final Candidate candidate) {
         final String[] periods = candidate.getPeriodsInUkr().split("-");
-        int startYear = Integer.parseInt(periods[INDEX_0]);
-        int endYear = Integer.parseInt(periods[INDEX_1]);
+        int firstYear = Integer.parseInt(periods[FIRST_YEAR_INDEX]);
+        int lastYear = Integer.parseInt(periods[LAST_YEAR_INDEX]);
         return candidate.getAge() >= AGE_THRESHOLD && candidate.isAllowedToVote()
                 && candidate.getNationality().equals("Ukrainian")
-                && endYear - startYear >= PERIOD_THRESHOLD;
+                && lastYear - firstYear >= PERIOD_THRESHOLD;
     }
 }
