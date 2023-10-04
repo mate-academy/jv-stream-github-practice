8f7f966e87dff5997dace54fd8e62b89b887211a
Olga Starkova <ole4kastar79@gmail.com>
2023-10-04 23:22:42 +0300

    StreamPractice of Starikova Olga

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,27 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import model.Candidate;
+
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    private static final int AGE_MIN_VALID = 35;
+    private static final String NATIONALITY_VALID = "Ukrainian";
+    private static final int YEARS_MIN_VALID = 10;
+    private static final String YEARS_REGEX_SPLIT = "-";
+    private static final int YEARS_INDEX_START = 0;
+    private static final int YEARS_INDEX_END = 1;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split(YEARS_REGEX_SPLIT);
+        int yearsCount = Integer.parseInt(years[YEARS_INDEX_END]) - Integer.parseInt(years[YEARS_INDEX_START]);
+        if (candidate.isAllowedToVote() && candidate.getAge() >= AGE_MIN_VALID
+                && candidate.getNationality().equals(NATIONALITY_VALID)
+                && yearsCount >= YEARS_MIN_VALID) {
+            return true;
+        }
+        return false;
+    }
 }
