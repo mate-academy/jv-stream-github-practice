2ee6579dcb61f7ecd8cf62578eabc41293ea9ecd
Roman Menzheres <roma.menzheres@gmail.com>
2023-10-10 19:19:09 +0300

    Realized 'CandidateValidator'. Added realization for methods in 'StreamPractice' class

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.Arrays;
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int NOT_SUIT = 0;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && Arrays.stream(candidate.getPeriodsInUkr().split("-"))
+                .map(Integer::parseInt)
+                .reduce((from, to) -> to - from)
+                .orElse(NOT_SUIT) > 10;
+    }
 }
