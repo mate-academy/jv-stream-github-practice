548a36f64fe27297a833a4ae9049f62e60a107be
Lukian Muzyka <lukianmuzyka@gmail.com>
2023-10-17 15:17:03 +0300

    Created separate Predicate realisation in CandidateValidator.java

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final String REQUIRED_NATIONALITY = "Ukrainian";
+    private static final String SEPARATOR = "-";
+    private static final int MIN_RESIDENCE_DURATION = 10;
+    private static final int MIN_AGE = 35;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] residenceRange = candidate.getPeriodsInUkr().split(SEPARATOR);
+        return candidate.getAge() >= MIN_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(REQUIRED_NATIONALITY)
+                && Integer.parseInt(residenceRange[1])
+                - Integer.parseInt(residenceRange[0]) >= MIN_RESIDENCE_DURATION;
+    }
 }
