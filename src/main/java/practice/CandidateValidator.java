82e3feaab4c3c3da5493092a00bd2261f789ef63
Nazario Lupen <nazario.lupen@gmail.com>
2023-10-24 00:27:30 +0300

    first_commit

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,26 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final int BEGINNING_INDEX = 0;
+    private static final int END_INDEX = 1;
+    private static final int MIN_PERIOD_OF_STAY = 10;
+    private static final String NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        if (candidate.getPeriodsInUkr() == null) {
+            return false;
+        }
+        String[] periodResidenceInUkraine = candidate.getPeriodsInUkr().split("-");
+        return candidate.getAge() >= MIN_AGE && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && (Integer.parseInt(periodResidenceInUkraine[END_INDEX])
+                - Integer.parseInt(periodResidenceInUkraine[BEGINNING_INDEX]))
+                >= MIN_PERIOD_OF_STAY;
+    }
+
 }
