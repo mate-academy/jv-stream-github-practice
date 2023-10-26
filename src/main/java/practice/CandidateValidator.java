ba1e04f232b21488084b7f23f508df67fe9c5940
Igor Korolevich <igorkorolj@gmail.com>
2023-10-26 10:26:52 +0300

    Simplifyed if method in CandidateValidator, changed names in test to more descriptive , changed user name from expected to user

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,18 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import model.Candidate;
+
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] ages = candidate.getPeriodsInUkr().split("-");
+        int ageFrom = Integer.parseInt(ages[0]);
+        int ageTo = Integer.parseInt(ages[1]);
+        return candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && ageTo - ageFrom >= 10;
+    }
 }
