f0c6eb4a9eb97760faca37cf3913d7c34f44739c
Vasyl Palokha <xaski514@gmail.com>
2023-10-26 21:19:04 +0300

    First commin

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int PERIOD_TO_NUMBER = 1;
+    private static final int PERIOD_FROM_NUMBER = 0;
+    private static final int PERIOD_IN_UKRAINE = 10;
+    private static final int VALID_NUMBER = 35;
+    private static final String SPLITTER = "-";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= VALID_NUMBER
+                && candidate.isAllowedToVote()
+                && "Ukrainian".equals(candidate.getNationality())
+                && Integer.parseInt(candidate.getPeriodsInUkr().split(SPLITTER)[PERIOD_TO_NUMBER])
+                - Integer.parseInt(candidate.getPeriodsInUkr().split(SPLITTER)[PERIOD_FROM_NUMBER])
+                >= PERIOD_IN_UKRAINE;
+    }
+
 }
