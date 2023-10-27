32e2a2c6d2de9e1f9de01d7ce3ee67efd46f2cbe
Nataliia Shtykh <forstudying93@gmail.com>
2023-10-27 20:13:34 +0300

    solved the task stream-practice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,26 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_ALLOWABLE_AGE = 35;
+    private static final int MIN_YEARS_OF_RESIDENCE = 10;
+    private static final String UKRAINIAN_NATIONALITY = "Ukrainian";
+    private static final String HYPHEN = "-";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MIN_ALLOWABLE_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(UKRAINIAN_NATIONALITY)
+                && getNumberOfYearsInUkraine(candidate);
+    }
+
+    private boolean getNumberOfYearsInUkraine(Candidate candidate) {
+        String[] yearsInUkraine = candidate.getPeriodsInUkr().split(HYPHEN);
+        int numberOfYearsInUkraine =
+                Integer.parseInt(yearsInUkraine[1]) - Integer.parseInt(yearsInUkraine[0]);
+        return numberOfYearsInUkraine >= MIN_YEARS_OF_RESIDENCE;
+    }
 }
