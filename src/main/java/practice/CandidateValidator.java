c277db87a0782539c299144bef5a6e943272e127
barbequeue <p99kab00@gmail.com>
2023-10-28 20:46:41 +0300

    Implement CandidateValidator predicate functional interface

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,25 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final String CITIZENSHIP = "Ukrainian";
+    private static final int MIN_CITIZENSHIP_PERIOD = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MIN_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(CITIZENSHIP)
+                && calculatePeriod(candidate.getPeriodsInUkr()) >= MIN_CITIZENSHIP_PERIOD;
+    }
+
+    private static int calculatePeriod(String period) {
+        int separatorIndex = period.indexOf('-');
+        int begin = Integer.parseInt(period.substring(0, separatorIndex));
+        int end = Integer.parseInt(period.substring(separatorIndex + 1));
+        return end - begin;
+    }
 }
