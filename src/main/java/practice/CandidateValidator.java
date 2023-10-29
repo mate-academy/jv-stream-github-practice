ea2cbbd1bd94feccef53b068fab26ff8506a6c79
Anton Babych <antonbabi13@gmail.com>
2023-10-29 20:42:23 +0200

    Done all the task, create own Predicate for Candidate.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,30 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        if (!"Ukrainian".equals(candidate.getNationality())) {
+            return false;
+        }
+        if (candidate.getAge() < 35) {
+            return false;
+        }
+        if (!candidate.isAllowedToVote()) {
+            return false;
+        }
+        String[] periods = candidate.getPeriodsInUkr().split("-");
+        if (periods.length != 2) {
+            return false;
+        }
+        int startYear = Integer.parseInt(periods[0]);
+        int endYear = Integer.parseInt(periods[1]);
+        if ((endYear - startYear) < 10) {
+            return false;
+        }
+        return true;
+    }
+
 }
