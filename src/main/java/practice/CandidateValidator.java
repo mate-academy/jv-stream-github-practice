8f81734f5d234e4816de9037fcc5ba15f7cc8ad7
Igor Korolevich <igorkorolj@gmail.com>
2023-10-30 00:46:09 +0200

    fixed errors mentioned in the pool request

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,14 +4,20 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    public static final int MINIMAL_ACCEPTABLE_CANDIDATE_AGE = 35;
+    public static final int INDEX_OF_SINCE_YEAR_IN_COUNTRY = 0;
+    public static final int INDEX_OF_TO_YEAR_IN_COUNTRY = 1;
+    public static final String CANDIDATE_ACCEPTABLE_COUNTRY = "Ukrainian";
+    public static final int MINIMAL_ACCEPTABLE_AGE_IN_COUNTRY = 10;
+
     @Override
     public boolean test(Candidate candidate) {
         String[] ages = candidate.getPeriodsInUkr().split("-");
-        int ageFrom = Integer.parseInt(ages[0]);
-        int ageTo = Integer.parseInt(ages[1]);
-        return candidate.getAge() >= 35
+        int ageFrom = Integer.parseInt(ages[INDEX_OF_SINCE_YEAR_IN_COUNTRY]);
+        int ageTo = Integer.parseInt(ages[INDEX_OF_TO_YEAR_IN_COUNTRY]);
+        return candidate.getAge() >= MINIMAL_ACCEPTABLE_CANDIDATE_AGE
                 && candidate.isAllowedToVote()
-                && candidate.getNationality().equals("Ukrainian")
-                && ageTo - ageFrom >= 10;
+                && candidate.getNationality().equals(CANDIDATE_ACCEPTABLE_COUNTRY)
+                && ageTo - ageFrom >= MINIMAL_ACCEPTABLE_AGE_IN_COUNTRY;
     }
 }
