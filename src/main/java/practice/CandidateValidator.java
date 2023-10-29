2359bfc0d579f787331d2af7410fa14dba83a214
Andriy-Hrynevetskyi <andrijgrineveckij@gmail.com>
2023-10-29 15:37:08 +0200

    implemented all methods of StreamPractice according to task

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,28 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final String REQUIRED_NATIONALITY = "Ukrainian";
+    private static final String SPLIT_REGEX = "-";
+    private static final int MIN_PERIOD_OF_LIVING = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        boolean isAgeAllowed = candidate.getAge() >= MIN_AGE;
+        boolean isAllowedToVote = candidate.isAllowedToVote();
+        boolean isNationalityAllowed = candidate.getNationality().equals(REQUIRED_NATIONALITY);
+        boolean isLivingTenYearsInUkraine
+                = hasTenYearsPeriodOfLivingInUkraine(candidate.getPeriodsInUkr());
+
+        return isAgeAllowed && isNationalityAllowed && isAllowedToVote && isLivingTenYearsInUkraine;
+    }
+
+    private boolean hasTenYearsPeriodOfLivingInUkraine(String period) {
+        String[] periodLength = period.split(SPLIT_REGEX);
+        return Integer.parseInt(periodLength[1])
+                - Integer.parseInt(periodLength[0]) >= MIN_PERIOD_OF_LIVING;
+    }
 }
