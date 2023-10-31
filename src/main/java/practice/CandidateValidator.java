b331d897c2ddb4117fea14072443b23771245f2f
Dmytro Potapov <potapovds1993@gmail.com>
2023-10-31 16:04:02 +0200

    Stream-HW-solution

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,13 +1,13 @@
 package practice;
 
-import model.Candidate;
-
 import java.util.function.Predicate;
+import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int AGE_OF_CANDIDACY = 35;
     private static final String NATIONALITY = "Ukrainian";
     private static final int MINIMUM_YEARS_LIVING = 10;
+
     @Override
     public boolean test(Candidate candidate) {
         if (!candidate.isAllowedToVote()) {
--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,10 +1,12 @@
 package practice;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
 import model.Candidate;
 import model.Cat;
 import model.Person;
@@ -34,13 +36,13 @@ public class StreamPractice {
      * return the average of all odd numbers from the list or throw NoSuchElementException.
      * But before that subtract 1 from each element on an odd position (having the odd index).
      */
+
     public Double getOddNumsAverage(List<Integer> numbers) {
-        OptionalDouble average = IntStream.range(0, numbers.size())
-                .filter(index -> index % 2 == 1)
-                .mapToDouble(index -> numbers.get(index) - 1)
-                .filter(number -> number % 2 == 1)
-                .average();
-        return average.getAsDouble();
+        return IntStream.range(0, numbers.size())
+                .mapToDouble(index -> index % 2 == 1 ? numbers.get(index) - 1 : numbers.get(index))
+                .filter(numb -> numb % 2 == 1)
+                .average()
+                .orElseThrow(NoSuchElementException::new);
     }
 
     /**
@@ -67,9 +69,11 @@ public class StreamPractice {
      * Example: select people of working age
      * (from 18 y.o. and to 60 y.o. for men and to 55 y.o. for women inclusively).
      */
+
     public List<Person> getWorkablePeople(int fromAge, int femaleToAge,
                                           int maleToAge, List<Person> peopleList) {
         Predicate<Person> isWorkablePersons = new Predicate<Person>() {
+
             @Override
             public boolean test(Person person) {
                 return (person.getSex() == Person.Sex.MAN && person.getAge() >= fromAge
@@ -78,7 +82,7 @@ public class StreamPractice {
                         && person.getAge() <= femaleToAge);
             }
         };
-        return  peopleList.stream()
+        return peopleList.stream()
                 .filter(isWorkablePersons)
                 .collect(Collectors.toList());
     }
@@ -88,9 +92,11 @@ public class StreamPractice {
      * and each `Cat` having a `name` and `age`),
      * return the names of all cats whose owners are women from `femaleAge` years old inclusively.
      */
+
     public List<String> getCatsNames(List<Person> peopleList, int femaleAge) {
-        return  peopleList.stream()
-                .filter(person -> person.getSex() == Person.Sex.WOMAN && person.getAge() >= femaleAge)
+        return peopleList.stream()
+                .filter(person -> person.getSex() == Person.Sex.WOMAN
+                        && person.getAge() >= femaleAge)
                 .map(Person::getCats)
                 .flatMap(Collection::stream)
                 .map(Cat::getName)
