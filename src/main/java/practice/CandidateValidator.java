ba4d205e51cb58cbd960e90bfc4e7dfd9773ee22
Epikano <epikano@gmail.com>
2023-11-06 01:44:45 +0200

    Added jv-stream-github-practice solution.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,23 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_ALLOWED_AGE = 35;
+    private static final String ALLOWED_NATIONALITY = "Ukrainian";
+    private static final int MIN_PERIOD_IN_UKRAINE = 10;
+    private static final int FROM_YEAR_INDEX = 0;
+    private static final int TO_YEAR_INDEX = 1;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split("-");
+        int candidatePeriodInUkraine =
+                Integer.parseInt(years[TO_YEAR_INDEX]) - Integer.parseInt(years[FROM_YEAR_INDEX]);
+        return candidate.isAllowedToVote()
+                && (candidate.getAge() >= MIN_ALLOWED_AGE)
+                && candidate.getNationality().equals(ALLOWED_NATIONALITY)
+                && (candidatePeriodInUkraine >= MIN_PERIOD_IN_UKRAINE);
+    }
 }
