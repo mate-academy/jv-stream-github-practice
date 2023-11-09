9a8712515e1888e4dcc7eccf7a7f77e44827f5ad
Danial <danialbest55@gmail.com>
2023-11-09 14:41:46 +0200

    Change hardcode and add constants

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,23 +4,27 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    private static boolean isUkrainianResidentFor10Years(String periodsInUkr) {
+    private static final int ALLOWED_AGE = 10;
+    private static final int YEARSAGE = 2;
+    private static final int OLDER_AGE = 35;
+
+    private boolean isResidentFor10Years(String periodsInUkr) {
         String[] years = periodsInUkr.split("-");
-        if (years.length != 2) {
+        if (years.length != YEARSAGE) {
             return false;
         }
 
         int startYear = Integer.parseInt(years[0]);
         int endYear = Integer.parseInt(years[1]);
 
-        return endYear - startYear >= 10;
+        return endYear - startYear >= ALLOWED_AGE;
     }
 
     @Override
     public boolean test(Candidate candidate) {
-        return candidate.getAge() >= 35
+        return candidate.getAge() >= OLDER_AGE
                 && candidate.isAllowedToVote()
                 && "Ukrainian".equals(candidate.getNationality())
-                && isUkrainianResidentFor10Years(candidate.getPeriodsInUkr());
+                && isResidentFor10Years(candidate.getPeriodsInUkr());
     }
 }
