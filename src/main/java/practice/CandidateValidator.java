710d498433fde28acd4cc292a4c75e9265af434a
Mutaborr <123407682+Mutaborr@users.noreply.github.com>
2023-11-10 19:14:28 +0200

    Commit StreamPractice #1

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,25 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    private static final int MIN_AGE_CANDIDATE = 35;
+    private static final int MIN_AGE_LIVED = 10;
+
+    private static final String CORRECT_NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+
+        String[] periodsInUkr = candidate.getPeriodsInUkr().split("-");
+
+        int livedDuration = Integer.parseInt(periodsInUkr[1]) - Integer.parseInt(periodsInUkr[0]);
+
+        return candidate.getAge() >= MIN_AGE_CANDIDATE
+                && candidate.getNationality().equals(CORRECT_NATIONALITY)
+                && candidate.isAllowedToVote()
+                && livedDuration >= MIN_AGE_LIVED;
+    }
 }
