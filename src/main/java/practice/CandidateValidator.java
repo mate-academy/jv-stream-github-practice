ac85cb153fe64dc954497cabfbe19850552ccd92
Bohdan Chupika <boroda4436@users.noreply.github.com>
2023-11-13 13:08:17 +0200

    Done Stream tasks

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,28 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import model.Candidate;
+
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int LIVE_IN_UKRAINE = 10;
+
+    private static final boolean ALLOWED_TO_VOTE = true;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() > MIN_AGE
+                && candidate.isAllowedToVote() == ALLOWED_TO_VOTE
+                && candidate.getNationality().equals(NATIONALITY)
+                && hasLivedUkraineForRequiredYears(candidate.getPeriodsInUkr());
+    }
+
+    private boolean hasLivedUkraineForRequiredYears(String period) {
+        String[] str = period.split("-");
+
+        return str.length == 2 && Integer.parseInt(str[1])
+                - Integer.parseInt(str[0]) >= LIVE_IN_UKRAINE;
+    }
 }
