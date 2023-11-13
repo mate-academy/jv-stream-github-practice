b83ee4d4e3a9115510c9c4cd4f518243fe4440b9
Bohdan Chupika <boroda4436@users.noreply.github.com>
2023-11-13 13:08:17 +0200

    My hometask Stream

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,28 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    public static final int MIN_AGE = 35;
+    public static final String NATIONALITY = "Ukrainian";
+    public static final int PERIOD_OF_RESIDENCE = 10;
+    public static final int START_INDEX_OF_RESIDENCE = 0;
+    public static final int END_INDEX_OF_RESIDENCE = 1;
+    public static final String SEPARATOR = "-";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MIN_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && yearsInUkraine(candidate.getPeriodsInUkr()) >= PERIOD_OF_RESIDENCE;
+    }
+
+    private int yearsInUkraine(String periodsInUkr) {
+        String[] periods = periodsInUkr.split(SEPARATOR);
+        int startYear = Integer.parseInt(periods[START_INDEX_OF_RESIDENCE]);
+        int endYear = Integer.parseInt(periods[END_INDEX_OF_RESIDENCE]);
+        return endYear - startYear + 1;
+    }
 }
