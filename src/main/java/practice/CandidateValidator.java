3de7cfc86c8c99d14db88885e5ffaa1f65364d89
a_shtuka <ashtuka@gmail.com>
2023-11-15 23:19:34 +0200

    racticed working with streams

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,27 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final int MIN_LIVE_PERIOD = 10;
+    private static final int LIVE_FROM_INDEX = 0;
+    private static final int LIVE_TO_INDEX = 1;
+    private static final String NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.isAllowedToVote()
+            && candidate.getAge() >= MIN_AGE
+            && candidate.getNationality().equals(NATIONALITY)
+            && isLivedEnough(candidate.getPeriodsInUkr());
+    }
+
+    private boolean isLivedEnough(String period) {
+        String[] years = period.split("-");
+        int livedPeriod = Integer.parseInt(years[LIVE_TO_INDEX])
+                - Integer.parseInt(years[LIVE_FROM_INDEX]);
+        return livedPeriod >= MIN_LIVE_PERIOD;
+    }
 }
