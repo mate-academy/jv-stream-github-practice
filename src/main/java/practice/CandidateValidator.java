5b627671fb31b7c92383b24ef5e917c7549d4ab0
Volodymyr Shashkov <finansist33333324@gmail.com>
2023-11-18 09:00:13 +0200

    jv-stream-solution

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,24 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.Arrays;
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int MIN_PERIOD_IN_UKRAINE = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] period = candidate.getPeriodsInUkr().split("-");
+
+        Integer periodsInUkr = Arrays.stream(period)
+                .map(s -> Integer.parseInt(s))
+                .reduce(0, (x, y) -> Math.abs(x - y));
+
+        return candidate.getAge() >= MIN_AGE && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && periodsInUkr >= MIN_PERIOD_IN_UKRAINE;
+    }
 }
