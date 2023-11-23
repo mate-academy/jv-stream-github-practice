31c3e981a8fd8161d0b62606d23242f9edae3ffd
Oleksandra Dorofieieva <olexsandradorofeieiva@gmail.com>
2023-11-23 15:33:55 +0200

    implements methods: findMinEvenNumber(), getOddNumsAverage(), selectMenByAge(), getWorkablePeople(), getCatsNames(), validateCandidates(); create own Predicate in CandidateValidator;

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,24 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int VALID_CANDIDATE_AGE = 35;
+    private static final int VALID_PERIOD_IN_UKR = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= VALID_CANDIDATE_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && calculateYearsInUkr(candidate.getPeriodsInUkr()) >= VALID_PERIOD_IN_UKR;
+    }
+
+    private int calculateYearsInUkr(String periodInUkr) {
+        String[] partOfPeriodInUkr = periodInUkr.split("-");
+        int startYearInUkr = Integer.parseInt(partOfPeriodInUkr[0]);
+        int endYearInUkr = Integer.parseInt(partOfPeriodInUkr[1]);
+        return endYearInUkr - startYearInUkr;
+    }
 }
