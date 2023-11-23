842298fbffcec037dd98dac8830a2cb4e2ad19a6
Alex-Solod <solodyhin_2@icloud.com>
2023-11-23 21:13:35 +0200

    Task 1: Extract minimum even integer from a list of strings containing multiple numbers
    Task 2:
    Subtract 1 from odd-indexed elements and calculate average of odd numbers
    
    Task 3:
    Select men of army recruitment age (18-27 years) from a list of People
    
    Task 4:
    Select people of working age (18-60 for men, 18-55 for women) from a list of People
    
    Task 5:
    Return names of cats owned by women of a certain age from a list of People
    
    Task 6:
    Validate candidates for president: age ≥ 35, allowed to vote, Ukrainian nationality, and lived in Ukraine for 10 years
    Also implemented a Predicate parametrized CandidateValidator method

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,22 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        if (candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && candidate.getPeriodsInUkr() != null) {
+            String[] periods = candidate.getPeriodsInUkr().split("-");
+            if (periods.length == 2) {
+                int startYear = Integer.parseInt(periods[0]);
+                int endYear = Integer.parseInt(periods[1]);
+                return endYear - startYear >= 10;
+            }
+        }
+        return false;
+    }
 }
