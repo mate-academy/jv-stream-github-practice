deca489a3b0fbfd3580f6dd274e83d2f28a4df8b
Oleh Shvets <olehpost69@gmail.com>
2023-11-24 19:33:14 +0300

    stream_practice_firstCommit

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final int MIN_RESIDENCY_YEARS = 10;
+    private static final String REQUIRED_NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] yearsRange = candidate.getPeriodsInUkr().split("-");
+        int stayDuration = Integer.parseInt(yearsRange[1]) - Integer.parseInt(yearsRange[0]);
+        return candidate.isAllowedToVote()
+                && candidate.getAge() >= MIN_AGE
+                && candidate.getNationality().equals(REQUIRED_NATIONALITY)
+                && stayDuration >= MIN_RESIDENCY_YEARS;
+    }
 }
