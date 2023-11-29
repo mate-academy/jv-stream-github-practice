48b95276ea218aa17fee555a2fa60ef18701438e
Mykola Horianin <anyavic2379@gmail.com>
2023-11-29 15:43:36 +0200

    Completed practical tasks

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,24 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.Arrays;
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final int MIN_YEARS = 10;
+    private static final String SPACE_SEPARATOR = "-";
+    private static final String NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        int[] years = Arrays.stream(candidate.getPeriodsInUkr().split(SPACE_SEPARATOR))
+                .mapToInt(y -> Integer.parseInt(y))
+                .toArray();
+        int periodsInUkr = years[1] - years[0];
+        return candidate.getAge() >= MIN_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && periodsInUkr >= MIN_YEARS;
+    }
 }
