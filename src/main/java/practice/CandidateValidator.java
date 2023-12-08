a6a120bc70e8603b948b6ee4a562342d54384526
Vadim <waterfallkadas@gmail.com>
2023-12-08 22:35:04 +0200

    Solution 08/12

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final String SPLITTER = "-";
+    private static final int MAX_AGE = 35;
+    private static final int FROM_YEAR_INDEX = 0;
+    private static final int TO_YEAR_INDEX = 1;
+    private static final int MIN_YEARS_IN_UKRAINE = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] periodInUkraine = candidate.getPeriodsInUkr().split(SPLITTER);
+        return candidate.getAge() >= MAX_AGE && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && (Integer.parseInt(periodInUkraine[TO_YEAR_INDEX])
+                - Integer.parseInt(periodInUkraine[FROM_YEAR_INDEX])) >= MIN_YEARS_IN_UKRAINE;
+    }
 }
