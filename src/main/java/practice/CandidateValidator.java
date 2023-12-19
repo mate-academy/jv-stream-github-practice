8a8ddcbab41a137ea5463f2eda4555bdc3e19e42
Snizhana Sosiedova <snejik1111@gmail.com>
2023-12-20 00:03:58 +0200

    Realized methods using Stream API

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,18 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String periodsInUkr = candidate.getPeriodsInUkr();
+        String[] periods = periodsInUkr.split("-");
+        int yearsInUkr = Integer.parseInt(periods[1]) - Integer.parseInt(periods[0]);
+        return candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && yearsInUkr >= 10;
+    }
 }
