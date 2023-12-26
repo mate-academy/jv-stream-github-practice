a7c2af3d424d86f706d4a92d4f7b5d6d7e28db5f
AdamMudrak <mudrets2000@gmail.com>
2023-12-26 23:37:00 +0200

    Extract all literals to constants with meaningful names

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,15 +5,17 @@ import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_PERIOD_ALLOWED = 10;
+    private static final String PERIOD_IN_UKR_SPLITTER = "-";
+    private static final String REQUIRED_NATIONALITY = "Ukrainian";
 
     @Override
     public boolean test(Candidate candidate) {
-        String[] periodInUkraine = candidate.getPeriodsInUkr().split("-");
+        String[] periodInUkraine = candidate.getPeriodsInUkr().split(PERIOD_IN_UKR_SPLITTER);
         int fromYear = Integer.parseInt(periodInUkraine[0]);
         int toYear = Integer.parseInt(periodInUkraine[1]);
         return candidate.getAge() >= 35
                 && candidate.isAllowedToVote()
-                && candidate.getNationality().equals("Ukrainian")
+                && candidate.getNationality().equals(REQUIRED_NATIONALITY)
                 && toYear - fromYear >= MIN_PERIOD_ALLOWED;
     }
 }
