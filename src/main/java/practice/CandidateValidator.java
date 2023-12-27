bfef21f1789889e3dd3d6415070dff3614693271
AdamMudrak <mudrets2000@gmail.com>
2023-12-27 14:38:14 +0200

    Extract even more data to constant fields

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,18 +4,19 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    private static final int MIN_PERIOD_ALLOWED = 10;
     private static final String PERIOD_IN_UKR_SPLITTER = "-";
+    private static final int MIN_AGE_ALLOWED = 35;
     private static final String REQUIRED_NATIONALITY = "Ukrainian";
+    private static final int MIN_PERIOD_ALLOWED = 10;
 
     @Override
     public boolean test(Candidate candidate) {
         String[] periodInUkraine = candidate.getPeriodsInUkr().split(PERIOD_IN_UKR_SPLITTER);
-        int fromYear = Integer.parseInt(periodInUkraine[0]);
-        int toYear = Integer.parseInt(periodInUkraine[1]);
-        return candidate.getAge() >= 35
+        int inUkraineFromYear = Integer.parseInt(periodInUkraine[0]);
+        int inUkraineToYear = Integer.parseInt(periodInUkraine[1]);
+        return candidate.getAge() >= MIN_AGE_ALLOWED
                 && candidate.isAllowedToVote()
                 && candidate.getNationality().equals(REQUIRED_NATIONALITY)
-                && toYear - fromYear >= MIN_PERIOD_ALLOWED;
+                && inUkraineFromYear - inUkraineToYear >= MIN_PERIOD_ALLOWED;
     }
 }
