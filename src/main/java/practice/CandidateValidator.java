6c7d42bf7f83455a2b7c9550289ff6425d1b7536
letmerelaxpls <larsubivec@gmail.com>
2024-01-06 17:33:47 +0400

    added more constants in CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,6 +4,8 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int FIRST_YEAR_INDEX = 0;
+    private static final int SECOND_YEAR_INDEX = 1;
     private static final int MINIMUM_AGE = 35;
     private static final String ALLOWED_NATIONALITY = "Ukrainian";
     private static final int MINIMUM_PERIODS_IN_UKRAINE = 10;
@@ -18,7 +20,8 @@ public class CandidateValidator implements Predicate<Candidate> {
 
     private int periodsToInt(Candidate candidate) {
         String[] years = candidate.getPeriodsInUkr().split("-");
-        return Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
+        return Integer.parseInt(years[SECOND_YEAR_INDEX])
+                - Integer.parseInt(years[FIRST_YEAR_INDEX]);
     }
     //write your code here
 }
