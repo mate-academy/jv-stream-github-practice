d38ab335340976c0c6cb4ce6337a232d506e0e5e
Vlad Bondarev <bondarev.21@icloud.com>
2024-01-11 17:44:04 +0200

    changed solution with added constants

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,15 +6,17 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int YEAR_FROM_INDEX = 0;
     private static final int YEAR_TO_INDEX = 1;
+    private static final int REQUIRED_YEARS_OF_LIVING = 10;
+    private static final int REQUIRED_AGE = 35;
 
     @Override
     public boolean test(Candidate candidate) {
         String[] candidateLivingDate = candidate.getPeriodsInUkr().split("-");
         boolean candidateLivingIsValid = Integer.parseInt(candidateLivingDate[YEAR_TO_INDEX])
                 - Integer.parseInt(candidateLivingDate[YEAR_FROM_INDEX])
-                >= 10;
+                >= REQUIRED_YEARS_OF_LIVING;
         return candidate.isAllowedToVote()
-                && candidate.getAge() >= 35
+                && candidate.getAge() >= REQUIRED_AGE
                 && candidate.getNationality().equals("Ukrainian")
                 && candidateLivingIsValid;
     }
