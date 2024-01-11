61630ac922b29abe7a3f2e1a211b90ae729b8c5e
Vlad Bondarev <bondarev.21@icloud.com>
2024-01-11 17:41:19 +0200

    hm for review

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int YEAR_FROM_INDEX = 0;
+    private static final int YEAR_TO_INDEX = 1;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] candidateLivingDate = candidate.getPeriodsInUkr().split("-");
+        boolean candidateLivingIsValid = Integer.parseInt(candidateLivingDate[YEAR_TO_INDEX])
+                - Integer.parseInt(candidateLivingDate[YEAR_FROM_INDEX])
+                >= 10;
+        return candidate.isAllowedToVote()
+                && candidate.getAge() >= 35
+                && candidate.getNationality().equals("Ukrainian")
+                && candidateLivingIsValid;
+    }
 }
