09dcb3d51f37a733a35687873d2e7cb45eb48abf
Anna Suprun <evgenevnaanna989@gmail.com>
2024-01-12 20:54:19 +0200

    created CandidateValidator, methods in StreamPractice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_APPLICATION_AGE = 35;
+    private static final int MIN_RESIDENCE_TIME = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.isAllowedToVote() && candidate.getNationality().equals("Ukrainian")
+                && candidate.getAge() >= MIN_APPLICATION_AGE
+                && isValidYearsInUkr(candidate.getPeriodsInUkr());
+    }
+
+    private boolean isValidYearsInUkr(String periods) {
+        String[] years = periods.split("-");
+        return (Integer.parseInt(years[1]) - Integer.parseInt(years[0])) >= MIN_RESIDENCE_TIME;
+    }
 }
