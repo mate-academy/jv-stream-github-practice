e9b55c023ebb3f1f006ab9af9623e59a83699d3c
Vlad Bondarev <bondarev.21@icloud.com>
2024-01-12 13:41:20 +0200

    hm after review

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,23 +1,24 @@
 package practice;
 
+import java.util.Objects;
 import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int YEAR_FROM_INDEX = 0;
     private static final int YEAR_TO_INDEX = 1;
-    private static final int REQUIRED_YEARS_OF_LIVING = 10;
-    private static final int REQUIRED_AGE = 35;
+    private static final int MIN_ALLOWED_YEARS = 10;
+    private static final int MIN_ALLOWED_AGE = 10;
+    private static final String REQUIRED_NATIONALITY = "Ukrainian";
+    private static final String SEPARATOR = "-";
 
     @Override
     public boolean test(Candidate candidate) {
-        String[] candidateLivingDate = candidate.getPeriodsInUkr().split("-");
-        boolean candidateLivingIsValid = Integer.parseInt(candidateLivingDate[YEAR_TO_INDEX])
-                - Integer.parseInt(candidateLivingDate[YEAR_FROM_INDEX])
-                >= REQUIRED_YEARS_OF_LIVING;
+        String[] periodInUkraine = candidate.getPeriodsInUkr().split(SEPARATOR);
         return candidate.isAllowedToVote()
-                && candidate.getAge() >= REQUIRED_AGE
-                && candidate.getNationality().equals("Ukrainian")
-                && candidateLivingIsValid;
+                && candidate.getAge() >= MIN_ALLOWED_AGE
+                && Objects.equals(candidate.getNationality(), REQUIRED_NATIONALITY)
+                && Integer.parseInt(periodInUkraine[YEAR_TO_INDEX])
+                - Integer.parseInt(periodInUkraine[YEAR_FROM_INDEX]) >= MIN_ALLOWED_YEARS;
     }
 }
--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,9 +1,6 @@
 package practice;
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -12,26 +9,25 @@ import model.Cat;
 import model.Person;
 
 public class StreamPractice {
+    private static final String COMMA = ",";
+    private static final String REGEX_TO_CHECK_WHETHER_NUMBER = "-?\\d+";
     private static final int NUMBER_TO_SUBTRACT = 1;
 
     public int findMinEvenNumber(List<String> numbers) {
         return numbers
                 .stream()
-                .flatMap(string -> Arrays.stream(string.replaceAll(",", " ").split(" ")))
-                .map(string -> string.matches("-?\\d+")
-                        ? Integer.parseInt(string)
-                        : -1)
-                .mapToInt(number -> number)
-                .filter(number -> number % 2 == 0)
-                .min()
-                .orElseThrow(() -> new RuntimeException(
-                        "Can't get min value from list: < Here is our input 'numbers' > "
-                                + numbers));
+                .flatMap(string -> Arrays.stream(string.split(COMMA)))
+                .filter(string -> string.matches(REGEX_TO_CHECK_WHETHER_NUMBER))
+                .mapToInt(Integer::parseInt)
+                .filter(integer -> integer % 2 == 0)
+                .min().orElseThrow(() -> new NoSuchElementException(
+                        "Can't get min value from list: < Here is our input 'numbers' >" + numbers));
     }
 
     public Double getOddNumsAverage(List<Integer> numbers) {
-        IntStream.range(0, numbers.size())
-                .filter(integer -> integer % 2 != 0)
+        IntStream
+                .range(0, numbers.size())
+                .filter(i -> i % 2 != 0)
                 .forEach(index -> numbers.set(index, numbers.get(index) - NUMBER_TO_SUBTRACT));
         return numbers
                 .stream()
@@ -45,41 +41,37 @@ public class StreamPractice {
         return peopleList
                 .stream()
                 .filter(person -> person.getSex().equals(Person.Sex.MAN)
-                        && person.getAge() >= fromAge
-                        && person.getAge() <= toAge)
+                            && person.getAge() >= fromAge
+                            && person.getAge() <= toAge)
                 .collect(Collectors.toList());
     }
 
     public List<Person> getWorkablePeople(int fromAge, int femaleToAge,
                                           int maleToAge, List<Person> peopleList) {
-        Predicate<Person> personPredicate = person -> {
-            return person.getSex().equals(Person.Sex.MAN)
-                    ? (person.getAge() <= maleToAge && person.getAge() >= fromAge)
-                    : (person.getAge() <= femaleToAge && person.getAge() >= fromAge);
-        };
         return peopleList
                 .stream()
-                .filter(personPredicate)
+                .filter(person -> (person.getSex().equals(Person.Sex.MAN))
+                        ? (person.getAge() >= fromAge && person.getAge() <= maleToAge)
+                        : (person.getAge() >= fromAge && person.getAge() <= femaleToAge))
                 .collect(Collectors.toList());
     }
 
     public List<String> getCatsNames(List<Person> peopleList, int femaleAge) {
         return peopleList
                 .stream()
-                .filter(person -> Objects.equals(person.getSex(), Person.Sex.WOMAN)
+                .filter(person -> person.getSex().equals(Person.Sex.WOMAN)
                         && person.getCats() != null
                         && person.getAge() >= femaleAge)
-                .map(Person::getCats)
-                .flatMap(Collection::stream)
-                .map(Cat::getName)
+                .flatMap(person -> person.getCats()
+                        .stream()
+                        .map(Cat::getName))
                 .collect(Collectors.toList());
     }
 
     public List<String> validateCandidates(List<Candidate> candidates) {
-        CandidateValidator candidateValidator = new CandidateValidator();
         return candidates
                 .stream()
-                .filter(candidateValidator)
+                .filter(new CandidateValidator())
                 .map(Candidate::getName)
                 .sorted()
                 .collect(Collectors.toList());
