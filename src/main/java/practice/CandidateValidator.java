4af18b287d359cafb3d4c23652fd498c2c3cce20
almanac1914 <144943174+almanac1914@users.noreply.github.com>
2024-01-15 06:57:46 -0700

    constants implemented

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,12 +4,17 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int YEARS_IN_UKRAINE = 10;
+
     @Override
     public boolean test(Candidate candidate) {
         String[] partsOfYearsInUkraine = candidate.getPeriodsInUkr().split("-");
         int periodInUkraine = Integer.parseInt(partsOfYearsInUkraine[1])
                 - Integer.parseInt(partsOfYearsInUkraine[0]);
-        return candidate.getAge() >= 35 && candidate.isAllowedToVote()
-                && candidate.getNationality().equals("Ukrainian") && periodInUkraine >= 10;
+        return candidate.getAge() >= MIN_AGE && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && periodInUkraine >= YEARS_IN_UKRAINE;
     }
 }
