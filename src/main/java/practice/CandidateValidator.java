18a6e2c36ab0dc73b4d5762249f03cf0f33bb345
Bohdan Kost <kostbogdan@icloud.com>
2024-01-21 15:31:40 +0200

    magic numbers replaced by constants

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -8,6 +8,8 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_AGE = 35;
     private static final String REQUIRED_NATIONALITY = "Ukrainian";
     private static final int MIN_PERIOD = 10;
+    private static final int PERIOD_START_INDEX = 0;
+    private static final int PERIOD_END_INDEX = 1;
 
     @Override
     public boolean test(Candidate candidate) {
@@ -15,7 +17,7 @@ public class CandidateValidator implements Predicate<Candidate> {
         return candidate.getAge() >= MIN_AGE
                 && candidate.isAllowedToVote()
                 && candidate.getNationality().equals(REQUIRED_NATIONALITY)
-                && (Integer.parseInt(periodInUkr[1])
-                - Integer.parseInt(periodInUkr[0]) >= MIN_PERIOD);
+                && (Integer.parseInt(periodInUkr[PERIOD_END_INDEX])
+                - Integer.parseInt(periodInUkr[PERIOD_START_INDEX]) >= MIN_PERIOD);
     }
 }
