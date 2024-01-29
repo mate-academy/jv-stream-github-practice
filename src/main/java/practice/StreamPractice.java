2515fd5f3414d4cd6d9335995a734a941ae7f538
Bohdan <bohdan2704@gmail.com>
2024-01-29 22:33:43 +0200

    removed import *

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,6 +1,9 @@
 package practice;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import model.Candidate;
