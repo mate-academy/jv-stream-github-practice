4e87e9df52abe8c3158687667800d03094e42caf
Mykhailo Kazarian <misha.kazaryan2@gmail.com>
2024-02-03 20:43:21 +0200

    Jv stream practice task solution

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.Arrays;
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int AGE_RESTRICTION = 35;
+    private static final int VALID_PERIOD_OF_STAY = 10;
+    private static final String ADMISSIBLE_NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= AGE_RESTRICTION
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(ADMISSIBLE_NATIONALITY)
+                && Arrays.stream(candidate.getPeriodsInUkr().split("-"))
+                .map(Integer::parseInt).reduce(0, (a, b) -> b - a) >= VALID_PERIOD_OF_STAY;
+    }
     //write your code here
 }
