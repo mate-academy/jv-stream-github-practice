700f5219f27fbd30ffa108037493d53cd30d0df7
Orest 40th <student2528@bmet.org.ua>
2024-02-12 15:53:08 +0200

    introduced new constants in CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,17 +6,18 @@ import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
     private static final String CANDIDATE_VALIDATOR_EX = "-";
+    private static final int MIN_RESIDENCE_TIME = 10;
+    private static final int MIN_AGE = 35;
 
     @Override
     public boolean test(Candidate candidate) {
-        int residenceYears = Arrays.stream(candidate.getPeriodsInUkr()
-                        .split(CANDIDATE_VALIDATOR_EX))
+        int residenceYears = Arrays.stream(candidate.getPeriodsInUkr().split(CANDIDATE_VALIDATOR_EX))
                 .mapToInt(Integer::parseInt)
                 .reduce((first, second) -> second - first)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid range format"));
 
-        return candidate.getAge() >= 35 && candidate.isAllowedToVote()
+        return candidate.getAge() >= MIN_AGE && candidate.isAllowedToVote()
                 && candidate.getNationality().equals("Ukrainian")
-                && residenceYears >= 10;
+                && residenceYears >= MIN_RESIDENCE_TIME;
     }
 }
