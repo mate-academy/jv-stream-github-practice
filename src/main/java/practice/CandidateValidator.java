dd534801818f6a24a4cbb427443a2bea1612a5c6
Danylo Kishchenko <amfich@gmail.com>
2024-02-22 22:12:10 +0200

    added constant field SEPARATOR to 'CandidateValidator' class

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -9,10 +9,11 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_YEARS_IN_UKR = 10;
     private static final byte FIRST_YEAR_INDEX = 0;
     private static final byte LAST_YEAR_INDEX = 1;
+    private static final String SEPARATOR = "-";
 
     @Override
     public boolean test(Candidate candidate) {
-        String[] periodInUkr = candidate.getPeriodsInUkr().split("-");
+        String[] periodInUkr = candidate.getPeriodsInUkr().split(SEPARATOR);
         int yearsInUkr = Integer.parseInt(periodInUkr[LAST_YEAR_INDEX])
                 - Integer.parseInt(periodInUkr[FIRST_YEAR_INDEX]);
         return candidate.getAge() >= MIN_AGE && candidate.getNationality().equals(NATIONALITY)
