7af27af75e72e365b3a87f873a89370e24037156
Oleksii Lytvynenko <oleksiila@gmail.com>
2024-02-29 03:13:53 +0000

    Initial commit

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,26 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int MIN_AGE = 35;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MIN_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality() == NATIONALITY
+                && hasLivedInUkraineForTenYears(candidate);
+    }
+
+    private boolean hasLivedInUkraineForTenYears(Candidate candidate) {
+        String[] split = candidate.getPeriodsInUkr().split("-");
+        if (Integer.parseInt(split[1]) - Integer.parseInt(split[0]) >= 10) {
+            return true;
+        }
+        return false;
+    }
 }
+
