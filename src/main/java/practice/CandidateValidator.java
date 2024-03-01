132b4f08ab6f7dbbd4b7cc735bc06eb67437779d
Vlad Nadtochiy <v20102002n@gmail.com>
2024-03-01 21:15:29 +0200

    3rd commit

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,6 +5,11 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    public static final int MIN_YEAR_OF_PERIOD = 1900;
+    public static final int MAX_YEAR_OF_PERIOD = Year.now().getValue();
+    public static final int MIN_AGE_FOR_CANDIDATE = 35;
+    public static final String NATIONALITY = "Ukrainian";
+    public static final int MIN_YEARS_IN_UKRAINE = 10;
 
     @Override
     public boolean test(Candidate candidate) {
@@ -12,13 +17,14 @@ public class CandidateValidator implements Predicate<Candidate> {
             throw new RuntimeException("Incorrect periods");
         }
         String periods = candidate.getPeriodsInUkr();
-        int firstYear = Integer.parseInt(periods.substring(0, periods.indexOf("-")));
-        int lastYear = Integer.parseInt(periods.substring(periods.indexOf("-") + 1));
-        if (firstYear < 1900 || lastYear > Year.now().getValue() || firstYear > lastYear) {
+        final int firstYear = Integer.parseInt(periods.substring(0, periods.indexOf("-")));
+        final int lastYear = Integer.parseInt(periods.substring(periods.indexOf("-") + 1));
+        if (firstYear < MIN_YEAR_OF_PERIOD || lastYear > MAX_YEAR_OF_PERIOD
+                || firstYear > lastYear) {
             throw new RuntimeException("Incorrect periods");
         }
-        return candidate.isAllowedToVote() && candidate.getAge() >= 35
-                && candidate.getNationality().equals("Ukrainian")
-                && lastYear - firstYear >= 10;
+        return candidate.isAllowedToVote() && candidate.getAge() >= MIN_AGE_FOR_CANDIDATE
+                && candidate.getNationality().equals(NATIONALITY)
+                && lastYear - firstYear >= MIN_YEARS_IN_UKRAINE;
     }
 }
