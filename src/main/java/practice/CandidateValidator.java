e0ab9d738ce9ff57cc482380e77db3da414891e3
Vlad Nadtochiy <v20102002n@gmail.com>
2024-03-01 20:07:38 +0200

    new commit

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,24 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.time.Year;
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    @Override
+    public boolean test(Candidate candidate) {
+        if (!candidate.getPeriodsInUkr().contains("-")) {
+            throw new RuntimeException("Incorrect periods");
+        }
+        String periods = candidate.getPeriodsInUkr();
+        int firstYear = Integer.parseInt(periods.substring(0, periods.indexOf("-")));
+        int lastYear = Integer.parseInt(periods.substring(periods.indexOf("-") + 1));
+        if (firstYear < 1900 || lastYear > Year.now().getValue() || firstYear > lastYear) {
+            throw new RuntimeException("Incorrect periods");
+        }
+        return candidate.isAllowedToVote() && candidate.getAge() >= 35
+                && candidate.getNationality().equals("Ukrainian")
+                && lastYear - firstYear >= 10;
+    }
 }
