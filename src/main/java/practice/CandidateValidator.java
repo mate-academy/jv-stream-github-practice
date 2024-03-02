69fc7a618089f7158d50c9c4cae53ed2baf378a2
Vlad Nadtochiy <v20102002n@gmail.com>
2024-03-02 15:28:16 +0200

    6th

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -8,6 +8,8 @@ public class CandidateValidator implements Predicate<Candidate> {
     public static final int MIN_AGE_FOR_CANDIDATE = 35;
     public static final String NATIONALITY = "Ukrainian";
     public static final int MIN_YEARS_IN_UKRAINE = 10;
+    public static final int INDEX_OF_FIRST_YEAR_IN_UKRAINE = 0;
+    public static final int INDEX_OF_LAST_YEAR_IN_UKRAINE = 1;
 
     @Override
     public boolean test(Candidate candidate) {
@@ -16,6 +18,7 @@ public class CandidateValidator implements Predicate<Candidate> {
                 .toArray();
         return candidate.isAllowedToVote() && candidate.getAge() >= MIN_AGE_FOR_CANDIDATE
                 && candidate.getNationality().equals(NATIONALITY)
-                && intPeriod[1] - intPeriod[0] >= MIN_YEARS_IN_UKRAINE;
+                && intPeriod[INDEX_OF_LAST_YEAR_IN_UKRAINE]
+                - intPeriod[INDEX_OF_FIRST_YEAR_IN_UKRAINE] >= MIN_YEARS_IN_UKRAINE;
     }
 }
