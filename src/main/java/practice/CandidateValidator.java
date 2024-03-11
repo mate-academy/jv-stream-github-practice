a1a0a72061c36885cdcbb263b9641e9bbb9cb28d
Ivan Prystaia <greqit@gmail.com>
2024-03-11 19:45:04 +0200

    Fix magic numbers

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,18 +4,21 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    private static final int minAge = 35;
-    private static final int minYearsInUkr = 10;
+    private static final int MIN_AGE = 35;
+    private static final int MIN_YEARS_IN_UKR = 10;
+    private static final int INDEX_START_YEAR = 0;
+    private static final int INDEX_END_YEAR = 1;
 
     @Override
     public boolean test(Candidate candidate) {
         String nationality = "Ukrainian";
         String[] years = candidate.getPeriodsInUkr().split("-");
-        int yearsInUkr = Integer.parseInt(years[1]) - Integer.parseInt(years[0]);
+        int yearsInUkr = Integer.parseInt(years[INDEX_END_YEAR])
+                - Integer.parseInt(years[INDEX_START_YEAR]);
 
-        return candidate.getAge() >= minAge
+        return candidate.getAge() >= MIN_AGE
                 && candidate.isAllowedToVote()
                 && candidate.getNationality().equals(nationality)
-                && yearsInUkr >= minYearsInUkr;
+                && yearsInUkr >= MIN_YEARS_IN_UKR;
     }
 }
