63666a5fca1db145c414d8cf29de8004d0f2f0f6
Vlasenko Yevhenii <vlasenkoevy@gmail.com>
2024-03-13 11:49:51 +0200

    implemented all methods int the task

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.isAllowedToVote()
+                && candidate.getAge() >= 35
+                && candidate.getNationality().equals("Ukrainian")
+                && calculatePeriodInUkraine(candidate) >= 10;
+    }
+
+    private int calculatePeriodInUkraine(Candidate candidate) {
+        String[] stringDates = candidate.getPeriodsInUkr().split("-");
+        return Integer.parseInt(stringDates[1]) - Integer.parseInt(stringDates[0]);
+    }
 }
