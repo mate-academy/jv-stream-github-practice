0b02e6e49ffe8c56e8feacb0293b385ecb074282
John <pandastars535@gmail.com>
2024-03-14 15:52:42 +0200

    implemented getOddNumsAverage and selectMenByAge methods

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,12 +1,14 @@
 package practice;
 
+import model.Candidate;
+import model.Person;
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Stream;
-
-import model.Candidate;
-import model.Person;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class StreamPractice {
     /**
@@ -22,7 +24,7 @@ public class StreamPractice {
                 .filter(e -> Integer.parseInt(e) % 2 == 0)
                 .mapToInt(Integer::parseInt)
                 .min()
-                .orElseThrow(() -> new RuntimeException("Can't get min value from list"));
+                .orElseThrow(() -> new RuntimeException("Can't get min value from list:" + numbers));
     }
 
     /**
@@ -31,7 +33,13 @@ public class StreamPractice {
      * But before that subtract 1 from each element on an odd position (having the odd index).
      */
     public Double getOddNumsAverage(List<Integer> numbers) {
-        return 0D;
+
+        return IntStream.range(0, numbers.size())
+                .mapToObj(i -> i % 2 == 1 ? numbers.get(i) - 1 : numbers.get(i))
+                .filter(e -> e % 2 == 1)
+                .mapToDouble(value -> value)
+                .average()
+                .orElseThrow(() -> new NoSuchElementException("Can't get value from list:"));
     }
 
     /**
@@ -43,7 +51,9 @@ public class StreamPractice {
      * Example: select men who can be recruited to army (from 18 to 27 years old inclusively).
      */
     public List<Person> selectMenByAge(List<Person> peopleList, int fromAge, int toAge) {
-        return Collections.emptyList();
+        return peopleList.stream().filter(p -> p.getSex() == Person.Sex.MAN
+                        && (p.getAge() >= fromAge && p.getAge() <= toAge))
+                .collect(Collectors.toList());
     }
 
     /**
@@ -58,6 +68,7 @@ public class StreamPractice {
      */
     public List<Person> getWorkablePeople(int fromAge, int femaleToAge,
                                           int maleToAge, List<Person> peopleList) {
+        peopleList.stream()
         return Collections.emptyList();
     }
 
