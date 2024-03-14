60e334cbb2044acd10eea2bfc01fcd167fd95d68
Mykola Skrypalov <skripalov1@gmail.com>
2024-03-14 16:30:57 +0200

    Java stream practice solution

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,25 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final String UKR_NATIONALITY = "Ukrainian";
+    private static final int LIVED_IN_UKRAINE_FROM_INDEX = 1;
+    private static final int LIVED_IN_UKRAINE_TO_INDEX = 0;
+    private static final int MIN_YEARS_IN_UKRAINE = 10;
+    private static final int MIN_ALLOWED_CANDIDATE_AGE = 35;
+    private static final String SPLITERATOR = "-";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] yearsInUkraine = candidate.getPeriodsInUkr().split(SPLITERATOR);
+        int yearsInUA = Integer.parseInt(yearsInUkraine[LIVED_IN_UKRAINE_FROM_INDEX])
+                        - Integer.parseInt(yearsInUkraine[LIVED_IN_UKRAINE_TO_INDEX]);
+
+        return candidate.getAge() >= MIN_ALLOWED_CANDIDATE_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(UKR_NATIONALITY)
+                && yearsInUA >= MIN_YEARS_IN_UKRAINE;
+    }
 }
