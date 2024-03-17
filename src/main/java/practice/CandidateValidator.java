9fa1e838ceac22a48890d3edc0fbe064582ff006
HAIOVIY <gaevoywork@gmail.com>
2024-03-17 20:54:11 +0200

    implementes methods

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,25 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final String REQUIRED_NATIONALITY = "Ukrainian";
+    private static final int MIN_PERIOD_IN_UKRAINE = 10;
+    private static final String SPLITTER = "-";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MIN_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(REQUIRED_NATIONALITY)
+                && timeLivedInUkraine(candidate) >= MIN_PERIOD_IN_UKRAINE;
+    }
+
+    private static int timeLivedInUkraine(Candidate candidate) {
+        String[] periodDate = candidate.getPeriodsInUkr().split(SPLITTER);
+        return Integer.parseInt(periodDate[1]) - Integer.parseInt(periodDate[0]);
+    }
+
 }
