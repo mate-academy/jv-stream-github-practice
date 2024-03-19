5c21373a17443638009aa61645e998c53a56d6ac
Denys <Eidenn005@gmail.com>
2024-03-19 13:32:13 +0200

    made suggested changes

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,22 +4,21 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    public static final int FROM_YEAR_INDEX = 0;
     public static final int LIVE_IN_UKR = 10;
+    public static final int TILL_YEAR_INDEX = 1;
+    public static final int VOTE_AGE = 35;
     public static final String NATIONALITY = "Ukrainian";
-    private static final int VOTE_AGE = 35;
+    public static final String YEAR_SEPARATOR = "-";
 
     @Override
     public boolean test(Candidate candidate) {
-        boolean isElegibleAgeToVote = candidate.getAge() >= VOTE_AGE;
-        boolean isElegibleLiveInUkr = periodLiveInUkr(candidate);
-        boolean isElegibleNationality = candidate.getNationality().equals(NATIONALITY);
-        boolean isElegibleToVote = candidate.isAllowedToVote();
-        return isElegibleAgeToVote && isElegibleNationality
-                && isElegibleLiveInUkr && isElegibleToVote;
-    }
-
-    public boolean periodLiveInUkr(Candidate candidate) {
-        String[] periods = candidate.getPeriodsInUkr().split("-");
-        return Integer.parseInt(periods[1]) - Integer.parseInt(periods[0]) > LIVE_IN_UKR;
+        String[] periods = candidate.getPeriodsInUkr().split(YEAR_SEPARATOR);
+        int periodLiveInUkr = Integer.parseInt(periods[TILL_YEAR_INDEX])
+                - Integer.parseInt(periods[FROM_YEAR_INDEX]);
+        return candidate.getAge() >= VOTE_AGE
+                && periodLiveInUkr >= LIVE_IN_UKR
+                && candidate.getNationality().equals(NATIONALITY)
+                && candidate.isAllowedToVote();
     }
 }
--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -2,6 +2,8 @@ package practice;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import model.Candidate;
@@ -9,10 +11,14 @@ import model.Cat;
 import model.Person;
 
 public class StreamPractice {
+    public static final String NUMBER_SEPARATOR = ",";
+    private CandidateValidator candidateValidator = new CandidateValidator();
+
     public int findMinEvenNumber(List<String> numbers) {
         return numbers.stream()
-                .flatMapToInt(s -> Arrays.stream(s.split(",")).mapToInt(Integer::parseInt))
-                .filter(num -> num % 2 == 0)
+                .flatMapToInt(s -> Arrays.stream(s.split(NUMBER_SEPARATOR))
+                        .mapToInt(Integer::parseInt))
+                .filter(this::isEven)
                 .min()
                 .orElseThrow(() -> new RuntimeException("Can't get min value from list: "
                         + numbers));
@@ -20,25 +26,22 @@ public class StreamPractice {
 
     public Double getOddNumsAverage(List<Integer> numbers) {
         return IntStream.range(0, numbers.size())
-                .map(i -> i % 2 != 0 ? numbers.get(i) - 1 : numbers.get(i))
-                .filter(i -> i % 2 != 0)
+                .map(i -> !isEven(i) ? numbers.get(i) - 1 : numbers.get(i))
+                .filter(i -> !isEven(i))
                 .average()
-                .orElseThrow();
+                .orElseThrow(() -> new NoSuchElementException("No odds numbers!"));
     }
 
     public List<Person> selectMenByAge(List<Person> peopleList, int fromAge, int toAge) {
         return peopleList.stream()
-                .filter(p -> p.getAge() >= fromAge && p.getAge() <= toAge
-                        && p.getSex().equals(Person.Sex.MAN))
+                .filter(p -> isManValid(fromAge, toAge, p))
                 .collect(Collectors.toList());
     }
 
     public List<Person> getWorkablePeople(int fromAge, int femaleToAge,
                                           int maleToAge, List<Person> peopleList) {
         return peopleList.stream()
-                .filter(p -> p.getAge() >= fromAge
-                        && ((p.getSex().equals(Person.Sex.MAN) && p.getAge() <= maleToAge)
-                        || (p.getSex().equals(Person.Sex.WOMAN) && p.getAge() <= femaleToAge)))
+                .filter(p -> isPersonValid(fromAge, femaleToAge, maleToAge, p))
                 .collect(Collectors.toList());
     }
 
@@ -51,11 +54,29 @@ public class StreamPractice {
     }
 
     public List<String> validateCandidates(List<Candidate> candidates) {
-        CandidateValidator candidateValidator = new CandidateValidator();
         return candidates.stream()
                 .filter(candidateValidator)
                 .map(Candidate::getName)
                 .sorted()
                 .collect(Collectors.toList());
+    }
+
+    public boolean isEven(int num) {
+        return num % 2 == 0;
+    }
+
+    private Predicate<Person> isWomanValid(int femaleAge) {
+        return person -> person.getAge() >= femaleAge && person.getSex() == Person.Sex.WOMAN;
+    }
+
+    private boolean isManValid(int fromAge, int toAge, Person person) {
+        return person.getSex() == Person.Sex.MAN
+                && person.getAge() <= toAge && person.getAge() >= fromAge;
+    }
+
+    private boolean isPersonValid(int fromAge, int femaleToAge, int maleToAge, Person person) {
+        return person.getAge() >= fromAge
+                && ((person.getSex().equals(Person.Sex.MAN) && person.getAge() <= maleToAge)
+                || (person.getSex().equals(Person.Sex.WOMAN) && person.getAge() <= femaleToAge));
     }
 }
