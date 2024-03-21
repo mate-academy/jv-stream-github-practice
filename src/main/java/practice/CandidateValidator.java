aab0f79a76252bbe45857dd8b3f000730b9d0ba1
Yana Bilyk <bilyk.yn@gmail.com>
2024-03-21 14:26:26 -0400

    Jv-stream-github-practice, impl Predicate, practiced Stream commands and etc.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,25 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import model.Candidate;
+
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final String UKR_NATIONALITY = "Ukrainian";
+    private static final int START_AGE = 35;
+    private static final int MIN_PERIOD = 10;
+    private static final int START_YEAR_INDEX = 0;
+    private static final int END_YEAR_INDEX = 1;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] period = candidate.getPeriodsInUkr().split("-");
+        int startYear = Integer.parseInt(period[START_YEAR_INDEX]);
+        int endYear = Integer.parseInt(period[END_YEAR_INDEX]);
+        int periodInUA = endYear - startYear;
+        return candidate.getAge() >= START_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(UKR_NATIONALITY)
+                && periodInUA >= MIN_PERIOD;
+    }
 }
