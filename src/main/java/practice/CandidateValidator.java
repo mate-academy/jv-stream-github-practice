8e46069a8def43404cc9ec767818fc612dcb4323
Evhen Timofeiev <timofeevzhenya2107@gmail.com>
2024-03-23 21:43:15 +0200

    solve the problem

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,22 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && calculateYearsInUkraine(candidate.getPeriodsInUkr()) >= 10;
+    }
+
+    private int calculateYearsInUkraine(String periodsInUkr) {
+        // Предполагается, что periodsInUkr имеет формат "XXXX-YYYY"
+        String[] years = periodsInUkr.split("-");
+        int startYear = Integer.parseInt(years[0]);
+        int endYear = Integer.parseInt(years[1]);
+        return endYear - startYear + 1;
+    }
 }
