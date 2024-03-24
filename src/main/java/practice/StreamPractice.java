17817a359f60eeb25fa1c9b6404167e2a85f5d3d
Dmytro Makhanok <dmmhnk23@gmail.com>
2024-03-24 23:28:56 +0200

    Finished task

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -6,7 +6,6 @@ import java.util.NoSuchElementException;
 import java.util.OptionalInt;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
 import model.Candidate;
 import model.Cat;
 import model.Person;
