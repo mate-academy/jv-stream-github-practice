8b6dcbf61807f138ddafa692b1f74b8f0b576349
Serhii Kopervas <sergkopervas@gmail.com>
2024-03-25 20:28:34 +0100

    Commit 2. Remove comments.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.Arrays;
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.isAllowedToVote() && candidate.getAge() >= 35
+                && candidate.getNationality().equals("Ukrainian")
+                && calcPeriodInUkr(candidate.getPeriodsInUkr()) >= 10;
+    }
+
+    private int calcPeriodInUkr(String period) {
+        return Arrays.stream(period.split("-"))
+                .mapToInt(Integer::parseInt)
+                .reduce((startYear, endYear) -> endYear - startYear)
+                .orElse(0);
+    }
 }
