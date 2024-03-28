d509b73730185cb8b932ef585b0ab757c38d30e8
MarynaBodina <bananafox2002@gmail.com>
2024-03-28 15:57:37 +0200

    "stream practice completed"

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,15 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import model.Candidate;
+
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() > 35 &&
+                candidate.isAllowedToVote() &&
+                candidate.getNationality().equals("Ukrainian") &&
+                candidate.getPeriodsInUkr().length() >= 10;
+    }
 }
