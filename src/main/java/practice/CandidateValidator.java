a3db93c1822ec2487fee9fc9ab71888d5def1a67
Nikita <nik.pogrebnoy@gmail.com>
2024-04-01 14:55:59 +0300

    fixed magic numbers

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,13 +4,16 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int CANDIDATE_MIN_AGE = 35;
+    private static final int MIN_PERIOD_IN_UKRAINE = 10;
+
     @Override
     public boolean test(Candidate candidate) {
         int periodInUkrFrom = Integer.parseInt(candidate.getPeriodsInUkr().substring(0, 4));
         int periodInUkrTo = Integer.parseInt(candidate.getPeriodsInUkr().substring(5));
         int periodInUkr = periodInUkrTo - periodInUkrFrom;
-        return candidate.getAge() >= 35 && candidate.isAllowedToVote()
+        return candidate.getAge() >= CANDIDATE_MIN_AGE && candidate.isAllowedToVote()
                 && candidate.getNationality().equals("Ukrainian")
-                && periodInUkr >= 10;
+                && periodInUkr >= MIN_PERIOD_IN_UKRAINE;
     }
 }
