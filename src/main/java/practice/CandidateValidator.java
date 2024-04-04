d454d505e9d459d694d9aa3a9e59918ce1f506ba
vladislavdzadevic <vladdzyadevich@gmail.com>
2024-04-04 13:17:22 +0200

    Solution of HW stream practice.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,25 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    private static int calculateYearsInUkraine(String periodsInUkr) {
+        int totalYears = 0;
+        String[] periods = periodsInUkr.split("-");
+        if (periods.length == 2) {
+            totalYears = Integer.parseInt(periods[1]) - Integer.parseInt(periods[0]) + 1;
+        }
+        return totalYears;
+
+    }
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equalsIgnoreCase("ukrainian")
+                && calculateYearsInUkraine(candidate.getPeriodsInUkr()) >= 10;
+    }
 }
