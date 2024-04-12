bed9852a5c463cc94bf5aa4ca4d461df12773d76
1 <glebpashko2004@gmail.com>
2024-04-12 15:10:32 +0300

    Created class streamPractice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,22 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static int MIN_AGE = 35;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] periods = candidate.getPeriodsInUkr().split("-");
+        boolean checkPeriod = Integer.parseInt(periods[1]) - Integer.parseInt(periods[0]) > 10;
+        if (candidate.getAge() >= MIN_AGE
+                && candidate.getNationality().equalsIgnoreCase("ukrainian")
+                && candidate.isAllowedToVote()
+                && checkPeriod) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
