33daa90b19a53991bee05190306f5cae2f735c5b
1 <glebpashko2004@gmail.com>
2024-04-17 13:10:53 +0300

    Changed class streamPractice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,13 +6,15 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_AGE = 35;
     private static final int MIN_YEAR_LIVE = 10;
+    private static final int PERIOD_LIVE_FROM = 0;
+    private static final int PERIOD_LIVE_TO = 1;
     private static final String NATIONAL = "ukrainian";
 
     @Override
     public boolean test(Candidate candidate) {
         String[] periods = candidate.getPeriodsInUkr().split("-");
-        boolean checkPeriod = Integer.parseInt(periods[1])
-                - Integer.parseInt(periods[0]) > MIN_YEAR_LIVE;
+        boolean checkPeriod = Integer.parseInt(periods[PERIOD_LIVE_TO])
+                - Integer.parseInt(periods[PERIOD_LIVE_FROM]) > MIN_YEAR_LIVE;
 
         return candidate.getAge() >= MIN_AGE
                 && candidate.getNationality().equalsIgnoreCase(NATIONAL)
