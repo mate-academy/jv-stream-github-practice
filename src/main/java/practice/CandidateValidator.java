ee237f701e5145ef6a339f1264a99acb77603d68
Banderan <bartoszwojcik2004@gmail.com>
2024-05-16 10:55:18 +0200

    finish stream practice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -3,26 +3,26 @@ package practice;
 import java.util.function.Predicate;
 import model.Candidate;
 
-public class CandidateValidator implements Predicate {
+public class CandidateValidator implements Predicate<Candidate> {
     //write your code here
     private static final int MINIMAL_AGE = 35;
     private static final String NATIONALITY = "Ukrainian";
     private static final int LIVED_TEN_YEARS = 10;
 
     @Override
-    public boolean test(Object o) {
-        Candidate candidate = (Candidate) o;
-        boolean isHeHaveEnoughYears = candidate.getAge() > MINIMAL_AGE;
+    public boolean test(Candidate candidate) {
+        boolean isHeHaveEnoughYears = candidate.getAge() >= MINIMAL_AGE;
         boolean isHeHaveCorrectNationality = candidate.getNationality()
                 .equals(NATIONALITY);
 
         int from = 0;
         int to = 1;
         String[] splitPeriods = candidate.getPeriodsInUkr().split("-");
-        int livedInCountry = Integer.parseInt(splitPeriods[1])
-                - Integer.parseInt(splitPeriods[0]);
-        boolean isHeLivedInCountryEnough = livedInCountry > LIVED_TEN_YEARS;
+        int livedInCountry = Integer.parseInt(splitPeriods[to])
+                - Integer.parseInt(splitPeriods[from]);
+        boolean isHeLivedInCountryEnough = livedInCountry >= LIVED_TEN_YEARS;
 
-        return isHeHaveEnoughYears && isHeHaveCorrectNationality && isHeLivedInCountryEnough;
+        return isHeHaveEnoughYears && isHeHaveCorrectNationality
+                && isHeLivedInCountryEnough && candidate.isAllowedToVote();
     }
 }
