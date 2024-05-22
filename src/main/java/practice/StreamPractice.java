e4732f79018cbb79e3e4feaebda65a848c3dd86a
olexanderbondarenko <bondarenkoov.dev@gmail.com>
2024-05-22 13:33:09 +0200

    fix the code style

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,6 +1,10 @@
 package practice;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
