3eaa65d769606b2b1fc42affc96f78a0f8b7f96c
oKukHtina <olakukhtina95@gmail.com>
2024-05-23 22:56:18 +0300

    implemented all methods in class StreamPractice

---
+++ src/main/java/practice/PersonValidator.java
@@ -0,0 +1,26 @@
+package practice;
+
+import java.util.function.Predicate;
+import model.Person;
+
+public class PersonValidator implements Predicate<Person> {
+    private final int fromAge;
+    private final int femaleToAge;
+    private final int maleToAge;
+
+    public PersonValidator(int fromAge, int femaleToAge, int maleToAge) {
+        this.fromAge = fromAge;
+        this.femaleToAge = femaleToAge;
+        this.maleToAge = maleToAge;
+    }
+
+    @Override
+    public boolean test(Person person) {
+        return person.getSex() == Person.Sex.MAN
+                && person.getAge() >= fromAge
+                && person.getAge() <= maleToAge
+                || person.getSex() == Person.Sex.WOMAN
+                && person.getAge() >= fromAge
+                && person.getAge() <= femaleToAge;
+    }
+}
