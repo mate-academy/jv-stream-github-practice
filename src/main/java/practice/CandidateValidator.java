9e3b90e8fd30dd74ccd83776d81e435e7f4eef5e
Yaromyr Matviichuk <yaromyrmatviichuk@gmail.com>
2024-05-30 14:16:46 +0300

    I solved stream-practice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,28 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int REQUIRE_MIN_AGE = 35;
+    private static final int REQUIRE_LIVING = 10;
+    private static final int START_LIVING = 0;
+    private static final int CURRENT_LIVING = 1;
+
+    public CandidateValidator() {
+    }
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= REQUIRE_MIN_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && isLengthOfLivingSuitable(candidate);
+    }
+
+    private boolean isLengthOfLivingSuitable(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split("-");
+        return Integer.parseInt(years[CURRENT_LIVING]) - Integer.parseInt(years[START_LIVING])
+                >= REQUIRE_LIVING;
+    }
 }
