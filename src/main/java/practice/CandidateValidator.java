92eb17f226257f92fa0f7444ef49a08085e32aee
Anton <panyutin15@gmail.com>
2024-06-12 14:54:33 +0300

    added solition of the task

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,22 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int LOWER_AGE_LIMIT = 35;
+    private static final String UKRAINIAN_NATIONALITY = "Ukrainian";
+    private static final int YEARS_NUMBER_LIVED_IN_UKRAINE = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= LOWER_AGE_LIMIT && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(UKRAINIAN_NATIONALITY)
+                && (getNumbetOfYears(candidate.getPeriodsInUkr()) >= YEARS_NUMBER_LIVED_IN_UKRAINE);
+    }
+
+    private int getNumbetOfYears(String string) {
+        String[] strings = string.split("-");
+        return Integer.parseInt(strings[1]) - Integer.parseInt(strings[0]);
+    }
 }
