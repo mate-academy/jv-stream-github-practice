d504e67354a7b4d2b3bf8d3e801300ef6e1e7fd5
Oleksii <sukhenko.oleksii@gmail.com>
2024-06-16 14:00:50 +0300

    First commit

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,26 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && "Ukrainian".equals(candidate.getNationality())
+                && calculateYearsInUkraine(candidate.getPeriodsInUkr()) >= 10;
+    }
+
+    private int calculateYearsInUkraine(String periods) {
+        int totalYears = 0;
+        String[] parts = periods.split("-");
+        if (parts.length == 2) {
+            int startYear = Integer.parseInt(parts[0]);
+            int endYear = Integer.parseInt(parts[1]);
+            totalYears = endYear - startYear + 1;
+
+        }
+        return totalYears;
+    }
 }
