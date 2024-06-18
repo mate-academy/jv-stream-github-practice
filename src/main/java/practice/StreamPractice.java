779d128bd0092e58b90ef0513268f22b0892a55a
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-06-18 13:00:42 +0300

    StreamPractice.java

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -68,8 +68,8 @@ public class StreamPractice {
     public List<Person> getWorkablePeople(int fromAge, int femaleToAge,
                                           int maleToAge, List<Person> peopleList) {
         return peopleList.stream()
-                .filter(p -> (p.getSex() == Person.Sex.MAN && p.getAge() <= maleToAge) ||
-                             (p.getSex() == Person.Sex.WOMAN && p.getAge() <= femaleToAge))
+                .filter(p -> (p.getSex() == Person.Sex.MAN && p.getAge() <= maleToAge)
+                        || (p.getSex() == Person.Sex.WOMAN && p.getAge() <= femaleToAge))
                 .filter(p -> p.getAge() >= fromAge)
                 .collect(Collectors.toList());
     }
