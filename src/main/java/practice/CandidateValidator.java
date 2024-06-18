201206279f98fc271c60c9fd6d2a9c4d28a10d96
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-06-18 13:01:02 +0300

    CandidateValidator.java

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,9 +6,9 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     @Override
     public boolean test(Candidate candidate) {
-        return candidate.getAge() >= 35 &&
-               candidate.isAllowedToVote() &&
-               "Ukrainian".equals(candidate.getNationality()) &&
-               candidate.getPeriodsInUkr().matches(".*\\d{4}-\\d{4}.*");
+        return candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && "Ukrainian".equals(candidate.getNationality())
+                && candidate.getPeriodsInUkr().matches(".*\\d{4}-\\d{4}.*");
     }
 }
