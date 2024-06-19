f7be8cb1bbc666e6bc87aa87e93941533c7b37a9
flamehost <flamehost@syneforge.com>
2024-06-19 23:37:20 +0300

    display edits

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -6,7 +6,6 @@ import java.util.NoSuchElementException;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
 import model.Candidate;
 import model.Cat;
 import model.Person;
