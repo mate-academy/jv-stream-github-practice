a23c078388b088821421362deb0d228c0a553780
Kate Drevilo <rabmargo@gmail.com>
2024-06-20 13:57:02 +0300

    implemented public class StreamPractice, created class CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,35 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+  public boolean test(Candidate candidate) {
+        if (candidate.getAge() < 35) {
+            return false;
+        }
+        if (!candidate.isAllowedToVote()) {
+            return false;
+        }
+        if (!"Ukrainian".equalsIgnoreCase(candidate.getNationality())) {
+            return false;
+        }
+        return livedInUkraineForTenYears(candidate.getPeriodsInUkr());
+    }
+
+    private boolean livedInUkraineForTenYears(String periodsInUkr) {
+        String[] periods = periodsInUkr.split(",");
+        int totalYears = 0;
+
+        for (String period : periods) {
+            String[] years = period.split("-");
+            if (years.length == 2) {
+                int startYear = Integer.parseInt(years[0]);
+                int endYear = Integer.parseInt(years[1]);
+                totalYears += (endYear - startYear);
+            }
+        }
+        return totalYears >= 10;
+    }
 }
