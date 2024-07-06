1d07952021cb8eced244ca56ed099de71e97bba1
Bohdan Buhrak <bohdan0410@gmail.com>
2024-07-06 13:40:16 +0300

    Implement CandidateValidator and implement StreamPractice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,25 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
     //write your code here
+    private static final int REQUIRED_AGE_TO_VOTE = 35;
+    private static final String REQUIRED_NATIONALITY_TO_VOTE = "Ukrainian";
+    private static final String REGEX_FOR_SPLIT = "-";
+    private static final int DATE_FROM = 0;
+    private static final int DATA_TO = 1;
+    private static final int YEARS_THRESHOLD = 5;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] periodsInUkr = candidate.getPeriodsInUkr().split(REGEX_FOR_SPLIT);
+
+        return candidate.getAge() >= REQUIRED_AGE_TO_VOTE
+                && candidate.isAllowedToVote()
+                && REQUIRED_NATIONALITY_TO_VOTE.equals(candidate.getNationality())
+                && (Integer.parseInt(periodsInUkr[DATA_TO])
+                - Integer.parseInt(periodsInUkr[DATE_FROM])) > YEARS_THRESHOLD;
+    }
 }
