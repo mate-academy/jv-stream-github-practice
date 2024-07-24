0c37c4b969c4b53341bb52dbab4b2f2f3e5c0eb3
Sasha Fetisov <sasafetisov03@gmail.com>
2024-07-24 14:10:55 +0300

    stream-practice implementations by fetis-off

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,29 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    private Predicate<Candidate> validator = candidate -> {
+        String[] parts = candidate.getPeriodsInUkr().split("-");
+        int startYear = Integer.parseInt(parts[0]);
+        int endYear = Integer.parseInt(parts[1]);
+        if (candidate.getAge() >= 35
+                && candidate.getNationality().equals("Ukrainian")
+                && candidate.isAllowedToVote()
+                && endYear - startYear >= 10) {
+            return true;
+        }
+        return false;
+    };
+
+    public Predicate<Candidate> getValidator() {
+        return validator;
+    }
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return validator.test(candidate);
+    }
 }
