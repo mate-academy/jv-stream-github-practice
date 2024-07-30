26008fc52c695efa9553c0f59ddcf48eeadc76d0
smagles <lesialist@icloud.com>
2024-07-30 14:35:59 +0200

    Implement stream processing tasks and candidate validation logic

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,33 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import model.Candidate;
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int INDEX_START_YEAR = 0;
+    private static final int INDEX_END_YEAR = 1;
+    private static final int PERIOD = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MIN_AGE &&
+                candidate.isAllowedToVote() &&
+                NATIONALITY.equals(candidate.getNationality()) &&
+                hasLivedInUkraineFor10Years(candidate.getPeriodsInUkr());
+    }
+
+    private boolean hasLivedInUkraineFor10Years(String periodsInUkr) {
+        String[] periods = periodsInUkr.split(",");
+        int totalYears = 0;
+
+        for (String period : periods) {
+            String[] years = period.split("-");
+            int startYear = Integer.parseInt(years[INDEX_START_YEAR]);
+            int endYear = Integer.parseInt(years[INDEX_END_YEAR]);
+            totalYears = endYear - startYear;
+        }
+        return totalYears >= PERIOD;
+    }
 }
