3f519843d855b326e5a4729341d2a0a6d4db65f9
Valeria <valeriaua97@gmail.com>
2024-07-31 18:48:32 +0200

    Task done

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,31 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.Objects;
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final String SEPARATOR = "-";
+    private static final int START_YEAR_INDEX = 0;
+    private static final int END_YEAR_INDEX = 1;
+    private static final int MIN_PERIOD_IN_UKRAINE = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MIN_AGE
+                && candidate.isAllowedToVote()
+                && Objects.equals(candidate.getNationality(),
+                NATIONALITY)
+                && checkTimeLivingInCountry(candidate);
+    }
+
+    private boolean checkTimeLivingInCountry(Candidate candidate) {
+        String[] dates =
+                candidate.getPeriodsInUkr().split(SEPARATOR);
+        return Integer.parseInt(dates[END_YEAR_INDEX])
+                - Integer.parseInt(dates[START_YEAR_INDEX])
+                >= MIN_PERIOD_IN_UKRAINE;
+    }
 }
