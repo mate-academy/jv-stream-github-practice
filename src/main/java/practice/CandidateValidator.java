a737519e3d421a737fe290219ab5a33f773482ba
vdsfww <loppprt@gmail.com>
2024-08-11 16:30:24 +0300

    fixed №2

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,38 +6,30 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_AGE = 35;
     private static final int LIVING_PERIOD = 10;
-    private static final int START_LIVING_DATA = 0;
-    private static final int CURRENT_LIVING_DATA = 1;
+    private static final String UKRAINIAN_NATIONALITY = "Ukrainian";
+    private static final int INDEX_LEAVE_FROM = 0;
+    private static final int INDEX_LEAVE_TO = 1;
 
     @Override
     public boolean test(Candidate candidate) {
         return candidate != null
                 && candidate.getAge() >= MIN_AGE
                 && candidate.isAllowedToVote()
-                && "Ukrainian".equals(candidate.getNationality())
-                && hasLivedInUkraineForAtLeast10Years(candidate.getPeriodsInUkr());
+                && UKRAINIAN_NATIONALITY.equals(candidate.getNationality())
+                && isValidByResidenceInUkraine(candidate.getPeriodsInUkr());
     }
 
-    public boolean hasLivedInUkraineForAtLeast10Years(String periodsInUkr) {
-        if (periodsInUkr == null) {
+    private boolean isValidByResidenceInUkraine(String periodsInUkr) {
+        String[] yearsInUkraine = periodsInUkr.split("\\D+");
+        if (yearsInUkraine.length < 2) {
             return false;
         }
-
-        String[] period = periodsInUkr.split("-");
-        if (period.length < 2) {
-            return false;
-        }
-
-        int startYear;
-        int currentYear;
-
         try {
-            startYear = Integer.parseInt(period[START_LIVING_DATA]);
-            currentYear = Integer.parseInt(period[CURRENT_LIVING_DATA]);
+            int fromYear = Integer.parseInt(yearsInUkraine[INDEX_LEAVE_FROM]);
+            int toYear = Integer.parseInt(yearsInUkraine[INDEX_LEAVE_TO]);
+            return (toYear - fromYear) >= LIVING_PERIOD;
         } catch (NumberFormatException e) {
             return false;
         }
-
-        return Math.abs(currentYear - startYear) >= LIVING_PERIOD;
     }
 }
