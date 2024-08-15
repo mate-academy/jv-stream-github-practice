7e289eb327a02744392bb94ae5c0bb1f5991e9bd
dmitry <dmarmul228@gmail.com>
2024-08-15 18:30:28 +0300

    Create implementation of all methods in StreamPractice class,make own implementation of Predicate parametrized with Candidate in CandidateValidator class

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,22 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final String SEPARATOR = "-";
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int MIN_PERIOD = 10;
+    private static final int MIN_AGE = 35;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String years = candidate.getPeriodsInUkr();
+        int periodsInUkr = Integer.parseInt(years.substring(years.indexOf(SEPARATOR) + 1))
+                - Integer.parseInt(years.substring(0, years.indexOf(SEPARATOR)));
+        return candidate.getAge() >= MIN_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && periodsInUkr >= MIN_PERIOD;
+    }
 }
