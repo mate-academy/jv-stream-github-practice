636c6fcfe6421e38c5286d3c401e37683e8a48dc
rainett <96131229+rainett@users.noreply.github.com>
2024-08-21 13:02:49 +0300

    implemented methods using streams, implemented CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,27 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final String ALLOWED_NATIONALITY = "Ukrainian";
+    private static final int MINIMAL_AGE = 35;
+    private static final int YEARS_IN_COUNTRY = 10;
+    private static final int YEARS_FROM_INDEX = 0;
+    private static final int YEARS_TO_INDEX = 1;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MINIMAL_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(ALLOWED_NATIONALITY)
+                && hasLivedForYears(candidate);
+    }
+
+    private boolean hasLivedForYears(Candidate candidate) {
+        String[] split = candidate.getPeriodsInUkr().split("-");
+        int from = Integer.parseInt(split[YEARS_FROM_INDEX]);
+        int to = Integer.parseInt(split[YEARS_TO_INDEX]);
+        return to - from >= YEARS_IN_COUNTRY;
+    }
 }
