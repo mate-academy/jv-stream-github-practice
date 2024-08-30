2ff852e616b23b22bd7e518c94fd7c4472f1c804
d.nosenko <daniil.nosenkoo@gmail.com>
2024-08-30 20:02:34 +0300

    some fixes

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -2,7 +2,6 @@ package practice;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import model.Candidate;
@@ -38,7 +37,7 @@ public class StreamPractice {
                     .map(i -> i % 2 != 0 ? numbers.get(i) - 1 : numbers.get(i))
                     .filter(n -> n % 2 != 0)
                     .average()
-                    .orElseThrow(NoSuchElementException::new);
+                    .orElseThrow();
     }
 
     /**
@@ -51,7 +50,7 @@ public class StreamPractice {
      */
     public List<Person> selectMenByAge(List<Person> peopleList, int fromAge, int toAge) {
         return peopleList.stream()
-                    .filter(person -> person.getSex().equals(Person.Sex.MAN)
+                    .filter(person -> person.getSex() == Person.Sex.MAN
                             && person.getAge() >= fromAge
                             && person.getAge() <= toAge)
                     .collect(Collectors.toList());
@@ -86,7 +85,7 @@ public class StreamPractice {
      */
     public List<String> getCatsNames(List<Person> peopleList, int femaleAge) {
         return peopleList.stream()
-                .filter(person -> person.getSex().equals(Person.Sex.WOMAN)
+                .filter(person -> person.getSex() == Person.Sex.WOMAN
                         && person.getAge() >= femaleAge)
                 .flatMap(person -> person.getCats()
                         .stream()).map(Cat::getName)
