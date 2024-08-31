df0bb026d29a32bc3ec135fc501a983e179af35f
d.nosenko <daniil.nosenkoo@gmail.com>
2024-08-31 17:20:14 +0300

    fixes 2

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -19,7 +19,6 @@ public class StreamPractice {
     public int findMinEvenNumber(List<String> numbers) {
         return numbers.stream()
                 .flatMap(s -> Arrays.stream(s.split(",")))
-                .map(String::trim)
                 .map(Integer::parseInt)
                 .filter(n -> n % 2 == 0)
                 .min(Integer::compare)
