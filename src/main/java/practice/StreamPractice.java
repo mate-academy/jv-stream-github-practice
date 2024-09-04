10f2400e3e2e77eaa6a534736e0ee5fc4dc149a9
Yura Yurchuk <yurayurchuk88@gmail.com>
2024-09-04 19:01:11 +0300

    cleaned up the project with maven

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,15 +1,14 @@
 package practice;
 
-import model.Candidate;
-import model.Cat;
-import model.Person;
-
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import model.Candidate;
+import model.Cat;
+import model.Person;
 
 public class StreamPractice {
     /**
