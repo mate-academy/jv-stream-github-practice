6f151c3cc4a8546694f4890f68c7c96eb4ed5ec4
Petro Shchur <pepe2.98.12@icloud.com>
2024-09-12 13:35:55 -0400

    CandidateValidator  added StreamPractice added

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,19 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && "Ukrainian".equals(candidate.getNationality())
+                && calculateYearsInUkraine(candidate.getPeriodsInUkr()) >= 10;
+    }
+
+    private int calculateYearsInUkraine(String periodsInUkr) {
+        String[] periods = periodsInUkr.split("-");
+        return Integer.parseInt(periods[1]) - Integer.parseInt(periods[0]);
+    }
 }
