1f5e13761fd77b7077e3a71f354c46d14c47267e
Oleksii <obabych1@stu.vistula.edu.pl>
2024-10-01 15:37:40 +0200

    fix mistake

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -8,6 +8,8 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int INCLUDE_START_AND_END_YEAR = 1;
     private static final int FROM_YEAR = 0;
     private static final int TO_YEAR = 1;
+    private static final int MIN_AGE_OF_CANDIDATES = 35;
+    private static final int MIN_YEARS_OF_RESIDENCE = 10;
 
     @Override
     public boolean test(Candidate candidate) {
@@ -16,9 +18,9 @@ public class CandidateValidator implements Predicate<Candidate> {
 
     private boolean isEligible(Candidate candidate) {
         return candidate.isAllowedToVote()
-                && candidate.getAge() >= 35
+                && candidate.getAge() >= MIN_AGE_OF_CANDIDATES
                 && candidate.getNationality() == Nationality.UKR.getValue()
-                && calculateYearsOfResidence(candidate.getPeriodsInUkr()) >= 10;
+                && calculateYearsOfResidence(candidate.getPeriodsInUkr()) >= MIN_YEARS_OF_RESIDENCE;
     }
 
     private int calculateYearsOfResidence(String period) {
