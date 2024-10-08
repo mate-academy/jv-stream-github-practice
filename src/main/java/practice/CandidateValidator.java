8b2142acd9a9e08cc00afcaed167d5057094fb98
StoneBlood <brkalix@gmail.com>
2024-10-08 17:58:54 +0300

    deleted the variable validator and added the new CandidateValidator() in .filter()

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -9,19 +9,19 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final String NATIONALITY = "Ukrainian";
     private static final String DELIMITER = "-";
 
-    private boolean checkTimeLivingInCountry(Candidate candidate) {
-        String[] time = candidate.getPeriodsInUkr().split(DELIMITER);
-        int startPeriod = Integer.parseInt(time[0]);
-        int finishPeriod = Integer.parseInt(time[1]);
-
-        return (finishPeriod - startPeriod) >= MIN_PERIODS_IN_UKRAINE;
-    }
-
     @Override
     public boolean test(Candidate candidate) {
         return candidate.getAge() >= MIN_AGE_FOR_CANDIDATE
                 && candidate.isAllowedToVote()
                 && candidate.getNationality().equals(NATIONALITY)
                 && checkTimeLivingInCountry(candidate);
+    }
+
+    private boolean checkTimeLivingInCountry(Candidate candidate) {
+        String[] time = candidate.getPeriodsInUkr().split(DELIMITER);
+        int startPeriod = Integer.parseInt(time[0]);
+        int finishPeriod = Integer.parseInt(time[1]);
+
+        return (finishPeriod - startPeriod) >= MIN_PERIODS_IN_UKRAINE;
     }
 }
--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -101,9 +101,8 @@ public class StreamPractice {
      * parametrized with Candidate in CandidateValidator.
      */
     public List<String> validateCandidates(List<Candidate> candidates) {
-        CandidateValidator validator = new CandidateValidator();
         return candidates.stream()
-                .filter(validator)
+                .filter(new CandidateValidator())
                 .map(Candidate::getName)
                 .sorted()
                 .toList();
