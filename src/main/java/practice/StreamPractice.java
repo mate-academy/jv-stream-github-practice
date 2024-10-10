00079db9bd3afa397e27a46655df13b1e039f776
homeworkfirst <zookotelif@gmail.com>
2024-10-10 16:28:46 +0300

    StreamPractice changed

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,13 +1,13 @@
 package practice;
 
 import java.util.Arrays;
-import java.util.stream.Collectors;
+ /**import java.util.stream.Collectors;*/
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.IntStream;
 import model.Candidate;
 import model.Person;
-import model.Cat;
+ /**import model.Cat;*/
 
 public class StreamPractice {
 
