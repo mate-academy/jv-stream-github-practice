4ef76e0348fa78d368e5a0f7e0f81f81227a54b9
iurii <kenu21@gmail.com>
2024-10-14 17:30:26 +0200

    STREAM: refactored

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,7 +4,6 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-
     private static final int MIN_AGE = 35;
     private static final int MIN_PERIOD = 10;
     private static final String UKRAINIAN = "Ukrainian";
