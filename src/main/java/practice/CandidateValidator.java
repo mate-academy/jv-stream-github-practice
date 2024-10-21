f6e6ccc8c68f682fb87763a0892ded743bb2238d
Serhiy <sergie3007@gmail.com>
2024-10-22 01:13:59 +0300

    Implement StreamPractice, CandidateValidator classes

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,26 @@
 package practice;
 
-public class CandidateValidator {
+import model.Candidate;
+
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
     //write your code here
+    private static final int MINIMAL_CANDIDATE_AGE = 35;
+    private static final int REQUIRED_LIVING_YEARS = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MINIMAL_CANDIDATE_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && isCandidateLiveForTenYears(candidate);
+    }
+
+    private boolean isCandidateLiveForTenYears(Candidate candidate) {
+        String[] periodsSplit = candidate.getPeriodsInUkr().split("-");
+        int firstDate = Integer.parseInt(periodsSplit[0]);
+        int secondDate = Integer.parseInt(periodsSplit[1]);
+        return secondDate - firstDate > REQUIRED_LIVING_YEARS;
+    }
 }
--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,11 +1,20 @@
 package practice;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import model.Candidate;
+import model.Cat;
 import model.Person;
 
 public class StreamPractice {
+    private static final int NUMBERS_START_INDEX = 0;
+    private static final int ODD_ADJUSTMENT = 1;
+    private static final int EVEN_CHECK = 2;
+    private static final int MODULO_CHECK = 0;
+
     /**
      * Given list of strings where each element contains 1+ numbers:
      * input = {"5,30,100", "0,22,7", ...}
@@ -14,7 +23,14 @@ public class StreamPractice {
      * "Can't get min value from list: < Here is our input 'numbers' >"
      */
     public int findMinEvenNumber(List<String> numbers) {
-        return 0;
+        OptionalInt min = numbers.stream()
+                .map(s -> Arrays.stream(s.split("[^-0-9]+")).toList())
+                .flatMap(List::stream)
+                .mapToInt(Integer::parseInt)
+                .filter(value -> value % EVEN_CHECK == MODULO_CHECK)
+                .min();
+        return min.orElseThrow(()
+                -> new RuntimeException("Can't get min value from list: " + numbers));
     }
 
     /**
@@ -22,8 +38,13 @@ public class StreamPractice {
      * return the average of all odd numbers from the list or throw NoSuchElementException.
      * But before that subtract 1 from each element on an odd position (having the odd index).
      */
-    public Double getOddNumsAverage(List<Integer> numbers) {
-        return 0D;
+    public double getOddNumsAverage(List<Integer> numbers) {
+        OptionalDouble average = IntStream.range(NUMBERS_START_INDEX, numbers.size())
+                .mapToObj(i -> i % EVEN_CHECK == ODD_ADJUSTMENT ? numbers.get(i) - ODD_ADJUSTMENT : numbers.get(i))
+                .filter(n -> n % EVEN_CHECK != MODULO_CHECK)
+                .mapToInt(n -> n)
+                .average();
+        return average.orElseThrow(NoSuchElementException::new);
     }
 
     /**
@@ -35,7 +56,18 @@ public class StreamPractice {
      * Example: select men who can be recruited to army (from 18 to 27 years old inclusively).
      */
     public List<Person> selectMenByAge(List<Person> peopleList, int fromAge, int toAge) {
-        return Collections.emptyList();
+        Predicate<Person> menByAgePredicate = new Predicate<Person>() {
+            @Override
+            public boolean test(Person person) {
+                return person.getAge() >= fromAge
+                        && person.getAge() <= toAge
+                        && person.getSex() == Person.Sex.MAN;
+            }
+        };
+
+        return peopleList.stream()
+                .filter(menByAgePredicate)
+                .collect(Collectors.toList());
     }
 
     /**
@@ -50,7 +82,20 @@ public class StreamPractice {
      */
     public List<Person> getWorkablePeople(int fromAge, int femaleToAge,
                                           int maleToAge, List<Person> peopleList) {
-        return Collections.emptyList();
+        Predicate<Person> workablePeoplePredicate = new Predicate<Person>() {
+            @Override
+            public boolean test(Person person) {
+                return person.getAge() >= fromAge
+                        && (person.getSex() == Person.Sex.WOMAN
+                        && person.getAge() <= femaleToAge
+                        || (person.getSex() == Person.Sex.MAN
+                        && person.getAge() <= maleToAge));
+            }
+        };
+
+        return peopleList.stream()
+                .filter(workablePeoplePredicate)
+                .collect(Collectors.toList());
     }
 
     /**
@@ -59,7 +104,13 @@ public class StreamPractice {
      * return the names of all cats whose owners are women from `femaleAge` years old inclusively.
      */
     public List<String> getCatsNames(List<Person> peopleList, int femaleAge) {
-        return Collections.emptyList();
+        return peopleList.stream()
+                .filter(person -> person.getSex() == Person.Sex.WOMAN
+                        && person.getAge() >= femaleAge)
+                .map(Person::getCats)
+                .flatMap(List::stream)
+                .map(Cat::getName)
+                .collect(Collectors.toList());
     }
 
     /**
@@ -75,6 +126,10 @@ public class StreamPractice {
      * parametrized with Candidate in CandidateValidator.
      */
     public List<String> validateCandidates(List<Candidate> candidates) {
-        return Collections.emptyList();
+        return candidates.stream()
+                .filter(new CandidateValidator())
+                .map(Candidate::getName)
+                .sorted()
+                .collect(Collectors.toList());
     }
 }
