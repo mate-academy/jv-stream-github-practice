c73f7f1d20d760aa923fb666c54cf5d01a078da4
Hetalova <nhetalova@luxoft.com>
2024-10-24 13:32:57 +0300

    Added solution for stream practice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,19 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] arr = candidate.getPeriodsInUkr().split("-");
+        int periodOfLivingInCountry = Integer.parseInt(arr[1]) - Integer.parseInt(arr[0]);
+        if (candidate.getAge() >= 35 && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && periodOfLivingInCountry >= 10) {
+            return true;
+        }
+        return false;
+    }
 }
