9ff3d170e34b630f136255f0439783894b81fbc7
jekaG <jekagrusha@gmail.com>
2024-10-26 11:24:39 +0300

    fixed after review4

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,6 +6,7 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int PERIOD_IN_UKRAINE = 10;
     private static final int CANDIDATE_OLD_TO = 35;
+    private static final String NATIONALITY = "Ukrainian";
 
     @Override
     public boolean test(Candidate candidate) {
@@ -14,22 +15,13 @@ public class CandidateValidator implements Predicate<Candidate> {
         try {
             int period = Integer.parseInt(periods[1]) - Integer.parseInt(periods[0]);
 
-            if (period < PERIOD_IN_UKRAINE) {
+            if (period < PERIOD_IN_UKRAINE && candidate.getAge() < CANDIDATE_OLD_TO
+            && !candidate.getNationality().equals(NATIONALITY)
+            && !candidate.isAllowedToVote()) {
                 return false;
             }
         } catch (NumberFormatException e) {
             throw new RuntimeException("Invalid period format: unable to parse year",e);
-        }
-
-        if (candidate.getAge() < CANDIDATE_OLD_TO) {
-            return false;
-        }
-        if (!candidate.getNationality().equals("Ukrainian")) {
-            return false;
-        }
-
-        if (!candidate.isAllowedToVote()) {
-            return false;
         }
         return true;
     }
