2c54c07c8df83ffd0cf10883f01f492b23557df6
Roman <romanvoynahiy@gmail.com>
2024-10-29 16:44:12 +0200

    Implement stream-based methods from processing data from lists of string, integers, and custom objects.

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,18 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] yearsInUkr = candidate.getPeriodsInUkr().split("-");
+        int startYear = Integer.parseInt(yearsInUkr[0]);
+        int endYear = Integer.parseInt(yearsInUkr[1]);
+
+        return candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && (endYear - startYear) >= 10;
+    }
 }
