50d8fa90ccfd22a515325bca8524f69db66bd1ba
Loiev Roman <robolito0420@gmail.com>
2024-10-31 12:17:57 +0200

    Wrote my own implementation of Predicate<Candidate> in CandidateValidator.java Implemented some methods in StreamPractice.java

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,19 @@
 package practice;
 
-public class CandidateValidator {
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split("-");
+        boolean isValidAge = candidate.getAge() >= 35;
+        boolean isValidVoteAllow = candidate.isAllowedToVote();
+        boolean isValidNationality = candidate.getNationality().equals("Ukrainian");
+        boolean isValidYearsPeriod = Integer.parseInt(years[1]) - Integer.parseInt(years[0]) >= 10;
+        return isValidAge && isValidNationality && isValidYearsPeriod && isValidVoteAllow;
+    }
+
     //write your code here
 }
