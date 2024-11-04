51a382cc09487cf411e37231f855d3fba830bbe9
PawelCp <capiukpawel@gmail.com>
2024-11-04 15:15:46 +0100

    Some fixes after Mentor review

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -18,8 +18,10 @@ public class CandidateValidator implements Predicate<Candidate> {
     }
 
     private boolean livedInUkraineFor10Years(String periodsInUkr) {
-        String[] years = periodsInUkr.split("-");
-        if (years.length == 2) {
+        if (periodsInUkr == null || !periodsInUkr.matches("\\d+\\s*-\\s*\\d+")) {
+            System.out.println("Invalid period format! Please enter correct value with format: 'yyyy-yyyy'");
+        } else {
+            String[] years = periodsInUkr.split("-");
             try {
                 int startYear = Integer.parseInt(years[0]);
                 int endYear = Integer.parseInt(years[1]);
@@ -27,8 +29,6 @@ public class CandidateValidator implements Predicate<Candidate> {
             } catch (NumberFormatException e) {
                 throw new NumberFormatException("Invalid year format!");
             }
-        } else {
-            System.out.println("Invalid input periods format! Please enter correct value with format: 'yyyy-yyyy'");
         }
         return false;
     }
