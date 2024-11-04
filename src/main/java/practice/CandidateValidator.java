cdb251d70daad32353e32957a6aed8f2cd7d2c87
PawelCp <capiukpawel@gmail.com>
2024-11-04 15:05:23 +0100

    Some fixes after Mentor review

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,6 @@
 package practice;
 
+import java.util.Objects;
 import java.util.function.Predicate;
 import model.Candidate;
 
@@ -12,16 +13,22 @@ public class CandidateValidator implements Predicate<Candidate> {
     public boolean test(Candidate candidate) {
         return candidate.getAge() >= MIN_AGE_OF_THE_CANDIDATE
                 && candidate.isAllowedToVote()
-                && candidate.getNationality().equals(NATIONALITY)
+                && Objects.equals(candidate.getNationality(), NATIONALITY)
                 && livedInUkraineFor10Years(candidate.getPeriodsInUkr());
     }
 
     private boolean livedInUkraineFor10Years(String periodsInUkr) {
         String[] years = periodsInUkr.split("-");
         if (years.length == 2) {
-            int startYear = Integer.parseInt(years[0]);
-            int endYear = Integer.parseInt(years[1]);
-            return (endYear - startYear >= MIN_LIVING_IN_COUNTRY);
+            try {
+                int startYear = Integer.parseInt(years[0]);
+                int endYear = Integer.parseInt(years[1]);
+                return (endYear - startYear >= MIN_LIVING_IN_COUNTRY);
+            } catch (NumberFormatException e) {
+                throw new NumberFormatException("Invalid year format!");
+            }
+        } else {
+            System.out.println("Invalid input periods format! Please enter correct value with format: 'yyyy-yyyy'");
         }
         return false;
     }
