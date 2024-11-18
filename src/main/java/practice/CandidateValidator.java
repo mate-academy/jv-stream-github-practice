881c7e0e8d1fe0c0672db0bb93a5b689d0149319
Vadym Dmytriv <vadymdmytriv@gmail.com>
2024-11-18 16:15:55 +0100

    Finished StreamPractice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= 35
+                && candidate.getNationality().equals("Ukrainian")
+                && periodMoreThanTen(candidate.getPeriodsInUkr())
+                && candidate.isAllowedToVote();
+    }
+
+    private boolean periodMoreThanTen(String periodsInUkr) {
+        String[] period = periodsInUkr.split("-");
+        int start = Integer.parseInt(period[0]);
+        int end = Integer.parseInt(period[1]);
+        return (end - start) >= 10;
+    }
 }
