99799edd19af3f2e6fbeb4c1a74ad3803ac473de
skrypkasv <skrypkasv@gmail.com>
2024-11-20 15:21:23 +0200

    create psf field DASH

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,10 +7,11 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final String NATIONALITY = "Ukrainian";
     private static final Integer MIN_AGE = 35;
     private static final Integer MIN_PERIOD = 10;
+    private static final String DASH = "-";
 
     @Override
     public boolean test(Candidate candidate) {
-        String[] years = candidate.getPeriodsInUkr().split("-");
+        String[] years = candidate.getPeriodsInUkr().split(DASH);
         int fromYear = Integer.parseInt(years[0]);
         int toYear = Integer.parseInt(years[1]);
         int period = toYear - fromYear;
