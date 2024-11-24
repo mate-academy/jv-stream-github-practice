c7bdc3d3786c2d60c3372fb1ada0ab029882c6df
Mihail <mishadyakivnich@gmail.com>
2024-11-24 20:19:43 +0200

    resolve

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,22 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final int MIN_YEARS_IN_UKRAINE = 10;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int FROM_YEAR_INDEX = 0;
+    private static final int TO_YEAR_INDEX = 1;
+    private static final String LINE_SEPARATOR = "-";
+
+    public boolean test (Candidate candidate) {
+        String[] period = candidate.getPeriodsInUkr().split(LINE_SEPARATOR);
+        int yearsInUkraine = Integer.parseInt(period[TO_YEAR_INDEX])
+                - Integer.parseInt(period[FROM_YEAR_INDEX]);
+        return candidate.getAge() >= MIN_AGE && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && yearsInUkraine >= MIN_YEARS_IN_UKRAINE;
+    }
 }
