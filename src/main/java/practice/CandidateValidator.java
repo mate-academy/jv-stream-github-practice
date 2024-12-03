0e20d9ba9be19ada1f5fd912e02fe50e100dfa18
Karol Gajda <karolgaj97@interia.pl>
2024-12-03 15:16:41 +0100

    first solution

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,18 @@
 package practice;
 
-public class CandidateValidator {
+import model.Candidate;
+
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] PeriodsInUkr = candidate.getPeriodsInUkr().split("-");
+        return candidate.getAge() >= 35
+                && candidate.getNationality().equals("Ukrainian")
+                && candidate.isAllowedToVote()
+                && Integer.parseInt(PeriodsInUkr[1]) - Integer.parseInt(PeriodsInUkr[0]) > 10;
+    }
     //write your code here
 }
