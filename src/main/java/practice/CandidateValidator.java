849eda6e4edc9ecc8760f424b6a580b0eecc26bd
Anton <windsweptofficial@gmail.com>
2024-12-03 20:41:32 +0100

    fixed-3
    .min() - not working for me without Integer::compareTo

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,17 +4,18 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    private final static int YEAR_FROM = 0;
-    private final static int YEAR_TO = 1;
-    private final static int MINIMAL_PERIOD = 10;
-    private final static int MINIMAL_AGE = 35;
-    private final static String SPLITERATOR = "-";
-    private final static String ALLOWED_NATIONALITY = "Ukrainian";
+    private static final int YEAR_FROM = 0;
+    private static final int YEAR_TO = 1;
+    private static final int MINIMAL_PERIOD = 10;
+    private static final int MINIMAL_AGE = 35;
+    private static final String SPLITERATOR = "-";
+    private static final String ALLOWED_NATIONALITY = "Ukrainian";
 
     @Override
     public boolean test(Candidate candidate) {
         String[] years = candidate.getPeriodsInUkr().split(SPLITERATOR);
-        if (Integer.parseInt(years[YEAR_TO]) - Integer.parseInt(years[YEAR_FROM]) < MINIMAL_PERIOD) {
+        if (Integer.parseInt(years[YEAR_TO]) - Integer.parseInt(years[YEAR_FROM])
+                < MINIMAL_PERIOD) {
             return false;
         }
         return candidate.getAge() >= MINIMAL_AGE
