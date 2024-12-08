7c0384768ea3db6b16e30d9230fb27eff83d5a0b
Pavlo Sukhanko <swtlouisiana@gmail.com>
2024-12-08 19:20:37 +0000

    Implementation of all methods from tasks and class CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,22 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        if (candidate == null) {
+            return false;
+        }
+
+        boolean oldEnough = candidate.getAge() >= 35;
+        boolean nationality = candidate.getNationality().equals("Ukrainian");
+        boolean canVote = candidate.isAllowedToVote();
+        String[] ages = candidate.getPeriodsInUkr().split("-");
+        boolean liveEnoughInUkraine = Integer.parseInt(ages[1])
+                - Integer.parseInt(ages[0]) >= 10;
+
+        return oldEnough && nationality && canVote && liveEnoughInUkraine;
+    }
 }
