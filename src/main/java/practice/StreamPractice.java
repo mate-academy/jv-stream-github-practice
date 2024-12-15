8721295de3e348450bfaa9a1640e4008f1f1ac65
Heorhii Tsarenko <heorhii.tsarenko@gmail.com>
2024-12-15 13:54:35 +0200

    delete unused import statement

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,11 +1,9 @@
 package practice;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
-import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
 
 import model.Candidate;
