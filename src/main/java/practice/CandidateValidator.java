9cdf373a609467b38d297683be54217aa11b3f99
Heorhii Tsarenko <heorhii.tsarenko@gmail.com>
2024-12-15 13:53:55 +0200

    complete all tasks

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,37 @@
 package practice;
 
-public class CandidateValidator {
+import model.Candidate;
+
+import java.util.Arrays;
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    @Override
+    public boolean test(Candidate candidate) {
+        if (candidate.getAge() < 35) {
+            return false;
+        }
+
+        if (!candidate.isAllowedToVote()) {
+            return false;
+        }
+
+        if (!"Ukrainian".equals(candidate.getNationality())) {
+            return false;
+        }
+
+        String[] periods = candidate.getPeriodsInUkr().split(",");
+        int totalYearInUkraine = Arrays.stream(periods)
+                .mapToInt(period -> {
+                    String[] range = period.trim().split("-");
+                    int startYear = Integer.parseInt(range[0]);
+                    int endYear = Integer.parseInt(range[1]);
+                    return endYear - startYear + 1; // Учитываем включение последнего года
+                })
+                .sum();
+
+        return totalYearInUkraine >= 10;
+    }
     //write your code here
 }
