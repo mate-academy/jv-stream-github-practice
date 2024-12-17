7956069a7712710ce09fc2d65eed0fd0cb353fa7
Ostap <ostzat@gmail.com>
2024-12-17 15:07:48 +0100

    fixes

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,7 +7,7 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_AGE = 35;
     private static final int MINIMAL_PERIOD = 10;
     private static final String REQUIRED_NATIONALITY = "Ukrainian";
-    private static final String SPLITERATOR = ",";
+    private static final String SPLITERATOR = "-";
     private static final int YEAR_FROM = 0;
     private static final int YEAR_TO = 1;
 
@@ -15,14 +15,14 @@ public class CandidateValidator implements Predicate<Candidate> {
     public boolean test(Candidate candidate) {
         String periodsInUkr = candidate.getPeriodsInUkr();
 
-        String[] years = periodsInUkr.split("-");
+        String[] years = periodsInUkr.split(SPLITERATOR);
         if (years.length != 2) {
             return false;
         }
 
         try {
-            int yearFrom = Integer.parseInt(years[0]);
-            int yearTo = Integer.parseInt(years[1]);
+            int yearFrom = Integer.parseInt(years[YEAR_FROM]);
+            int yearTo = Integer.parseInt(years[YEAR_TO]);
 
             if (yearTo - yearFrom < MINIMAL_PERIOD) {
                 return false;
