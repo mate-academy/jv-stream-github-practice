aa1361545b049e26faeb8c076e153836f2e112de
Bohdan_SSX <mr.dimov77@gmail.com>
2024-12-25 18:57:03 +0200

    Implemented methods with lambda

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,28 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    private static final int MINIMAL_AGE = 35;
+    private static final int MINIMAL_YEARS_IN_UA = 10;
+    private static final String NATIONALITY = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        List<Integer> years = Arrays.stream(candidate.getPeriodsInUkr().split("-"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        if (candidate.getAge() >= MINIMAL_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && (years.get(1) - years.get(0)) >= MINIMAL_YEARS_IN_UA) {
+            return true;
+        }
+        return false;
+    }
 }
