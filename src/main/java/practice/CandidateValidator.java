e5d397526d44341ce844b83132fff9108fe8e537
Bohdan_SSX <mr.dimov77@gmail.com>
2024-12-27 10:04:02 +0200

    Fixed : - Coma and dash constants represented - changed getOddNumsAverage method impl

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -11,17 +11,18 @@ public class CandidateValidator implements Predicate<Candidate> {
     private static final int MINIMAL_YEARS_IN_UA = 10;
     private static final String NATIONALITY = "Ukrainian";
     private static final int START_LIVING_YEAR_INDEX = 0;
-    private static final int END__LIVING_YEAR_INDEX = 1;
+    private static final int END_LIVING_YEAR_INDEX = 1;
+    private static final String DASH = "-";
 
     @Override
     public boolean test(Candidate candidate) {
-        List<Integer> years = Arrays.stream(candidate.getPeriodsInUkr().split("-"))
+        List<Integer> years = Arrays.stream(candidate.getPeriodsInUkr().split(DASH))
                 .map(Integer::parseInt)
                 .toList();
         return candidate.getAge() >= MINIMAL_AGE
                 && candidate.isAllowedToVote()
                 && candidate.getNationality().equals(NATIONALITY)
-                && (years.get(END__LIVING_YEAR_INDEX)
+                && (years.get(END_LIVING_YEAR_INDEX)
                 - years.get(START_LIVING_YEAR_INDEX)) >= MINIMAL_YEARS_IN_UA;
     }
 }
--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,7 +1,6 @@
 package practice;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.function.Predicate;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -10,12 +9,13 @@ import model.Cat;
 import model.Person;
 
 public class StreamPractice {
+    private static final String COMMA = ",";
 
     private static final Predicate<Candidate> VALIDATOR = new CandidateValidator();
 
     public int findMinEvenNumber(List<String> numbers) {
         return numbers.stream()
-                .flatMap(s -> Stream.of(s.split(",")))
+                .flatMap(s -> Stream.of(s.split(COMMA)))
                 .mapToInt(Integer::parseInt)
                 .filter(n -> n % 2 == 0)
                 .min()
@@ -25,36 +25,35 @@ public class StreamPractice {
 
     public Double getOddNumsAverage(List<Integer> numbers) {
         return IntStream.range(0, numbers.size())
-                .mapToObj(i -> {
+                .mapToDouble(i -> {
                     int value = numbers.get(i);
                     return i % 2 != 0 ? value - 1 : value;
                 })
-                .filter(n -> n % 2 != 0)
-                .mapToDouble(Double::valueOf)
+                .filter(n -> n % 2 != 0.0)
                 .average()
-                .orElseThrow(() -> new NoSuchElementException("No odd numbers found"));
+                .getAsDouble();
     }
 
     public List<Person> selectMenByAge(List<Person> peopleList, int fromAge, int toAge) {
         return peopleList.stream()
                 .filter(p -> p.getAge() >= fromAge
                         && p.getAge() <= toAge
-                        && p.getSex().equals(Person.Sex.MAN))
+                        && p.getSex() == Person.Sex.MAN)
                 .toList();
     }
 
     public List<Person> getWorkablePeople(int fromAge, int femaleToAge,
                                           int maleToAge, List<Person> peopleList) {
         return peopleList.stream()
-                .filter(person -> person.getAge() >= fromAge)
-                .filter(person -> person.getSex() == Person.Sex.MAN ? person.getAge() <= maleToAge
-                        : person.getAge() <= femaleToAge)
+                .filter(person -> person.getAge() >= fromAge
+                        && (person.getSex() == Person.Sex.MAN ? person.getAge() <= maleToAge
+                                : person.getAge() <= femaleToAge))
                 .toList();
     }
 
     public List<String> getCatsNames(List<Person> peopleList, int femaleAge) {
         return peopleList.stream()
-                .filter(p -> p.getSex().equals(Person.Sex.WOMAN)
+                .filter(p -> p.getSex() == Person.Sex.WOMAN
                         && p.getAge() >= femaleAge)
                 .flatMap(p -> p.getCats().stream())
                 .map(Cat::getName)
