f9ad7d03397a577af8dc51a5124d442b777c333e
DariaBakal <bakal.dasha01@gmail.com>
2024-12-29 22:34:17 +0200

    stream practice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,38 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+/**
+ * Your help with a election is needed. Given list of candidates, where each element has
+ * Candidate.class type. Check which candidates are eligible to apply for president position and
+ * return their names sorted alphabetically. The requirements are: person should be older than 35
+ * years, should be allowed to vote, have nationality - 'Ukrainian' and live in Ukraine for 10
+ * years. For the last requirement use field periodsInUkr, which has following view: "2002-2015" We
+ * want to reuse our validation in future, so let's write our own impl of Predicate parametrized
+ * with Candidate in CandidateValidator.
+ */
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int PERIOD_IN_UKRAINE = 10;
+
+    @Override
+    public boolean test(Candidate c) {
+        if (c.getAge() < MIN_AGE) {
+            return false;
+        }
+        if (!c.isAllowedToVote()) {
+            return false;
+        }
+        if (!NATIONALITY.equals(c.getNationality())) {
+            return false;
+        }
+        String[] splitedPeriod = c.getPeriodsInUkr().split("-");
+        int startYear = Integer.parseInt(splitedPeriod[0]);
+        int endYear = Integer.parseInt(splitedPeriod[1]);
+
+        return (endYear - startYear) >= PERIOD_IN_UKRAINE;
+    }
 }
+
