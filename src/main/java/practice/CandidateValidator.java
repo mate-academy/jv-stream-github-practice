411d7149b153f9c9892b8987da11d8983f06198e
Teslenko Daniil <burgalhashem@gmail.com>
2024-12-30 23:14:13 +0200

    reset changes test file

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,25 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final String NATIONALITY = "Ukrainian";
+    private static final String REGEX = "-";
+    private static final int CANDIDATE_AGE = 35;
+    private static final int PERIOD_IF_YEARS = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= CANDIDATE_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(NATIONALITY)
+                && isLivingTenYears(candidate.getPeriodsInUkr());
+    }
+
+    private boolean isLivingTenYears(String livingYears) {
+        String[] splitLivingYears = livingYears.split(REGEX);
+        return (Integer.parseInt(splitLivingYears[1])
+                - Integer.parseInt(splitLivingYears[0])) >= PERIOD_IF_YEARS;
+    }
 }
