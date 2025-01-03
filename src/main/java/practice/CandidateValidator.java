8cb02861829d2f3cbc2839aadacf264064b7be1d
Nikita Myroniuk <nikitamix1sstt@gmail.com>
2025-01-04 00:56:29 +0100

    jv-stream-github-practice-01

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,25 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.Arrays;
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final int REQUIRED_YEARS = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= MIN_AGE
+                && candidate.isAllowedToVote()
+                && "Ukrainian".equalsIgnoreCase(candidate.getNationality())
+                && hasLivedInUkraineFor10Years(candidate.getPeriodsInUkr());
+    }
+
+    private boolean hasLivedInUkraineFor10Years(String periodsInUkr) {
+        return Arrays.stream(periodsInUkr.split(","))
+                .map(period -> period.split("-"))
+                .mapToInt(years -> Integer.parseInt(years[1]) - Integer.parseInt(years[0]))
+                .sum() >= REQUIRED_YEARS;
+    }
 }
