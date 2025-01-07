a5a1ca2ad7f8f0e5cd4e7b2e7619731553e4a14c
KaterynaRiznychenko <riznychenko2021mf12@student.karazin.ua>
2025-01-07 19:21:19 +0200

    replaced if statements by return and added constants

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -2,30 +2,31 @@ package practice;
 
 import java.util.Arrays;
 import java.util.function.Predicate;
+
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int AGE_FACTOR = 35;
+    private static final int YEARS_IN_UKRAINE_FACTOR = 10;
+    private static final int START_YEAR = 0;
+    private static final int END_YEAR = 1;
+    private static final String UKRAINE_NATIONALITY_FACTOR = "Ukrainian";
+    private static final String COMMA = ",";
+    private static final String PERIOD_DELIMITER = "-";
+
     @Override
     public boolean test(Candidate candidate) {
-        if (candidate.getAge() < 35) {
-            return false;
-        }
-        if (!candidate.isAllowedToVote()) {
-            return false;
-        }
-        if (!candidate.getNationality().equals("Ukrainian")) {
-            return false;
-        }
+        return candidate.getAge() >= AGE_FACTOR &&
+                candidate.isAllowedToVote() &&
+                candidate.getNationality().equals(UKRAINE_NATIONALITY_FACTOR) &&
+                Arrays.stream(candidate.getPeriodsInUkr().split(COMMA))
+                        .mapToInt(period -> {
+                            String[] periods = period.split(PERIOD_DELIMITER);
+                            int startYear = Integer.parseInt(periods[START_YEAR]);
+                            int endYear = Integer.parseInt(periods[END_YEAR]);
+                            return endYear - startYear;
+                        })
+                        .sum() >= YEARS_IN_UKRAINE_FACTOR;
 
-        String[] candidateList = candidate.getPeriodsInUkr().split(",");
-        int yearsInUkr = Arrays.stream(candidateList)
-                .mapToInt(period -> {
-                    String[] periods = period.split("-");
-                    int startYear = Integer.parseInt(periods[0]);
-                    int endYear = Integer.parseInt(periods[1]);
-                    return endYear - startYear;
-                })
-                .sum();
-        return yearsInUkr >= 10;
     }
 }
