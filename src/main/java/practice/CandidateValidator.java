5fe774f49bab2e367e3b8e926ae375abe8ac7c76
Sasha Koval <kovalsana40@gmail.com>
2025-01-12 15:17:02 +0200

    I have some questions to failed test

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
+import model.Candidate;
+
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] split1 = candidate.getPeriodsInUkr().split("-");
+        int period = Math.abs(Integer.parseInt(split1[0]) - Integer.parseInt(split1[1]));
+        if (candidate.isAllowedToVote() && candidate.getAge() > 35
+                && candidate.getNationality().equals("Ukrainian")
+                && period > 10) {
+            return true;
+        }
+        return false;
+    }
     //write your code here
+
 }
