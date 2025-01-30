ccff41bdef123825f3064e7f86130f447a82b0b5
Ivan Koshovyi <ivan2023koshovyi@gmail.com>
2025-01-30 15:26:23 +0200

    Task successfully completed

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.Arrays;
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        if (candidate.getAge() >= 35 && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")) {
+            int[] split = Arrays.stream(candidate.getPeriodsInUkr()
+                            .split("-"))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            return split[1] - split[0] >= 10;
+        }
+        return false;
+    }
 }
