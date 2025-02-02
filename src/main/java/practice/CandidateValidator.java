e141ad6831e69aa703cbd37edfbb85e8e3580b36
Ivan Koshovyi <ivan2023koshovyi@gmail.com>
2025-02-02 21:04:23 +0200

    created constants for all magic numbers and literals

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,16 +7,20 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int FROMAGE = 35;
     private static final int YEARSINUKRAINE = 10;
+    private static final String UKRAINIAN_NATIONALITY = "Ukrainian";
+    private static final String PERIOD_DELIMITER = "-";
+    private static final int START_INDEX_YEAR = 0;
+    private static final int END_INDEX_YEAR = 1;
 
     @Override
     public boolean test(Candidate candidate) {
         if (candidate.getAge() >= FROMAGE && candidate.isAllowedToVote()
-                && candidate.getNationality().equals("Ukrainian")) {
+                && candidate.getNationality().equals(UKRAINIAN_NATIONALITY)) {
             int[] split = Arrays.stream(candidate.getPeriodsInUkr()
-                            .split("-"))
+                            .split(PERIOD_DELIMITER))
                     .mapToInt(Integer::parseInt)
                     .toArray();
-            return split[1] - split[0] >= YEARSINUKRAINE;
+            return split[END_INDEX_YEAR] - split[START_INDEX_YEAR] >= YEARSINUKRAINE;
         }
         return false;
     }
