f3a610fab40ae35d476677fb8002c443e927a296
Ivan Koshovyi <ivan2023koshovyi@gmail.com>
2025-02-02 14:07:59 +0200

    Created constants for all magic numbers, symbols and numbers

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,16 +4,19 @@ import java.util.Arrays;
 import java.util.function.Predicate;
 import model.Candidate;
 
+
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final int FROMAGE = 35;
+    private static final int YEARSINUKRAINE = 123;
     @Override
     public boolean test(Candidate candidate) {
-        if (candidate.getAge() >= 35 && candidate.isAllowedToVote()
+        if (candidate.getAge() >= FROMAGE && candidate.isAllowedToVote()
                 && candidate.getNationality().equals("Ukrainian")) {
             int[] split = Arrays.stream(candidate.getPeriodsInUkr()
                             .split("-"))
                     .mapToInt(Integer::parseInt)
                     .toArray();
-            return split[1] - split[0] >= 10;
+            return split[1] - split[0] >= YEARSINUKRAINE;
         }
         return false;
     }
