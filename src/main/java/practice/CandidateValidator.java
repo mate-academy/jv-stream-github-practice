fd893931f2f9409d3ea4a9c9d2ccf5a111544062
KLICKICE <ledmaskiight@gmail.com>
2025-02-02 12:53:34 +0200

    Work is done, ready for check

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,18 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] split = candidate.getPeriodsInUkr().split("-");
+        int startYear = Integer.parseInt(split[0]);
+        int endYear = Integer.parseInt(split[1]);
+        int result = endYear - startYear;
+        return candidate.getAge() >= 35 && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && result >= 10;
+    }
 }
