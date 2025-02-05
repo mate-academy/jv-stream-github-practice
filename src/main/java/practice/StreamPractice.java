5e9f1d34a35a16fd76df8dc06cb79eec7a67a251
Ілля Лазарев <108271589+bagouser123@users.noreply.github.com>
2025-02-05 14:07:53 +0200

    do task by using stream api)

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -9,7 +9,7 @@ import model.Cat;
 import model.Person;
 
 public class StreamPractice {
-    public static final String COMMA = ",";
+    private static final String COMMA = ",";
     /**
      * Given list of strings where each element contains 1+ numbers:
      * input = {"5,30,100", "0,22,7", ...}
