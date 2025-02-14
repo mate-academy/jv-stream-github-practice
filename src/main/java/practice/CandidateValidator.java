4100c1ffa4b711554954c628639979379fd938c3
Your_Name Your_Surname <your@email>
2025-02-14 12:51:10 +0200

    added solutions to tasks

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int NEED_AGE = 27;
+    private static final int NEED_PERIOD = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] candidatePeriod = candidate.getPeriodsInUkr().split("-");
+        int periodInUrk = Integer.parseInt(candidatePeriod[1])
+                - Integer.parseInt(candidatePeriod[0]);
+        return candidate.getAge() >= NEED_AGE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals("Ukrainian")
+                && periodInUrk > NEED_PERIOD;
+    }
 }
