302b299ddd16524068c1fabecbca486e089df79a
Tetiana Hrytsenko <shviridtanya107@gmail.com>
2025-03-04 21:01:45 +0100

    changed public access modifier to private, combined map() and flatMap() methods in flatMap(), removed unnecessary boolean field and validation

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,19 +4,18 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    public static final int MIN_AGE = 35;
-    public static final boolean IS_ELIGIBLE_TO_VOTE = true;
-    public static final String NATIONALITY = "Ukrainian";
-    public static final int MIN_YEARS_IN_COUNTRY = 10;
-    public static final String DASH = "-";
-    public static final int INDEX_FROM = 0;
-    public static final int INDEX_TO = 1;
+    private static final int MIN_AGE = 35;
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int MIN_YEARS_IN_COUNTRY = 10;
+    private static final String DASH = "-";
+    private static final int INDEX_FROM = 0;
+    private static final int INDEX_TO = 1;
 
     @Override
     public boolean test(Candidate candidate) {
         return candidate.getAge() >= MIN_AGE
                 && candidate.getNationality().equals(NATIONALITY)
-                && candidate.isAllowedToVote() == IS_ELIGIBLE_TO_VOTE
+                && candidate.isAllowedToVote()
                 && getYearsInUkraine(candidate.getPeriodsInUkr()) >= MIN_YEARS_IN_COUNTRY;
     }
 
--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,19 +1,15 @@
 package practice;
 
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-
 import model.Candidate;
 import model.Cat;
 import model.Person;
 
 public class StreamPractice {
-    public static final String COMMA = ",";
+    private static final String COMMA = ",";
 
     /**
      * Given list of strings where each element contains 1+ numbers:
@@ -24,8 +20,7 @@ public class StreamPractice {
      */
     public int findMinEvenNumber(List<String> numbers) {
         return numbers.stream()
-                .map(s -> s.split(COMMA))
-                .flatMap(Arrays::stream)
+                .flatMap(s -> Arrays.stream(s.split(COMMA)))
                 .mapToInt(Integer::parseInt)
                 .filter(n -> n % 2 == 0)
                 .min()
@@ -62,7 +57,7 @@ public class StreamPractice {
         return peopleList.stream()
                 .filter(p -> p.getSex().equals(Person.Sex.MAN) && p.getAge() >= fromAge
                         && p.getAge() <= toAge)
-                .collect(Collectors.toList());
+                .toList();
     }
 
     /**
@@ -81,7 +76,7 @@ public class StreamPractice {
                 .filter(p -> p.getAge() >= fromAge
                         && (p.getSex() == Person.Sex.MAN && p.getAge() <= maleToAge
                         || p.getSex() == Person.Sex.WOMAN && p.getAge() <= femaleToAge))
-                .collect(Collectors.toList());
+                .toList();
     }
 
     /**
@@ -92,10 +87,9 @@ public class StreamPractice {
     public List<String> getCatsNames(List<Person> peopleList, int femaleAge) {
         return peopleList.stream()
                 .filter(p -> p.getSex() == Person.Sex.WOMAN && p.getAge() >= femaleAge)
-                .map(Person::getCats)
-                .flatMap(Collection::stream)
+                .flatMap(p -> p.getCats().stream())
                 .map(Cat::getName)
-                .collect(Collectors.toList());
+                .toList();
     }
 
     /**
@@ -116,6 +110,6 @@ public class StreamPractice {
                 .filter(candidateValidator)
                 .map(Candidate::getName)
                 .sorted()
-                .collect(Collectors.toList());
+                .toList();
     }
 }
