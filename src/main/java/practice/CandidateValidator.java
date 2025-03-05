fed3db22cae9970c12085f1c87de903a7b6c3d20
Kamil Szerszen <szerszenk1997@gmail.com>
2025-03-05 19:52:26 +0100

    Tasks ready

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,16 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] periodsInUkr = candidate.getPeriodsInUkr().split("-");
+        int fromYear = Integer.parseInt(periodsInUkr[0]);
+        int toYear = Integer.parseInt(periodsInUkr[1]);
+        int year = toYear - fromYear;
+        return year > 10;
+    }
 }
