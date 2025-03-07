3bf2a3d142db6fc17e94e3b36207e01bcd5bd86d
Luchko Roman <romander09@gmail.com>
2025-03-07 12:26:32 +0200

    do ex

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.Objects;
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_YEAR = 35;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        String[] years = candidate.getPeriodsInUkr().split("-");
+        return candidate.getAge() >= MIN_YEAR && candidate.isAllowedToVote()
+                && Objects.equals(candidate.getNationality(), "Ukrainian")
+                && Integer.parseInt(years[1]) - Integer.parseInt(years[0]) >= 10;
+    }
 }
+
+
+
+
