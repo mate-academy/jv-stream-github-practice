3a48a525ff20c5dbf3a3351c25d5a8638172159c
Vitaliy <vitaliyufc@gmail.com>
2025-03-12 23:03:29 +0300

    Done Stream tasks

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -23,4 +23,5 @@ public class CandidateValidator implements Predicate<Candidate> {
         return str.length == 2 && Integer.parseInt(str[1])
                 - Integer.parseInt(str[0]) >= LIVE_IN_UKRAINE;
     }
+}
 
