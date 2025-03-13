11e65a0036100c72885eaad40d68f1a782fba994
Kostyantyn Savchenko <kostyantyn.savchenko@gmail.com>
2025-03-13 22:47:07 +0200

    v2.1 after 3d required changes by Mate mentor Elena-Bruyako

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,21 +5,24 @@ import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
     private static final int MIN_AGE = 35;
-    private static final String UA = "Ukrainian";
+    private static final String NATIONALITY = "Ukrainian";
+    private static final int PERIOD_LENGTH_OK = 2;
+    private static final int MIN_LIVE_YEARS = 10;
+    private static final String SPLITTER = "-";
 
     @Override
     public boolean test(Candidate candidate) {
         if (candidate.getAge() < MIN_AGE || !candidate.isAllowedToVote()
-                || !candidate.getNationality().equals(UA)) {
+                || !candidate.getNationality().equals(NATIONALITY)) {
             return false;
         }
-        String[] period = candidate.getPeriodsInUkr().split("-");
-        if (period.length != 2) {
+        String[] period = candidate.getPeriodsInUkr().split(SPLITTER);
+        if (period.length != PERIOD_LENGTH_OK) {
             return false;
         }
         int start = Integer.parseInt(period[0]);
         int end = Integer.parseInt(period[1]);
-        if ((end - start) < 10) {
+        if ((end - start) < MIN_LIVE_YEARS) {
             return false;
         }
         return true;
