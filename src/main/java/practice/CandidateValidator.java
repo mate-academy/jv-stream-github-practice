88d57ea6eef5dcf57858a07618c394faa03aa6b8
Nikita Chaikovskyi <cha9n.1488@gmail.com>
2025-03-15 11:25:48 +0200

    Don4

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,6 +6,9 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     private static final String NATIONAL = "Ukrainian";
     private static final int MIN_AGE = 35;
+    private static final String SPLIT_SIMBOL = "-";
+    private static final int START_YEARS_INDEX = 0;
+    private static final int FINISH_YEARS_INDEX = 1;
 
     @Override
     public boolean test(Candidate candidate) {
@@ -15,9 +18,9 @@ public class CandidateValidator implements Predicate<Candidate> {
             return false;
         }
 
-        String[] years = candidate.getPeriodsInUkr().split("-");
-        int startYears = Integer.parseInt(years[0]);
-        int finishYears = Integer.parseInt(years[1]);
+        String[] years = candidate.getPeriodsInUkr().split(SPLIT_SIMBOL);
+        int startYears = Integer.parseInt(years[START_YEARS_INDEX]);
+        int finishYears = Integer.parseInt(years[FINISH_YEARS_INDEX]);
 
         if ((finishYears - startYears) < 10) {
             return false;
