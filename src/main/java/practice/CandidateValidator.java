6851f01bdd57f5fe61272a885e147732470272ca
Yevhen Koroliuk <zheckakoroluk@gmail.com>
2025-03-17 17:27:29 +0200

    excuted

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,38 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+
+    @Override
+    public boolean test(Candidate candidate) {
+        if (candidate.getAge() < 35) {
+            return false;
+        }
+
+        if (!"Ukrainian".equals(candidate.getNationality())) {
+            return false;
+        }
+        if (!candidate.isAllowedToVote()) {
+            return false;
+        }
+
+        String[] periods = candidate.getPeriodsInUkr().split("-");
+        if (periods.length < 2) {
+            return false;
+        }
+
+        try {
+            int startYear = Integer.parseInt(periods[0]);
+            int endYear = Integer.parseInt(periods[1]);
+            if (endYear - startYear < 10) {
+                return false;
+            }
+        } catch (NumberFormatException e) {
+            return false;
+        }
+
+        return true;
+    }
 }
