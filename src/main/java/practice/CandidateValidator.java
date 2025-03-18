25d9e75e4a10d1dfd87db524cfa872fe4a417196
Bohdan <111230201+FireFalcons@users.noreply.github.com>
2025-03-18 18:29:53 +0200

    added constants to the CandidateValidator class and fixed the error message when calling the findMinEvenNumber method

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,21 +4,24 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final String ukr = "Ukrainian";
+    private static final int period = 10;
+    private static final int years = 2;
 
     @Override
     public boolean test(Candidate candidate) {
         return candidate.getAge() >= 35
             && candidate.isAllowedToVote()
-            && candidate.getNationality().equalsIgnoreCase("Ukrainian")
+            && candidate.getNationality().equalsIgnoreCase(ukr)
             && isEligiblePeriod(candidate.getPeriodsInUkr());
     }
 
     public boolean isEligiblePeriod(String periodsUkr) {
         String[] periods = periodsUkr.split("-");
-        if (periods.length == 2) {
+        if (periods.length == years) {
             int startYear = Integer.parseInt(periods[0]);
             int endYear = Integer.parseInt(periods[1]);
-            return (endYear - startYear) >= 10;
+            return (endYear - startYear) >= period;
         }
         return false;
     }
--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -18,7 +18,7 @@ public class StreamPractice {
             .mapToInt(Integer::parseInt)
             .filter(n -> n % 2 == 0)
             .min()
-            .orElseThrow(() -> new RuntimeException("Can't get min value from list"));
+            .orElseThrow(() -> new RuntimeException("Can't get min value from list: " + numbers));
     }
 
     public Double getOddNumsAverage(List<Integer> numbers) {
