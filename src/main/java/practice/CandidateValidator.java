502e7f6de6749f4f86bc8887f4af304a25ade9d4
Maks <lisichenkowork@gmail.com>
2025-03-23 20:53:50 +0100

    firts commit

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,19 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private int minAgeOfCandidate = 35;
+    private String nationalityOfCandidate = "Ukrainian";
+    private int minPeriod = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= minAgeOfCandidate
+                && candidate.getNationality().equals(nationalityOfCandidate)
+                && candidate.isAllowedToVote()
+                && Integer.parseInt(candidate.getPeriodsInUkr().split("-")[1])
+                - Integer.parseInt(candidate.getPeriodsInUkr().split("-")[0]) >= minPeriod;
+    }
 }
