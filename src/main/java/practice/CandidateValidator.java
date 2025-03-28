586761ab7077f924e261b74fee23ead7fee6607a
Alexandros <olek4004@gmail.com>
2025-03-28 13:33:55 +0100

    finished

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,26 +1,27 @@
 package practice;
 
 import model.Candidate;
+
 import java.util.function.Predicate;
 
 public class CandidateValidator implements Predicate<Candidate> {
 
-  @Override
-  public boolean test(Candidate candidate) {
-    return candidate.getAge() >= 35
-            && candidate.isAllowedToVote()
-            && "Ukrainian".equals(candidate.getNationality())
-            && hasLivedInUkraineFor10Years(candidate.getPeriodsInUkr());
-  }
-
-  private boolean hasLivedInUkraineFor10Years(String periodsInUkr) {
-    try {
-      String[] years = periodsInUkr.split("-");
-      int startYear = Integer.parseInt(years[0]);
-      int endYear = Integer.parseInt(years[1]);
-      return (endYear - startYear) >= 10;
-    } catch (Exception e) {
-      return false;
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= 35
+                && candidate.isAllowedToVote()
+                && "Ukrainian".equals(candidate.getNationality())
+                && hasLivedInUkraineFor10Years(candidate.getPeriodsInUkr());
     }
-  }
+
+    private boolean hasLivedInUkraineFor10Years(String periodsInUkr) {
+        try {
+            String[] years = periodsInUkr.split("-");
+            int startYear = Integer.parseInt(years[0]);
+            int endYear = Integer.parseInt(years[1]);
+            return (endYear - startYear) >= 10;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 }
