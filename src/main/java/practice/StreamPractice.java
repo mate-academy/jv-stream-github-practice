fcfe382c5639d93f7bfb04f5ef58d6474ed37f84
Weronika Rzekiecka <weronikarzekiecka@interia.pl>
2025-04-04 13:35:28 +0200

    practice

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -1,8 +1,11 @@
 package practice;
 
 import java.util.Collections;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import model.Candidate;
+import model.Cat;
 import model.Person;
 
 public class StreamPractice {
@@ -14,7 +17,16 @@ public class StreamPractice {
      * "Can't get min value from list: < Here is our input 'numbers' >"
      */
     public int findMinEvenNumber(List<String> numbers) {
-        return 0;
+        if (numbers.isEmpty()) {
+            throw new RuntimeException("Can't get min value from list");
+        }
+        return numbers.stream()
+                .flatMap(s -> Arrays.stream(s.split(",")))
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .filter(n -> n % 2 == 0)
+                .min(Comparator.naturalOrder())
+                .get();
     }
 
     /**
@@ -23,43 +35,70 @@ public class StreamPractice {
      * But before that subtract 1 from each element on an odd position (having the odd index).
      */
     public Double getOddNumsAverage(List<Integer> numbers) {
-        return 0D;
+        for (int i = 0; i < numbers.size(); i++) {
+            if (!(i % 2 == 0)) {
+                numbers.set(i,numbers.get(i) - 1);
+            }
+        }
+        return numbers.stream()
+                .filter(n -> !(n % 2 == 0))
+                .mapToDouble(i -> i)
+                .average()
+                .getAsDouble();
+
     }
 
     /**
-     * Given a List of `Person` instances (having `name`, `age` and `sex` fields),
-     * for example, `Arrays.asList( new Person(«Victor», 16, Sex.MAN),
-     * new Person(«Helen», 42, Sex.WOMAN))`,
-     * select from the List only men whose age is from `fromAge` to `toAge` inclusively.
+     * Given a List of Person instances (having name, age and sex fields),
+     * for example, Arrays.asList( new Person(«Victor», 16, Sex.MAN),
+     * new Person(«Helen», 42, Sex.WOMAN)),
+     * select from the List only men whose age is from fromAge to toAge inclusively.
      * <p>
      * Example: select men who can be recruited to army (from 18 to 27 years old inclusively).
      */
     public List<Person> selectMenByAge(List<Person> peopleList, int fromAge, int toAge) {
-        return Collections.emptyList();
+        return peopleList.stream()
+                .filter(p -> p.getSex() == Person.Sex.MAN)
+                .filter(p -> p.getAge() <= toAge && p.getAge() >= fromAge)
+                .toList();
     }
 
     /**
-     * Given a List of `Person` instances (having `name`, `age` and `sex` fields),
-     * for example, `Arrays.asList( new Person(«Victor», 16, Sex.MAN),
-     * new Person(«Helen», 42, Sex.WOMAN))`,
-     * select from the List only people whose age is from `fromAge` and to `maleToAge` (for men)
-     * or to `femaleToAge` (for women) inclusively.
+     * Given a List of Person instances (having name, age and sex fields),
+     * for example, Arrays.asList( new Person(«Victor», 16, Sex.MAN),
+     * new Person(«Helen», 42, Sex.WOMAN)),
+     * select from the List only people whose age is from fromAge and to maleToAge (for men)
+     * or to femaleToAge (for women) inclusively.
      * <p>
      * Example: select people of working age
      * (from 18 y.o. and to 60 y.o. for men and to 55 y.o. for women inclusively).
      */
     public List<Person> getWorkablePeople(int fromAge, int femaleToAge,
                                           int maleToAge, List<Person> peopleList) {
-        return Collections.emptyList();
+        return peopleList.stream()
+                .filter(p -> {
+                    if (p.getSex() == Person.Sex.MAN) {
+                        return p.getAge() <= maleToAge && p.getAge() >= fromAge;
+                    } else {
+                        return p.getAge() <= femaleToAge && p.getAge() >= fromAge;
+                    }
+                })
+                .toList();
     }
 
     /**
-     * Given a List of `Person` instances (having `name`, `age`, `sex` and `cats` fields,
-     * and each `Cat` having a `name` and `age`),
-     * return the names of all cats whose owners are women from `femaleAge` years old inclusively.
+     * Given a List of Person instances (having name, age, sex and cats fields,
+     * and each Cat having a name and age),
+     * return the names of all cats whose owners are women from femaleAge years old inclusively.
      */
     public List<String> getCatsNames(List<Person> peopleList, int femaleAge) {
-        return Collections.emptyList();
+        return peopleList.stream()
+                .filter(p -> p.getSex() == Person.Sex.WOMAN && p.getAge() >= femaleAge)
+                .map(Person::getCats)
+                .flatMap(List::stream)
+                .distinct()
+                .map(Cat::getName)
+                .toList();
     }
 
     /**
@@ -75,6 +114,12 @@ public class StreamPractice {
      * parametrized with Candidate in CandidateValidator.
      */
     public List<String> validateCandidates(List<Candidate> candidates) {
-        return Collections.emptyList();
+        CandidateValidator candidateValidator = new CandidateValidator();
+        return candidates.stream()
+                .filter(candidateValidator::test)
+                .map(Candidate::getName)
+                .sorted()
+                .toList();
     }
+
 }
