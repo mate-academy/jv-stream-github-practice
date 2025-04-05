d31974f95693051889f5f76d65d648c0c54f8c05
Vladyslav Bedryk <vladikbedrikl@gmail.com>
2025-04-05 18:43:10 +0200

    implemented methods with using streams

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,26 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final String UKRAINIAN_NATIONALITY = "Ukrainian";
+    private static final int AGE_FOR_VOTING = 35;
+    private static final int LIVE_IN_UKR = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        if (candidate.getAge() >= AGE_FOR_VOTING
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(UKRAINIAN_NATIONALITY)
+                && livingInUkr(candidate) >= LIVE_IN_UKR) {
+            return true;
+        }
+        return false;
+    }
+
+    private int livingInUkr(Candidate candidate) {
+        String[] periodInUkraine = candidate.getPeriodsInUkr().split("-");
+        return Integer.parseInt(periodInUkraine[1]) - Integer.parseInt(periodInUkraine[0]);
+    }
 }
