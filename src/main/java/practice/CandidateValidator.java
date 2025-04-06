f57275fa38c199144ffdd88fd7dd987ea3726604
Roman Malyarenko <malyarenko.roman@gmail.com>
2025-04-06 22:09:35 +0300

    completed all the tasks of the stream practice

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,26 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int INITIAL_AGE_CANDIDATE = 35;
+    private static final int DURATION_OF_STAY_IN_UKRAINE = 10;
+    private static final String REQUIRED_NATIONALITY = "Ukrainian";
+    private static final int FIRST_INDEX_ARRAY = 0;
+    private static final int SECOND_INDEX_ARRAY = 1;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() >= INITIAL_AGE_CANDIDATE
+                && candidate.isAllowedToVote()
+                && candidate.getNationality().equals(REQUIRED_NATIONALITY)
+                && periodsToInt(candidate.getPeriodsInUkr()) >= DURATION_OF_STAY_IN_UKRAINE;
+    }
+    
+    private int periodsToInt(String period) {
+        String[] periodToArray = period.split("-");
+        return Integer.parseInt(periodToArray[SECOND_INDEX_ARRAY])
+                - Integer.parseInt(periodToArray[FIRST_INDEX_ARRAY]);
+    }
 }
