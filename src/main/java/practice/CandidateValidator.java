c05f721547ef361cbfb088ef84cad6bfcb3f6d76
Weronika Rzekiecka <weronikarzekiecka@interia.pl>
2025-04-07 11:10:24 +0200

    solution

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -5,23 +5,29 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
-    private static final int eligibleAge = 35;
-    private static final String eligibleNationality = "Ukrainian";
-    private static final int requiredPeriodInUkr = 10;
+    private static final int ELIGIBLE_AGE = 35;
+    private static final String ELIGIBLE_NATIONALITY = "Ukrainian";
+    private static final int REQUIRED_PERIOD_IN_UKR = 10;
 
     @Override
     public boolean test(Candidate candidate) {
-        if (candidate.getAge() >= eligibleAge
-                && candidate.getNationality().equals(eligibleNationality)
-                && candidate.isAllowedToVote()) {
-            int[] period = Arrays.stream(candidate.getPeriodsInUkr().split("-"))
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
-            if (period[1] - period[0] >= requiredPeriodInUkr) {
-                return true;
-            }
+        if (candidate.getAge() < ELIGIBLE_AGE) {
+            return false;
         }
-        return false;
+
+        if (!ELIGIBLE_NATIONALITY.equals(candidate.getNationality())) {
+            return false;
+        }
+
+        if (!candidate.isAllowedToVote()) {
+            return false;
+        }
+
+        int[] period = Arrays.stream(candidate.getPeriodsInUkr().split("-"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        return (period[1] - period[0]) >= REQUIRED_PERIOD_IN_UKR;
     }
-    //write your code here
+
 }
