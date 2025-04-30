e3fc13aba29322786be59eff87a65b54a20ea41d
anton <anton.surzhok@gmail.com>
2025-04-30 16:45:45 +0300

    Add changes

--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -1,8 +1,8 @@
 package model;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Person {
     private String name;
@@ -10,11 +10,12 @@ public class Person {
     private Sex sex;
     private List<Cat> cats;
 
+    public enum Sex {
+        MAN, WOMAN
+    }
+
     public Person(String name, int age, Sex sex) {
-        this.name = name;
-        this.age = age;
-        this.sex = sex;
-        cats = new ArrayList<>();
+        this(name, age, sex, new ArrayList<>());
     }
 
     public Person(String name, int age, Sex sex, List<Cat> cats) {
@@ -24,53 +25,39 @@ public class Person {
         this.cats = cats;
     }
 
-    public String getName() {
-        return name;
+    public String getName() { return name; }
+    public int getAge() { return age; }
+    public Sex getSex() { return sex; }
+    public List<Cat> getCats() { return cats; }
+
+    // Метод 3: Чоловіки від from до to років
+    public static List<Person> getRecruitMen(List<Person> people, int fromAge, int toAge) {
+        return people.stream()
+                .filter(p -> p.getSex() == Sex.MAN)
+                .filter(p -> p.getAge() >= fromAge && p.getAge() <= toAge)
+                .collect(Collectors.toList());
     }
 
-    public int getAge() {
-        return age;
+    // Метод 4: Люди працездатного віку
+    public static List<Person> getWorkingPeople(List<Person> people, int fromAge, int maleToAge, int femaleToAge) {
+        return people.stream()
+                .filter(p -> p.getAge() >= fromAge)
+                .filter(p -> (p.getSex() == Sex.MAN && p.getAge() <= maleToAge)
+                        || (p.getSex() == Sex.WOMAN && p.getAge() <= femaleToAge))
+                .collect(Collectors.toList());
     }
 
-    public Sex getSex() {
-        return sex;
-    }
-
-    public List<Cat> getCats() {
-        return cats;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Person person = (Person) o;
-        return age == person.age
-            && Objects.equals(name, person.name)
-            && sex == person.sex
-            && Objects.equals(cats, person.cats);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, age, sex, cats);
+    // Метод 5: Імена котів жінок від femaleAge
+    public static List<String> getCatsNamesOfWomen(List<Person> people, int femaleAge) {
+        return people.stream()
+                .filter(p -> p.getSex() == Sex.WOMAN && p.getAge() >= femaleAge)
+                .flatMap(p -> p.getCats().stream())
+                .map(Cat::getName)
+                .collect(Collectors.toList());
     }
 
     @Override
     public String toString() {
-        return "Person{"
-            + "name='" + name + '\''
-            + ", age=" + age
-            + ", sex=" + sex
-            + ", catList=" + cats
-            + '}';
-    }
-
-    public enum Sex {
-        MAN, WOMAN
+        return "Person{name='" + name + "', age=" + age + ", sex=" + sex + ", cats=" + cats + '}';
     }
 }
