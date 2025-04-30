eebe7b58ae66fd08c54742b7c560a9cd9072d8b5
Oleksii Korovnichenko <korovnichenkoA@gmail.com>
2025-04-30 19:02:40 +0300

    hw18-solution StreamPractice ver1.0

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,29 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private String nationality = "Ukrainian";
+
+    @Override
+    public boolean test(Candidate candidate) {
+        return candidate.getAge() > 35
+                && candidate.isAllowedToVote()
+                && nationality.equals(candidate.getNationality())
+                && hasLivedInUkraineForTenYears(candidate.getPeriodsInUkr());
+    }
+
+    private boolean hasLivedInUkraineForTenYears(String periodsInUkr) {
+        if (periodsInUkr == null) {
+            return false;
+        }
+
+        String[] years = periodsInUkr.split("-");
+        int from = Integer.parseInt(years[0]);
+        int to = Integer.parseInt(years[1]);
+
+        return (to - from) >= 10;
+    }
+
 }
