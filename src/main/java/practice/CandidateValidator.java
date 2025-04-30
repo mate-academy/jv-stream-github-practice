3bbdfa7d2214bcb1f1231153a9b4defe6116dbc0
Beliar24 <blackdid7@gmail.com>
2025-05-01 01:50:24 +0300

    implement methods

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,20 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate c) {
+        int fromAge = 35;
+        return c.getAge() >= fromAge
+                && c.isAllowedToVote()
+                && c.getNationality().equals("Ukrainian")
+                && getYearsInUkraine(c.getPeriodsInUkr()) >= 10;
+    }
+
+    private int getYearsInUkraine(String periodsInUkr) {
+        String[] period = periodsInUkr.split("-");
+        return Integer.parseInt(period[1]) - Integer.parseInt(period[0]);
+    }
 }
