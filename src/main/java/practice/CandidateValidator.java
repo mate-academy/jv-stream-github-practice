7ede1d5690c69a420678cc3dcbf729721b70e4c9
Oleksandr <odombrovskyi26@gmail.com>
2025-05-29 19:31:58 +0300

    fixed

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -6,10 +6,14 @@ import model.Candidate;
 public class CandidateValidator implements Predicate<Candidate> {
     @Override
     public boolean test(Candidate candidate) {
-        String[] year = candidate.getPeriodsInUkr().split("-");
+        String period = candidate.getPeriodsInUkr();
+        if (period == null || !period.matches("\\d{4}-\\d{4}")) {
+            return false;
+        }
+        String[] years = period.split("-");
         return candidate.getAge() >= 35
                     && candidate.isAllowedToVote()
                     && candidate.getNationality().equals("Ukrainian")
-                    && Integer.parseInt(year[1]) - Integer.parseInt(year[0]) >= 10;
+                    && Integer.parseInt(years[1]) - Integer.parseInt(years[0]) >= 10;
     }
 }
