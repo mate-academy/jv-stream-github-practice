a9ea4eb987877b5002f2b57c22700a1c4499d0c8
DZotsenko <diana.zotsenko@gmail.com>
2025-06-04 16:08:52 +0300

    added trim() method to CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -7,8 +7,8 @@ public class CandidateValidator implements Predicate<Candidate> {
     @Override
     public boolean test(Candidate candidate) {
         String[] yearsOfLivingInUkraine = candidate.getPeriodsInUkr().split("-");
-        int startYear = Integer.parseInt(yearsOfLivingInUkraine[0]);
-        int endYear = Integer.parseInt(yearsOfLivingInUkraine[1]);
+        int startYear = Integer.parseInt(yearsOfLivingInUkraine[0].trim());
+        int endYear = Integer.parseInt(yearsOfLivingInUkraine[1].trim());
         return candidate.getAge() >= 35
                 && candidate.isAllowedToVote()
                 && "Ukrainian".equals(candidate.getNationality())
