d66a90ea6698de9795db67a5838b70c62270f7cc
Lentezz <yaroslav.golovan@gmail.com>
2025-06-07 21:19:58 +0300

    Implement all methods

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,21 @@
 package practice;
 
-public class CandidateValidator {
-    //write your code here
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+import model.Candidate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    private static final int MIN_AGE = 35;
+    private static final int LIVE_IN_UKRAINE = 10;
+
+    @Override
+    public boolean test(Candidate candidate) {
+        int liveInUkraine = Stream.of(candidate.getPeriodsInUkr().split("-"))
+                .mapToInt(Integer::parseInt)
+                .reduce((i, j) -> j - i).orElse(0);
+        return candidate.isAllowedToVote()
+                && candidate.getAge() >= MIN_AGE
+                && candidate.getNationality().equals("Ukrainian")
+                && liveInUkraine >= LIVE_IN_UKRAINE;
+    }
 }
