d7ff8128e0cc645c31bda771c6bff02c1334ebb9
Artem Bohachenko <bogart2012g00g1e@gmail.com>
2025-06-08 19:36:47 +0300

    was realized class CandidateValidator

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -1,5 +1,32 @@
 package practice;
 
-public class CandidateValidator {
+import model.Candidate;
+
+import java.util.function.Predicate;
+
+public class CandidateValidator implements Predicate<Candidate> {
+    @Override
+    public boolean test(Candidate candidate) {
+        if (candidate.getAge() < 35) {
+            return false;
+        }
+        if (!candidate.isAllowedToVote()) {
+            return false;
+        }
+        if (!candidate.getNationality().equals("Ukrainian")) {
+            return false;
+        }
+        return getPeriodInUkraineInt(candidate.getPeriodsInUkr()) >= 10;
+    }
+
+    private int getPeriodInUkraineInt(String period) {
+        if (period != null && !period.isEmpty()) {
+            String[] years = period.split("-");
+            return years.length > 1
+                    ? Integer.parseInt(years[1]) - Integer.parseInt(years[0])
+                    : Integer.parseInt(years[0]);
+        }
+        return 0;
+    }
     //write your code here
 }
