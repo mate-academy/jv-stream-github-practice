232543345904987d0db06066fb4ebf47036e1831
Woterix <dimkabibimka333@gmail.com>
2025-06-24 20:33:08 +0100

    Nice 5

--- src/main/java/practice/CandidateValidator.java
+++ src/main/java/practice/CandidateValidator.java
@@ -4,9 +4,11 @@ import java.util.function.Predicate;
 import model.Candidate;
 
 public class CandidateValidator implements Predicate<Candidate> {
+    private static final String DATES_SPLIT = "-";
+
     @Override
     public boolean test(Candidate candidate) {
-        String[] dates = candidate.getPeriodsInUkr().split("-");
+        String[] dates = candidate.getPeriodsInUkr().split(DATES_SPLIT);
         dates[0] = dates[0].trim();
         dates[1] = dates[1].trim();
         int inUkr = Integer.parseInt(dates[1]) - Integer.parseInt(dates[0]) + 1;
